eb9ffe69665651bfffec8598fa9e05a94b3f6e4c
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-11-25 08:50:47 -0600

    add /blog/all

--- modules/blog/src/main/package.scala
+++ modules/blog/src/main/package.scala
@@ -3,4 +3,8 @@ package lidraughts
 package object blog extends PackageObject {
 
   private[blog] def logger = lidraughts.log("blog")
+
+  lazy val thisYear = org.joda.time.DateTime.now.getYear
+
+  lazy val allYears = (thisYear to 2019 by -1).toList
 }
