b2cfa80f1b20f2beca57617ae28fa89fa17c0f84
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-12-03 22:27:30 +0100

    favicon for api

--- app/views/site/bits.scala
+++ app/views/site/bits.scala
@@ -30,6 +30,7 @@ object bits {
     <title>Lidraughts.org API reference</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
+    <link rel="icon" type="image/png" href="https://lidraughts.org/assets/images/favicon-32-white.png">
     <style>body { margin: 0; padding: 0; }</style>
   </head>
   <body>
