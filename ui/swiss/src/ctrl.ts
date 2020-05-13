4386771104b04b921fb46a84cbea221f48c731e3
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-05-13 10:57:59 -0600

    minify swiss json

--- ui/swiss/src/ctrl.ts
+++ ui/swiss/src/ctrl.ts
@@ -24,7 +24,7 @@ export default class SwissCtrl {
 
   constructor(opts: SwissOpts, redraw: () => void) {
     this.opts = opts;
-    this.data = opts.data;
+    this.data = this.readData(opts.data);
     this.redraw = redraw;
     this.trans = window.lidraughts.trans(opts.i18n);
     this.socket = makeSocket(opts.socketSend, this);
@@ -37,12 +37,11 @@ export default class SwissCtrl {
   }
 
   reload = (data: SwissData): void => {
-    this.data = {...this.data, ...data};
+    this.data = {...this.data, ...this.readData(data)};
     this.data.me = data.me; // to account for removal on withdraw
     this.data.nextRound = data.nextRound; // to account for removal
-    this.loadPage(data.standing);
+    this.loadPage(this.data.standing);
     if (this.focusOnMe) this.scrollToMe();
-    // if (data.featured) this.startWatching(data.featured.id);
     this.joinSpinner = false;
     this.redirectToMyGame();
     this.redrawNbRounds();
@@ -81,7 +80,7 @@ export default class SwissCtrl {
   };
 
   loadPage = (data: Standing) => {
-    if (!data.failed || !this.pages[data.page]) this.pages[data.page] = data.players;
+    this.pages[data.page] = this.readStanding(data).players;
   }
 
   setPage = (page: number) => {
@@ -135,4 +134,17 @@ export default class SwissCtrl {
 
   private redrawNbRounds = () =>
     $('.swiss__meta__round').text(`${this.data.round}/${this.data.nbRounds}`);
+
+  private readData = (data: SwissData) => ({
+    ...data,
+    standing: this.readStanding(data.standing)
+  });
+
+  private readStanding = (standing: Standing) => ({
+    ...standing,
+    players: standing.players.map(p => ({
+      ...p,
+      sheet: xhr.readSheetMin(p.sheetMin)
+    }))
+  });
 }
