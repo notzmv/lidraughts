2e0e35c18d538887bbb8ca55e93c42026979ea24
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-05-14 19:40:43 -0600

    show configured number of rounds

--- app/views/swiss/side.scala
+++ app/views/swiss/side.scala
@@ -27,7 +27,7 @@ object side {
               )
             } else s.perfType.map(_.name),
             separator,
-            span(cls := "swiss__meta__round")(s"${s.round}/${s.actualNbRounds}"),
+            span(cls := "swiss__meta__round")(s"${s.round}/${s.settings.nbRounds}"),
             " rounds"
           ),
           if (s.settings.rated) trans.ratedTournament() else trans.casualTournament(),
