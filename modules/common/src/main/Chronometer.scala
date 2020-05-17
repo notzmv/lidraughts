lichess-org/lila

better monitor redis publish

modules/common/src/main/Chronometer.scala
@@ -74,4 +74,9 @@ object Chronometer {
     lila.mon.recPath(path)(nowNanos - start)
     res
   }
+
+  def start = new {
+    private val s = nowNanos
+    def mon(path: lila.mon.RecPath): Unit = lila.mon.recPath(path)(nowNanos - s)
+  }
 }

notzmv/lidraughts

modules/common/src/main/Chronometer.scala
@@ -74,4 +74,9 @@ object Chronometer {
     lidraughts.mon.recPath(path)(nowNanos - start)
     res
   }
+
+  def start = new {
+    private val s = nowNanos
+    def mon(path: lidraughts.mon.RecPath): Unit = lidraughts.mon.recPath(path)(nowNanos - s)
+  }
 }