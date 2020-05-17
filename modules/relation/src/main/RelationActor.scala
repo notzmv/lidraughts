lichess-org/lila

remote socket /notified

modules/relation/src/main/RelationActor.scala
@@ -19,7 +19,7 @@ private[relation] final class RelationActor(
   private var previousOnlineIds = Set.empty[ID]
 
   override def preStart(): Unit = {
-    context.system.lilaBus.subscribe(self, 'startGame, 'finishGame, 'study)
+    context.system.lilaBus.subscribe(self, 'startGame, 'finishGame, 'study, 'reloadOnlineFriends)
   }
 
   override def postStop(): Unit = {

modules/socket/src/main/Env.scala
@@ -11,6 +11,7 @@ final class Env(
     system: ActorSystem,
     config: Config,
     lifecycle: play.api.inject.ApplicationLifecycle,
+    hub: lila.hub.Env,
     settingStore: lila.memo.SettingStore.Builder
 ) {
 
@@ -31,6 +32,7 @@ final class Env(
     chanIn = "site-in",
     chanOut = "site-out",
     lifecycle = lifecycle,
+    notificationActor = hub.notification,
     bus = system.lilaBus
   )
 
@@ -49,6 +51,7 @@ object Env {
     system = lila.common.PlayApp.system,
     config = lila.common.PlayApp loadConfig "socket",
     lifecycle = lila.common.PlayApp.lifecycle,
+    hub = lila.hub.Env.current,
     settingStore = lila.memo.Env.current.settingStore
   )
 }

notzmv/lidraughts

modules/relation/src/main/RelationActor.scala
@@ -19,7 +19,7 @@ private[relation] final class RelationActor(
   private var previousOnlineIds = Set.empty[ID]
 
   override def preStart(): Unit = {
-    context.system.lidraughtsBus.subscribe(self, 'startGame, 'finishGame, 'study)
+    context.system.lidraughtsBus.subscribe(self, 'startGame, 'finishGame, 'study, 'reloadOnlineFriends)
   }
 
   override def postStop(): Unit = {

modules/socket/src/main/Env.scala
@@ -11,6 +11,7 @@ final class Env(
     system: ActorSystem,
     config: Config,
     lifecycle: play.api.inject.ApplicationLifecycle,
+    hub: lidraughts.hub.Env,
     settingStore: lidraughts.memo.SettingStore.Builder
 ) {
 
@@ -31,6 +32,7 @@ final class Env(
     chanIn = "site-in",
     chanOut = "site-out",
     lifecycle = lifecycle,
+    notificationActor = hub.notification,
     bus = system.lidraughtsBus
   )
 
@@ -49,6 +51,7 @@ object Env {
     system = lidraughts.common.PlayApp.system,
     config = lidraughts.common.PlayApp loadConfig "socket",
     lifecycle = lidraughts.common.PlayApp.lifecycle,
+    hub = lidraughts.hub.Env.current,
     settingStore = lidraughts.memo.Env.current.settingStore
   )
 }