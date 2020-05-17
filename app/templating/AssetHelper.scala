lichess-org/lila

new remote socket domain, live setting for user selection

app/templating/AssetHelper.scala
@@ -15,6 +15,7 @@ trait AssetHelper { self: I18nHelper with SecurityHelper =>
   val siteDomain = lila.api.Env.current.Net.Domain
   val assetDomain = lila.api.Env.current.Net.AssetDomain
   val socketDomain = lila.api.Env.current.Net.SocketDomain
+  val remoteSocketDomain = lila.api.Env.current.Net.RemoteSocketDomain
   val vapidPublicKey = lila.push.Env.current.WebVapidPublicKey
 
   val sameAssetDomain = siteDomain == assetDomain
@@ -103,9 +104,17 @@ trait AssetHelper { self: I18nHelper with SecurityHelper =>
   def basicCsp(implicit req: RequestHeader): ContentSecurityPolicy = {
     val assets = if (req.secure) "https://" + assetDomain else assetDomain
     val socket = (if (req.secure) "wss://" else "ws://") + socketDomain + (if (socketDomain.contains(":")) "" else ":*")
+    val remoteSocket = (if (req.secure) "wss://" else "ws://") + remoteSocketDomain + (if (remoteSocketDomain.contains(":")) "" else ":*")
     ContentSecurityPolicy(
       defaultSrc = List("'self'", assets),
-      connectSrc = List("'self'", assets, socket, lila.api.Env.current.ExplorerEndpoint, lila.api.Env.current.TablebaseEndpoint),
+      connectSrc = List(
+        "'self'",
+        assets,
+        socket,
+        remoteSocket,
+        lila.api.Env.current.ExplorerEndpoint,
+        lila.api.Env.current.TablebaseEndpoint
+      ),
       styleSrc = List("'self'", "'unsafe-inline'", assets),
       fontSrc = List("'self'", assetDomain, "https://fonts.gstatic.com"),
       frameSrc = List("'self'", assets, "https://www.youtube.com", "https://player.twitch.tv"),

app/templating/Environment.scala
@@ -3,6 +3,7 @@ package templating
 
 import scala.concurrent.duration._
 
+import lila.api.Context
 import lila.api.Env.{ current => apiEnv }
 import lila.app.ui.ScalatagsTemplate._
 
@@ -52,7 +53,12 @@ object Environment
   def reportNbOpen: Int =
     lila.report.Env.current.api.nbOpen.awaitOrElse(10.millis, 0)
 
-  def NotForKids(f: => Frag)(implicit ctx: lila.api.Context) = if (ctx.kid) emptyFrag else f
+  def NotForKids(f: => Frag)(implicit ctx: Context) = if (ctx.kid) emptyFrag else f
 
   val spinner: Frag = raw("""<div class="spinner"><svg viewBox="0 0 40 40"><circle cx=20 cy=20 r=18 fill="none"></circle></svg></div>""")
+
+  def maybeRemoteSocketDomain(implicit ctx: Context): Option[String] = {
+    val regex = Env.socket.socketRemoteUsersSetting.get()
+    regex == "." || ctx.userId.exists(regex.r.matches)
+  } option remoteSocketDomain
 }

ui/site/src/main.js
@@ -276,7 +276,13 @@
       lichess.pubsub.on('content_loaded', renderTimeago);
 
       if (!window.customWS) setTimeout(function() {
-        if (lichess.socket === null) lichess.socket = lichess.StrongSocket("/socket/v4", false);
+        if (lichess.socket === null) {
+          lichess.socket = lichess.StrongSocket("/socket/v4", false, {
+            options: {
+              remoteSocketDomain: document.body.getAttribute('data-remote-socket-domain')
+            }
+          });
+        }
       }, 300);
 
       var initiatingHtml = '<div class="initiating">' + lichess.spinnerHtml + '</div>';

notzmv/lidraughts

app/templating/AssetHelper.scala
@@ -15,6 +15,7 @@ trait AssetHelper { self: I18nHelper with SecurityHelper =>
   val siteDomain = lidraughts.api.Env.current.Net.Domain
   val assetDomain = lidraughts.api.Env.current.Net.AssetDomain
   val socketDomain = lidraughts.api.Env.current.Net.SocketDomain
+  val remoteSocketDomain = lidraughts.api.Env.current.Net.RemoteSocketDomain
   val vapidPublicKey = lidraughts.push.Env.current.WebVapidPublicKey
 
   val sameAssetDomain = siteDomain == assetDomain
@@ -107,9 +108,17 @@ trait AssetHelper { self: I18nHelper with SecurityHelper =>
   def basicCsp(implicit req: RequestHeader): ContentSecurityPolicy = {
     val assets = if (req.secure) "https://" + assetDomain else assetDomain
     val socket = (if (req.secure) "wss://" else "ws://") + socketDomain + (if (socketDomain.contains(":")) "" else ":*")
+    val remoteSocket = (if (req.secure) "wss://" else "ws://") + remoteSocketDomain + (if (remoteSocketDomain.contains(":")) "" else ":*")
     ContentSecurityPolicy(
       defaultSrc = List("'self'", assets),
-      connectSrc = List("'self'", assets, socket, lidraughts.api.Env.current.ExplorerEndpoint, lidraughts.api.Env.current.TablebaseEndpoint),
+      connectSrc = List(
+        "'self'",
+        assets,
+        socket,
+        remoteSocket,
+        lidraughts.api.Env.current.ExplorerEndpoint,
+        lidraughts.api.Env.current.TablebaseEndpoint
+      ),
       styleSrc = List("'self'", "'unsafe-inline'", assets),
       fontSrc = List("'self'", assetDomain, "https://fonts.gstatic.com"),
       frameSrc = List("'self'", assets, "https://www.youtube.com", "https://player.twitch.tv"),

app/templating/Environment.scala
@@ -3,6 +3,7 @@ package templating
 
 import scala.concurrent.duration._
 
+import lidraughts.api.Context
 import lidraughts.api.Env.{ current => apiEnv }
 import lidraughts.app.ui.ScalatagsTemplate._
 
@@ -52,7 +53,12 @@ object Environment
   def isChatPanicEnabled =
     lidraughts.chat.Env.current.panic.enabled
 
-  def NotForKids(f: => Frag)(implicit ctx: lidraughts.api.Context) = if (ctx.kid) emptyFrag else f
+  def NotForKids(f: => Frag)(implicit ctx: Context) = if (ctx.kid) emptyFrag else f
 
   val spinner: Frag = raw("""<div class="spinner"><svg viewBox="0 0 40 40"><circle cx=20 cy=20 r=18 fill="none"></circle></svg></div>""")
+
+  def maybeRemoteSocketDomain(implicit ctx: Context): Option[String] = {
+    val regex = Env.socket.socketRemoteUsersSetting.get()
+    regex == "." || ctx.userId.exists(regex.r.matches)
+  } option remoteSocketDomain
 }

ui/site/src/main.js
@@ -277,7 +277,13 @@
       lidraughts.pubsub.on('content_loaded', renderTimeago);
 
       if (!window.customWS) setTimeout(function() {
-        if (lidraughts.socket === null) lidraughts.socket = lidraughts.StrongSocket("/socket/v3", false);
+        if (lidraughts.socket === null) {
+          lidraughts.socket = lidraughts.StrongSocket("/socket/v3", false, {
+            options: {
+              remoteSocketDomain: document.body.getAttribute('data-remote-socket-domain')
+            }
+          });
+        }
       }, 300);
 
       var initiatingHtml = '<div class="initiating">' + lidraughts.spinnerHtml + '</div>';