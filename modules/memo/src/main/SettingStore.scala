lichess-org/lila

safer regex live settings

modules/memo/src/main/SettingStore.scala
@@ -1,5 +1,8 @@
 package lila.memo
 
+import scala.util.matching.Regex
+import scala.util.Try
+
 import lila.db.dsl._
 import play.api.data._, Forms._
 
@@ -70,9 +73,17 @@ object SettingStore {
     implicit val stringsBsonHandler = lila.db.dsl.isoHandler(stringsIso)
     implicit val stringsReader = StringReader.fromIso(stringsIso)
   }
+  object Regex {
+    val regexIso = lila.common.Iso.string[Regex](_.r, _.toString)
+    implicit val regexBsonHandler = lila.db.dsl.isoHandler(regexIso)
+    implicit val regexReader = StringReader.fromIso(regexIso)
+  }
 
   final class Formable[A](val form: A => Form[_])
   object Formable {
+    implicit val regexFormable = new Formable[Regex](v => Form(single(
+      "v" -> text.verifying(t => Try(t.r).isSuccess)
+    )) fill v.toString)
     implicit val booleanFormable = new Formable[Boolean](v => Form(single("v" -> boolean)) fill v)
     implicit val intFormable = new Formable[Int](v => Form(single("v" -> number)) fill v)
     implicit val stringFormable = new Formable[String](v => Form(single("v" -> text)) fill v)

modules/socket/src/main/Env.scala
@@ -41,9 +41,11 @@ final class Env(
     text = "Send extra debugging to websockets.".some
   )
 
-  val socketRemoteUsersSetting = settingStore[String](
+  import lila.memo.SettingStore.Regex._
+  import lila.memo.SettingStore.Formable.regexFormable
+  val socketRemoteUsersSetting = settingStore[scala.util.matching.Regex](
     "socketRemoteUsers",
-    default = "",
+    default = "".r,
     text = "Regex selecting user IDs using remote socket".some
   )
 }

notzmv/lidraughts

modules/memo/src/main/SettingStore.scala
@@ -1,5 +1,8 @@
 package lidraughts.memo
 
+import scala.util.matching.Regex
+import scala.util.Try
+
 import lidraughts.db.dsl._
 import play.api.data._, Forms._
 
@@ -70,9 +73,17 @@ object SettingStore {
     implicit val stringsBsonHandler = lidraughts.db.dsl.isoHandler(stringsIso)
     implicit val stringsReader = StringReader.fromIso(stringsIso)
   }
+  object Regex {
+    val regexIso = lidraughts.common.Iso.string[Regex](_.r, _.toString)
+    implicit val regexBsonHandler = lidraughts.db.dsl.isoHandler(regexIso)
+    implicit val regexReader = StringReader.fromIso(regexIso)
+  }
 
   final class Formable[A](val form: A => Form[_])
   object Formable {
+    implicit val regexFormable = new Formable[Regex](v => Form(single(
+      "v" -> text.verifying(t => Try(t.r).isSuccess)
+    )) fill v.toString)
     implicit val booleanFormable = new Formable[Boolean](v => Form(single("v" -> boolean)) fill v)
     implicit val intFormable = new Formable[Int](v => Form(single("v" -> number)) fill v)
     implicit val stringFormable = new Formable[String](v => Form(single("v" -> text)) fill v)

modules/socket/src/main/Env.scala
@@ -41,9 +41,11 @@ final class Env(
     text = "Send extra debugging to websockets.".some
   )
 
-  val socketRemoteUsersSetting = settingStore[String](
+  import lidraughts.memo.SettingStore.Regex._
+  import lidraughts.memo.SettingStore.Formable.regexFormable
+  val socketRemoteUsersSetting = settingStore[scala.util.matching.Regex](
     "socketRemoteUsers",
-    default = "",
+    default = "".r,
     text = "Regex selecting user IDs using remote socket".some
   )
 }