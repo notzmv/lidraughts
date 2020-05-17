lichess-org/lila

remote socket: no longer remove games on finish
but only when unwatched

modules/bot/src/main/GameStateStream.scala
@@ -9,10 +9,10 @@ import chess.format.FEN
 import lila.chat.Chat
 import lila.chat.UserLine
 import lila.game.Event.ReloadOwner
-import lila.game.actorApi.{ AbortedBy, MoveGameEvent }
+import lila.game.actorApi.{ AbortedBy, FinishGame, MoveGameEvent }
 import lila.game.{ Game, GameRepo }
 import lila.hub.actorApi.map.Tell
-import lila.hub.actorApi.round.{ MoveEvent, FinishGameId }
+import lila.hub.actorApi.round.MoveEvent
 import lila.round.actorApi.round.{ DrawNo, DrawYes }
 import lila.socket.actorApi.BotConnected
 import lila.user.User
@@ -38,7 +38,7 @@ final class GameStateStream(
 
           private val classifiers = List(
             MoveGameEvent makeSymbol id,
-            'finishGameId, 'abortGame,
+            'finishGame, 'abortGame,
             Chat classify Chat.Id(id),
             Chat classify Chat.Id(s"$id/w")
           )
@@ -69,7 +69,7 @@ final class GameStateStream(
             case MoveGameEvent(g, _, _) if g.id == id => pushState(g)
             case lila.chat.actorApi.ChatLine(chatId, UserLine(username, _, text, false, false)) =>
               pushChatLine(username, text, chatId.value.size == Game.gameIdSize)
-            case FinishGameId(gameId) if gameId == id => onGameOver
+            case FinishGame(g, _, _) if g.id == id => onGameOver
             case AbortedBy(pov) if pov.gameId == id => onGameOver
             case SetOnline =>
               setConnected(true)

notzmv/lidraughts

modules/bot/src/main/GameStateStream.scala
@@ -9,10 +9,10 @@ import draughts.format.FEN
 
 import lidraughts.chat.Chat
 import lidraughts.chat.UserLine
-import lidraughts.game.actorApi.{ AbortedBy, MoveGameEvent }
+import lidraughts.game.actorApi.{ AbortedBy, FinishGame, MoveGameEvent }
 import lidraughts.game.{ Game, GameRepo }
 import lidraughts.hub.actorApi.map.Tell
-import lidraughts.hub.actorApi.round.{ MoveEvent, FinishGameId }
+import lidraughts.hub.actorApi.round.MoveEvent
 import lidraughts.socket.actorApi.BotConnected
 import lidraughts.user.User
 
@@ -37,7 +37,7 @@ final class GameStateStream(
 
           private val classifiers = List(
             MoveGameEvent makeSymbol id,
-            'finishGameId, 'abortGame,
+            'finishGame, 'abortGame,
             Chat classify Chat.Id(id),
             Chat classify Chat.Id(s"$id/w")
           )
@@ -68,7 +68,7 @@ final class GameStateStream(
             case MoveGameEvent(g, _, _) if g.id == id => pushState(g)
             case lidraughts.chat.actorApi.ChatLine(chatId, UserLine(username, _, text, false, false)) =>
               pushChatLine(username, text, chatId.value.size == Game.gameIdSize)
-            case FinishGameId(gameId) if gameId == id => onGameOver
+            case FinishGame(g, _, _) if g.id == id => onGameOver
             case AbortedBy(pov) if pov.gameId == id => onGameOver
             case SetOnline =>
               setConnected(true)