lichess-org/lila

remote socket monitoring and robustness

modules/socket/src/main/SocketTrouper.scala
@@ -196,5 +196,5 @@ trait LoneSocket { self: SocketTrouper[_] =>
     this ! lila.socket.actorApi.Broom
     lila.mon.socket.queueSize(monitoringName)(queueSize)
   }
-  system.lilaBus.subscribe(this, 'deploy, 'shutdown, 'announce)
+  system.lilaBus.subscribe(this, 'deploy, 'announce)
 }

notzmv/lidraughts

modules/socket/src/main/SocketTrouper.scala
@@ -196,5 +196,5 @@ trait LoneSocket { self: SocketTrouper[_] =>
     this ! lidraughts.socket.actorApi.Broom
     lidraughts.mon.socket.queueSize(monitoringName)(queueSize)
   }
-  system.lidraughtsBus.subscribe(this, 'deploy, 'shutdown, 'announce)
+  system.lidraughtsBus.subscribe(this, 'deploy, 'announce)
 }