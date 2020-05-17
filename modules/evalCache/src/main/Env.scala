lichess-org/lila

remote socket support for evalPut - untested

modules/evalCache/src/main/Env.scala
@@ -16,9 +16,9 @@ final class Env(
 
   private val CollectionEvalCache = config getString "collection.eval_cache"
 
-  private lazy val truster = new EvalCacheTruster
+  private lazy val truster = new EvalCacheTruster(asyncCache)
 
-  private lazy val upgrade = new EvalCacheUpgrade(asyncCache)
+  private lazy val upgrade = new EvalCacheUpgrade
 
   lazy val api = new EvalCacheApi(
     coll = db(CollectionEvalCache),
@@ -36,10 +36,17 @@ final class Env(
   system.lilaBus.subscribeFun('socketLeave) {
     case lila.socket.actorApi.SocketLeave(uid, _) => upgrade unregister uid
   }
+
+  // remote socket support
   system.lilaBus.subscribeFun(Symbol("remoteSocketIn:evalGet")) {
-    case RemoteSocketTellSriIn(sri, d) =>
+    case RemoteSocketTellSriIn(sri, _, d) =>
       socketHandler.evalGet(Uid(sri), d, res => system.lilaBus.publish(RemoteSocketTellSriOut(sri, res), 'remoteSocketOut))
   }
+  system.lilaBus.subscribeFun(Symbol("remoteSocketIn:evalPut")) {
+    case RemoteSocketTellSriIn(sri, Some(userId), d) =>
+      socketHandler.untrustedEvalPut(Uid(sri), userId, d)
+  }
+  // END remote socket support
 
   def cli = new lila.common.Cli {
     def process = {

modules/evalCache/src/main/EvalCacheTruster.scala
@@ -1,13 +1,14 @@
 package lila.evalCache
 
+import scala.concurrent.duration._
 import org.joda.time.{ DateTime, Days }
 
 import lila.security.Granter
-import lila.user.User
+import lila.user.{ User, UserRepo }
 
-private final class EvalCacheTruster {
+private final class EvalCacheTruster(asyncCache: lila.memo.AsyncCache.Builder) {
 
-  import EvalCacheEntry.Trust
+  import EvalCacheEntry.{ Trust, TrustedUser }
 
   private val LOWER = Trust(-9999)
   private val HIGHER = Trust(9999)
@@ -22,9 +23,18 @@ private final class EvalCacheTruster {
         nbGamesBonus(user)
     }
 
-  def makeTrusted(user: User) = EvalCacheEntry.TrustedUser(apply(user), user)
+  private val userIdCache = asyncCache.multi[User.ID, Option[TrustedUser]](
+    name = "evalCache.userIdTrustCache  ",
+    f = userId => UserRepo named userId map2 makeTrusted,
+    expireAfter = _.ExpireAfterWrite(10 minutes),
+    resultTimeout = 10 seconds
+  )
 
-  def shouldPut(user: User) = !apply(user).isTooLow
+  def cachedTrusted(userId: User.ID): Fu[Option[TrustedUser]] = userIdCache get userId
+
+  def makeTrusted(user: User) = TrustedUser(apply(user), user)
+
+  def shouldPut(user: User) = apply(user).isEnough
 
   // 0 days = -1
   // 1 month = 0

modules/evalCache/src/main/JsonHandlers.scala
@@ -26,14 +26,16 @@ object JsonHandlers {
     .add("cp", pv.score.cp)
     .add("mate", pv.score.mate)
 
-  def readPut(trustedUser: TrustedUser, o: JsObject): Option[Input.Candidate] = for {
-    d <- o obj "d"
-    variant = chess.variant.Variant orDefault ~d.str("variant")
+  private[evalCache] def readPut(trustedUser: TrustedUser, o: JsObject): Option[Input.Candidate] =
+    o obj "d" flatMap { readPutData(trustedUser, _) }
+
+  private[evalCache] def readPutData(trustedUser: TrustedUser, d: JsObject): Option[Input.Candidate] = for {
     fen <- d str "fen"
     knodes <- d int "knodes"
     depth <- d int "depth"
     pvObjs <- d objs "pvs"
     pvs <- pvObjs.map(parsePv).sequence.flatMap(_.toNel)
+    variant = chess.variant.Variant orDefault ~d.str("variant")
   } yield Input.Candidate(variant, fen, Eval(
     pvs = pvs,
     knodes = Knodes(knodes),

notzmv/lidraughts

modules/evalCache/src/main/Env.scala
@@ -16,9 +16,9 @@ final class Env(
 
   private val CollectionEvalCache = config getString "collection.eval_cache"
 
-  private lazy val truster = new EvalCacheTruster
+  private lazy val truster = new EvalCacheTruster(asyncCache)
 
-  private lazy val upgrade = new EvalCacheUpgrade(asyncCache)
+  private lazy val upgrade = new EvalCacheUpgrade
 
   lazy val api = new EvalCacheApi(
     coll = db(CollectionEvalCache),
@@ -36,10 +36,17 @@ final class Env(
   system.lidraughtsBus.subscribeFun('socketLeave) {
     case lidraughts.socket.actorApi.SocketLeave(uid, _) => upgrade unregister uid
   }
+
+  // remote socket support
   system.lidraughtsBus.subscribeFun(Symbol("remoteSocketIn:evalGet")) {
-    case RemoteSocketTellSriIn(sri, d) =>
+    case RemoteSocketTellSriIn(sri, _, d) =>
       socketHandler.evalGet(Uid(sri), d, res => system.lidraughtsBus.publish(RemoteSocketTellSriOut(sri, res), 'remoteSocketOut))
   }
+  system.lidraughtsBus.subscribeFun(Symbol("remoteSocketIn:evalPut")) {
+    case RemoteSocketTellSriIn(sri, Some(userId), d) =>
+      socketHandler.untrustedEvalPut(Uid(sri), userId, d)
+  }
+  // END remote socket support
 
   def cli = new lidraughts.common.Cli {
     def process = {

modules/evalCache/src/main/EvalCacheTruster.scala
@@ -1,13 +1,14 @@
 package lidraughts.evalCache
 
+import scala.concurrent.duration._
 import org.joda.time.{ DateTime, Days }
 
 import lidraughts.security.Granter
-import lidraughts.user.User
+import lidraughts.user.{ User, UserRepo }
 
-private final class EvalCacheTruster {
+private final class EvalCacheTruster(asyncCache: lidraughts.memo.AsyncCache.Builder) {
 
-  import EvalCacheEntry.Trust
+  import EvalCacheEntry.{ Trust, TrustedUser }
 
   private val LOWER = Trust(-9999)
   private val HIGHER = Trust(9999)
@@ -22,9 +23,18 @@ private final class EvalCacheTruster {
         nbGamesBonus(user)
     }
 
-  def makeTrusted(user: User) = EvalCacheEntry.TrustedUser(apply(user), user)
+  private val userIdCache = asyncCache.multi[User.ID, Option[TrustedUser]](
+    name = "evalCache.userIdTrustCache  ",
+    f = userId => UserRepo named userId map2 makeTrusted,
+    expireAfter = _.ExpireAfterWrite(10 minutes),
+    resultTimeout = 10 seconds
+  )
 
-  def shouldPut(user: User) = !apply(user).isTooLow
+  def cachedTrusted(userId: User.ID): Fu[Option[TrustedUser]] = userIdCache get userId
+
+  def makeTrusted(user: User) = TrustedUser(apply(user), user)
+
+  def shouldPut(user: User) = apply(user).isEnough
 
   // 0 days = -1
   // 1 month = 0

modules/evalCache/src/main/JsonHandlers.scala
@@ -35,14 +35,16 @@ object JsonHandlers {
   ).add("cp", pv.score.cp)
     .add("win", pv.score.win)
 
-  def readPut(trustedUser: TrustedUser, o: JsObject): Option[Input.Candidate] = for {
-    d <- o obj "d"
-    variant = draughts.variant.Variant orDefault ~d.str("variant")
+  private[evalCache] def readPut(trustedUser: TrustedUser, o: JsObject): Option[Input.Candidate] =
+    o obj "d" flatMap { readPutData(trustedUser, _) }
+
+  private[evalCache] def readPutData(trustedUser: TrustedUser, d: JsObject): Option[Input.Candidate] = for {
     fen <- d str "fen"
     knodes <- d int "knodes"
     depth <- d int "depth"
     pvObjs <- d objs "pvs"
     pvs <- pvObjs.map(parsePv).sequence.flatMap(_.toNel)
+    variant = draughts.variant.Variant orDefault ~d.str("variant")
   } yield Input.Candidate(variant, fen, Eval(
     pvs = pvs,
     knodes = Knodes(knodes),