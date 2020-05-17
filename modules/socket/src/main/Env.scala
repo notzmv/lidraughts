lichess-org/lila

redis remote WS with scredis WIP
it works, but pulls akka 2.5 - where our play requires 2.4

modules/socket/src/main/Env.scala
@@ -2,21 +2,43 @@ package lila.socket
 
 import akka.actor._
 import com.typesafe.config.Config
+import scala.concurrent.duration._
+import scala.concurrent.Future
 
 import actorApi._
 
 final class Env(
     system: ActorSystem,
+    config: Config,
+    lifecycle: play.api.inject.ApplicationLifecycle,
     settingStore: lila.memo.SettingStore.Builder
 ) {
 
-  import scala.concurrent.duration._
+  private val settings = new {
+    val RedisHost = config getString "redis.host"
+    val RedisPort = config getInt "redis.port"
+  }
+  import settings._
 
   private val population = new Population(system)
 
   private val moveBroadcast = new MoveBroadcast(system)
 
   private val userRegister = new UserRegister(system)
+
+  private val redis = new scredis.Redis(host = RedisHost, port = RedisPort)
+
+  private val remoteSocket = new RemoteSocket(
+    redis = redis,
+    chanIn = "site-in",
+    chanOut = "site-out",
+    bus = system.lilaBus
+  )
+
+  lifecycle.addStopHook { () =>
+    logger.info("Stopping the Redis client...")
+    Future(redis.quit())
+  }
 
   system.scheduler.schedule(5 seconds, 1 seconds) { population ! PopulationTell }
 
@@ -31,6 +53,8 @@ object Env {
 
   lazy val current = "socket" boot new Env(
     system = lila.common.PlayApp.system,
+    config = lila.common.PlayApp loadConfig "socket",
+    lifecycle = lila.common.PlayApp.lifecycle,
     settingStore = lila.memo.Env.current.settingStore
   )
 }

notzmv/lidraughts

modules/socket/src/main/Env.scala
@@ -2,21 +2,43 @@ package lidraughts.socket
 
 import akka.actor._
 import com.typesafe.config.Config
+import scala.concurrent.duration._
+import scala.concurrent.Future
 
 import actorApi._
 
 final class Env(
     system: ActorSystem,
+    config: Config,
+    lifecycle: play.api.inject.ApplicationLifecycle,
     settingStore: lidraughts.memo.SettingStore.Builder
 ) {
 
-  import scala.concurrent.duration._
+  private val settings = new {
+    val RedisHost = config getString "redis.host"
+    val RedisPort = config getInt "redis.port"
+  }
+  import settings._
 
   private val population = new Population(system)
 
   private val moveBroadcast = new MoveBroadcast(system)
 
   private val userRegister = new UserRegister(system)
+
+  private val redis = new scredis.Redis(host = RedisHost, port = RedisPort)
+
+  private val remoteSocket = new RemoteSocket(
+    redis = redis,
+    chanIn = "site-in",
+    chanOut = "site-out",
+    bus = system.lidraughtsBus
+  )
+
+  lifecycle.addStopHook { () =>
+    logger.info("Stopping the Redis client...")
+    Future(redis.quit())
+  }
 
   system.scheduler.schedule(5 seconds, 1 seconds) { population ! PopulationTell }
 
@@ -31,6 +53,8 @@ object Env {
 
   lazy val current = "socket" boot new Env(
     system = lidraughts.common.PlayApp.system,
+    config = lidraughts.common.PlayApp loadConfig "socket",
+    lifecycle = lidraughts.common.PlayApp.lifecycle,
     settingStore = lidraughts.memo.Env.current.settingStore
   )
 }