lichess-org/lila

implement evalGet and eval sub for remote socket
completely untested - don't deploy this

modules/evalCache/src/main/EvalCacheSocketHandler.scala
@@ -1,6 +1,7 @@
 package lila.evalCache
 
 import play.api.libs.json._
+import chess.variant.Variant
 
 import chess.format.FEN
 import lila.socket._
@@ -27,20 +28,29 @@ final class EvalCacheSocketHandler(
       JsonHandlers.readPut(tu, o) foreach { api.put(tu, _, uid) }
     }
 
-    case ("evalGet", o) => for {
-      d <- o obj "d"
-      variant = chess.variant.Variant orDefault ~d.str("variant")
-      fen <- d str "fen" map FEN.apply
-      multiPv = (d int "mpv") | 1
-      path <- d str "path"
-    } {
-      api.getEvalJson(variant, fen, multiPv) foreach {
-        _ foreach { json =>
-          member push Socket.makeMessage("evalHit", json + ("path" -> JsString(path)))
-        }
-      }
-      if (d.value contains "up")
-        upgrade.register(uid, member, variant, fen, multiPv, path)
+    case ("evalGet", o) => o obj "d" foreach { d =>
+      evalGet(uid, d,
+        reply = res => member push Socket.makeMessage("evalHit", res),
+        subscribe = (variant, fen, multiPv, path) => upgrade.register(uid, variant, fen, multiPv, path)(member.push))
     }
+  }
+
+  private[evalCache] def evalGet(
+    uid: Socket.Uid,
+    d: JsObject,
+    reply: JsObject => Unit,
+    subscribe: (Variant, FEN, Int, String) => Unit
+  ): Unit = for {
+    fen <- d str "fen" map FEN.apply
+    variant = Variant orDefault ~d.str("variant")
+    multiPv = (d int "mpv") | 1
+    path <- d str "path"
+  } {
+    api.getEvalJson(variant, fen, multiPv) foreach {
+      _ foreach { json =>
+        reply(json + ("path" -> JsString(path)))
+      }
+    }
+    if (d.value contains "up") subscribe(variant, fen, multiPv, path)
   }
 }

notzmv/lidraughts

modules/evalCache/src/main/EvalCacheSocketHandler.scala
@@ -1,6 +1,7 @@
 package lidraughts.evalCache
 
 import play.api.libs.json._
+import draughts.variant.Variant
 
 import draughts.format.FEN
 import lidraughts.socket._
@@ -27,20 +28,29 @@ final class EvalCacheSocketHandler(
       JsonHandlers.readPut(tu, o) foreach { api.put(tu, _, uid) }
     }
 
-    case ("evalGet", o) => for {
-      d <- o obj "d"
-      variant = draughts.variant.Variant orDefault ~d.str("variant")
-      fen <- d str "fen" map FEN.apply
-      multiPv = (d int "mpv") | 1
-      path <- d str "path"
-    } {
-      api.getEvalJson(variant, fen, multiPv) foreach {
-        _ foreach { json =>
-          member push Socket.makeMessage("evalHit", json + ("path" -> JsString(path)))
-        }
-      }
-      if (d.value contains "up")
-        upgrade.register(uid, member, variant, fen, multiPv, path)
+    case ("evalGet", o) => o obj "d" foreach { d =>
+      evalGet(uid, d,
+        reply = res => member push Socket.makeMessage("evalHit", res),
+        subscribe = (variant, fen, multiPv, path) => upgrade.register(uid, variant, fen, multiPv, path)(member.push))
     }
+  }
+
+  private[evalCache] def evalGet(
+    uid: Socket.Uid,
+    d: JsObject,
+    reply: JsObject => Unit,
+    subscribe: (Variant, FEN, Int, String) => Unit
+  ): Unit = for {
+    fen <- d str "fen" map FEN.apply
+    variant = Variant orDefault ~d.str("variant")
+    multiPv = (d int "mpv") | 1
+    path <- d str "path"
+  } {
+    api.getEvalJson(variant, fen, multiPv) foreach {
+      _ foreach { json =>
+        reply(json + ("path" -> JsString(path)))
+      }
+    }
+    if (d.value contains "up") subscribe(variant, fen, multiPv, path)
   }
 }