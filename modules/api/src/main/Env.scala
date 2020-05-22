lichess-org/lila

simul remote socket WIP

modules/api/src/main/Env.scala
@@ -33,6 +33,8 @@ final class Env(
     isPlaying: lila.user.User.ID => Boolean,
     pools: List[lila.pool.PoolConfig],
     challengeJsonView: lila.challenge.JsonView,
+    remoteSocketApi: lila.socket.RemoteSocket,
+    siteRemoteSocket: lila.site.SiteRemoteSocket,
     val isProd: Boolean
 ) {
 
@@ -148,6 +150,10 @@ final class Env(
 
   lazy val cli = new Cli(system.lilaBus)
 
+  //   new RemoteSocket(remoteSocketApi, Map(
+  //     "site-in" -> siteRemoteSocket.handler
+  //   ))
+
   KamonPusher.start(system) {
     new KamonPusher(countUsers = () => userEnv.onlineUserIdMemo.count)
   }
@@ -192,6 +198,8 @@ object Env {
     isPlaying = lila.relation.Env.current.online.isPlaying,
     pools = lila.pool.Env.current.api.configs,
     challengeJsonView = lila.challenge.Env.current.jsonView,
+    remoteSocketApi = lila.socket.Env.current.remoteSocket,
+    siteRemoteSocket = lila.site.Env.current.remoteSocket,
     isProd = lila.common.PlayApp.isProd
   )
 }

modules/simul/src/main/Socket.scala
@@ -21,7 +21,7 @@ private[simul] final class Socket(
     lightUser: lila.common.LightUser.Getter,
     sriTtl: Duration,
     keepMeAlive: () => Unit
-) extends SocketTrouper[SimulSocketMember](system, uidTtl) with Historical[SimulSocketMember, Messadata] {
+) extends SocketTrouper[SimulSocketMember](system, sriTtl) with Historical[SimulSocketMember, Messadata] {
 
   lilaBus.subscribe(this, chatClassifier)
 
@@ -69,14 +69,10 @@ private[simul] final class Socket(
     case GetUserIdsP(promise) => promise success members.values.flatMap(_.userId)
 
     case Join(sri, user, version, promise) =>
-      val (enumerator, channel) = Concurrent.broadcast[JsValue]
-      val member = SimulSocketMember(channel, user)
+      val member = SimulSocketMember(sri, user.map(_.id), user.exists(_.troll), lilaBus)
       addMember(sri, member)
       notifyCrowd
-      promise success Connected(
-        prependEventsSince(version, enumerator, member),
-        member
-      )
+      getEventsSince(version, member) foreach member.push
 
     case NotifyCrowd =>
       delayedCrowdNotification = false
@@ -91,7 +87,7 @@ private[simul] final class Socket(
     if (members.nonEmpty) keepMeAlive()
   }
 
-  override protected def afterQuit(sri: lila.socket.Socket.Sri, member: Member) = notifyCrowd
+  override protected def afterQuit(sri: lila.socket.Socket.Sri, member: SimulSocketMember) = notifyCrowd
 
   def notifyCrowd: Unit =
     if (!delayedCrowdNotification) {
@@ -99,6 +95,6 @@ private[simul] final class Socket(
       system.scheduler.scheduleOnce(500 millis)(this ! NotifyCrowd)
     }
 
-  protected def shouldSkipMessageFor(message: Message, member: Member) =
+  protected def shouldSkipMessageFor(message: Message, member: SimulSocketMember) =
     message.metadata.trollish && !member.troll
 }

modules/simul/src/main/actorApi.scala
@@ -1,18 +1,19 @@
 package lila.simul
 package actorApi
 
-import scala.concurrent.Promise
 import play.api.libs.json.JsObject
+import scala.concurrent.Promise
 
 import lila.game.Game
-import lila.socket.Socket.{ Sri, SocketVersion }
 import lila.socket.RemoteSocketMember
+import lila.socket.Socket.{ Sri, SocketVersion }
 import lila.user.User
 
 private[simul] case class SimulSocketMember(
-    push: JsObject => Unit,
+    sri: Sri,
     userId: Option[String],
-    troll: Boolean
+    troll: Boolean,
+    bus: lila.common.Bus
 ) extends RemoteSocketMember
 
 private[simul] case class Messadata(trollish: Boolean = false)

notzmv/lidraughts

modules/api/src/main/Env.scala
@@ -33,6 +33,8 @@ final class Env(
     isPlaying: lidraughts.user.User.ID => Boolean,
     pools: List[lidraughts.pool.PoolConfig],
     challengeJsonView: lidraughts.challenge.JsonView,
+    remoteSocketApi: lidraughts.socket.RemoteSocket,
+    siteRemoteSocket: lidraughts.site.SiteRemoteSocket,
     val isProd: Boolean
 ) {
 
@@ -140,6 +142,10 @@ final class Env(
 
   lazy val cli = new Cli(system.lidraughtsBus)
 
+  //   new RemoteSocket(remoteSocketApi, Map(
+  //     "site-in" -> siteRemoteSocket.handler
+  //   ))
+
   KamonPusher.start(system) {
     new KamonPusher(countUsers = () => userEnv.onlineUserIdMemo.count)
   }
@@ -184,6 +190,8 @@ object Env {
     isPlaying = lidraughts.relation.Env.current.online.isPlaying,
     pools = lidraughts.pool.Env.current.api.configs,
     challengeJsonView = lidraughts.challenge.Env.current.jsonView,
+    remoteSocketApi = lidraughts.socket.Env.current.remoteSocket,
+    siteRemoteSocket = lidraughts.site.Env.current.remoteSocket,
     isProd = lidraughts.common.PlayApp.isProd
   )
 }

modules/simul/src/main/Socket.scala
@@ -20,7 +20,7 @@ private[simul] final class Socket(
     lightUser: lidraughts.common.LightUser.Getter,
     uidTtl: Duration,
     keepMeAlive: () => Unit
-) extends SocketTrouper[SimulSocketMember](system, uidTtl) with Historical[SimulSocketMember, Messadata] {
+) extends SocketTrouper[SimulSocketMember](system, sriTtl) with Historical[SimulSocketMember, Messadata] {
 
   lidraughtsBus.subscribe(this, chatClassifier)
 
@@ -76,15 +76,11 @@ private[simul] final class Socket(
 
     case GetUserIdsP(promise) => promise success members.values.flatMap(_.userId)
 
-    case Join(uid, user, version, promise) =>
-      val (enumerator, channel) = Concurrent.broadcast[JsValue]
-      val member = SimulSocketMember(channel, user)
-      addMember(uid, member)
+    case Join(sri, user, version, promise) =>
+      val member = SimulSocketMember(sri, user.map(_.id), user.exists(_.troll), lidraughtsBus)
+      addMember(sri, member)
       notifyCrowd
-      promise success Connected(
-        prependEventsSince(version, enumerator, member),
-        member
-      )
+      getEventsSince(version, member) foreach member.push
 
     case NotifyCrowd =>
       delayedCrowdNotification = false
@@ -99,7 +95,7 @@ private[simul] final class Socket(
     if (members.nonEmpty) keepMeAlive()
   }
 
-  override protected def afterQuit(uid: lidraughts.socket.Socket.Uid, member: Member) = notifyCrowd
+  override protected def afterQuit(sri: lidraughts.socket.Socket.Sri, member: SimulSocketMember) = notifyCrowd
 
   def notifyCrowd: Unit =
     if (!delayedCrowdNotification) {
@@ -107,7 +103,7 @@ private[simul] final class Socket(
       system.scheduler.scheduleOnce(500 millis)(this ! NotifyCrowd)
     }
 
-  protected def shouldSkipMessageFor(message: Message, member: Member) =
+  protected def shouldSkipMessageFor(message: Message, member: SimulSocketMember) =
     message.metadata.trollish && !member.troll
 
   private val noMessadata = Messadata()

modules/simul/src/main/actorApi.scala
@@ -3,17 +3,17 @@ package actorApi
 
 import play.api.libs.json.JsObject
 import scala.concurrent.Promise
-import play.api.libs.json.JsObject
 
 import lidraughts.game.Game
-import lidraughts.socket.Socket.{ Uid, SocketVersion }
 import lidraughts.socket.RemoteSocketMember
+import lidraughts.socket.Socket.{ Sri, SocketVersion }
 import lidraughts.user.User
 
 private[simul] case class SimulSocketMember(
-    push: JsObject => Unit,
+    sri: Sri,
     userId: Option[String],
-    troll: Boolean
+    troll: Boolean,
+    bus: lidraughts.common.Bus
 ) extends RemoteSocketMember
 
 private[simul] case class Messadata(trollish: Boolean = false)