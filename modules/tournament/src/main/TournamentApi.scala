lichess-org/lila

more sri rename fixes

modules/tournament/src/main/TournamentApi.scala
@@ -377,9 +377,9 @@ final class TournamentApi(
             _ foreach { currentPairing =>
               roundMap.tell(currentPairing.gameId, AbortForce)
             }
-          } >> PairingRepo.opponentsOf(tour.id, userId).flatMap { sris =>
+          } >> PairingRepo.opponentsOf(tour.id, userId).flatMap { uids =>
             PairingRepo.removeByTourAndUserId(tour.id, userId) >>
-              lila.common.Future.applySequentially(sris.toList)(updatePlayer(tour, none))
+              lila.common.Future.applySequentially(uids.toList)(updatePlayer(tour, none))
           }
         else if (tour.isFinished && tour.winnerId.contains(userId))
           PlayerRepo winner tour.id flatMap {

notzmv/lidraughts

modules/tournament/src/main/TournamentApi.scala
@@ -413,9 +413,9 @@ final class TournamentApi(
             _ foreach { currentPairing =>
               roundMap.tell(currentPairing.gameId, AbortForce)
             }
-          } >> PairingRepo.opponentsOf(tour.id, userId).flatMap { sris =>
+          } >> PairingRepo.opponentsOf(tour.id, userId).flatMap { uids =>
             PairingRepo.removeByTourAndUserId(tour.id, userId) >>
-              lidraughts.common.Future.applySequentially(sris.toList)(updatePlayer(tour, none))
+              lidraughts.common.Future.applySequentially(uids.toList)(updatePlayer(tour, none))
           }
         else if (tour.isFinished && tour.winnerId.contains(userId))
           PlayerRepo winner tour.id flatMap {