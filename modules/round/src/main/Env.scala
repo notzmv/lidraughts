lichess-org/lila

Revert "Add socket debug info"
This reverts commit e58a959bbaea5f13d325810dc32472790330004f.

modules/round/src/main/Env.scala
@@ -36,8 +36,7 @@ final class Env(
     evalCacheHandler: lila.evalCache.EvalCacheSocketHandler,
     isBotSync: lila.common.LightUser.IsBotSync,
     slackApi: lila.slack.SlackApi,
-    ratingFactors: () => lila.rating.RatingFactors,
-    val socketDebug: () => Boolean
+    ratingFactors: () => lila.rating.RatingFactors
 ) {
 
   private val settings = new {
@@ -279,7 +278,6 @@ object Env {
     evalCacheHandler = lila.evalCache.Env.current.socketHandler,
     isBotSync = lila.user.Env.current.lightUserApi.isBotSync,
     slackApi = lila.slack.Env.current.api,
-    ratingFactors = lila.rating.Env.current.ratingFactorsSetting.get,
-    socketDebug = lila.socket.Env.current.socketDebugSetting.get
+    ratingFactors = lila.rating.Env.current.ratingFactorsSetting.get
   )
 }

notzmv/lidraughts

modules/round/src/main/Env.scala
@@ -35,8 +35,7 @@ final class Env(
     evalCache: lidraughts.evalCache.EvalCacheApi,
     evalCacheHandler: lidraughts.evalCache.EvalCacheSocketHandler,
     isBotSync: lidraughts.common.LightUser.IsBotSync,
-    ratingFactors: () => lidraughts.rating.RatingFactors,
-    val socketDebug: () => Boolean
+    ratingFactors: () => lidraughts.rating.RatingFactors
 ) {
 
   private val settings = new {
@@ -281,7 +280,6 @@ object Env {
     evalCache = lidraughts.evalCache.Env.current.api,
     evalCacheHandler = lidraughts.evalCache.Env.current.socketHandler,
     isBotSync = lidraughts.user.Env.current.lightUserApi.isBotSync,
-    ratingFactors = lidraughts.rating.Env.current.ratingFactorsSetting.get,
-    socketDebug = lidraughts.socket.Env.current.socketDebugSetting.get
+    ratingFactors = lidraughts.rating.Env.current.ratingFactorsSetting.get
   )
 }