lichess-org/lila

simul remote socket WIP

modules/lobby/src/main/actorApi.scala
@@ -4,7 +4,7 @@ package actorApi
 import scala.concurrent.Promise
 
 import lila.game.Game
-import lila.socket.SocketMember
+import lila.socket.DirectSocketMember
 import lila.socket.Socket.{ Uid, Uids }
 import lila.user.User
 
@@ -13,7 +13,7 @@ private[lobby] case class Member(
     user: Option[LobbyUser],
     uid: Uid,
     mobile: Boolean
-) extends SocketMember {
+) extends DirectSocketMember {
 
   val userId = user.map(_.id)
 }

modules/round/src/main/actorApi.scala
@@ -8,12 +8,12 @@ import chess.{ MoveMetrics, Color }
 
 import lila.common.{ IpAddress, IsMobile }
 import lila.socket.Socket.{ SocketVersion, Uid }
-import lila.socket.SocketMember
+import lila.socket.DirectSocketMember
 import lila.user.User
 
 case class EventList(events: List[lila.game.Event])
 
-sealed trait Member extends SocketMember {
+sealed trait Member extends DirectSocketMember {
 
   val color: Color
   val playerIdOption: Option[String]

modules/site/src/main/actorApi.scala
@@ -3,14 +3,14 @@ package actorApi
 
 import scala.concurrent.Promise
 
-import lila.socket.SocketMember
+import lila.socket.DirectSocketMember
 import lila.socket.Socket.Uid
 
 case class Member(
     channel: JsChannel,
     userId: Option[String],
     flag: Option[String]
-) extends SocketMember {
+) extends DirectSocketMember {
 
   def isApi = flag has "api"
 }

modules/socket/src/main/RemoteSocket.scala
@@ -8,6 +8,7 @@ import chess.Centis
 import lila.common.{ Chronometer, WithResource }
 import lila.hub.actorApi.relation.ReloadOnlineFriends
 import lila.hub.actorApi.round.{ MoveEvent, Mlat }
+import lila.hub.actorApi.security.CloseAccount
 import lila.hub.actorApi.socket.{ SendTo, SendTos, WithUserIds, RemoteSocketTellSriIn, RemoteSocketTellSriOut }
 import lila.hub.actorApi.{ Deploy, Announce }
 
@@ -41,12 +42,13 @@ private final class RemoteSocket(
     val TellFlag = "tell/flag"
     val TellSri = "tell/sri"
     val Mlat = "mlat"
+    val DisconnectUser = "disconnect/user"
   }
 
   private val connectedUserIds = collection.mutable.Set.empty[String]
   private val watchedGameIds = collection.mutable.Set.empty[String]
 
-  bus.subscribeFun('moveEvent, 'socketUsers, 'deploy, 'announce, 'mlat, 'sendToFlag, 'remoteSocketOut) {
+  bus.subscribeFun('moveEvent, 'socketUsers, 'deploy, 'announce, 'mlat, 'sendToFlag, 'remoteSocketOut, 'accountClose) {
     case MoveEvent(gameId, fen, move) =>
       if (watchedGameIds(gameId)) send(Out.Move, gameId, move, fen)
     case SendTos(userIds, payload) =>
@@ -64,6 +66,8 @@ private final class RemoteSocket(
       send(Out.TellFlag, flag, Json stringify payload)
     case RemoteSocketTellSriOut(sri, payload) =>
       send(Out.TellSri, sri, Json stringify payload)
+    case CloseAccount(userId) =>
+      send(Out.DisconnectUser, userId)
     case WithUserIds(f) =>
       f(connectedUserIds)
   }

modules/socket/src/main/SocketMember.scala
@@ -1,15 +1,34 @@
 package lila.socket
 
-import play.api.libs.json.JsValue
+import play.api.libs.json.{ JsObject, JsValue }
 
 trait SocketMember {
 
-  protected val channel: JsChannel
   val userId: Option[String]
-
   def isAuth = userId.isDefined
+
+  def push(msg: JsValue): Unit
+
+  def end: Unit
+}
+
+trait DirectSocketMember extends SocketMember {
+
+  protected val channel: JsChannel
 
   def push(msg: JsValue) = channel push msg
 
   def end = channel.end
+}
+
+trait RemoteSocketMember extends SocketMember {
+
+  protected def bus: lila.common.Bus
+  protected def sri: Socket.Uid
+
+  def push(msg: JsValue) = msg.asOpt[JsObject] foreach { obj =>
+    bus.publish(lila.hub.actorApi.socket.RemoteSocketTellSriOut(sri.value, obj), 'remoteSocketOut)
+  }
+
+  def end = () // meh
 }

modules/tournament/src/main/actorApi.scala
@@ -4,7 +4,7 @@ package actorApi
 import scala.concurrent.Promise
 
 import lila.game.Game
-import lila.socket.Socket.{ Uid, SocketVersion }
+import lila.socket.Socket.{ Uid, DirectSocketVersion }
 import lila.socket.SocketMember
 import lila.user.User
 
@@ -12,7 +12,7 @@ private[tournament] case class Member(
     channel: JsChannel,
     userId: Option[String],
     troll: Boolean
-) extends SocketMember
+) extends DirectSocketMember
 
 private[tournament] object Member {
   def apply(channel: JsChannel, user: Option[User]): Member = Member(

notzmv/lidraughts

modules/lobby/src/main/actorApi.scala
@@ -4,7 +4,7 @@ package actorApi
 import scala.concurrent.Promise
 
 import lidraughts.game.Game
-import lidraughts.socket.SocketMember
+import lidraughts.socket.DirectSocketMember
 import lidraughts.socket.Socket.{ Uid, Uids }
 import lidraughts.user.User
 
@@ -13,7 +13,7 @@ private[lobby] case class Member(
     user: Option[LobbyUser],
     uid: Uid,
     mobile: Boolean
-) extends SocketMember {
+) extends DirectSocketMember {
 
   val userId = user.map(_.id)
 }

modules/round/src/main/actorApi.scala
@@ -8,12 +8,12 @@ import draughts.{ MoveMetrics, Color }
 
 import lidraughts.common.{ IpAddress, IsMobile }
 import lidraughts.socket.Socket.{ SocketVersion, Uid }
-import lidraughts.socket.SocketMember
+import lidraughts.socket.DirectSocketMember
 import lidraughts.user.User
 
 case class EventList(events: List[lidraughts.game.Event])
 
-sealed trait Member extends SocketMember {
+sealed trait Member extends DirectSocketMember {
 
   val color: Color
   val playerIdOption: Option[String]

modules/site/src/main/actorApi.scala
@@ -3,14 +3,14 @@ package actorApi
 
 import scala.concurrent.Promise
 
-import lidraughts.socket.SocketMember
+import lidraughts.socket.DirectSocketMember
 import lidraughts.socket.Socket.Uid
 
 case class Member(
     channel: JsChannel,
     userId: Option[String],
     flag: Option[String]
-) extends SocketMember {
+) extends DirectSocketMember {
 
   def isApi = flag has "api"
 }

modules/socket/src/main/RemoteSocket.scala
@@ -8,6 +8,7 @@ import draughts.Centis
 import lidraughts.common.{ Chronometer, WithResource }
 import lidraughts.hub.actorApi.relation.ReloadOnlineFriends
 import lidraughts.hub.actorApi.round.{ MoveEvent, Mlat }
+import lidraughts.hub.actorApi.security.CloseAccount
 import lidraughts.hub.actorApi.socket.{ SendTo, SendTos, WithUserIds, RemoteSocketTellSriIn, RemoteSocketTellSriOut }
 import lidraughts.hub.actorApi.{ Deploy, Announce }
 
@@ -41,12 +42,13 @@ private final class RemoteSocket(
     val TellFlag = "tell/flag"
     val TellSri = "tell/sri"
     val Mlat = "mlat"
+    val DisconnectUser = "disconnect/user"
   }
 
   private val connectedUserIds = collection.mutable.Set.empty[String]
   private val watchedGameIds = collection.mutable.Set.empty[String]
 
-  bus.subscribeFun('moveEvent, 'socketUsers, 'deploy, 'announce, 'mlat, 'sendToFlag, 'remoteSocketOut) {
+  bus.subscribeFun('moveEvent, 'socketUsers, 'deploy, 'announce, 'mlat, 'sendToFlag, 'remoteSocketOut, 'accountClose) {
     case MoveEvent(gameId, fen, move) =>
       if (watchedGameIds(gameId)) send(Out.Move, gameId, move, fen)
     case SendTos(userIds, payload) =>
@@ -64,6 +66,8 @@ private final class RemoteSocket(
       send(Out.TellFlag, flag, Json stringify payload)
     case RemoteSocketTellSriOut(sri, payload) =>
       send(Out.TellSri, sri, Json stringify payload)
+    case CloseAccount(userId) =>
+      send(Out.DisconnectUser, userId)
     case WithUserIds(f) =>
       f(connectedUserIds)
   }

modules/socket/src/main/SocketMember.scala
@@ -1,15 +1,34 @@
 package lidraughts.socket
 
-import play.api.libs.json.JsValue
+import play.api.libs.json.{ JsObject, JsValue }
 
 trait SocketMember {
 
-  protected val channel: JsChannel
   val userId: Option[String]
-
   def isAuth = userId.isDefined
+
+  def push(msg: JsValue): Unit
+
+  def end: Unit
+}
+
+trait DirectSocketMember extends SocketMember {
+
+  protected val channel: JsChannel
 
   def push(msg: JsValue) = channel push msg
 
   def end = channel.end
+}
+
+trait RemoteSocketMember extends SocketMember {
+
+  protected def bus: lidraughts.common.Bus
+  protected def sri: Socket.Uid
+
+  def push(msg: JsValue) = msg.asOpt[JsObject] foreach { obj =>
+    bus.publish(lidraughts.hub.actorApi.socket.RemoteSocketTellSriOut(sri.value, obj), 'remoteSocketOut)
+  }
+
+  def end = () // meh
 }

modules/tournament/src/main/actorApi.scala
@@ -4,7 +4,7 @@ package actorApi
 import scala.concurrent.Promise
 
 import lidraughts.game.Game
-import lidraughts.socket.Socket.{ Uid, SocketVersion }
+import lidraughts.socket.Socket.{ Uid, DirectSocketVersion  }
 import lidraughts.socket.SocketMember
 import lidraughts.user.User
 
@@ -12,7 +12,7 @@ private[tournament] case class Member(
     channel: JsChannel,
     userId: Option[String],
     troll: Boolean
-) extends SocketMember
+) extends DirectSocketMember
 
 private[tournament] object Member {
   def apply(channel: JsChannel, user: Option[User]): Member = Member(