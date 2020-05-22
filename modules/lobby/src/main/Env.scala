lichess-org/lila

lobby remote socket WIP

modules/lobby/src/main/Env.scala
@@ -4,16 +4,19 @@ import akka.actor._
 import com.typesafe.config.Config
 import scala.concurrent.duration._
 
+import lila.user.User
+
 final class Env(
     config: Config,
     db: lila.db.Env,
     hub: lila.hub.Env,
     onStart: String => Unit,
-    blocking: String => Fu[Set[String]],
+    blocking: User.ID => Fu[Set[User.ID]],
     playban: String => Fu[Option[lila.playban.TempBan]],
     gameCache: lila.game.Cached,
     poolApi: lila.pool.PoolApi,
     asyncCache: lila.memo.AsyncCache.Builder,
+    remoteSocketApi: lila.socket.RemoteSocket,
     system: ActorSystem
 ) {
 
@@ -68,6 +71,13 @@ final class Env(
   )
   system.lilaBus.subscribe(socketHandler, 'nbMembers, 'nbRounds)
 
+  private val remoteSocket = new LobbyRemoteSocket(
+    remoteSocketApi = remoteSocketApi,
+    socket = socket,
+    blocking = blocking,
+    bus = system.lilaBus
+  )
+
   private val abortListener = new AbortListener(seekApi, lobbyTrouper)
 
   system.lilaBus.subscribeFun('abortGame) {
@@ -87,6 +97,7 @@ object Env {
     gameCache = lila.game.Env.current.cached,
     poolApi = lila.pool.Env.current.api,
     asyncCache = lila.memo.Env.current.asyncCache,
+    remoteSocketApi = lila.socket.Env.current.remoteSocket,
     system = lila.common.PlayApp.system
   )
 }

modules/lobby/src/main/LobbyRemoteSocket.scala
@@ -3,16 +3,35 @@ package lila.lobby
 import play.api.libs.json._
 
 import lila.socket.RemoteSocket._, Protocol._
+import lila.user.{ User, UserRepo }
 
 final class LobbyRemoteSocket(
-    remoteSocketApi: lila.socket.RemoteSocket
+    remoteSocketApi: lila.socket.RemoteSocket,
+    socket: LobbySocket,
+    blocking: User.ID => Fu[Set[User.ID]],
+    bus: lila.common.Bus
 ) {
 
   private val send: (Path, Args*) => Unit = remoteSocketApi.sendTo("lobby-out") _
 
   private val handler: Handler = {
-    case m @ In.ConnectSri(sri, userId) => remoteSocketApi baseHandler m
+    case In.ConnectSri(sri, userOpt) =>
+      userOpt map In.ConnectUser.apply foreach remoteSocketApi.baseHandler.lift
+      userOpt ?? UserRepo.enabledById foreach { user =>
+        (user ?? (u => blocking(u.id))) foreach { blocks =>
+          val member = actorApi.LobbySocketMember(bus, user, blocks, sri)
+          socket ! actorApi.JoinRemote(member)
+        }
+      }
+    case In.DisconnectSri(sri, userOpt) =>
+      userOpt map In.DisconnectUser.apply foreach remoteSocketApi.baseHandler.lift
   }
 
-  remoteSocketApi.subscribe("lobby-in")(handler orElse remoteSocketApi.baseHandler)
+  remoteSocketApi.subscribe("lobby-in", In.baseReader)(handler orElse remoteSocketApi.baseHandler)
+}
+
+object LobbyRemoteSocket {
+
+  object Protocol {
+  }
 }

modules/lobby/src/main/LobbySocket.scala
@@ -37,15 +37,14 @@ private[lobby] final class LobbySocket(
       promise success Socket.Sris(members.keySet.map(Socket.Sri.apply)(scala.collection.breakOut))
       lila.mon.lobby.socket.idle(idleSris.size)
       lila.mon.lobby.socket.hookSubscribers(hookSubscriberSris.size)
-      lila.mon.lobby.socket.mobile(members.count(_._2.mobile))
 
     case Cleanup =>
       idleSris retain members.contains
       hookSubscriberSris retain members.contains
 
-    case Join(sri, user, blocks, mobile, promise) =>
+    case Join(sri, user, blocks, promise) =>
       val (enumerator, channel) = Concurrent.broadcast[JsValue]
-      val member = LobbySocketMember(channel, user, blocks, sri, mobile)
+      val member = LobbySocketMember(channel, user, blocks, sri)
       addMember(sri, member)
       promise success Connected(enumerator, member)
 
@@ -67,7 +66,7 @@ private[lobby] final class LobbySocket(
         }
       }
       if (hook.likePoolFiveO) withMember(hook.sri) { member =>
-        lila.mon.lobby.hook.createdLikePoolFiveO(member.mobile)()
+        lila.mon.lobby.hook.createdLikePoolFiveO()
       }
 
     case AddSeek(_) => notifySeeks
@@ -89,23 +88,23 @@ private[lobby] final class LobbySocket(
 
     case JoinHook(sri, hook, game, creatorColor) =>
       withMember(hook.sri) { member =>
-        lila.mon.lobby.hook.joinMobile(member.mobile)()
+        lila.mon.lobby.hook.join()
         notifyPlayerStart(game, creatorColor)(member)
       }
       withMember(sri) { member =>
-        lila.mon.lobby.hook.joinMobile(member.mobile)()
+        lila.mon.lobby.hook.join()
         if (hook.likePoolFiveO)
-          lila.mon.lobby.hook.acceptedLikePoolFiveO(member.mobile)()
+          lila.mon.lobby.hook.acceptedLikePoolFiveO()
         notifyPlayerStart(game, !creatorColor)(member)
       }
 
     case JoinSeek(userId, seek, game, creatorColor) =>
       membersByUserId(seek.user.id) foreach { member =>
-        lila.mon.lobby.seek.joinMobile(member.mobile)()
+        lila.mon.lobby.seek.join()
         notifyPlayerStart(game, creatorColor)(member)
       }
       membersByUserId(userId) foreach { member =>
-        lila.mon.lobby.seek.joinMobile(member.mobile)()
+        lila.mon.lobby.seek.join()
         notifyPlayerStart(game, !creatorColor)(member)
       }
 

modules/lobby/src/main/actorApi.scala
@@ -4,21 +4,19 @@ package actorApi
 import scala.concurrent.Promise
 
 import lila.game.Game
-import lila.socket.{ SocketMember, DirectSocketMember, RemoteSocketMember }
 import lila.socket.Socket.{ Sri, Sris }
+import lila.socket.{ SocketMember, DirectSocketMember, RemoteSocketMember }
 import lila.user.User
 
 private[lobby] sealed trait LobbySocketMember extends SocketMember {
   val user: Option[LobbyUser]
   val sri: Sri
-  val mobile: Boolean
 }
 
 private[lobby] case class LobbyDirectSocketMember(
     channel: JsChannel,
     user: Option[LobbyUser],
-    sri: Sri,
-    mobile: Boolean
+    sri: Sri
 ) extends LobbySocketMember with DirectSocketMember {
   val userId = user.map(_.id)
 }
@@ -26,28 +24,25 @@ private[lobby] case class LobbyDirectSocketMember(
 private[lobby] case class LobbyRemoteSocketMember(
     bus: lila.common.Bus,
     user: Option[LobbyUser],
-    sri: Sri,
-    mobile: Boolean
+    sri: Sri
 ) extends LobbySocketMember with RemoteSocketMember {
   val userId = user.map(_.id)
 }
 
 private[lobby] object LobbySocketMember {
 
-  def apply(channel: JsChannel, user: Option[User], blocking: Set[String], sri: Sri, mobile: Boolean): LobbyDirectSocketMember =
+  def apply(channel: JsChannel, user: Option[User], blocking: Set[String], sri: Sri): LobbyDirectSocketMember =
     LobbyDirectSocketMember(
       channel = channel,
       user = user map { LobbyUser.make(_, blocking) },
-      sri = sri,
-      mobile = mobile
+      sri = sri
     )
 
-  def apply(bus: lila.common.Bus, user: Option[User], blocking: Set[String], sri: Sri, mobile: Boolean): LobbyRemoteSocketMember =
+  def apply(bus: lila.common.Bus, user: Option[User], blocking: Set[String], sri: Sri): LobbyRemoteSocketMember =
     LobbyRemoteSocketMember(
       bus = bus,
       user = user map { LobbyUser.make(_, blocking) },
-      sri = sri,
-      mobile = mobile
+      sri = sri
     )
 }
 
@@ -68,7 +63,7 @@ private[lobby] case class BiteHook(hookId: String, sri: Sri, user: Option[LobbyU
 private[lobby] case class BiteSeek(seekId: String, user: LobbyUser)
 private[lobby] case class JoinHook(sri: Sri, hook: Hook, game: Game, creatorColor: chess.Color)
 private[lobby] case class JoinSeek(userId: String, seek: Seek, game: Game, creatorColor: chess.Color)
-private[lobby] case class Join(sri: Sri, user: Option[User], blocking: Set[String], mobile: Boolean, promise: Promise[Connected])
+private[lobby] case class Join(sri: Sri, user: Option[User], blocking: Set[String], promise: Promise[Connected])
 private[lobby] case class JoinRemote(member: LobbyRemoteSocketMember)
 private[lobby] case object Resync
 private[lobby] case class HookIds(ids: Vector[String])

notzmv/lidraughts

modules/lobby/src/main/Env.scala
@@ -5,17 +5,20 @@ import com.typesafe.config.Config
 import lidraughts.common.Strings
 import scala.concurrent.duration._
 
+import lidraughts.user.User
+
 final class Env(
     config: Config,
     db: lidraughts.db.Env,
     hub: lidraughts.hub.Env,
     onStart: String => Unit,
-    blocking: String => Fu[Set[String]],
+    blocking: User.ID => Fu[Set[User.ID]],
     playban: String => Fu[Option[lidraughts.playban.TempBan]],
     gameCache: lidraughts.game.Cached,
     poolApi: lidraughts.pool.PoolApi,
     asyncCache: lidraughts.memo.AsyncCache.Builder,
     settingStore: lidraughts.memo.SettingStore.Builder,
+    remoteSocketApi: lidraughts.socket.RemoteSocket,
     system: ActorSystem
 ) {
 
@@ -79,6 +82,13 @@ final class Env(
   )
   system.lidraughtsBus.subscribe(socketHandler, 'nbMembers, 'nbRounds)
 
+  private val remoteSocket = new LobbyRemoteSocket(
+    remoteSocketApi = remoteSocketApi,
+    socket = socket,
+    blocking = blocking,
+    bus = system.lidraughtsBus
+  )
+
   private val abortListener = new AbortListener(seekApi, lobbyTrouper)
 
   system.lidraughtsBus.subscribeFun('abortGame) {
@@ -99,6 +109,7 @@ object Env {
     poolApi = lidraughts.pool.Env.current.api,
     asyncCache = lidraughts.memo.Env.current.asyncCache,
     settingStore = lidraughts.memo.Env.current.settingStore,
+    remoteSocketApi = lila.socket.Env.current.remoteSocket,
     system = lidraughts.common.PlayApp.system
   )
 }

modules/lobby/src/main/LobbyRemoteSocket.scala
@@ -3,16 +3,35 @@ package lidraughts.lobby
 import play.api.libs.json._
 
 import lidraughts.socket.RemoteSocket._, Protocol._
+import lidraughts.user.{ User, UserRepo }
 
 final class LobbyRemoteSocket(
-    remoteSocketApi: lidraughts.socket.RemoteSocket
+    remoteSocketApi: lidraughts.socket.RemoteSocket,
+    socket: LobbySocket,
+    blocking: User.ID => Fu[Set[User.ID]],
+    bus: lidraughts.common.Bus
 ) {
 
   private val send: (Path, Args*) => Unit = remoteSocketApi.sendTo("lobby-out") _
 
   private val handler: Handler = {
-    case m @ In.ConnectSri(sri, userId) => remoteSocketApi baseHandler m
+    case In.ConnectSri(sri, userOpt) =>
+      userOpt map In.ConnectUser.apply foreach remoteSocketApi.baseHandler.lift
+      userOpt ?? UserRepo.enabledById foreach { user =>
+        (user ?? (u => blocking(u.id))) foreach { blocks =>
+          val member = actorApi.LobbySocketMember(bus, user, blocks, sri)
+          socket ! actorApi.JoinRemote(member)
+        }
+      }
+    case In.DisconnectSri(sri, userOpt) =>
+      userOpt map In.DisconnectUser.apply foreach remoteSocketApi.baseHandler.lift
   }
 
-  remoteSocketApi.subscribe("lobby-in")(handler orElse remoteSocketApi.baseHandler)
+  remoteSocketApi.subscribe("lobby-in", In.baseReader)(handler orElse remoteSocketApi.baseHandler)
+}
+
+object LobbyRemoteSocket {
+
+  object Protocol {
+  }
 }

modules/lobby/src/main/LobbySocket.scala
@@ -37,15 +37,14 @@ private[lobby] final class LobbySocket(
       promise success Socket.Sris(members.keySet.map(Socket.Sri.apply)(scala.collection.breakOut))
       lidraughts.mon.lobby.socket.idle(idleSris.size)
       lidraughts.mon.lobby.socket.hookSubscribers(hookSubscriberSris.size)
-      lidraughts.mon.lobby.socket.mobile(members.count(_._2.mobile))
 
     case Cleanup =>
       idleSris retain members.contains
       hookSubscriberSris retain members.contains
 
-    case Join(sri, user, blocks, mobile, promise) =>
+    case Join(sri, user, blocks, promise) =>
       val (enumerator, channel) = Concurrent.broadcast[JsValue]
-      val member = LobbySocketMember(channel, user, blocks, sri, mobile)
+      val member = LobbySocketMember(channel, user, blocks, sri)
       addMember(sri, member)
       promise success Connected(enumerator, member)
 
@@ -69,7 +68,7 @@ private[lobby] final class LobbySocket(
         }
       }
       if (hook.likePoolFiveO) withMember(hook.sri) { member =>
-        lidraughts.mon.lobby.hook.createdLikePoolFiveO(member.mobile)()
+        lidraughts.mon.lobby.hook.createdLikePoolFiveO()
       }
 
     case AddSeek(_) => notifySeeks
@@ -91,23 +90,23 @@ private[lobby] final class LobbySocket(
 
     case JoinHook(sri, hook, game, creatorColor) =>
       withMember(hook.sri) { member =>
-        lidraughts.mon.lobby.hook.joinMobile(member.mobile)()
+        lidraughts.mon.lobby.hook.join()
         notifyPlayerStart(game, creatorColor)(member)
       }
       withMember(sri) { member =>
-        lidraughts.mon.lobby.hook.joinMobile(member.mobile)()
+        lidraughts.mon.lobby.hook.join()
         if (hook.likePoolFiveO)
-          lidraughts.mon.lobby.hook.acceptedLikePoolFiveO(member.mobile)()
+          lidraughts.mon.lobby.hook.acceptedLikePoolFiveO()
         notifyPlayerStart(game, !creatorColor)(member)
       }
 
     case JoinSeek(userId, seek, game, creatorColor) =>
       membersByUserId(seek.user.id) foreach { member =>
-        lidraughts.mon.lobby.seek.joinMobile(member.mobile)()
+        lidraughts.mon.lobby.seek.join()
         notifyPlayerStart(game, creatorColor)(member)
       }
       membersByUserId(userId) foreach { member =>
-        lidraughts.mon.lobby.seek.joinMobile(member.mobile)()
+        lidraughts.mon.lobby.seek.join()
         notifyPlayerStart(game, !creatorColor)(member)
       }
 

modules/lobby/src/main/actorApi.scala
@@ -4,21 +4,19 @@ package actorApi
 import scala.concurrent.Promise
 
 import lidraughts.game.Game
-import lidraughts.socket.{ SocketMember, DirectSocketMember, RemoteSocketMember }
 import lidraughts.socket.Socket.{ Sri, Sris }
+import lidraughts.socket.{ SocketMember, DirectSocketMember, RemoteSocketMember }
 import lidraughts.user.User
 
 private[lobby] sealed trait LobbySocketMember extends SocketMember {
   val user: Option[LobbyUser]
   val sri: Sri
-  val mobile: Boolean
 }
 
 private[lobby] case class LobbyDirectSocketMember(
     channel: JsChannel,
     user: Option[LobbyUser],
-    sri: Sri,
-    mobile: Boolean
+    sri: Sri
 ) extends LobbySocketMember with DirectSocketMember {
   val userId = user.map(_.id)
 }
@@ -26,28 +24,25 @@ private[lobby] case class LobbyDirectSocketMember(
 private[lobby] case class LobbyRemoteSocketMember(
     bus: lidraughts.common.Bus,
     user: Option[LobbyUser],
-    sri: Sri,
-    mobile: Boolean
+    sri: Sri
 ) extends LobbySocketMember with RemoteSocketMember {
   val userId = user.map(_.id)
 }
 
 private[lobby] object LobbySocketMember {
 
-  def apply(channel: JsChannel, user: Option[User], blocking: Set[String], sri: Sri, mobile: Boolean): LobbyDirectSocketMember =
+  def apply(channel: JsChannel, user: Option[User], blocking: Set[String], sri: Sri): LobbyDirectSocketMember =
     LobbyDirectSocketMember(
       channel = channel,
       user = user map { LobbyUser.make(_, blocking) },
-      sri = sri,
-      mobile = mobile
+      sri = sri
     )
 
-  def apply(bus: lidraughts.common.Bus, user: Option[User], blocking: Set[String], sri: Sri, mobile: Boolean): LobbyRemoteSocketMember =
+  def apply(bus: lidraughts.common.Bus, user: Option[User], blocking: Set[String], sri: Sri): LobbyRemoteSocketMember =
     LobbyRemoteSocketMember(
       bus = bus,
       user = user map { LobbyUser.make(_, blocking) },
-      sri = sri,
-      mobile = mobile
+      sri = sri
     )
 }
 
@@ -68,7 +63,7 @@ private[lobby] case class BiteHook(hookId: String, sri: Sri, user: Option[LobbyU
 private[lobby] case class BiteSeek(seekId: String, user: LobbyUser)
 private[lobby] case class JoinHook(sri: Sri, hook: Hook, game: Game, creatorColor: draughts.Color)
 private[lobby] case class JoinSeek(userId: String, seek: Seek, game: Game, creatorColor: draughts.Color)
-private[lobby] case class Join(sri: Sri, user: Option[User], blocking: Set[String], mobile: Boolean, promise: Promise[Connected])
+private[lobby] case class Join(sri: Sri, user: Option[User], blocking: Set[String], promise: Promise[Connected])
 private[lobby] case class JoinRemote(member: LobbyRemoteSocketMember)
 private[lobby] case object Resync
 private[lobby] case class HookIds(ids: Vector[String])