lichess-org/lila

simul remote socket WIP

modules/challenge/src/main/ChallengeSocket.scala
@@ -9,7 +9,7 @@ import scala.concurrent.Promise
 import lila.socket.actorApi.{ Connected => _, _ }
 import lila.socket.SocketTrouper
 import lila.socket.Socket.{ Uid, GetVersion, SocketVersion }
-import lila.socket.{ History, Historical }
+import lila.socket.{ History, Historical, DirectSocketMember }
 
 private final class ChallengeSocket(
     system: ActorSystem,
@@ -55,7 +55,7 @@ private object ChallengeSocket {
       channel: JsChannel,
       userId: Option[String],
       owner: Boolean
-  ) extends lila.socket.SocketMember {
+  ) extends DirectSocketMember {
     val troll = false
   }
 

modules/simul/src/main/actorApi.scala
@@ -6,14 +6,14 @@ import play.api.libs.json.JsObject
 
 import lila.game.Game
 import lila.socket.Socket.{ Uid, SocketVersion }
-import lila.socket.SocketMember
+import lila.socket.RemoteSocketMember
 import lila.user.User
 
-private[simul] case class SimulMember(
+private[simul] case class SimulSocketMember(
     push: JsObject => Unit,
     userId: Option[String],
     troll: Boolean
-)
+) extends RemoteSocketMember
 
 private[simul] case class Messadata(trollish: Boolean = false)
 
@@ -29,7 +29,7 @@ private[simul] case class StartSimul(firstGame: Game, hostId: String)
 private[simul] case class HostIsOn(gameId: String)
 private[simul] case object Reload
 private[simul] case object Aborted
-private[simul] case class Connected(member: SimulMember)
+private[simul] case class Connected(member: SimulSocketMember)
 
 private[simul] case object NotifyCrowd
 

modules/study/src/main/StudySocket.scala
@@ -11,7 +11,7 @@ import chess.format.pgn.Glyphs
 import lila.hub.Trouper
 import lila.socket.actorApi.{ Connected => _, _ }
 import lila.socket.Socket.{ Uid, GetVersion, SocketVersion }
-import lila.socket.{ SocketTrouper, History, Historical, AnaDests }
+import lila.socket.{ SocketTrouper, History, Historical, AnaDests, DirectSocketMember }
 import lila.tree.Node.{ Shapes, Comment }
 import lila.user.User
 import lila.chat.Chat
@@ -278,7 +278,7 @@ object StudySocket {
       channel: JsChannel,
       userId: Option[String],
       troll: Boolean
-  ) extends lila.socket.SocketMember
+  ) extends DirectSocketMember
 
   case class Who(u: String, s: Uid)
   import JsonView.uidWriter

modules/tournament/src/main/actorApi.scala
@@ -4,8 +4,8 @@ package actorApi
 import scala.concurrent.Promise
 
 import lila.game.Game
-import lila.socket.Socket.{ Uid, DirectSocketVersion }
-import lila.socket.SocketMember
+import lila.socket.Socket.{ Uid, SocketVersion }
+import lila.socket.DirectSocketMember
 import lila.user.User
 
 private[tournament] case class Member(

notzmv/lidraughts

modules/challenge/src/main/ChallengeSocket.scala
@@ -10,7 +10,7 @@ import lidraughts.hub.TimeBomb
 import lidraughts.socket.actorApi.{ Connected => _, _ }
 import lidraughts.socket.SocketTrouper
 import lidraughts.socket.Socket.{ Uid, GetVersion, SocketVersion }
-import lidraughts.socket.{ History, Historical }
+import lidraughts.socket.{ History, Historical, DirectSocketMember }
 
 private final class ChallengeSocket(
     system: ActorSystem,
@@ -56,7 +56,7 @@ private object ChallengeSocket {
       channel: JsChannel,
       userId: Option[String],
       owner: Boolean
-  ) extends lidraughts.socket.SocketMember {
+  ) extends DirectSocketMember {
     val troll = false
   }
 

modules/simul/src/main/actorApi.scala
@@ -7,14 +7,14 @@ import play.api.libs.json.JsObject
 
 import lidraughts.game.Game
 import lidraughts.socket.Socket.{ Uid, SocketVersion }
-import lidraughts.socket.SocketMember
+import lidraughts.socket.RemoteSocketMember
 import lidraughts.user.User
 
-private[simul] case class SimulMember(
+private[simul] case class SimulSocketMember(
     push: JsObject => Unit,
     userId: Option[String],
     troll: Boolean
-)
+) extends RemoteSocketMember
 
 private[simul] case class Messadata(trollish: Boolean = false)
 
@@ -31,7 +31,7 @@ private[simul] case class HostIsOn(gameId: String)
 private[simul] case class ReloadEval(gameId: String, json: JsObject)
 private[simul] case object Reload
 private[simul] case object Aborted
-private[simul] case class Connected(member: SimulMember)
+private[simul] case class Connected(member: SimulSocketMember)
 
 private[simul] case object NotifyCrowd
 

modules/study/src/main/StudySocket.scala
@@ -12,7 +12,7 @@ import lidraughts.chat.Chat
 import lidraughts.hub.Trouper
 import lidraughts.socket.actorApi.{ Connected => _, _ }
 import lidraughts.socket.Socket.{ Uid, GetVersion, SocketVersion }
-import lidraughts.socket.{ SocketTrouper, History, Historical, AnaDests }
+import lidraughts.socket.{ SocketTrouper, History, Historical, AnaDests, DirectSocketMember }
 import lidraughts.tree.Node.{ Shapes, Comment }
 import lidraughts.user.User
 
@@ -293,7 +293,7 @@ object StudySocket {
       channel: JsChannel,
       userId: Option[String],
       troll: Boolean
-  ) extends lidraughts.socket.SocketMember
+  ) extends DirectSocketMember
 
   case class Who(u: String, s: Uid)
   import JsonView.uidWriter

modules/tournament/src/main/actorApi.scala
@@ -4,8 +4,8 @@ package actorApi
 import scala.concurrent.Promise
 
 import lidraughts.game.Game
-import lidraughts.socket.Socket.{ Uid, DirectSocketVersion  }
-import lidraughts.socket.SocketMember
+import lidraughts.socket.Socket.{ Uid, SocketVersion  }
+import lidraughts.socket.DirectSocketMember
 import lidraughts.user.User
 
 private[tournament] case class Member(