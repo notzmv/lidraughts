lichess-org/lila

backpedal on simul remote sockets

modules/simul/src/main/Env.scala
@@ -5,13 +5,12 @@ import com.typesafe.config.Config
 import scala.concurrent.duration._
 
 import lila.hub.{ Duct, DuctMap, TrouperMap }
-import lila.socket.{ History, RemoteSocket }
+import lila.socket.History
 import lila.socket.Socket.{ GetVersion, SocketVersion }
 
 final class Env(
     config: Config,
     system: ActorSystem,
-    remoteSocket: RemoteSocket,
     scheduler: lila.common.Scheduler,
     db: lila.db.Env,
     hub: lila.hub.Env,
@@ -63,8 +62,7 @@ final class Env(
     broomFrequency = 3691 millis
   )
 
-  lazy val socketHandler = new SimulSocketHandler(
-    remoteSocket = remoteSocket,
+  lazy val socketHandler = new SocketHandler(
     hub = hub,
     socketMap = socketMap,
     chat = hub.chat,
@@ -140,7 +138,6 @@ object Env {
   lazy val current = "simul" boot new Env(
     config = lila.common.PlayApp loadConfig "simul",
     system = lila.common.PlayApp.system,
-    remoteSocket = lila.socket.Env.current.remoteSocket,
     scheduler = lila.common.PlayApp.scheduler,
     db = lila.db.Env.current,
     hub = lila.hub.Env.current,

modules/simul/src/main/SocketHandler.scala
@@ -1,0 +1,50 @@
+package lila.simul
+
+import akka.actor._
+import akka.pattern.ask
+
+import actorApi._
+import lila.chat.Chat
+import lila.common.ApiVersion
+import lila.hub.actorApi.map._
+import lila.hub.Trouper
+import lila.socket.actorApi.{ Connected => _, _ }
+import lila.socket.Handler
+import lila.socket.Socket.{ Sri, SocketVersion }
+import lila.user.User
+
+private[simul] final class SocketHandler(
+    hub: lila.hub.Env,
+    socketMap: SocketMap,
+    chat: ActorSelection,
+    exists: Simul.ID => Fu[Boolean]
+) {
+
+  def join(
+    simulId: String,
+    sri: Sri,
+    user: Option[User],
+    version: Option[SocketVersion],
+    apiVersion: ApiVersion
+  ): Fu[Option[JsSocketHandler]] =
+    exists(simulId) flatMap {
+      _ ?? {
+        val socket = socketMap getOrMake simulId
+        socket.ask[Connected](Join(sri, user, version, _)) map {
+          case Connected(enum, member) => Handler.iteratee(
+            hub,
+            lila.chat.Socket.in(
+              chatId = Chat.Id(simulId),
+              member = member,
+              chat = chat,
+              publicSource = lila.hub.actorApi.shutup.PublicSource.Simul(simulId).some
+            ),
+            member,
+            socket,
+            sri,
+            apiVersion
+          ) -> enum
+        } map some
+      }
+    }
+}

modules/simul/src/main/actorApi.scala
@@ -1,20 +1,26 @@
 package lila.simul
 package actorApi
 
-import play.api.libs.json.JsObject
 import scala.concurrent.Promise
 
 import lila.game.Game
-import lila.socket.RemoteSocketMember
 import lila.socket.Socket.{ Sri, SocketVersion }
+import lila.socket.DirectSocketMember
 import lila.user.User
 
 private[simul] case class SimulSocketMember(
-    sri: Sri,
+    channel: JsChannel,
     userId: Option[String],
-    troll: Boolean,
-    bus: lila.common.Bus
-) extends RemoteSocketMember
+    troll: Boolean
+) extends DirectSocketMember
+
+private[simul] object SimulSocketMember {
+  def apply(channel: JsChannel, user: Option[User]): SimulSocketMember = SimulSocketMember(
+    channel = channel,
+    userId = user map (_.id),
+    troll = user.??(_.troll)
+  )
+}
 
 private[simul] case class Messadata(trollish: Boolean = false)
 
@@ -30,7 +36,7 @@ private[simul] case class StartSimul(firstGame: Game, hostId: String)
 private[simul] case class HostIsOn(gameId: String)
 private[simul] case object Reload
 private[simul] case object Aborted
-private[simul] case class Connected(member: SimulSocketMember)
+private[simul] case class Connected(enumerator: JsEnumerator, member: SimulSocketMember)
 
 private[simul] case object NotifyCrowd
 

notzmv/lidraughts

modules/simul/src/main/Env.scala
@@ -5,13 +5,12 @@ import com.typesafe.config.Config
 import scala.concurrent.duration._
 
 import lidraughts.hub.{ Duct, DuctMap, TrouperMap }
-import lidraughts.socket.{ History, RemoteSocket }
+import lidraughts.socket.History
 import lidraughts.socket.Socket.{ GetVersion, SocketVersion }
 
 final class Env(
     config: Config,
     system: ActorSystem,
-    remoteSocket: RemoteSocket,
     scheduler: lidraughts.common.Scheduler,
     evalCacheApi: lidraughts.evalCache.EvalCacheApi,
     db: lidraughts.db.Env,
@@ -72,8 +71,7 @@ final class Env(
     broomFrequency = 3691 millis
   )
 
-  lazy val socketHandler = new SimulSocketHandler(
-    remoteSocket = remoteSocket,
+  lazy val socketHandler = new SocketHandler(
     hub = hub,
     socketMap = socketMap,
     chat = hub.chat,
@@ -167,7 +165,6 @@ object Env {
   lazy val current = "simul" boot new Env(
     config = lidraughts.common.PlayApp loadConfig "simul",
     system = lidraughts.common.PlayApp.system,
-    remoteSocket = lidraughts.socket.Env.current.remoteSocket,
     scheduler = lidraughts.common.PlayApp.scheduler,
     evalCacheApi = lidraughts.evalCache.Env.current.api,
     db = lidraughts.db.Env.current,

modules/simul/src/main/SocketHandler.scala
@@ -1,0 +1,50 @@
+package lidraughts.simul
+
+import akka.actor._
+import akka.pattern.ask
+
+import actorApi._
+import lidraughts.chat.Chat
+import lidraughts.common.ApiVersion
+import lidraughts.hub.actorApi.map._
+import lidraughts.hub.Trouper
+import lidraughts.socket.actorApi.{ Connected => _, _ }
+import lidraughts.socket.Handler
+import lidraughts.socket.Socket.{ Sri, SocketVersion }
+import lidraughts.user.User
+
+private[simul] final class SocketHandler(
+    hub: lidraughts.hub.Env,
+    socketMap: SocketMap,
+    chat: ActorSelection,
+    exists: Simul.ID => Fu[Boolean]
+) {
+
+  def join(
+    simulId: String,
+    sri: Sri,
+    user: Option[User],
+    version: Option[SocketVersion],
+    apiVersion: ApiVersion
+  ): Fu[Option[JsSocketHandler]] =
+    exists(simulId) flatMap {
+      _ ?? {
+        val socket = socketMap getOrMake simulId
+        socket.ask[Connected](Join(sri, user, version, _)) map {
+          case Connected(enum, member) => Handler.iteratee(
+            hub,
+            lidraughts.chat.Socket.in(
+              chatId = Chat.Id(simulId),
+              member = member,
+              chat = chat,
+              publicSource = lidraughts.hub.actorApi.shutup.PublicSource.Simul(simulId).some
+            ),
+            member,
+            socket,
+            sri,
+            apiVersion
+          ) -> enum
+        } map some
+      }
+    }
+}

modules/simul/src/main/actorApi.scala
@@ -5,16 +5,23 @@ import play.api.libs.json.JsObject
 import scala.concurrent.Promise
 
 import lidraughts.game.Game
-import lidraughts.socket.RemoteSocketMember
+import lidraughts.socket.DirectSocketMember
 import lidraughts.socket.Socket.{ Sri, SocketVersion }
 import lidraughts.user.User
 
 private[simul] case class SimulSocketMember(
-    sri: Sri,
+    channel: JsChannel,
     userId: Option[String],
-    troll: Boolean,
-    bus: lidraughts.common.Bus
-) extends RemoteSocketMember
+    troll: Boolean
+) extends DirectSocketMember
+
+private[simul] object SimulSocketMember {
+  def apply(channel: JsChannel, user: Option[User]): SimulSocketMember = SimulSocketMember(
+    channel = channel,
+    userId = user map (_.id),
+    troll = user.??(_.troll)
+  )
+}
 
 private[simul] case class Messadata(trollish: Boolean = false)
 
@@ -31,7 +38,7 @@ private[simul] case class HostIsOn(gameId: String)
 private[simul] case class ReloadEval(gameId: String, json: JsObject)
 private[simul] case object Reload
 private[simul] case object Aborted
-private[simul] case class Connected(member: SimulSocketMember)
+private[simul] case class Connected(enumerator: JsEnumerator, member: SimulSocketMember)
 
 private[simul] case object NotifyCrowd
 