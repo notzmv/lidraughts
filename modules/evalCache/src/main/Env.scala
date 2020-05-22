lichess-org/lila

consistent naming for sri

modules/evalCache/src/main/Env.scala
@@ -5,7 +5,7 @@ import play.api.libs.json.JsValue
 import scala.concurrent.duration._
 
 import lila.hub.actorApi.socket.{ RemoteSocketTellSriIn, RemoteSocketTellSriOut }
-import lila.socket.Socket.Uid
+import lila.socket.Socket.Sri
 
 final class Env(
     config: Config,
@@ -35,17 +35,17 @@ final class Env(
   )
 
   system.lilaBus.subscribeFun('socketLeave) {
-    case lila.socket.actorApi.SocketLeave(uid, _) => upgrade unregister uid
+    case lila.socket.actorApi.SocketLeave(sri, _) => upgrade unregister sri
   }
 
   // remote socket support
   system.lilaBus.subscribeFun(Symbol("remoteSocketIn:evalGet")) {
     case RemoteSocketTellSriIn(sri, _, d) =>
-      socketHandler.evalGet(Uid(sri), d, res => system.lilaBus.publish(RemoteSocketTellSriOut(sri, res), 'remoteSocketOut))
+      socketHandler.evalGet(Sri(sri), d, res => system.lilaBus.publish(RemoteSocketTellSriOut(sri, res), 'remoteSocketOut))
   }
   system.lilaBus.subscribeFun(Symbol("remoteSocketIn:evalPut")) {
     case RemoteSocketTellSriIn(sri, Some(userId), d) =>
-      socketHandler.untrustedEvalPut(Uid(sri), userId, d)
+      socketHandler.untrustedEvalPut(Sri(sri), userId, d)
   }
   // END remote socket support
 

modules/relay/src/main/RelaySync.scala
@@ -3,7 +3,7 @@ package lila.relay
 import org.joda.time.DateTime
 
 import chess.format.pgn.{ Tag, Tags }
-import lila.socket.Socket.Uid
+import lila.socket.Socket.Sri
 import lila.study._
 
 private final class RelaySync(
@@ -23,7 +23,7 @@ private final class RelaySync(
               case Some(chapter) => updateChapter(study, chapter, game)
               case None => createChapter(study, game) flatMap { chapter =>
                 chapters.find(_.isEmptyInitial).ifTrue(chapter.order == 2).?? { initial =>
-                  studyApi.deleteChapter(study.ownerId, study.id, initial.id, socketUid)
+                  studyApi.deleteChapter(study.ownerId, study.id, initial.id, socketSri)
                 } inject chapter.root.mainline.size
               }
             }
@@ -58,7 +58,7 @@ private final class RelaySync(
                 studyId = study.id,
                 position = Position(chapter, path).ref,
                 clock = c.some,
-                uid = socketUid
+                sri = socketSri
               )
             }
             path -> none
@@ -73,7 +73,7 @@ private final class RelaySync(
             studyId = study.id,
             position = Position(chapter, path).ref,
             toMainline = true,
-            uid = socketUid
+            sri = socketSri
           ) >> chapterRepo.setRelayPath(chapter.id, path)
         } >> newNode.?? { node =>
           lila.common.Future.fold(node.mainline)(Position(chapter, path).ref) {
@@ -82,7 +82,7 @@ private final class RelaySync(
               studyId = study.id,
               position = position,
               node = n,
-              uid = socketUid,
+              sri = socketSri,
               opts = moveOpts.copy(clock = n.clock),
               relay = Chapter.Relay(
                 index = game.index,
@@ -117,7 +117,7 @@ private final class RelaySync(
         studyId = study.id,
         chapterId = chapter.id,
         tags = chapterNewTags,
-        uid = socketUid
+        sri = socketSri
       ) >> {
         chapterNewTags.resultColor.isDefined ?? onChapterEnd(study.id, chapter.id)
       }
@@ -161,7 +161,7 @@ private final class RelaySync(
           lastMoveAt = DateTime.now
         ).some
       )
-      studyApi.doAddChapter(study, chapter, sticky = false, uid = socketUid) inject chapter
+      studyApi.doAddChapter(study, chapter, sticky = false, sri = socketSri) inject chapter
     }
 
   private val moveOpts = MoveOpts(
@@ -171,7 +171,7 @@ private final class RelaySync(
     clock = none
   )
 
-  private val socketUid = Uid("")
+  private val socketSri = Sri("")
 
   private def vs(tags: Tags) = s"${tags(_.White) | "?"} - ${tags(_.Black) | "?"}"
 

ui/lobby/src/boot.js
@@ -266,7 +266,7 @@ module.exports = function(cfg, element) {
         var poolMember = hookToPoolMember(color, $form.serializeArray());
         $.modal.close();
         var call = {
-          url: $form.attr('action').replace(/uid-placeholder/, lichess.StrongSocket.sri),
+          url: $form.attr('action').replace(/sri-placeholder/, lichess.StrongSocket.sri),
           data: $form.serialize() + "&color=" + color,
           type: 'post'
         };

notzmv/lidraughts

modules/evalCache/src/main/Env.scala
@@ -5,7 +5,7 @@ import play.api.libs.json.JsValue
 import scala.concurrent.duration._
 
 import lidraughts.hub.actorApi.socket.{ RemoteSocketTellSriIn, RemoteSocketTellSriOut }
-import lidraughts.socket.Socket.Uid
+import lidraughts.socket.Socket.Sri
 
 final class Env(
     config: Config,
@@ -35,17 +35,17 @@ final class Env(
   )
 
   system.lidraughtsBus.subscribeFun('socketLeave) {
-    case lidraughts.socket.actorApi.SocketLeave(uid, _) => upgrade unregister uid
+    case lidraughts.socket.actorApi.SocketLeave(sri, _) => upgrade unregister sri
   }
 
   // remote socket support
   system.lidraughtsBus.subscribeFun(Symbol("remoteSocketIn:evalGet")) {
     case RemoteSocketTellSriIn(sri, _, d) =>
-      socketHandler.evalGet(Uid(sri), d, res => system.lidraughtsBus.publish(RemoteSocketTellSriOut(sri, res), 'remoteSocketOut))
+      socketHandler.evalGet(Sri(sri), d, res => system.lidraughtsBus.publish(RemoteSocketTellSriOut(sri, res), 'remoteSocketOut))
   }
   system.lidraughtsBus.subscribeFun(Symbol("remoteSocketIn:evalPut")) {
     case RemoteSocketTellSriIn(sri, Some(userId), d) =>
-      socketHandler.untrustedEvalPut(Uid(sri), userId, d)
+      socketHandler.untrustedEvalPut(Sri(sri), userId, d)
   }
   // END remote socket support
 

modules/relay/src/main/RelaySync.scala
@@ -3,7 +3,7 @@ package lidraughts.relay
 import org.joda.time.DateTime
 
 import draughts.format.pdn.{ Tag, Tags }
-import lidraughts.socket.Socket.Uid
+import lidraughts.socket.Socket.Sri
 import lidraughts.study._
 
 private final class RelaySync(
@@ -23,7 +23,7 @@ private final class RelaySync(
               case Some(chapter) => updateChapter(study, chapter, game)
               case None => createChapter(study, game) flatMap { chapter =>
                 chapters.find(_.isEmptyInitial).ifTrue(chapter.order == 2).?? { initial =>
-                  studyApi.deleteChapter(study.ownerId, study.id, initial.id, socketUid)
+                  studyApi.deleteChapter(study.ownerId, study.id, initial.id, socketSri)
                 } inject chapter.root.mainline.size
               }
             }
@@ -75,7 +75,7 @@ private final class RelaySync(
                   studyId = study.id,
                   position = Position(chapter, path).ref,
                   clock = c.some,
-                  uid = socketUid,
+                  sri = socketSri,
                   relay = newMove ?? relay.some
                 )
               }
@@ -91,7 +91,7 @@ private final class RelaySync(
             studyId = study.id,
             position = Position(chapter, path).ref,
             toMainline = true,
-            uid = socketUid
+            sri = socketSri
           ) >> chapterRepo.setRelayPath(chapter.id, path)
         } >> newNode.?? { node =>
           lidraughts.common.Future.fold(node.mainline)(Position(chapter, path).ref) {
@@ -100,7 +100,7 @@ private final class RelaySync(
               studyId = study.id,
               position = position,
               node = n,
-              uid = socketUid,
+              sri = socketSri,
               opts = moveOpts.copy(clock = n.clock),
               relay = Chapter.Relay(
                 index = game.index,
@@ -136,7 +136,7 @@ private final class RelaySync(
         studyId = study.id,
         chapterId = chapter.id,
         tags = chapterNewTags,
-        uid = socketUid
+        sri = socketSri
       ) >> {
         chapterNewTags.resultColor.isDefined ?? onChapterEnd(study.id, chapter.id)
       }
@@ -181,7 +181,7 @@ private final class RelaySync(
           runningClock = game.root.children.nodeAt(game.root.mainlinePath).flatMap { _.runningClock }
         ).some
       )
-      studyApi.doAddChapter(study, chapter, sticky = false, uid = socketUid) inject chapter
+      studyApi.doAddChapter(study, chapter, sticky = false, sri = socketSri) inject chapter
     }
 
   private val moveOpts = MoveOpts(
@@ -191,7 +191,7 @@ private final class RelaySync(
     clock = none
   )
 
-  private val socketUid = Uid("")
+  private val socketSri = Sri("")
 
   private def vs(tags: Tags) = s"${tags(_.White) | "?"} - ${tags(_.Black) | "?"}"
 

ui/lobby/src/boot.js
@@ -265,7 +265,7 @@ module.exports = function(cfg, element) {
         var poolMember = false; // hookToPoolMember(color, $form.serializeArray());
         $.modal.close();
         var call = {
-          url: $form.attr('action').replace(/uid-placeholder/, lidraughts.StrongSocket.sri),
+          url: $form.attr('action').replace(/sri-placeholder/, lidraughts.StrongSocket.sri),
           data: $form.serialize() + "&color=" + color,
           type: 'post'
         };