lichess-org/lila

better show server latency

modules/round/src/main/MoveMonitor.scala
@@ -1,25 +1,28 @@
 package lila.round
 
-import scala.concurrent.duration._
 import akka.actor._
 import kamon._
 import metric.SubscriptionsDispatcher.TickMetricSnapshot
+import scala.concurrent.duration._
 
 private object MoveMonitor {
 
   def start(system: ActorSystem, channel: lila.socket.Channel) =
 
     Kamon.metrics.subscribe("trace", "round.move.trace", system.actorOf(Props(new Actor {
-      var current: Int = 0
+      var currentMicros: Int = 0
       context.system.scheduler.schedule(5 second, 2 second) {
-        channel ! lila.socket.Channel.Publish(lila.socket.Socket.makeMessage("mlat", current))
-        system.lilaBus.publish(lila.hub.actorApi.round.Mlat(current), 'mlat)
+        channel ! lila.socket.Channel.Publish(lila.socket.Socket.makeMessage(
+          "mlat",
+          (currentMicros / 100) / 10d
+        ))
+        system.lilaBus.publish(lila.hub.actorApi.round.Mlat(currentMicros), 'mlat)
       }
       def receive = {
         case tick: TickMetricSnapshot => tick.metrics.collectFirst {
           case (entity, snapshot) if entity.category == "trace" => snapshot
         } flatMap (_ histogram "elapsed-time") foreach { h =>
-          if (!h.isEmpty) current = Math.round(h.sum / h.numberOfMeasurements / 1000000).toInt atLeast 1
+          if (!h.isEmpty) currentMicros = Math.round(h.sum / h.numberOfMeasurements / 1000).toInt
         }
       }
     })))

notzmv/lidraughts

modules/round/src/main/MoveMonitor.scala
@@ -1,25 +1,28 @@
 package lidraughts.round
 
-import scala.concurrent.duration._
 import akka.actor._
 import kamon._
 import metric.SubscriptionsDispatcher.TickMetricSnapshot
+import scala.concurrent.duration._
 
 private object MoveMonitor {
 
   def start(system: ActorSystem, channel: lidraughts.socket.Channel) =
 
     Kamon.metrics.subscribe("trace", "round.move.trace", system.actorOf(Props(new Actor {
-      var current: Int = 0
+      var currentMicros: Int = 0
       context.system.scheduler.schedule(5 second, 2 second) {
-        channel ! lidraughts.socket.Channel.Publish(lidraughts.socket.Socket.makeMessage("mlat", current))
-        system.lidraughtsBus.publish(lidraughts.hub.actorApi.round.Mlat(current), 'mlat)
+        channel ! lidraughts.socket.Channel.Publish(lidraughts.socket.Socket.makeMessage(
+          "mlat",
+          (currentMicros / 100) / 10d
+        ))
+        system.lidraughtsBus.publish(lidraughts.hub.actorApi.round.Mlat(currentMicros), 'mlat)
       }
       def receive = {
         case tick: TickMetricSnapshot => tick.metrics.collectFirst {
           case (entity, snapshot) if entity.category == "trace" => snapshot
         } flatMap (_ histogram "elapsed-time") foreach { h =>
-          if (!h.isEmpty) current = Math.round(h.sum / h.numberOfMeasurements / 1000000).toInt atLeast 1
+          if (!h.isEmpty) currentMicros = Math.round(h.sum / h.numberOfMeasurements / 1000).toInt
         }
       }
     })))