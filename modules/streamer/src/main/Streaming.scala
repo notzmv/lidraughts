lichess-org/lila

twitch API credentials live settings and better validation

modules/streamer/src/main/Streaming.scala
@@ -20,7 +20,7 @@ final private class Streaming(
     keyword: Stream.Keyword,
     alwaysFeatured: () => lila.common.Strings,
     googleApiKey: Secret,
-    twitchClientId: Secret,
+    twitchCredentials: () => (String, String),
     lightUserApi: lila.user.LightUserApi
 ) extends Actor {
 
@@ -110,30 +110,33 @@ final private class Streaming(
       else fuccess(allTwitchStreamers.map(_._2))
     futureTwitchStreamers flatMap { twitchStreamers =>
       twitchStreamers.nonEmpty ?? {
-        val twitchUserIds = twitchStreamers.map(_.userId)
-        val url = ws
-          .url("https://api.twitch.tv/helix/streams")
+        val twitchUserIds      = twitchStreamers.map(_.userId)
+        val (clientId, secret) = twitchCredentials()
+        ws.url("https://api.twitch.tv/helix/streams")
           .withQueryStringParameters(
             (("first" -> maxIds.toString) :: twitchUserIds.map("user_login" -> _)): _*
           )
           .withHttpHeaders(
-            "Client-ID" -> twitchClientId.value
+            "Client-ID"     -> clientId,
+            "Authorization" -> s"Bearer $secret"
           )
-        url
           .get()
-          .flatMap { res =>
-            res.json.validate[Twitch.Result](twitchResultReads) match {
-              case JsSuccess(data, _) =>
-                fuccess(
-                  data.streams(
-                    keyword,
-                    streamers,
-                    alwaysFeatured().value.map(_.toLowerCase)
+          .flatMap {
+            case res if res.status == 200 =>
+              res.json.validate[Twitch.Result](twitchResultReads) match {
+                case JsSuccess(data, _) =>
+                  fuccess(
+                    data.streams(
+                      keyword,
+                      streamers,
+                      alwaysFeatured().value.map(_.toLowerCase)
+                    )
                   )
-                )
-              case JsError(err) =>
-                fufail(s"twitch ${res.status} $err ${~res.body.linesIterator.toList.headOption}")
-            }
+                case JsError(err) =>
+                  fufail(s"twitch ${res.status} $err ${~res.body.linesIterator.toList.headOption}")
+              }
+            case res =>
+              fufail(s"twitch ${res.status} ${~res.body.linesIterator.toList.headOption}")
           }
           .monSuccess(_.tv.streamer.twitch)
           .recover {

notzmv/lidraughts

modules/streamer/src/main/Streaming.scala
@@ -18,7 +18,7 @@ private final class Streaming(
     keyword: Stream.Keyword,
     alwaysFeatured: () => lidraughts.common.Strings,
     googleApiKey: String,
-    twitchClientId: String,
+    twitchCredentials: () => (String, String),
     lightUserApi: lidraughts.user.LightUserApi
 ) extends Actor {
 
@@ -109,27 +109,32 @@ private final class Streaming(
     futureTwitchStreamers flatMap { twitchStreamers =>
       twitchStreamers.nonEmpty ?? {
         val twitchUserIds = twitchStreamers.map(_.userId)
+        val (clientId, secret) = twitchCredentials()
         val url = WS.url("https://api.twitch.tv/helix/streams")
           .withQueryString(
             (("first" -> maxIds.toString) :: twitchUserIds.map("user_login" -> _)): _*
           )
           .withHeaders(
-            "Client-ID" -> twitchClientId
+            "Client-ID" -> clientId,
+            "Authorization" -> s"Bearer $secret"
           )
         if (twitchUserIds.size > 1) logger.info(url.uri.toString)
-        url.get().flatMap { res =>
-          res.json.validate[Twitch.Result](twitchResultReads) match {
-            case JsSuccess(data, _) =>
-              fuccess(
-                data.streams(
-                  keyword,
-                  streamers,
-                  alwaysFeatured().value.map(_.toLowerCase)
+        url.get().flatMap {
+          case res if res.status == 200 =>
+            res.json.validate[Twitch.Result](twitchResultReads) match {
+              case JsSuccess(data, _) =>
+                fuccess(
+                  data.streams(
+                    keyword,
+                    streamers,
+                    alwaysFeatured().value.map(_.toLowerCase)
+                  )
                 )
-              )
-            case JsError(err) =>
-              fufail(s"twitch ${res.status} $err ${~res.body.lines.toList.headOption}")
-          }
+              case JsError(err) =>
+                fufail(s"twitch ${res.status} $err ${~res.body.lines.toList.headOption}")
+            }
+          case res =>
+            fufail(s"twitch ${res.status} ${~res.body.lines.toList.headOption}")
         }.recover {
           case e: Exception =>
             logger.warn(e.getMessage)