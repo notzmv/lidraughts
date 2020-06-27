56c4d762e39d63f6321168db2187e2d06e5bbdd6
Stefan Schermann <stefan.schermann@gmail.com>
2020-06-27 17:51:04 +0200

    deprecate app versions without support for 64-variants

--- modules/api/src/main/Mobile.scala
+++ modules/api/src/main/Mobile.scala
@@ -13,7 +13,9 @@ object Mobile {
     def mustUpgrade(v: String) = mustUpgradeFromVersions(v)
 
     // only call if a more recent version is available in both stores!
-    private val mustUpgradeFromVersions = Set("0.0.0")
+    private val mustUpgradeFromVersions = Set(
+      "1.0.0", "1.0.1", "1.0.2", "1.0.3", "1.0.4" /* no support for 64-variants */
+    )
 
   }
 
