6ac42cf19ecb0d4c6ba4fa0ee73a2bfcd708e235
Thibault Duplessis <t@lichess.org>
2020-08-18 11:24:18 +0200

    fix visual bug with tournament clock

--- ui/tournament/src/view/header.ts
+++ ui/tournament/src/view/header.ts
@@ -21,7 +21,7 @@ function clock(ctrl: TournamentController): VNode | undefined {
   if (d.secondsToFinish) return h('div.clock', [
     h('div.time', {
       hook: startClock(d.secondsToFinish)
-    }, d.secondsToFinish)
+    })
   ]);
   if (d.secondsToStart) {
     if (d.secondsToStart > oneDayInSeconds) return h('div.clock', [
