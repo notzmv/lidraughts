0c8c45ee0521e9fc6ff8aec44e7730bcd21a26c0
Thibault Duplessis <t@lichess.org>
2020-08-25 13:42:06 +0200

    fix tests again

--- modules/common/src/test/RawHtmlTest.scala
+++ modules/common/src/test/RawHtmlTest.scala
@@ -138,8 +138,8 @@ class RawHtmlTest extends Specification {
       justMarkdownLinks(md) must_== md
     }
 
-    "addBr" in {
-      justMarkdownLinks("\n") must_== "<br>"
+    " not addBr" in {
+      justMarkdownLinks("\n") must_== "\n"
     }
 
     "escape html" in {
