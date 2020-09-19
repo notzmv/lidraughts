lichess-org/lila

service worker simplifications

public/javascripts/service-worker.js
@@ -1,21 +1,19 @@
 self.addEventListener('push', event => {
   const data = event.data.json();
-  event.waitUntil(self.registration.getNotifications().then(notifications => {
-    return self.registration.showNotification(data.title, {
-      badge: 'https://lichess1.org/assets/images/logo.256.png',
-      icon: 'https://lichess1.org/assets/images/logo.256.png',
-      body: data.body,
-      tag: data.tag,
-      data: data.payload,
-      requireInteraction: true
-    });
-  }));
+  return self.registration.showNotification(data.title, {
+    badge: 'https://lichess1.org/assets/images/logo.256.png',
+    icon: 'https://lichess1.org/assets/images/logo.256.png',
+    body: data.body,
+    tag: data.tag,
+    data: data.payload,
+    requireInteraction: true
+  });
 });
 
 self.addEventListener('notificationclick', event => {
   event.waitUntil(self.registration.getNotifications().then(notifications => {
     notifications.forEach(notification => notification.close());
-    return clients.matchAll({
+    return self.clients.matchAll({
       type: 'window',
       includeUncontrolled: true
     });
@@ -28,18 +26,18 @@ self.addEventListener('notificationclick', event => {
     else if (data.challengeId) url = '/' + data.challengeId;
 
     // focus open window with same url
-    for (let client of windowClients) {
-      var clientUrl = new URL(client.url, self.location.href);
+    for (const client of windowClients) {
+      const clientUrl = new URL(client.url, self.location.href);
       if (clientUrl.pathname === url && 'focus' in client) return client.focus();
     }
 
     // navigate from open homepage to url
-    for (let client of windowClients) {
-      var clientUrl = new URL(client.url, self.location.href);
+    for (const client of windowClients) {
+      const clientUrl = new URL(client.url, self.location.href);
       if (clientUrl.pathname === '/') return client.navigate(url);
     }
 
     // open new window
-    return clients.openWindow(url);
+    return self.clients.openWindow(url);
   }));
 });

ui/site/src/main.js
@@ -977,21 +977,20 @@
   // service worker //
   ////////////////////
 
-  var pushBeta = !!document.body.getAttribute('data-vapid');
-  if (pushBeta && 'serviceWorker' in navigator && 'Notification' in window && 'PushManager' in window) {
-    var workerUrl = lichess.assetUrl('javascripts/service-worker.js', {noVersion: true, sameDomain: true});
+  if ('serviceWorker' in navigator && 'Notification' in window && 'PushManager' in window) {
+    const workerUrl = lichess.assetUrl('javascripts/service-worker.js', {noVersion: true, sameDomain: true});
     navigator.serviceWorker.register(workerUrl, {scope: '/'});
   }
 
   lichess.pushSubscribe = function(ask) {
     if ('serviceWorker' in navigator && 'Notification' in window && 'PushManager' in window) {
       navigator.serviceWorker.ready.then(reg => {
-        var storage = lichess.storage.make('push-subscribed');
-        var vapid = document.body.getAttribute('data-vapid');
-        var allowed = (ask || Notification.permission === 'granted') && Notification.permission !== 'denied';
+        const storage = lichess.storage.make('push-subscribed');
+        const vapid = document.body.getAttribute('data-vapid');
+        const allowed = (ask || Notification.permission === 'granted') && Notification.permission !== 'denied';
         if (vapid && allowed) return reg.pushManager.getSubscription().then(sub => {
-          var resub = parseInt(storage.get() || '0', 10) + 43200000 < Date.now(); // 12 hours
-          var applicationServerKey = Uint8Array.from(atob(vapid), c => c.charCodeAt(0));
+          const resub = parseInt(storage.get() || '0', 10) + 43200000 < Date.now(); // 12 hours
+          const applicationServerKey = Uint8Array.from(atob(vapid), c => c.charCodeAt(0));
           if (!sub || resub) {
             return reg.pushManager.subscribe({
               userVisibleOnly: true,

notzmv/lidraughts

public/javascripts/service-worker.js
@@ -1,21 +1,19 @@
 self.addEventListener('push', event => {
   const data = event.data.json();
-  event.waitUntil(self.registration.getNotifications().then(notifications => {
-    return self.registration.showNotification(data.title, {
-      badge: 'https://lidraughts.org/assets/images/logo.256.png',
-      icon: 'https://lidraughts.org/assets/images/logo.256.png',
-      body: data.body,
-      tag: data.tag,
-      data: data.payload,
-      requireInteraction: true
-    });
-  }));
+  return self.registration.showNotification(data.title, {
+    badge: 'https://lidraughts.org/assets/images/logo.256.png',
+    icon: 'https://lidraughts.org/assets/images/logo.256.png',
+    body: data.body,
+    tag: data.tag,
+    data: data.payload,
+    requireInteraction: true
+  });
 });
 
 self.addEventListener('notificationclick', event => {
   event.waitUntil(self.registration.getNotifications().then(notifications => {
     notifications.forEach(notification => notification.close());
-    return clients.matchAll({
+    return self.clients.matchAll({
       type: 'window',
       includeUncontrolled: true
     });
@@ -28,18 +26,18 @@ self.addEventListener('notificationclick', event => {
     else if (data.challengeId) url = '/' + data.challengeId;
 
     // focus open window with same url
-    for (let client of windowClients) {
-      var clientUrl = new URL(client.url, self.location.href);
+    for (const client of windowClients) {
+      const clientUrl = new URL(client.url, self.location.href);
       if (clientUrl.pathname === url && 'focus' in client) return client.focus();
     }
 
     // navigate from open homepage to url
-    for (let client of windowClients) {
-      var clientUrl = new URL(client.url, self.location.href);
+    for (const client of windowClients) {
+      const clientUrl = new URL(client.url, self.location.href);
       if (clientUrl.pathname === '/') return client.navigate(url);
     }
 
     // open new window
-    return clients.openWindow(url);
+    return self.clients.openWindow(url);
   }));
 });

ui/site/src/main.js
@@ -981,21 +981,20 @@
   // service worker //
   ////////////////////
 
-  var pushBeta = !!document.body.getAttribute('data-vapid');
-  if (pushBeta && 'serviceWorker' in navigator && 'Notification' in window && 'PushManager' in window) {
-    var workerUrl = lidraughts.assetUrl('javascripts/service-worker.js', {noVersion: true, sameDomain: true});
+  if ('serviceWorker' in navigator && 'Notification' in window && 'PushManager' in window) {
+    const workerUrl = lidraughts.assetUrl('javascripts/service-worker.js', {noVersion: true, sameDomain: true});
     navigator.serviceWorker.register(workerUrl, {scope: '/'});
   }
 
   lidraughts.pushSubscribe = function(ask) {
     if ('serviceWorker' in navigator && 'Notification' in window && 'PushManager' in window) {
       navigator.serviceWorker.ready.then(reg => {
-        var storage = lidraughts.storage.make('push-subscribed');
-        var vapid = document.body.getAttribute('data-vapid');
-        var allowed = (ask || Notification.permission === 'granted') && Notification.permission !== 'denied';
+        const storage = lidraughts.storage.make('push-subscribed');
+        const vapid = document.body.getAttribute('data-vapid');
+        const allowed = (ask || Notification.permission === 'granted') && Notification.permission !== 'denied';
         if (vapid && allowed) return reg.pushManager.getSubscription().then(sub => {
-          var resub = parseInt(storage.get() || '0', 10) + 43200000 < Date.now(); // 12 hours
-          var applicationServerKey = Uint8Array.from(atob(vapid), c => c.charCodeAt(0));
+          const resub = parseInt(storage.get() || '0', 10) + 43200000 < Date.now(); // 12 hours
+          const applicationServerKey = Uint8Array.from(atob(vapid), c => c.charCodeAt(0));
           if (!sub || resub) {
             return reg.pushManager.subscribe({
               userVisibleOnly: true,