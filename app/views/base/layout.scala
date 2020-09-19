lichess-org/lila

better announcements WIP

app/views/base/layout.scala
@@ -2,11 +2,11 @@ package views.html.base
 
 import play.api.libs.json.Json
 
-import lila.api.Context
+import lila.api.{ Context, AnnounceStore }
 import lila.app.templating.Environment._
 import lila.app.ui.ScalatagsTemplate._
-import lila.common.{ Lang, ContentSecurityPolicy }
 import lila.common.String.html.safeJsonValue
+import lila.common.{ Lang, ContentSecurityPolicy }
 import lila.pref.Pref
 
 import controllers.routes
@@ -90,6 +90,7 @@ object layout {
   private val dataZoom = attr("data-zoom")
   private val dataPreload = attr("data-preload")
   private val dataNonce = attr("data-nonce")
+  private val dataAnnounce = attr("data-announce")
 
   def apply(
     title: String,
@@ -157,6 +158,7 @@ object layout {
         dataAssetVersion := assetVersion.value,
         dataNonce := ctx.nonce.ifTrue(sameAssetDomain).map(_.value),
         dataTheme := ctx.currentBg,
+        dataAnnounce := AnnounceStore.get.map(a => safeJsonValue(a.json)),
         style := zoomable option s"--zoom:${ctx.zoom}"
       )(
           blindModeForm,

app/views/dev.scala
@@ -60,7 +60,8 @@ object dev {
             ),
             h2("Command examples:"),
             pre("""uptime
-announce Lichess will undergo maintenance in 15 minutes!
+announce 10 minutes Lichess will restart!
+announce cancel
 change asset version
 puzzle disable 70000
 team disable foobar

modules/api/src/main/AnnounceStore.scala
@@ -1,0 +1,39 @@
+package lila.api
+
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
+import scala.concurrent.duration._
+import play.api.libs.json.Json
+import scala.util.Try
+
+import lila.hub.actorApi.Announce
+
+object AnnounceStore {
+
+  private var current = none[Announce]
+
+  def get: Option[Announce] = current
+
+  def set(announce: Option[Announce]) = {
+    current = announce
+  }
+
+  // examples:
+  // 5 minutes Lichess will restart
+  // 20 seconds Cthulhu will awake
+  def set(str: String): Option[Announce] = {
+    set(str.split(" ").toList match {
+      case length :: unit :: rest => Try {
+        val msg = rest mkString " "
+        val date = DateTime.now plusSeconds Duration(s"$length $unit").toSeconds.toInt
+        val isoDate = ISODateTimeFormat.dateTime print date
+        val json = Json.obj("msg" -> msg, "date" -> isoDate)
+        Announce(msg, date, json)
+      }.toOption
+      case _ => none
+    })
+    get
+  }
+
+  def cancel = Announce("", DateTime.now, Json.obj())
+}

ui/site/src/main.js
@@ -109,14 +109,7 @@
           return false;
         });
       },
-      announce: function(d) {
-        if (!$('#announce').length) $('body').append(
-          '<div id="announce" class="announce">' +
-          d.msg +
-          '<div class="actions"><a class="close">X</a></div>' +
-          '</div>'
-        ).find('#announce .close').click(function() { $('#announce').remove(); });
-      }
+      announce: lichess.announce
     },
     params: {},
     options: {
@@ -125,6 +118,19 @@
       isAuth: !!$('body').data('user')
     }
   });
+
+  lichess.announce = (d) => {
+    $('#announce').remove();
+    if (d.msg) $('body').append(
+      '<div id="announce" class="announce">' +
+      d.msg +
+      '<time class="timeago" datetime="' + d.date + '"></time>' +
+      '<div class="actions"><a class="close">X</a></div>' +
+      '</div>'
+    ).find('#announce .close').click(function() { $('#announce').remove(); });
+  };
+  const announce = $('body').data('announce');
+  if (announce) lichess.announce(announce);
 
   lichess.reverse = s => s.split('').reverse().join('');
   lichess.readServerFen = t => atob(lichess.reverse(t));

notzmv/lidraughts

app/views/base/layout.scala
@@ -2,11 +2,11 @@ package views.html.base
 
 import play.api.libs.json.Json
 
-import lidraughts.api.Context
+import lidraughts.api.{ Context, AnnounceStore }
 import lidraughts.app.templating.Environment._
 import lidraughts.app.ui.ScalatagsTemplate._
-import lidraughts.common.{ Lang, ContentSecurityPolicy }
 import lidraughts.common.String.html.safeJsonValue
+import lidraughts.common.{ Lang, ContentSecurityPolicy }
 import lidraughts.pref.Pref
 
 import controllers.routes
@@ -84,6 +84,7 @@ object layout {
   private val dataZoom = attr("data-zoom")
   private val dataPreload = attr("data-preload")
   private val dataNonce = attr("data-nonce")
+  private val dataAnnounce = attr("data-announce")
 
   def apply(
     title: String,
@@ -147,6 +148,7 @@ object layout {
         dataAssetVersion := assetVersion.value,
         dataNonce := ctx.nonce.ifTrue(sameAssetDomain).map(_.value),
         dataTheme := ctx.currentBg,
+        dataAnnounce := AnnounceStore.get.map(a => safeJsonValue(a.json)),
         style := zoomable option s"--zoom:${ctx.zoom}"
       )(
           blindModeForm,

app/views/dev.scala
@@ -60,7 +60,8 @@ object dev {
             ),
             h2("Command examples:"),
             pre("""uptime
-announce Lidraughts will undergo maintenance in 15 minutes!
+announce 10 minutes Lidraughts will restart!
+announce cancel
 change asset version
 puzzle disable [standard|frisian] 150
 team disable foobar

modules/api/src/main/AnnounceStore.scala
@@ -1,0 +1,39 @@
+package lidraughts.api
+
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
+import scala.concurrent.duration._
+import play.api.libs.json.Json
+import scala.util.Try
+
+import lidraughts.hub.actorApi.Announce
+
+object AnnounceStore {
+
+  private var current = none[Announce]
+
+  def get: Option[Announce] = current
+
+  def set(announce: Option[Announce]) = {
+    current = announce
+  }
+
+  // examples:
+  // 5 minutes Lichess will restart
+  // 20 seconds Cthulhu will awake
+  def set(str: String): Option[Announce] = {
+    set(str.split(" ").toList match {
+      case length :: unit :: rest => Try {
+        val msg = rest mkString " "
+        val date = DateTime.now plusSeconds Duration(s"$length $unit").toSeconds.toInt
+        val isoDate = ISODateTimeFormat.dateTime print date
+        val json = Json.obj("msg" -> msg, "date" -> isoDate)
+        Announce(msg, date, json)
+      }.toOption
+      case _ => none
+    })
+    get
+  }
+
+  def cancel = Announce("", DateTime.now, Json.obj())
+}

ui/site/src/main.js
@@ -109,14 +109,7 @@
           return false;
         });
       },
-      announce: function(d) {
-        if (!$('#announce').length) $('body').append(
-          '<div id="announce" class="announce">' +
-          d.msg +
-          '<div class="actions"><a class="close">X</a></div>' +
-          '</div>'
-        ).find('#announce .close').click(function() { $('#announce').remove(); });
-      }
+      announce: lidraughts.announce
     },
     params: {},
     options: {
@@ -125,6 +118,19 @@
       isAuth: !!$('body').data('user')
     }
   });
+
+  lidraughts.announce = (d) => {
+    $('#announce').remove();
+    if (d.msg) $('body').append(
+      '<div id="announce" class="announce">' +
+      d.msg +
+      '<time class="timeago" datetime="' + d.date + '"></time>' +
+      '<div class="actions"><a class="close">X</a></div>' +
+      '</div>'
+    ).find('#announce .close').click(function() { $('#announce').remove(); });
+  };
+  const announce = $('body').data('announce');
+  if (announce) lidraughts.announce(announce);
 
   lidraughts.reverse = s => s.split('').reverse().join('');
   lidraughts.readServerFen = t => atob(lidraughts.reverse(t));