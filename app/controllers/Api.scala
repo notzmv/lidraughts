lichess-org/lila

/api/user/<name>/tournament/created API endpoint

app/controllers/Api.scala
@@ -11,6 +11,7 @@ import lila.api.{ Context, GameApiV2, UserApi }
 import lila.app._
 import lila.common.PimpedJson._
 import lila.common.{ HTTPRequest, IpAddress, MaxPerPage, MaxPerSecond }
+import lila.user.UserRepo
 
 object Api extends LilaController {
 
@@ -71,7 +72,7 @@ object Api extends LilaController {
     UsersRateLimitPerIP(ip, cost = cost) {
       UsersRateLimitGlobal("-", cost = cost, msg = ip.value) {
         lila.mon.api.users.cost(cost)
-        lila.user.UserRepo nameds usernames map {
+        UserRepo nameds usernames map {
           _.map { Env.user.jsonView(_, none) }
         } map toApiResult map toHttp
       }
@@ -157,7 +158,7 @@ object Api extends LilaController {
     val cost = page * nb + 10
     UserGamesRateLimit(cost, req) {
       lila.mon.api.userGames.cost(cost)
-      lila.user.UserRepo named name flatMap {
+      UserRepo named name flatMap {
         _ ?? { user =>
           gameApi.byUser(
             user = user,
@@ -206,7 +207,7 @@ object Api extends LilaController {
 
   def currentTournaments = ApiRequest { implicit ctx =>
     Env.tournament.api.fetchVisibleTournaments flatMap
-      Env.tournament.scheduleJsonView.apply map Data.apply
+      Env.tournament.apiJsonView.apply map Data.apply
   }
 
   def tournament(id: String) = ApiRequest { req =>
@@ -244,8 +245,21 @@ object Api extends LilaController {
         GlobalLinearLimitPerIP(HTTPRequest lastRemoteAddress req) {
           import lila.tournament.JsonView.playerResultWrites
           val nb = getInt("nb", req) | Int.MaxValue
-          val enumerator = Env.tournament.api.resultStream(tour, 50, nb) &>
+          val enumerator = Env.tournament.api.resultStream(tour, MaxPerSecond(50), nb) &>
             Enumeratee.map(playerResultWrites.writes)
+          jsonStream(enumerator).fuccess
+        }
+      }
+    }
+  }
+
+  def tournamentsByOwner(name: String) = Action.async { req =>
+    (name != "lichess") ?? UserRepo.named(name) flatMap {
+      _ ?? { user =>
+        GlobalLinearLimitPerIP(HTTPRequest lastRemoteAddress req) {
+          val nb = getInt("nb", req) | Int.MaxValue
+          val enumerator = Env.tournament.api.byOwnerStream(user, MaxPerSecond(20), nb) &>
+            Enumeratee.mapM(Env.tournament.apiJsonView.fullJson)
           jsonStream(enumerator).fuccess
         }
       }
@@ -275,7 +289,7 @@ object Api extends LilaController {
   def activity(name: String) = ApiRequest { req =>
     UserActivityRateLimitPerIP(HTTPRequest lastRemoteAddress req, cost = 1) {
       lila.mon.api.activity.cost(1)
-      lila.user.UserRepo named name flatMap {
+      UserRepo named name flatMap {
         _ ?? { user =>
           Env.activity.read.recent(user) flatMap {
             _.map { Env.activity.jsonView(_, user) }.sequenceFu

modules/tournament/src/main/PlayerRepo.scala
@@ -5,6 +5,7 @@ import reactivemongo.api.{ CursorProducer, Cursor, ReadPreference }
 import reactivemongo.bson._
 
 import BSONHandlers._
+import lila.common.MaxPerSecond
 import lila.db.dsl._
 import lila.rating.Perf
 import lila.user.{ User, Perfs }

modules/tournament/src/main/TournamentApi.scala
@@ -9,7 +9,7 @@ import scala.concurrent.duration._
 import scala.concurrent.Promise
 
 import actorApi._
-import lila.common.{ Debouncer, LightUser }
+import lila.common.{ Debouncer, LightUser, MaxPerSecond }
 import lila.game.{ Game, LightGame, GameRepo, Pov, LightPov }
 import lila.hub.actorApi.lobby.ReloadTournaments
 import lila.hub.actorApi.map.Tell
@@ -23,7 +23,7 @@ import makeTimeout.short
 
 final class TournamentApi(
     cached: Cached,
-    scheduleJsonView: ScheduleJsonView,
+    apiJsonView: ApiJsonView,
     system: ActorSystem,
     sequencers: DuctMap[_],
     autoPairing: AutoPairing,
@@ -452,13 +452,13 @@ final class TournamentApi(
     TournamentRepo.calendar(from = from, to = from plusYears 1)
   }
 
-  def resultStream(tour: Tournament, perSecond: Int, nb: Int): Enumerator[Player.Result] = {
+  def resultStream(tour: Tournament, perSecond: MaxPerSecond, nb: Int): Enumerator[Player.Result] = {
     import reactivemongo.play.iteratees.cursorProducer
     import play.api.libs.iteratee._
     var rank = 0
     PlayerRepo.cursor(
       tournamentId = tour.id,
-      batchSize = perSecond
+      batchSize = perSecond.value
     ).bulkEnumerator(nb) &>
       lila.common.Iteratee.delay(1 second)(system) &>
       Enumeratee.mapConcat(_.toSeq) &>
@@ -468,6 +468,14 @@ final class TournamentApi(
           Player.Result(player, lu | LightUser.fallback(player.userId), rank)
         }
       }
+  }
+
+  def byOwnerStream(owner: User, perSecond: MaxPerSecond, nb: Int): Enumerator[Tournament] = {
+    import reactivemongo.play.iteratees.cursorProducer
+    import play.api.libs.iteratee._
+    TournamentRepo.cursor(owner, perSecond.value).bulkEnumerator(nb) &>
+      lila.common.Iteratee.delay(1 second)(system) &>
+      Enumeratee.mapConcat(_.toSeq)
   }
 
   private def playerPovs(tour: Tournament, userId: User.ID, nb: Int): Fu[List[LightPov]] =
@@ -492,7 +500,7 @@ final class TournamentApi(
   private object publish {
     private val debouncer = system.actorOf(Props(new Debouncer(15 seconds, {
       (_: Debouncer.Nothing) =>
-        fetchVisibleTournaments flatMap scheduleJsonView.apply foreach { json =>
+        fetchVisibleTournaments flatMap apiJsonView.apply foreach { json =>
           bus.publish(
             SendToFlag("tournament", Json.obj("t" -> "reload", "d" -> json)),
             'sendToFlag

modules/tournament/src/main/TournamentRepo.scala
@@ -3,6 +3,7 @@ package lila.tournament
 import chess.variant.Variant
 import org.joda.time.DateTime
 import reactivemongo.api.ReadPreference
+import reactivemongo.api.{ CursorProducer, Cursor, ReadPreference }
 
 import BSONHandlers._
 import lila.common.paginator.Paginator
@@ -241,4 +242,15 @@ object TournamentRepo {
       "startsAt" $gte from $lte to,
       "schedule.freq" $in Schedule.Freq.all.filter(_.isWeeklyOrBetter)
     )).sort($sort asc "startsAt").list[Tournament](none, ReadPreference.secondaryPreferred)
+
+  private[tournament] def cursor(
+    owner: lila.user.User,
+    batchSize: Int,
+    readPreference: ReadPreference = ReadPreference.secondaryPreferred
+  )(implicit cp: CursorProducer[Tournament]): cp.ProducedCursor = {
+    val query = coll
+      .find($doc("createdBy" -> owner.id))
+      .sort($sort desc "startsAt")
+    query.copy(options = query.options.batchSize(batchSize)).cursor[Tournament](readPreference)
+  }
 }

notzmv/lidraughts

app/controllers/Api.scala
@@ -11,6 +11,7 @@ import lidraughts.api.{ Context, GameApiV2, UserApi }
 import lidraughts.app._
 import lidraughts.common.PimpedJson._
 import lidraughts.common.{ HTTPRequest, IpAddress, MaxPerPage, MaxPerSecond }
+import lidraughts.user.UserRepo
 
 object Api extends LidraughtsController {
 
@@ -71,7 +72,7 @@ object Api extends LidraughtsController {
     UsersRateLimitPerIP(ip, cost = cost) {
       UsersRateLimitGlobal("-", cost = cost, msg = ip.value) {
         lidraughts.mon.api.users.cost(cost)
-        lidraughts.user.UserRepo nameds usernames map {
+        UserRepo nameds usernames map {
           _.map { Env.user.jsonView(_, none) }
         } map toApiResult map toHttp
       }
@@ -157,7 +158,7 @@ object Api extends LidraughtsController {
     val cost = page * nb + 10
     UserGamesRateLimit(cost, req) {
       lidraughts.mon.api.userGames.cost(cost)
-      lidraughts.user.UserRepo named name flatMap {
+      UserRepo named name flatMap {
         _ ?? { user =>
           gameApi.byUser(
             user = user,
@@ -207,7 +208,7 @@ object Api extends LidraughtsController {
 
   def currentTournaments = ApiRequest { implicit ctx =>
     Env.tournament.api.fetchVisibleTournaments flatMap
-      Env.tournament.scheduleJsonView.apply map Data.apply
+      Env.tournament.apiJsonView.apply map Data.apply
   }
 
   def tournament(id: String) = ApiRequest { req =>
@@ -245,8 +246,21 @@ object Api extends LidraughtsController {
         GlobalLinearLimitPerIP(HTTPRequest lastRemoteAddress req) {
           import lidraughts.tournament.JsonView.playerResultWrites
           val nb = getInt("nb", req) | Int.MaxValue
-          val enumerator = Env.tournament.api.resultStream(tour, 50, nb) &>
+          val enumerator = Env.tournament.api.resultStream(tour, MaxPerSecond(50), nb) &>
             Enumeratee.map(playerResultWrites.writes)
+          jsonStream(enumerator).fuccess
+        }
+      }
+    }
+  }
+
+  def tournamentsByOwner(name: String) = Action.async { req =>
+    (name != "lidraughts") ?? UserRepo.named(name) flatMap {
+      _ ?? { user =>
+        GlobalLinearLimitPerIP(HTTPRequest lastRemoteAddress req) {
+          val nb = getInt("nb", req) | Int.MaxValue
+          val enumerator = Env.tournament.api.byOwnerStream(user, MaxPerSecond(20), nb) &>
+            Enumeratee.mapM(Env.tournament.apiJsonView.fullJson)
           jsonStream(enumerator).fuccess
         }
       }
@@ -276,7 +290,7 @@ object Api extends LidraughtsController {
   def activity(name: String) = ApiRequest { req =>
     UserActivityRateLimitPerIP(HTTPRequest lastRemoteAddress req, cost = 1) {
       lidraughts.mon.api.activity.cost(1)
-      lidraughts.user.UserRepo named name flatMap {
+      UserRepo named name flatMap {
         _ ?? { user =>
           Env.activity.read.recent(user) flatMap {
             _.map { Env.activity.jsonView(_, user) }.sequenceFu

modules/tournament/src/main/PlayerRepo.scala
@@ -5,6 +5,7 @@ import reactivemongo.api.{ CursorProducer, Cursor, ReadPreference }
 import reactivemongo.bson._
 
 import BSONHandlers._
+import lidraughts.common.MaxPerSecond
 import lidraughts.db.dsl._
 import lidraughts.rating.Perf
 import lidraughts.user.{ User, Perfs }

modules/tournament/src/main/TournamentApi.scala
@@ -10,7 +10,7 @@ import scala.concurrent.duration._
 import scala.concurrent.Promise
 import actorApi._
 import lidraughts.common.paginator.Paginator
-import lidraughts.common.{ Debouncer, LightUser, MaxPerPage }
+import lidraughts.common.{ Debouncer, LightUser, MaxPerPage, MaxPerSecond }
 import lidraughts.game.{ Game, GameRepo, LightGame, LightPov, Pov }
 import lidraughts.hub.actorApi.lobby.ReloadTournaments
 import lidraughts.hub.actorApi.map.Tell
@@ -24,7 +24,7 @@ import makeTimeout.short
 
 final class TournamentApi(
     cached: Cached,
-    scheduleJsonView: ScheduleJsonView,
+    apiJsonView: ApiJsonView,
     system: ActorSystem,
     sequencers: DuctMap[_],
     autoPairing: AutoPairing,
@@ -500,13 +500,13 @@ final class TournamentApi(
     TournamentRepo.calendar(from = from, to = from plusYears 1)
   }
 
-  def resultStream(tour: Tournament, perSecond: Int, nb: Int): Enumerator[Player.Result] = {
+  def resultStream(tour: Tournament, perSecond: MaxPerSecond, nb: Int): Enumerator[Player.Result] = {
     import reactivemongo.play.iteratees.cursorProducer
     import play.api.libs.iteratee._
     var rank = 0
     PlayerRepo.cursor(
       tournamentId = tour.id,
-      batchSize = perSecond
+      batchSize = perSecond.value
     ).bulkEnumerator(nb) &>
       lidraughts.common.Iteratee.delay(1 second)(system) &>
       Enumeratee.mapConcat(_.toSeq) &>
@@ -516,6 +516,14 @@ final class TournamentApi(
           Player.Result(player, lu | LightUser.fallback(player.userId), rank)
         }
       }
+  }
+
+  def byOwnerStream(owner: User, perSecond: MaxPerSecond, nb: Int): Enumerator[Tournament] = {
+    import reactivemongo.play.iteratees.cursorProducer
+    import play.api.libs.iteratee._
+    TournamentRepo.cursor(owner, perSecond.value).bulkEnumerator(nb) &>
+      lidraughts.common.Iteratee.delay(1 second)(system) &>
+      Enumeratee.mapConcat(_.toSeq)
   }
 
   private def playerPovs(tour: Tournament, userId: User.ID, nb: Int): Fu[List[LightPov]] =
@@ -546,7 +554,7 @@ final class TournamentApi(
   private object publish {
     private val debouncer = system.actorOf(Props(new Debouncer(15 seconds, {
       (_: Debouncer.Nothing) =>
-        fetchVisibleTournaments flatMap scheduleJsonView.apply foreach { json =>
+        fetchVisibleTournaments flatMap apiJsonView.apply foreach { json =>
           bus.publish(
             SendToFlag("tournament", Json.obj("t" -> "reload", "d" -> json)),
             'sendToFlag

modules/tournament/src/main/TournamentRepo.scala
@@ -3,6 +3,7 @@ package lidraughts.tournament
 import draughts.variant.Variant
 import org.joda.time.DateTime
 import reactivemongo.api.ReadPreference
+import reactivemongo.api.{ CursorProducer, Cursor, ReadPreference }
 
 import BSONHandlers._
 import lidraughts.common.paginator.Paginator
@@ -254,4 +255,15 @@ object TournamentRepo {
       "startsAt" $gte from $lte to,
       "schedule.freq" $in Schedule.Freq.all.filter(_.isWeeklyOrBetter)
     )).sort($sort asc "startsAt").list[Tournament](none, ReadPreference.secondaryPreferred)
+
+  private[tournament] def cursor(
+    owner: lidraughts.user.User,
+    batchSize: Int,
+    readPreference: ReadPreference = ReadPreference.secondaryPreferred
+  )(implicit cp: CursorProducer[Tournament]): cp.ProducedCursor = {
+    val query = coll
+      .find($doc("createdBy" -> owner.id))
+      .sort($sort desc "startsAt")
+    query.copy(options = query.options.batchSize(batchSize)).cursor[Tournament](readPreference)
+  }
 }