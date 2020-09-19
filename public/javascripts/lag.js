lichess-org/lila

fix lag page socket URL

public/javascripts/lag.js
@@ -215,7 +215,7 @@ $(function() {
     $('.lag .answer span').hide().parent().find('.' + c).show();
   };
 
-  lichess.socket = new lichess.StrongSocket('/socket', false, {
+  lichess.socket = new lichess.StrongSocket('/socket/v4', false, {
     options: {
       name: "analyse",
       onFirstConnect: function() {

notzmv/lidraughts

public/javascripts/lag.js
@@ -215,7 +215,7 @@ $(function() {
     $('.lag .answer span').hide().parent().find('.' + c).show();
   };
 
-  lidraughts.socket = new lidraughts.StrongSocket('/socket', false, {
+  lidraughts.socket = new lidraughts.StrongSocket('/socket/v3', false, {
     options: {
       name: "analyse",
       onFirstConnect: function() {