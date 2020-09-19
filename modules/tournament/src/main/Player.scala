lichess-org/lila

team battle WIP

modules/tournament/src/main/Player.scala
@@ -1,8 +1,9 @@
 package lila.tournament
 
+import lila.common.LightUser
+import lila.hub.lightTeam.TeamId
 import lila.rating.Perf
 import lila.user.{ User, Perfs }
-import lila.common.LightUser
 
 import ornicar.scalalib.Random
 
@@ -15,7 +16,8 @@ private[tournament] case class Player(
     withdraw: Boolean = false,
     score: Int = 0,
     fire: Boolean = false,
-    performance: Int = 0
+    performance: Int = 0,
+    team: Option[TeamId] = None
 ) {
 
   def id = _id
@@ -42,11 +44,17 @@ private[tournament] object Player {
 
   case class Result(player: Player, lightUser: LightUser, rank: Int)
 
-  private[tournament] def make(tourId: Tournament.ID, user: User, perfLens: Perfs => Perf): Player = new Player(
+  private[tournament] def make(
+    tourId: Tournament.ID,
+    user: User,
+    perfLens: Perfs => Perf,
+    team: Option[TeamId]
+  ): Player = new Player(
     _id = Random.nextString(8),
     tourId = tourId,
     userId = user.id,
     rating = perfLens(user.perfs).intRating,
-    provisional = perfLens(user.perfs).provisional
+    provisional = perfLens(user.perfs).provisional,
+    team = team
   )
 }

modules/tournament/src/main/PlayerRepo.scala
@@ -6,6 +6,7 @@ import reactivemongo.bson._
 
 import BSONHandlers._
 import lila.db.dsl._
+import lila.hub.lightTeam.TeamId
 import lila.rating.Perf
 import lila.user.{ User, Perfs }
 
@@ -71,11 +72,11 @@ object PlayerRepo {
       coll.update(selectId(player._id), player).void
     }
 
-  def join(tourId: String, user: User, perfLens: Perfs => Perf) =
+  def join(tourId: String, user: User, perfLens: Perfs => Perf, team: Option[TeamId]) =
     find(tourId, user.id) flatMap {
       case Some(p) if p.withdraw => coll.update(selectId(p._id), $unset("w"))
       case Some(p) => funit
-      case None => coll.insert(Player.make(tourId, user, perfLens))
+      case None => coll.insert(Player.make(tourId, user, perfLens, team))
     } void
 
   def withdraw(tourId: String, userId: String) =

notzmv/lidraughts

modules/tournament/src/main/Player.scala
@@ -1,8 +1,9 @@
 package lidraughts.tournament
 
+import lidraughts.common.LightUser
+import lidraughts.hub.lightTeam.TeamId
 import lidraughts.rating.Perf
 import lidraughts.user.{ User, Perfs }
-import lidraughts.common.LightUser
 
 import ornicar.scalalib.Random
 
@@ -16,7 +17,8 @@ private[tournament] case class Player(
     score: Int = 0,
     ratingDiff: Int = 0,
     fire: Boolean = false,
-    performance: Int = 0
+    performance: Int = 0,
+    team: Option[TeamId] = None
 ) {
 
   def id = _id
@@ -43,11 +45,17 @@ private[tournament] object Player {
 
   case class Result(player: Player, lightUser: LightUser, rank: Int)
 
-  private[tournament] def make(tourId: Tournament.ID, user: User, perfLens: Perfs => Perf): Player = new Player(
+  private[tournament] def make(
+    tourId: Tournament.ID,
+    user: User,
+    perfLens: Perfs => Perf,
+    team: Option[TeamId]
+  ): Player = new Player(
     _id = Random.nextString(8),
     tourId = tourId,
     userId = user.id,
     rating = perfLens(user.perfs).intRating,
-    provisional = perfLens(user.perfs).provisional
+    provisional = perfLens(user.perfs).provisional,
+    team = team
   )
 }

modules/tournament/src/main/PlayerRepo.scala
@@ -7,6 +7,7 @@ import reactivemongo.bson._
 import BSONHandlers._
 import lidraughts.common.MaxPerSecond
 import lidraughts.db.dsl._
+import lidraughts.hub.lightTeam.TeamId
 import lidraughts.rating.Perf
 import lidraughts.user.{ User, Perfs }
 
@@ -75,11 +76,11 @@ object PlayerRepo {
       coll.update(selectId(player._id), player).void
     }
 
-  def join(tourId: String, user: User, perfLens: Perfs => Perf) =
+  def join(tourId: String, user: User, perfLens: Perfs => Perf, team: Option[TeamId]) =
     find(tourId, user.id) flatMap {
       case Some(p) if p.withdraw => coll.update(selectId(p._id), $unset("w"))
       case Some(p) => funit
-      case None => coll.insert(Player.make(tourId, user, perfLens))
+      case None => coll.insert(Player.make(tourId, user, perfLens, team))
     } void
 
   def withdraw(tourId: String, userId: String) =