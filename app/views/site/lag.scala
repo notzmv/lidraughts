lichess-org/lila

fix technically outdated explanation on /lag

app/views/site/lag.scala
@@ -57,8 +57,9 @@ object lag {
           p("You can find both these values at any time, by clicking your username in the top bar."),
           h2("Lag compensation"),
           p(
-            "Lichess compensates network lag, up to one second per move. ",
-            "After your move, ", strong("your average network lag is added to your clock"), ". ",
+            "Lichess compensates network lag. This includes sustained lag and occasional lag spikes. ",
+            "There are limits and heuristics based on time control and the compensated lag so far, ",
+            "so that the result should feel reasonable for both players. ",
             "As a result, having a higher network lag than your opponent is ", strong("not a handicap"), "!"
           )
         )

notzmv/lidraughts

app/views/site/lag.scala
@@ -57,8 +57,9 @@ object lag {
           p("You can find both these values at any time, by clicking your username in the top bar."),
           h2("Lag compensation"),
           p(
-            "Lidraughts compensates network lag, up to one second per move. ",
-            "After your move, ", strong("your average network lag is added to your clock"), ". ",
+            "Lidraughts compensates network lag. This includes sustained lag and occasional lag spikes. ",
+            "There are limits and heuristics based on time control and the compensated lag so far, ",
+            "so that the result should feel reasonable for both players. ",
             "As a result, having a higher network lag than your opponent is ", strong("not a handicap"), "!"
           )
         )