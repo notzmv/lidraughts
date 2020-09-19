lichess-org/lila

team battle WIP

app/controllers/Team.scala
@@ -3,6 +3,7 @@ package controllers
 import lila.api.Context
 import lila.app._
 import lila.common.{ HTTPRequest, MaxPerSecond }
+import lila.hub.lightTeam._
 import lila.security.Granter
 import lila.team.{ Joined, Motivate, Team => TeamModel, TeamRepo, MemberRepo }
 import lila.user.{ User => UserModel }
@@ -229,4 +230,7 @@ object Team extends LilaController {
   private def Owner(team: TeamModel)(a: => Fu[Result])(implicit ctx: Context): Fu[Result] =
     if (ctx.me.??(me => team.isCreator(me.id) || isGranted(_.ManageTeam))) a
     else renderTeam(team) map { Forbidden(_) }
+
+  private[controllers] def teamsIBelongTo(me: lila.user.User): Fu[List[LightTeam]] =
+    api mine me map { _.map(_.light) }
 }

app/controllers/Tournament.scala
@@ -18,6 +18,8 @@ object Tournament extends LilaController {
   private def env = Env.tournament
   private def repo = TournamentRepo
 
+  import Team.teamsIBelongTo
+
   private def tournamentNotFound(implicit ctx: Context) = NotFound(html.tournament.bits.notFound())
 
   private[controllers] val upcomingCache = Env.memo.asyncCache.single[(VisibleTournaments, List[Tour])](
@@ -172,8 +174,18 @@ object Tournament extends LilaController {
 
   def form = Auth { implicit ctx => me =>
     NoLameOrBot {
-      teamsIBelongTo(me) flatMap { teams =>
-        Ok(html.tournament.form(env.forms(me), env.forms, me, teams)).fuccess
+      teamsIBelongTo(me) map { teams =>
+        Ok(html.tournament.form(env.forms(me), env.forms, me, teams))
+      }
+    }
+  }
+
+  def formTeamBattle(teamId: String) = Auth { implicit ctx => me =>
+    NoLameOrBot {
+      Env.team.api.owns(teamId, me.id) map {
+        _ ?? {
+          Ok(html.tournament.form(env.forms(me, teamId.some), env.forms, me, Nil))
+        }
       }
     }
   }
@@ -210,9 +222,15 @@ object Tournament extends LilaController {
                 setup.password.isDefined) 1 else 4
               CreateLimitPerUser(me.id, cost = cost) {
                 CreateLimitPerIP(HTTPRequest lastRemoteAddress ctx.req, cost = cost) {
-                  env.api.createTournament(setup, me, teams, getUserTeamIds) flatMap { tour =>
-                    fuccess(Redirect(routes.Tournament.show(tour.id)))
-                  }
+                  env.api.createTournament(
+                    setup,
+                    me,
+                    teams,
+                    getUserTeamIds,
+                    Env.team.api.filterExistingIds
+                  ) flatMap { tour =>
+                      fuccess(Redirect(routes.Tournament.show(tour.id)))
+                    }
                 }(rateLimited)
               }(rateLimited)
             }
@@ -232,7 +250,7 @@ object Tournament extends LilaController {
     env.forms(me).bindFromRequest.fold(
       jsonFormErrorDefaultLang,
       setup => teamsIBelongTo(me) flatMap { teams =>
-        env.api.createTournament(setup, me, teams, getUserTeamIds) flatMap { tour =>
+        env.api.createTournament(setup, me, teams, getUserTeamIds, Env.team.api.filterExistingIds) flatMap { tour =>
           Env.tournament.jsonView(tour, none, none, getUserTeamIds, none, none, partial = false, lila.i18n.defaultLang)
         }
       } map { Ok(_) }
@@ -294,8 +312,6 @@ object Tournament extends LilaController {
     expireAfter = _.ExpireAfterWrite(15.seconds)
   )
 
-  private def getUserTeamIds(user: lila.user.User): Fu[TeamIdList] =
+  private def getUserTeamIds(user: lila.user.User): Fu[List[TeamId]] =
     Env.team.cached.teamIdsList(user.id)
-  private def teamsIBelongTo(me: lila.user.User): Fu[TeamIdsWithNames] =
-    Env.team.api.mine(me) map { _.map(t => t._id -> t.name) }
 }

app/views/tournament/form.scala
@@ -6,14 +6,14 @@ import play.api.data.{ Field, Form }
 import lila.api.Context
 import lila.app.templating.Environment._
 import lila.app.ui.ScalatagsTemplate._
-import lila.user.User
 import lila.tournament.{ Condition, DataForm }
+import lila.user.User
 
 import controllers.routes
 
 object form {
 
-  def apply(form: Form[_], config: DataForm, me: User, teams: lila.hub.lightTeam.TeamIdsWithNames)(implicit ctx: Context) = views.html.base.layout(
+  def apply(form: Form[_], config: DataForm, me: User, teams: List[lila.hub.lightTeam.LightTeam])(implicit ctx: Context) = views.html.base.layout(
     title = trans.newTournament.txt(),
     moreCss = cssTag("tournament.form"),
     moreJs = frag(
@@ -62,12 +62,14 @@ object form {
               a(cls := "show")(trans.showAdvancedSettings())
             ),
             div(cls := "form")(
-              form3.group(form("password"), trans.password(), help = raw("Make the tournament private, and restrict access with a password").some)(form3.input(_)),
+              form("teamBattle")("teams").value.isEmpty option
+                form3.group(form("password"), trans.password(), help = raw("Make the tournament private, and restrict access with a password").some)(form3.input(_)),
               condition(form, auto = true, teams = teams),
               input(tpe := "hidden", name := form("berserkable").name, value := "false"), // hack allow disabling berserk
               form3.group(form("startDate"), raw("Custom start date"), help = raw("""This overrides the "Time before tournament starts" setting""").some)(form3.flatpickr(_))
             )
           ),
+          form3.hidden(form("teamBattle")("teams")),
           form3.actions(
             a(href := routes.Tournament.home())(trans.cancel()),
             form3.submit(trans.createANewTournament(), icon = "g".some)
@@ -81,7 +83,7 @@ object form {
     if (auto) form3.hidden(field) else visible(field)
   )
 
-  def condition(form: Form[_], auto: Boolean, teams: lila.hub.lightTeam.TeamIdsWithNames)(implicit ctx: Context) = frag(
+  def condition(form: Form[_], auto: Boolean, teams: List[lila.hub.lightTeam.LightTeam])(implicit ctx: Context) = frag(
     form3.split(
       form3.group(form("conditions.nbRatedGame.nb"), raw("Minimum rated games"), half = true)(form3.select(_, Condition.DataForm.nbRatedGameChoices)),
       autoField(auto, form("conditions.nbRatedGame.perf")) { field =>
@@ -107,7 +109,9 @@ object form {
       form3.checkbox(form("berserkable"), raw("Allow Berserk"), help = raw("Let players halve their clock time to gain an extra point").some, half = true)
     ),
     (auto && teams.size > 0) ?? {
-      form3.group(form("conditions.teamMember.teamId"), raw("Only members of team"), half = false)(form3.select(_, List(("", "No Restriction")) ::: teams))
+      form3.group(form("conditions.teamMember.teamId"), raw("Only members of team"), half = false)(
+        form3.select(_, List(("", "No Restriction")) ::: teams.map(_.pair))
+      )
     }
   )
 

modules/team/src/main/Team.scala
@@ -24,6 +24,8 @@ case class Team(
   def disabled = !enabled
 
   def isCreator(user: String) = user == createdBy
+
+  def light = lila.hub.lightTeam.LightTeam(_id, name)
 }
 
 object Team {

notzmv/lidraughts

app/controllers/Team.scala
@@ -3,6 +3,7 @@ package controllers
 import lidraughts.api.Context
 import lidraughts.app._
 import lidraughts.common.{ HTTPRequest, MaxPerSecond }
+import lidraughts.hub.lightTeam._
 import lidraughts.security.Granter
 import lidraughts.team.{ Joined, Motivate, Team => TeamModel, TeamRepo, MemberRepo }
 import lidraughts.user.{ User => UserModel }
@@ -229,4 +230,7 @@ object Team extends LidraughtsController {
   private def Owner(team: TeamModel)(a: => Fu[Result])(implicit ctx: Context): Fu[Result] =
     if (ctx.me.??(me => team.isCreator(me.id) || isGranted(_.ManageTeam))) a
     else renderTeam(team) map { Forbidden(_) }
+
+  private[controllers] def teamsIBelongTo(me: lidraughts.user.User): Fu[List[LightTeam]] =
+    api mine me map { _.map(_.light) }
 }

app/controllers/Tournament.scala
@@ -18,6 +18,8 @@ object Tournament extends LidraughtsController {
   private def env = Env.tournament
   private def repo = TournamentRepo
 
+  import Team.teamsIBelongTo
+
   private def tournamentNotFound(implicit ctx: Context) = NotFound(html.tournament.bits.notFound())
 
   private[controllers] val upcomingCache = Env.memo.asyncCache.single[(VisibleTournaments, List[Tour])](
@@ -163,8 +165,18 @@ object Tournament extends LidraughtsController {
 
   def form = Auth { implicit ctx => me =>
     NoLameOrBot {
-      teamsIBelongTo(me) flatMap { teams =>
-        Ok(html.tournament.form.create(env.forms.create(me), env.forms, me, teams)).fuccess
+      teamsIBelongTo(me) map { teams =>
+        Ok(html.tournament.form.create(env.forms.create(me), env.forms, me, teams))
+      }
+    }
+  }
+
+  def formTeamBattle(teamId: String) = Auth { implicit ctx => me =>
+    NoLameOrBot {
+      Env.team.api.owns(teamId, me.id) map {
+        _ ?? {
+          Ok(html.tournament.form.create(env.forms.create(me, teamId.some), env.forms, me, Nil))
+        }
       }
     }
   }
@@ -202,9 +214,15 @@ object Tournament extends LidraughtsController {
                 setup.password.isDefined) 1 else 2
               CreateLimitPerUser(me.id, cost) {
                 CreateLimitPerIP(HTTPRequest lastRemoteAddress ctx.req, cost = 1) {
-                  env.api.createTournament(setup, me, teams, getUserTeamIds) map { tour =>
-                    Redirect(routes.Tournament.show(tour.id))
-                  }
+                  env.api.createTournament(
+                    setup,
+                    me,
+                    teams,
+                    getUserTeamIds,
+                    Env.team.api.filterExistingIds
+                  ) map { tour =>
+                      Redirect(routes.Tournament.show(tour.id))
+                    }
                 }(rateLimited)
               }(rateLimited)
             }
@@ -220,10 +238,10 @@ object Tournament extends LidraughtsController {
     else teamsIBelongTo(me) flatMap { teams => doApiCreate(me, teams) }
   }
 
-  private def doApiCreate(me: lidraughts.user.User, teams: TeamIdsWithNames)(implicit req: Request[_]): Fu[Result] =
+  private def doApiCreate(me: lidraughts.user.User, teams: List[lidraughts.hub.lightTeam.LightTeam])(implicit req: Request[_]): Fu[Result] =
     env.forms.create(me).bindFromRequest.fold(
       jsonFormErrorDefaultLang,
-      setup => env.api.createTournament(setup, me, teams, getUserTeamIds) flatMap { tour =>
+      setup => env.api.createTournament(setup, me, teams, getUserTeamIds, Env.team.api.filterExistingIds) flatMap { tour =>
         Env.tournament.jsonView(tour, none, none, getUserTeamIds, none, none, partial = false, lidraughts.i18n.defaultLang, none)
       } map { Ok(_) }
     )
@@ -323,8 +341,6 @@ object Tournament extends LidraughtsController {
     expireAfter = _.ExpireAfterWrite(15.seconds)
   )
 
-  private def getUserTeamIds(user: lidraughts.user.User): Fu[TeamIdList] =
+  private def getUserTeamIds(user: lidraughts.user.User): Fu[List[TeamId]] =
     Env.team.cached.teamIdsList(user.id)
-  private def teamsIBelongTo(me: lidraughts.user.User): Fu[TeamIdsWithNames] =
-    Env.team.api.mine(me) map { _.map(t => t._id -> t.name) }
 }

app/views/tournament/form.scala
@@ -7,14 +7,14 @@ import draughts.variant.{ Variant, Standard, Russian }
 import lidraughts.api.Context
 import lidraughts.app.templating.Environment._
 import lidraughts.app.ui.ScalatagsTemplate._
-import lidraughts.user.User
 import lidraughts.tournament.{ Condition, DataForm, Tournament }
+import lidraughts.user.User
 
 import controllers.routes
 
 object form {
 
-  def create(form: Form[_], config: DataForm, me: User, teams: lidraughts.hub.lightTeam.TeamIdsWithNames)(implicit ctx: Context) = views.html.base.layout(
+  def create(form: Form[_], config: DataForm, me: User, teams: List[lidraughts.hub.lightTeam.LightTeam])(implicit ctx: Context) = views.html.base.layout(
     title = trans.newTournament.txt(),
     moreCss = cssTag("tournament.form"),
     moreJs = frag(
@@ -47,6 +47,7 @@ object form {
                 fields.startDate()
               )
             ),
+            form3.hidden(form("teamBattle")("teams")),
             form3.actions(
               a(href := routes.Tournament.home())(trans.cancel()),
               form3.submit(trans.createANewTournament(), icon = "g".some)
@@ -57,7 +58,7 @@ object form {
       )
     }
 
-  def edit(tour: Tournament, form: Form[_], config: DataForm, me: User, teams: lidraughts.hub.lightTeam.TeamIdsWithNames)(implicit ctx: Context) = views.html.base.layout(
+  def edit(tour: Tournament, form: Form[_], config: DataForm, me: User, teams: List[lidraughts.hub.lightTeam.LightTeam])(implicit ctx: Context) = views.html.base.layout(
     title = tour.fullName,
     moreCss = cssTag("tournament.form"),
     moreJs = frag(
@@ -90,6 +91,7 @@ object form {
                 fields.berserkableHack
               )
             ),
+            form3.hidden(form("teamBattle")("teams")),
             form3.actions(
               a(href := routes.Tournament.show(tour.id))(trans.cancel()),
               form3.submit(trans.save(), icon = "g".some)
@@ -108,7 +110,7 @@ object form {
     if (auto) form3.hidden(field) else visible(field)
   )
 
-  def condition(form: Form[_], auto: Boolean, teams: lidraughts.hub.lightTeam.TeamIdsWithNames)(implicit ctx: Context) = frag(
+  def condition(form: Form[_], auto: Boolean, teams: List[lidraughts.hub.lightTeam.LightTeam])(implicit ctx: Context) = frag(
     form3.split(
       form3.group(form("conditions.nbRatedGame.nb"), raw("Minimum rated games"), half = true)(form3.select(_, Condition.DataForm.nbRatedGameChoices)),
       autoField(auto, form("conditions.nbRatedGame.perf")) { field =>
@@ -134,7 +136,9 @@ object form {
       form3.checkbox(form("berserkable"), raw("Allow Berserk"), help = raw("Let players halve their clock time to gain an extra point").some, half = true)
     ),
     (auto && teams.size > 0) ?? {
-      form3.group(form("conditions.teamMember.teamId"), trans.onlyMembersOfTeam(), half = false)(form3.select(_, List(("", trans.noRestriction.txt())) ::: teams))
+      form3.group(form("conditions.teamMember.teamId"), trans.onlyMembersOfTeam(), half = false)(
+        form3.select(_, List(("", trans.noRestriction.txt())) ::: teams.map(_.pair))
+      )
     }
   )
 
@@ -208,9 +212,10 @@ final private class TourFields(me: User, form: Form[_])(implicit ctx: Context) {
       form3.textarea(_)(rows := 3)
     )
   def password =
-    form3.group(form("password"), trans.password(), help = raw("Make the tournament private, and restrict access with a password").some)(
-      form3.input(_)
-    )
+    form("teamBattle")("teams").value.isEmpty option
+      form3.group(form("password"), trans.password(), help = raw("Make the tournament private, and restrict access with a password").some)(
+        form3.input(_)
+      )
   def berserkableHack =
     input(tpe := "hidden", st.name := form("berserkable").name, value := "false") // hack allow disabling berserk
   def startDate(withHelp: Boolean = true) =

modules/team/src/main/Team.scala
@@ -24,6 +24,8 @@ case class Team(
   def disabled = !enabled
 
   def isCreator(user: String) = user == createdBy
+
+  def light = lidraughts.hub.lightTeam.LightTeam(_id, name)
 }
 
 object Team {