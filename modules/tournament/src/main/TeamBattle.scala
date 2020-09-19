lichess-org/lila

team battle WIP

modules/tournament/src/main/TeamBattle.scala
@@ -4,6 +4,7 @@ import play.api.data._
 import play.api.data.Forms._
 
 import lila.hub.lightTeam._
+import lila.user.User
 
 case class TeamBattle(
     teams: Set[TeamId]
@@ -13,7 +14,16 @@ case class TeamBattle(
 
 object TeamBattle {
 
-  case class RankedTeamId(rank: Int, teamId: TeamId, magicScore: Int) {
+  case class RankedTeam(
+      rank: Int,
+      teamId: TeamId,
+      topPlayers: List[TopPlayer]
+  ) {
+    def magicScore = topPlayers.foldLeft(0)(_ + _.magicScore)
+    def score = topPlayers.foldLeft(0)(_ + _.score)
+  }
+
+  case class TopPlayer(userId: User.ID, magicScore: Int) {
     def score: Int = magicScore / 10000
   }
 
@@ -27,6 +37,7 @@ object TeamBattle {
       "teams" -> nonEmptyText
     )(Setup.apply)(Setup.unapply)
       .verifying("We need at least 2 teams", s => s.potentialTeamIds.size > 1)
+      .verifying("In this version of team battles, no more than 10 teams can be allowed.", s => s.potentialTeamIds.size <= 10)
 
     case class Setup(
         teams: String

ui/tournament/src/view/finished.ts
@@ -4,17 +4,18 @@ import TournamentController from '../ctrl';
 import { TournamentData, MaybeVNodes } from '../interfaces';
 import * as pagination from '../pagination';
 import { controls, standing, podium } from './arena';
+import { teamStanding } from './battle';
 import header from './header';
 import playerInfo from './playerInfo';
 import { numberRow } from './util';
 
 function confetti(data: TournamentData): VNode | undefined {
   if (data.me && data.isRecentlyFinished && window.lichess.once('tournament.end.canvas.' + data.id))
-  return h('canvas#confetti', {
-    hook: {
-      insert: _ => window.lichess.loadScript('javascripts/confetti.js')
-    }
-  });
+    return h('canvas#confetti', {
+      hook: {
+        insert: _ => window.lichess.loadScript('javascripts/confetti.js')
+      }
+    });
 }
 
 function stats(st, noarg): VNode {
@@ -37,7 +38,7 @@ export const name = 'finished';
 export function main(ctrl: TournamentController): MaybeVNodes {
   const pag = pagination.players(ctrl);
   return [
-    h('div.big_top', [
+    teamStanding(ctrl, 'finished') || h('div.big_top', [
       confetti(ctrl.data),
       header(ctrl),
       podium(ctrl)

notzmv/lidraughts

modules/tournament/src/main/TeamBattle.scala
@@ -4,6 +4,7 @@ import play.api.data._
 import play.api.data.Forms._
 
 import lidraughts.hub.lightTeam._
+import lidraughts.user.User
 
 case class TeamBattle(
     teams: Set[TeamId]
@@ -13,7 +14,16 @@ case class TeamBattle(
 
 object TeamBattle {
 
-  case class RankedTeamId(rank: Int, teamId: TeamId, magicScore: Int) {
+  case class RankedTeam(
+      rank: Int,
+      teamId: TeamId,
+      topPlayers: List[TopPlayer]
+  ) {
+    def magicScore = topPlayers.foldLeft(0)(_ + _.magicScore)
+    def score = topPlayers.foldLeft(0)(_ + _.score)
+  }
+
+  case class TopPlayer(userId: User.ID, magicScore: Int) {
     def score: Int = magicScore / 10000
   }
 
@@ -27,6 +37,7 @@ object TeamBattle {
       "teams" -> nonEmptyText
     )(Setup.apply)(Setup.unapply)
       .verifying("We need at least 2 teams", s => s.potentialTeamIds.size > 1)
+      .verifying("In this version of team battles, no more than 10 teams can be allowed.", s => s.potentialTeamIds.size <= 10)
 
     case class Setup(
         teams: String

ui/tournament/src/view/finished.ts
@@ -4,17 +4,18 @@ import TournamentController from '../ctrl';
 import { TournamentData, MaybeVNodes } from '../interfaces';
 import * as pagination from '../pagination';
 import { controls, standing, podium } from './arena';
+import { teamStanding } from './battle';
 import header from './header';
 import playerInfo from './playerInfo';
 import { numberRow } from './util';
 
 function confetti(data: TournamentData): VNode | undefined {
   if (data.me && data.isRecentlyFinished && window.lidraughts.once('tournament.end.canvas.' + data.id))
-  return h('canvas#confetti', {
-    hook: {
-      insert: _ => window.lidraughts.loadScript('javascripts/confetti.js')
-    }
-  });
+    return h('canvas#confetti', {
+      hook: {
+        insert: _ => window.lidraughts.loadScript('javascripts/confetti.js')
+      }
+    });
 }
 
 function stats(st, noarg): VNode {
@@ -37,7 +38,7 @@ export const name = 'finished';
 export function main(ctrl: TournamentController): MaybeVNodes {
   const pag = pagination.players(ctrl);
   return [
-    h('div.big_top', [
+    teamStanding(ctrl, 'finished') || h('div.big_top', [
       confetti(ctrl.data),
       header(ctrl),
       podium(ctrl)