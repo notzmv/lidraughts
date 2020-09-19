lichess-org/lila

team battle WIP

modules/team/src/main/BSONHandlers.scala
@@ -1,9 +1,12 @@
 package lila.team
 
+import lila.hub.lightTeam.LightTeam
+
 private object BSONHandlers {
 
   import lila.db.dsl.BSONJodaDateTimeHandler
   implicit val TeamBSONHandler = reactivemongo.bson.Macros.handler[Team]
   implicit val RequestBSONHandler = reactivemongo.bson.Macros.handler[Request]
   implicit val MemberBSONHandler = reactivemongo.bson.Macros.handler[Member]
+  implicit val LightTeamBSONHandler = reactivemongo.bson.Macros.handler[LightTeam]
 }

notzmv/lidraughts

modules/team/src/main/BSONHandlers.scala
@@ -1,9 +1,12 @@
 package lidraughts.team
 
+import lidraughts.hub.lightTeam.LightTeam
+
 private object BSONHandlers {
 
   import lidraughts.db.dsl.BSONJodaDateTimeHandler
   implicit val TeamBSONHandler = reactivemongo.bson.Macros.handler[Team]
   implicit val RequestBSONHandler = reactivemongo.bson.Macros.handler[Request]
   implicit val MemberBSONHandler = reactivemongo.bson.Macros.handler[Member]
+  implicit val LightTeamBSONHandler = reactivemongo.bson.Macros.handler[LightTeam]
 }