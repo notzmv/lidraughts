lichess-org/lila

handle OPTIONS requests

app/controllers/Main.scala
@@ -5,8 +5,8 @@ import play.api.data._, Forms._
 import play.api.libs.json._
 import play.api.mvc._
 
-import lila.app._
 import lila.api.Context
+import lila.app._
 import lila.common.HTTPRequest
 import lila.hub.actorApi.captcha.ValidCaptcha
 import makeTimeout.large

notzmv/lidraughts

app/controllers/Main.scala
@@ -5,8 +5,8 @@ import play.api.data._, Forms._
 import play.api.libs.json._
 import play.api.mvc._
 
-import lidraughts.app._
 import lidraughts.api.Context
+import lidraughts.app._
 import lidraughts.common.HTTPRequest
 import lidraughts.hub.actorApi.captcha.ValidCaptcha
 import makeTimeout.large