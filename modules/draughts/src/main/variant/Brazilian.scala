55ce27469bdaa6bbae6c4184f693af47a5680357
Stefan Schermann <stefan.schermann@gmail.com>
2020-10-02 22:08:39 +0200

    fix movegen

--- modules/draughts/src/main/variant/Brazilian.scala
+++ modules/draughts/src/main/variant/Brazilian.scala
@@ -20,10 +20,43 @@ case object Brazilian extends Variant(
   def moveDirsColor = Standard.moveDirsColor
   def moveDirsAll = Standard.moveDirsAll
 
-  override def finalizeBoard(board: Board, uci: format.Uci.Move, captured: Option[List[Piece]], situationBefore: Situation, finalSquare: Boolean): Board = Russian.finalizeBoard(board, uci, captured, situationBefore, finalSquare)
+  override def finalizeBoard(board: Board, uci: format.Uci.Move, captured: Option[List[Piece]], situationBefore: Situation, finalSquare: Boolean): Board = {
+    val remainingCaptures = finalSquare.fold(0, situationBefore.captureLengthFrom(uci.orig).getOrElse(0) - 1)
+    if (remainingCaptures > 0) board
+    else {
+      val whiteActors = board.actorsOf(Color.White)
+      val blackActors = board.actorsOf(Color.Black)
+      val whiteKings = whiteActors.count(_.piece is King)
+      val blackKings = blackActors.count(_.piece is King)
+      val whitePieces = whiteActors.size
+      val blackPieces = blackActors.size
+      def loneKing(strongPieces: Int, strongKings: Int, weakKing: Actor) =
+        strongPieces == 3 && strongKings >= 1 && weakKing.onLongDiagonal
+      val whiteLoneKing =
+        if (whiteKings == 1 && whitePieces == 1 && blackKings >= 1) {
+          loneKing(blackPieces, blackKings, whiteActors.head)
+        } else false
+      val blackLoneKing =
+        if (blackKings == 1 && blackPieces == 1 && whiteKings >= 1) {
+          loneKing(whitePieces, whiteKings, blackActors.head)
+        } else false
+      if (whiteLoneKing || blackLoneKing) {
+        board updateHistory { h =>
+          // "abuse" kingmove counter to count the amount of moves made on the long
+          // diagonal by the side with a lone king against 3 (see 7.2.7)
+          h.withKingMove(Color(whiteLoneKing), None, true)
+        } withoutGhosts
+      } else board.withoutGhosts
+    }
+  }
 
   def maxDrawingMoves(board: Board): Option[Int] = Russian.maxDrawingMoves(board)
   def updatePositionHashes(board: Board, move: Move, hash: draughts.PositionHash): PositionHash = Russian.updatePositionHashes(board, move, hash)
 
-  override def validSide(board: Board, strict: Boolean)(color: Color) = Russian.validSide(board, strict)(color)
+  override def validSide(board: Board, strict: Boolean)(color: Color) = {
+    val roles = board rolesOf color
+    (roles.count(_ == Man) > 0 || roles.count(_ == King) > 0) &&
+      (!strict || roles.size <= 12) &&
+      (!menOnPromotionRank(board, color) || board.ghosts != 0)
+  }
 }
