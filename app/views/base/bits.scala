f285c07fd9af6bcdf408eab16e5a1ae99420c3b8
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-10-21 17:54:00 +0200

    fix hidden mselect and tn-tg checkbox state after browser back (fixes #6760)
    https://stackoverflow.com/a/55927735/722291

--- app/views/base/bits.scala
+++ app/views/base/bits.scala
@@ -6,12 +6,19 @@ import lidraughts.app.ui.ScalatagsTemplate._
 
 object bits {
 
-  def mselect(id: String, current: Frag, items: List[Frag]) = div(cls := "mselect")(
-    input(tpe := "checkbox", cls := "mselect__toggle fullscreen-toggle", st.id := s"mselect-$id", aria.label := "Other variants"),
-    label(`for` := s"mselect-$id", cls := "mselect__label")(current),
-    label(`for` := s"mselect-$id", cls := "fullscreen-mask"),
-    st.nav(cls := "mselect__list")(items)
-  )
+  def mselect(id: String, current: Frag, items: List[Frag]) =
+    div(cls := "mselect")(
+      input(
+        tpe := "checkbox",
+        cls := "mselect__toggle fullscreen-toggle",
+        st.id := s"mselect-$id",
+        autocomplete := "off",
+        aria.label := "Other variants"
+      ),
+      label(`for` := s"mselect-$id", cls := "mselect__label")(current),
+      label(`for` := s"mselect-$id", cls := "fullscreen-mask"),
+      st.nav(cls := "mselect__list")(items)
+    )
 
   lazy val stage = a(
     href := "https://lidraughts.org",
