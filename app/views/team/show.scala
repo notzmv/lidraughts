lichess-org/lila

redesign the team page

app/views/team/show.scala
@@ -17,7 +17,7 @@ object show {
 
   def apply(
       t: Team,
-      members: Paginator[lila.team.MemberWithUser],
+      members: Paginator[lila.common.LightUser],
       info: lila.app.mashup.TeamInfo,
       chatOption: Option[lila.chat.UserChat.Mine],
       socketVersion: Option[lila.socket.Socket.SocketVersion]
@@ -54,69 +54,69 @@ object show {
           )}""")
         )
     )(
-      main(cls := "page-menu")(
-        bits.menu(none),
-        div(cls := "team-show page-menu__content box team-show", socketVersion.map { v =>
-          data("socket-version") := v.value
-        })(
-          div(cls := "box__top")(
-            h1(cls := "text", dataIcon := "f")(t.name, " ", em(trans.team.team.txt().toUpperCase)),
-            div(
-              if (t.disabled) span(cls := "staff")("CLOSED")
-              else nbMembers.plural(t.nbMembers, strong(t.nbMembers.localize))
-            )
-          ),
-          (info.mine || t.enabled) option div(cls := "team-show__content")(
+      main(cls := "team-show box", socketVersion.map { v =>
+        data("socket-version") := v.value
+      })(
+        div(cls := "box__top")(
+          h1(cls := "text", dataIcon := "f")(t.name, " ", em(trans.team.team.txt().toUpperCase)),
+          div(
+            if (t.disabled) span(cls := "staff")("CLOSED")
+            else nbMembers.plural(t.nbMembers, strong(t.nbMembers.localize))
+          )
+        ),
+        (info.mine || t.enabled) option div(cls := "team-show__content")(
+          div(cls := "team-show__content__col1")(
             st.section(cls := "team-show__meta")(
               p(teamLeaders(), ": ", fragList(t.leaders.toList.map { l =>
                 userIdLink(l.some)
-              })),
-              div(cls := "team-show__meta__actions")(
-                (t.enabled && !info.mine) option frag(
-                  if (info.requestedByMe) strong(beingReviewed())
-                  else ctx.isAuth option joinButton(t)
+              }))
+            ),
+            chatOption.isDefined option views.html.chat.frag,
+            div(cls := "team-show__actions")(
+              (t.enabled && !info.mine) option frag(
+                if (info.requestedByMe) strong(beingReviewed())
+                else ctx.isAuth option joinButton(t)
+              ),
+              (info.mine && !info.ledByMe) option
+                postForm(cls := "quit", action := routes.Team.quit(t.id))(
+                  submitButton(cls := "button button-empty button-red confirm")(quitTeam.txt())
                 ),
-                (info.mine && !info.ledByMe) option
-                  postForm(cls := "quit", action := routes.Team.quit(t.id))(
-                    submitButton(cls := "button button-empty button-red confirm")(quitTeam.txt())
-                  ),
-                (info.ledByMe || isGranted(_.Admin)) option
-                  a(href := routes.Team.edit(t.id), cls := "button button-empty text", dataIcon := "%")(
-                    trans.settings.settings()
-                  ),
-                info.ledByMe option frag(
-                  a(
-                    href := routes.Tournament.teamBattleForm(t.id),
-                    cls := "button button-empty text",
-                    dataIcon := "g"
-                  )(
-                    span(
-                      strong(teamBattle()),
-                      em(teamBattleOverview())
-                    )
-                  ),
-                  a(
-                    href := s"${routes.Tournament.form()}?team=${t.id}",
-                    cls := "button button-empty text",
-                    dataIcon := "g"
-                  )(
-                    span(
-                      strong(teamTournament()),
-                      em(teamTournamentOverview())
-                    )
-                  ),
-                  a(
-                    href := routes.Team.pmAll(t.id),
-                    cls := "button button-empty text",
-                    dataIcon := "e"
-                  )(
-                    span(
-                      strong(messageAllMembers()),
-                      em(messageAllMembersOverview())
-                    )
+              info.ledByMe option frag(
+                a(
+                  href := routes.Tournament.teamBattleForm(t.id),
+                  cls := "button button-empty text",
+                  dataIcon := "g"
+                )(
+                  span(
+                    strong(teamBattle()),
+                    em(teamBattleOverview())
+                  )
+                ),
+                a(
+                  href := s"${routes.Tournament.form()}?team=${t.id}",
+                  cls := "button button-empty text",
+                  dataIcon := "g"
+                )(
+                  span(
+                    strong(teamTournament()),
+                    em(teamTournamentOverview())
+                  )
+                ),
+                a(
+                  href := routes.Team.pmAll(t.id),
+                  cls := "button button-empty text",
+                  dataIcon := "e"
+                )(
+                  span(
+                    strong(messageAllMembers()),
+                    em(messageAllMembersOverview())
                   )
                 )
-              )
+              ),
+              (info.ledByMe || isGranted(_.Admin)) option
+                a(href := routes.Team.edit(t.id), cls := "button button-empty text", dataIcon := "%")(
+                  trans.settings.settings()
+                )
             ),
             div(cls := "team-show__members")(
               st.section(cls := "recent-members")(
@@ -124,11 +124,13 @@ object show {
                 div(cls := "userlist infinitescroll")(
                   pagerNext(members, np => routes.Team.show(t.id, np).url),
                   members.currentPageResults.map { member =>
-                    div(cls := "paginated")(userLink(member.user))
+                    div(cls := "paginated")(lightUserLink(member))
                   }
                 )
               )
-            ),
+            )
+          ),
+          div(cls := "team-show__content__col2")(
             st.section(cls := "team-show__desc")(
               standardFlash(),
               richText(t.description),
@@ -143,28 +145,23 @@ object show {
             div(cls := "team-show__tour-forum")(
               info.tournaments.nonEmpty option frag(
                 st.section(cls := "team-show__tour")(
-                  h2(dataIcon := "g", cls := "text")(trans.tournaments()),
+                  h2(trans.tournaments()),
                   info.tournaments.span(_.isCreated) match {
                     case (created, started) =>
-                      views.html.tournament.bits.forTeam(started ::: created.sortBy(_.startsAt))
+                      views.html.tournament.bits.forTeam(created.sortBy(_.startsAt) ::: started)
                   }
                 )
               ),
               ctx.noKid option
                 st.section(cls := "team-show__forum")(
-                  h2(dataIcon := "d", cls := "text")(
-                    a(href := teamForumUrl(t.id))(trans.forum()),
-                    " (",
-                    info.forumNbPosts,
-                    ")"
-                  ),
+                  h2(a(href := teamForumUrl(t.id))(trans.forum())),
                   info.forumPosts.take(10).map { post =>
-                    st.article(
-                      p(cls := "meta")(
-                        a(href := routes.ForumPost.redirect(post.postId))(post.topicName),
+                    a(cls := "team-show__forum__post", href := routes.ForumPost.redirect(post.postId))(
+                      div(cls := "meta")(
+                        strong(post.topicName),
                         em(
-                          userIdLink(post.userId, withOnline = false),
-                          " ",
+                          post.userId map usernameOrId,
+                          " • ",
                           momentFromNow(post.createdAt)
                         )
                       ),
@@ -173,8 +170,7 @@ object show {
                   },
                   a(cls := "more", href := teamForumUrl(t.id))(t.name, " ", trans.forum(), " »")
                 )
-            ),
-            chatOption.isDefined option views.html.chat.frag
+            )
           )
         )
       )

modules/team/src/main/PaginatorBuilder.scala
@@ -2,16 +2,17 @@ package lila.team
 
 import lila.common.config.MaxPerPage
 import lila.common.paginator._
+import lila.common.LightUser
 import lila.db.dsl._
 import lila.db.paginator._
 
 final private[team] class PaginatorBuilder(
     teamRepo: TeamRepo,
     memberRepo: MemberRepo,
-    userRepo: lila.user.UserRepo
+    lightUserApi: lila.user.LightUserApi
 )(implicit ec: scala.concurrent.ExecutionContext) {
   private val maxPerPage     = MaxPerPage(15)
-  private val maxUserPerPage = MaxPerPage(24)
+  private val maxUserPerPage = MaxPerPage(30)
 
   import BSONHandlers._
 
@@ -26,28 +27,26 @@ final private[team] class PaginatorBuilder(
     maxPerPage
   )
 
-  def teamMembers(team: Team, page: Int): Fu[Paginator[MemberWithUser]] = Paginator(
+  def teamMembers(team: Team, page: Int): Fu[Paginator[LightUser]] = Paginator(
     adapter = new TeamAdapter(team),
     page,
     maxUserPerPage
   )
 
-  final private class TeamAdapter(team: Team) extends AdapterLike[MemberWithUser] {
+  final private class TeamAdapter(team: Team) extends AdapterLike[LightUser] {
 
     val nbResults = fuccess(team.nbMembers)
 
-    def slice(offset: Int, length: Int): Fu[Seq[MemberWithUser]] =
+    def slice(offset: Int, length: Int): Fu[Seq[LightUser]] =
       for {
-        members <- memberRepo.coll.ext
-          .find(selector)
+        docs <- memberRepo.coll.ext
+          .find(selector, $doc("user" -> true, "_id" -> false))
           .sort(sorting)
           .skip(offset)
-          .cursor[Member]()
-          .gather[List](length)
-        users <- userRepo usersFromSecondary members.map(_.user)
-      } yield members zip users map {
-        case (member, user) => MemberWithUser(member, user)
-      }
+          .list[Bdoc](length)
+        userIds = docs.flatMap(_ string "user")
+        users <- lightUserApi asyncMany userIds
+      } yield users.flatten
     private def selector = memberRepo teamQuery team.id
     private def sorting  = $sort desc "date"
   }

notzmv/lidraughts

app/views/team/show.scala
@@ -15,7 +15,7 @@ object show {
 
   def apply(
     t: Team,
-    members: Paginator[lidraughts.team.MemberWithUser],
+    members: Paginator[lidraughts.common.LightUser],
     info: lidraughts.app.mashup.TeamInfo,
     chatOption: Option[lidraughts.chat.UserChat.Mine],
     socketVersion: Option[lidraughts.socket.Socket.SocketVersion]
@@ -50,45 +50,23 @@ object show {
           }""")
         )
     )(
-        main(cls := "page-menu")(
-          bits.menu(none),
-          div(cls := "team-show page-menu__content box team-show", socketVersion.map { v =>
-            data("socket-version") := v.value
-          })(
-            div(cls := "box__top")(
-              h1(cls := "text", dataIcon := "f")(t.name, " ", em("TEAM")),
-              div(
-                if (t.disabled) span(cls := "staff")("CLOSED")
-                else trans.nbMembers.plural(t.nbMembers, strong(t.nbMembers.localize))
-              )
-            ),
-            (info.mine || t.enabled) option div(cls := "team-show__content")(
+        main(cls := "team-show box", socketVersion.map { v =>
+          data("socket-version") := v.value
+        })(
+          div(cls := "box__top")(
+            h1(cls := "text", dataIcon := "f")(t.name, " ", em("TEAM")),
+            div(
+              if (t.disabled) span(cls := "staff")("CLOSED")
+              else trans.nbMembers.plural(t.nbMembers, strong(t.nbMembers.localize))
+            )
+          ),
+          (info.mine || t.enabled) option div(cls := "team-show__content")(
+            div(cls := "team-show__content__col1")(
               st.section(cls := "team-show__meta")(
                 p(trans.teamLeader(), ": ", userIdLink(t.createdBy.some))
               ),
-
-              div(cls := "team-show__members")(
-                st.section(cls := "recent-members")(
-                  h2(trans.teamRecentMembers()),
-                  div(cls := "userlist infinitescroll")(
-                    pagerNext(members, np => routes.Team.show(t.id, np).url),
-                    members.currentPageResults.map { member =>
-                      div(cls := "paginated")(userLink(member.user))
-                    }
-                  )
-                )
-              ),
-              st.section(cls := "team-show__desc")(
-                richText(t.description),
-                t.location.map { loc =>
-                  frag(br, trans.location(), ": ", richText(loc))
-                },
-                info.hasRequests option div(cls := "requests")(
-                  h2(info.requests.size, " join requests"),
-                  views.html.team.request.list(info.requests, t.some)
-                )
-              ),
-              st.section(cls := "team-show__actions")(
+              chatOption.isDefined option views.html.chat.frag,
+              div(cls := "team-show__actions")(
                 (t.enabled && !info.mine) option frag(
                   if (info.requestedByMe) strong("Your join request is being reviewed by the team leader")
                   else ctx.me.??(_.canTeam) option
@@ -100,8 +78,6 @@ object show {
                   postForm(cls := "quit", action := routes.Team.quit(t.id))(
                     submitButton(cls := "button button-empty button-red confirm")(trans.quitTeam.txt())
                   ),
-                (info.createdByMe || isGranted(_.Admin)) option
-                  a(href := routes.Team.edit(t.id), cls := "button button-empty text", dataIcon := "%")(trans.settings()),
                 info.createdByMe option frag(
                   a(href := routes.Tournament.teamBattleForm(t.id), cls := "button button-empty text", dataIcon := "g")(
                     span(
@@ -115,34 +91,53 @@ object show {
                       em("An arena tournament that only members of your team can join")
                     )
                   )
+                ),
+                (info.createdByMe || isGranted(_.Admin)) option
+                  a(href := routes.Team.edit(t.id), cls := "button button-empty text", dataIcon := "%")(trans.settings())
+              ),
+              div(cls := "team-show__members")(
+                st.section(cls := "recent-members")(
+                  h2(trans.teamRecentMembers()),
+                  div(cls := "userlist infinitescroll")(
+                    pagerNext(members, np => routes.Team.show(t.id, np).url),
+                    members.currentPageResults.map { member =>
+                      div(cls := "paginated")(lightUserLink(member))
+                    }
+                  )
+                )
+              )
+            ),
+            div(cls := "team-show__content__col2")(
+              st.section(cls := "team-show__desc")(
+                richText(t.description),
+                t.location.map { loc =>
+                  frag(br, trans.location(), ": ", richText(loc))
+                },
+                info.hasRequests option div(cls := "requests")(
+                  h2(info.requests.size, " join requests"),
+                  views.html.team.request.list(info.requests, t.some)
                 )
               ),
               div(cls := "team-show__tour-forum")(
                 info.tournaments.nonEmpty option frag(
                   st.section(cls := "team-show__tour")(
-                    h2(dataIcon := "g", cls := "text")(trans.tournaments()),
+                    h2(trans.tournaments()),
                     info.tournaments.span(_.isCreated) match {
                       case (created, started) =>
-                        frag(
-                          views.html.tournament.bits.forTeam(created.sortBy(_.startsAt)),
-                          views.html.tournament.bits.forTeam(started)
-                        )
+                        views.html.tournament.bits.forTeam(created.sortBy(_.startsAt) ::: started)
                     }
                   )
                 ),
                 NotForKids {
                   st.section(cls := "team-show__forum")(
-                    h2(dataIcon := "d", cls := "text")(
-                      a(href := teamForumUrl(t.id))(trans.forum()),
-                      " (", info.forumNbPosts, ")"
-                    ),
+                    h2(a(href := teamForumUrl(t.id))(trans.forum())),
                     info.forumPosts.take(10).map { post =>
-                      st.article(
-                        p(cls := "meta")(
-                          a(href := routes.ForumPost.redirect(post.postId))(post.topicName),
+                      a(cls := "team-show__forum__post", href := routes.ForumPost.redirect(post.postId))(
+                        div(cls := "meta")(
+                          strong(post.topicName),
                           em(
-                            userIdLink(post.userId, withOnline = false),
-                            " ",
+                            post.userId map usernameOrId,
+                            " • ",
                             momentFromNow(post.createdAt)
                           )
                         ),
@@ -151,8 +146,7 @@ object show {
                     },
                     a(cls := "more", href := teamForumUrl(t.id))(t.name, " ", trans.forum(), " »")
                   )
-                },
-                chatOption.isDefined option views.html.chat.frag
+                }
               )
             )
           )

modules/team/src/main/PaginatorBuilder.scala
@@ -2,6 +2,7 @@ package lidraughts.team
 
 import lidraughts.common.paginator._
 import lidraughts.common.MaxPerPage
+import lidraughts.common.LightUser
 import lidraughts.db.dsl._
 import lidraughts.db.paginator._
 import lidraughts.user.UserRepo
@@ -9,7 +10,8 @@ import lidraughts.user.UserRepo
 private[team] final class PaginatorBuilder(
     coll: Colls,
     maxPerPage: MaxPerPage,
-    maxUserPerPage: MaxPerPage
+    maxUserPerPage: MaxPerPage,
+    lightUserApi: lidraughts.user.LightUserApi
 ) {
 
   import BSONHandlers._
@@ -25,23 +27,26 @@ private[team] final class PaginatorBuilder(
     maxPerPage
   )
 
-  def teamMembers(team: Team, page: Int): Fu[Paginator[MemberWithUser]] = Paginator(
+  def teamMembers(team: Team, page: Int): Fu[Paginator[LightUser]] = Paginator(
     adapter = new TeamAdapter(team),
     page,
     maxUserPerPage
   )
 
-  private final class TeamAdapter(team: Team) extends AdapterLike[MemberWithUser] {
+  private final class TeamAdapter(team: Team) extends AdapterLike[LightUser] {
 
     val nbResults = fuccess(team.nbMembers)
 
-    def slice(offset: Int, length: Int): Fu[Seq[MemberWithUser]] = for {
-      members ← coll.member.find(selector)
-        .sort(sorting).skip(offset).cursor[Member]().gather[List](length)
-      users ← UserRepo usersFromSecondary members.map(_.user)
-    } yield members zip users map {
-      case (member, user) => MemberWithUser(member, user)
-    }
+    def slice(offset: Int, length: Int): Fu[Seq[LightUser]] =
+      for {
+        docs <- coll.member
+          .find(selector, $doc("user" -> true, "_id" -> false))
+          .sort(sorting)
+          .skip(offset)
+          .list[Bdoc](length)
+        userIds = docs.flatMap(_.getAs[String]("user"))
+        users <- lightUserApi asyncMany userIds
+      } yield users.flatten
     private def selector = MemberRepo teamQuery team.id
     private def sorting = $sort desc "date"
   }