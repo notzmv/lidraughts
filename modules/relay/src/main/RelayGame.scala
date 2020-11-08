lichess-org/lila

broadcast: never replace source games coming from lichess
only append

modules/relay/src/main/RelayGame.scala
@@ -21,9 +21,17 @@ case class RelayGame(
   def finished = end.isDefined
 
   def isEmpty = tags.value.isEmpty && root.children.nodes.isEmpty
+
+  lazy val looksLikeLichess = tags(_.Site) exists { site =>
+    RelayGame.lichessDomains exists { domain =>
+      site startsWith s"https://$domain/"
+    }
+  }
 }
 
 private object RelayGame {
+
+  val lichessDomains = List("lichess.org", "lichess.dev")
 
   val staticTags = List("white", "black", "round", "event", "site")
 }

modules/relay/src/main/RelaySync.scala
@@ -47,7 +47,7 @@ final private class RelaySync(
       chapters: List[Chapter],
       nbGames: Int
   ): Option[Chapter] =
-    if (nbGames == 1) chapters find game.staticTagsMatch
+    if (nbGames == 1 || game.looksLikeLichess) chapters find game.staticTagsMatch
     else chapters.find(_.relay.exists(_.index == game.index))
 
   private def updateChapter(study: Study, chapter: Chapter, game: RelayGame): Fu[NbMoves] =

notzmv/lidraughts

modules/relay/src/main/RelayGame.scala
@@ -21,9 +21,17 @@ case class RelayGame(
   def finished = end.isDefined
 
   def isEmpty = tags.value.isEmpty && root.children.nodes.isEmpty
+
+  lazy val looksLikeLidraughts = tags(_.Site) exists { site =>
+    RelayGame.lidraughtsDomains exists { domain =>
+      site startsWith s"https://$domain/"
+    }
+  }
 }
 
 private object RelayGame {
+
+  val lidraughtsDomains = List("lichess.org", "lichess.dev")
 
   val staticTags = List("white", "black", "round", "event", "site")
 }

modules/relay/src/main/RelaySync.scala
@@ -39,7 +39,7 @@ private final class RelaySync(
    * lidraughts will create a new chapter when the game player tags differ.
    */
   private def findCorrespondingChapter(game: RelayGame, chapters: List[Chapter], nbGames: Int): Option[Chapter] =
-    if (nbGames == 1) chapters find game.staticTagsMatch
+    if (nbGames == 1 || game.looksLikeLidraughts) chapters find game.staticTagsMatch
     else chapters.find(_.relay.exists(_.index == game.index))
 
   private def updateChapter(study: Study, chapter: Chapter, game: RelayGame): Fu[NbMoves] =