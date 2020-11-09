lichess-org/lila

swiss player search

app/controllers/Tournament.scala
@@ -153,8 +153,8 @@ final class Tournament(
       OptionFuResult(repo byId id) { tour =>
         api.pageOf(tour, UserModel normalize userId) flatMap {
           _ ?? { page =>
-            env.tournament.standingApi(tour, page) map { data =>
-              Ok(data) as JSON
+            JsonOk {
+              env.tournament.standingApi(tour, page)
             }
           }
         }

notzmv/lidraughts

app/controllers/Tournament.scala
@@ -149,8 +149,8 @@ object Tournament extends LidraughtsController {
     OptionFuResult(repo byId id) { tour =>
       env.api.pageOf(tour, UserModel normalize userId) flatMap {
         _ ?? { page =>
-          env.jsonView.standing(tour, page) map { data =>
-            Ok(data) as JSON
+          JsonOk {
+            env.jsonView.standing(tour, page)
           }
         }
       }