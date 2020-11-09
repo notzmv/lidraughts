lichess-org/lila

swiss monitoring & tweaks

modules/common/src/main/mon.scala
@@ -384,9 +384,10 @@ object mon {
     def standingOverload      = counter("swiss.standing.overload").withoutTags
     val tick                  = future("swiss.tick")
     val bbpairing             = timer("swiss.bbpairing").withoutTags
-    val tiebreakRecompute     = future("swiss.tiebreak.recompute")
+    val scoringRecompute      = future("swiss.scoring.recompute")
     val startRound            = future("swiss.director.startRound")
     def games(status: String) = histogram("swiss.ongoingGames").withTag("status", status)
+    val json                  = future("swiss.json")
   }
   object plan {
     val paypal  = histogram("plan.amount").withTag("service", "paypal")

modules/swiss/src/main/Env.scala
@@ -69,13 +69,13 @@ final class Env(
   }
 
   ResilientScheduler(
-    every = Every(2 seconds),
-    atMost = AtMost(15 seconds),
+    every = Every(1 seconds),
+    atMost = AtMost(20 seconds),
     initialDelay = 20 seconds
   ) { api.startPendingRounds }
 
   ResilientScheduler(
-    every = Every(20 seconds),
+    every = Every(10 seconds),
     atMost = AtMost(15 seconds),
     initialDelay = 20 seconds
   ) { api.checkOngoingGames }

notzmv/lidraughts

modules/common/src/main/mon.scala
@@ -464,9 +464,10 @@ object mon {
     def standingOverload = inc("swiss.standing.overload")
     val tick = rec("swiss.tick")
     val bbpairing = rec("swiss.bbpairing")
-    val tiebreakRecompute = rec("swiss.tiebreak.recompute")
+    val scoringRecompute = rec("swiss.scoring.recompute")
     val startRound = rec("swiss.director.startRound")
     def games(status: String) = rec(s"swiss.ongoingGames.$status")
+    val json = rec("swiss.json")
   }
   object plan {
     object amount {

modules/swiss/src/main/Env.scala
@@ -157,14 +157,14 @@ final class Env(
     }
 
   ResilientScheduler(
-    every = Every(2 seconds),
-    atMost = AtMost(15 seconds),
+    every = Every(1 second),
+    atMost = AtMost(20 seconds),
     initialDelay = 20 seconds,
     logger = logger branch "startPendingRounds"
   ) { api.startPendingRounds }(system)
 
   ResilientScheduler(
-    every = Every(20 seconds),
+    every = Every(10 seconds),
     atMost = AtMost(15 seconds),
     initialDelay = 20 seconds,
     logger = logger branch "checkOngoingGames"