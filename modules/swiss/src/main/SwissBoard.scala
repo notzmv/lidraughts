lichess-org/lila

swiss ongoing boards

modules/swiss/src/main/SwissBoard.scala
@@ -1,35 +1,46 @@
 package lila.swiss
 
 import scala.concurrent.duration._
-import play.api.libs.json._
 
 import lila.common.LightUser
 import lila.game.Game
 
 private case class SwissBoard(
     gameId: Game.ID,
-    p1: SwissBoard.Player,
-    p2: SwissBoard.Player
+    white: SwissBoard.Player,
+    black: SwissBoard.Player
 )
 
 private object SwissBoard {
   case class Player(user: LightUser, rank: Int, rating: Int)
+  case class WithGame(board: SwissBoard, game: Game)
 }
 
 final private class SwissBoardApi(
     colls: SwissColls,
     rankingApi: SwissRankingApi,
     cacheApi: lila.memo.CacheApi,
-    lightUserApi: lila.user.LightUserApi
+    lightUserApi: lila.user.LightUserApi,
+    gameProxyRepo: lila.round.GameProxyRepo
 )(implicit ec: scala.concurrent.ExecutionContext) {
 
-  private val displayBoards = 5
+  private val displayBoards = 6
 
   private val boardsCache = cacheApi.scaffeine
     .expireAfterWrite(60 minutes)
-    .build[Swiss.Id, JsArray]
+    .build[Swiss.Id, List[SwissBoard]]
 
-  def apply(id: Swiss.Id): JsArray = boardsCache getIfPresent id getOrElse Json.arr()
+  def apply(id: Swiss.Id): List[SwissBoard] = ~(boardsCache getIfPresent id)
+
+  def withGames(id: Swiss.Id): Fu[List[SwissBoard.WithGame]] =
+    apply(id)
+      .map { board =>
+        gameProxyRepo.game(board.gameId) map2 {
+          SwissBoard.WithGame(board, _)
+        }
+      }
+      .sequenceFu
+      .dmap(_.flatten)
 
   def update(data: SwissScoring.Result): Funit =
     data match {
@@ -38,91 +49,27 @@ final private class SwissBoardApi(
           boardsCache
             .put(
               swiss.id,
-              JsArray {
-                players.values.toList
-                  .filter(_.present)
-                  .sortBy(-_.score.value)
-                  .flatMap(player => pairings get player.number)
-                  .distinct
-                  .take(displayBoards)
-                  .flatMap { pairing =>
-                    for {
-                      p1 <- players get pairing.white
-                      p2 <- players get pairing.black
-                      u1 <- lightUserApi sync p1.userId
-                      u2 <- lightUserApi sync p2.userId
-                      r1 <- ranks get p1.number
-                      r2 <- ranks get p2.number
-                    } yield SwissJson.boardJson(
-                      SwissBoard(
-                        pairing.gameId,
-                        p1 = SwissBoard.Player(u1, r1, p1.rating),
-                        p2 = SwissBoard.Player(u2, r2, p2.rating)
-                      )
-                    )
-                  }
-              }
+              players.values.toList
+                .filter(_.present)
+                .sortBy(-_.score.value)
+                .flatMap(player => pairings get player.number filter (_.isOngoing))
+                .distinct
+                .take(displayBoards)
+                .flatMap { pairing =>
+                  for {
+                    p1 <- players get pairing.white
+                    p2 <- players get pairing.black
+                    u1 <- lightUserApi sync p1.userId
+                    u2 <- lightUserApi sync p2.userId
+                    r1 <- ranks get p1.number
+                    r2 <- ranks get p2.number
+                  } yield SwissBoard(
+                    pairing.gameId,
+                    white = SwissBoard.Player(u1, r1 + 1, p1.rating),
+                    black = SwissBoard.Player(u2, r2 + 1, p2.rating)
+                  )
+                }
             )
         }
     }
-
-  // def roundStart(swiss: Swiss) = cache.get(swiss.id)
-
-  // private def compute(id: Swiss.Id): Fu[List[SwissBoard]] =
-  //   colls.pairing
-  //     .aggregateList(maxDocs = maxBoards, readPreference = ReadPreference.primary) { implicit framework =>
-  //       import framework._
-  //       Match(
-  //         SwissPairing.fields { f =>
-  //           $doc(f.swissId -> id, f.status -> SwissPairing.ongoing)
-  //         }
-  //       ) -> List(
-  //         PipelineOperator(
-  //           $doc(
-  //             "$lookup" -> $doc(
-  //               "from" -> colls.player.name,
-  //               "let"  -> $doc("p" -> "$p"),
-  //               "pipeline" -> $arr(
-  //                 $doc(
-  //                   "$match" -> $doc(
-  //                     "$expr" -> $doc(
-  //                       "$and" -> $arr(
-  //                         $doc("$eq" -> $arr("$s", id))
-  //                           $doc ("$in" -> $arr("$n", "$$p"))
-  //                       )
-  //                     )
-  //                   )
-  //                 ),
-  //                 $doc("$project" -> SwissPlayer.fields { f =>
-  //                   $doc(f.id -> false, f.userId -> true, f.rating -> true)
-  //                 })
-  //               ),
-  //               "as" -> "players"
-  //             )
-  //           )
-  //         ),
-  //         Project(
-  //           $doc("_id" -> false, "g" -> true, "players" -> true, "rating" -> $doc("$average" -> "$players.r"))
-  //         ),
-  //         Sort(Descending("rating")),
-  //         Project($doc("rating" -> false)),
-  //         Limit(maxBoards)
-  //       )
-  //     }
-  //     .map(_.flatMap { doc =>
-  //       for {
-  //         gameId     <- doc.str("g")
-  //         playerDocs <- doc.getAsOpt[List[Bdoc]]("players")
-  //         players <-
-  //           playerDocs
-  //             .flatMap { doc =>
-  //               for {
-  //                 userId <- doc.str("u")
-  //                 rating <- doc.int("r")
-  //               } yield (userId, rating)
-  //             }
-  //             .some
-  //             .filter(_.size == 2)
-  //       } yield (g, players)
-  //     })
 }

modules/swiss/src/main/SwissJson.scala
@@ -38,6 +38,7 @@ final class SwissJson(
       page = reqPage orElse myInfo.map(_.page) getOrElse 1
       standing <- standingApi(swiss, page)
       podium   <- podiumJson(swiss)
+      boards   <- boardApi.withGames(swiss.id)
     } yield Json
       .obj(
         "id"        -> swiss.id.value,
@@ -61,7 +62,7 @@ final class SwissJson(
           (myInfo.isEmpty && swiss.isEnterable && isInTeam)
         },
         "standing" -> standing,
-        "boards"   -> boardApi(swiss.id)
+        "boards"   -> boards.map(boardJson)
       )
       .add("joinTeam" -> (!isInTeam).option(swiss.teamId))
       .add("socketVersion" -> socketVersion.map(_.value))
@@ -237,11 +238,13 @@ object SwissJson {
         "absent" -> i.player.absent
       )
 
-  private[swiss] def boardJson(board: SwissBoard) =
+  private[swiss] def boardJson(b: SwissBoard.WithGame) =
     Json.obj(
-      "id"    -> board.gameId,
-      "white" -> boardPlayerJson(board.p1),
-      "black" -> boardPlayerJson(board.p2)
+      "id"       -> b.game.id,
+      "fen"      -> (chess.format.Forsyth exportBoard b.game.board),
+      "lastMove" -> ~b.game.lastMoveKeys,
+      "white"    -> boardPlayerJson(b.board.white),
+      "black"    -> boardPlayerJson(b.board.black)
     )
 
   private def boardPlayerJson(player: SwissBoard.Player) =

ui/swiss/src/view/boards.ts
@@ -1,8 +1,48 @@
 import { h } from 'snabbdom'
 import { VNode } from 'snabbdom/vnode';
-import SwissCtrl from '../ctrl';
-import { , userName } from './util';
-import { MaybeVNodes, Player } from '../interfaces';
+import { player as renderPlayer } from './util';
+import { Board, BoardPlayer } from '../interfaces';
 
-export default function boards(ctrl: SwissCtrl) {
+export function many(boards: Board[]): VNode {
+  return h('div.swiss__boards.now-playing', boards.map(renderBoard));
+}
+
+export function top(boards: Board[]): VNode {
+  return h('div.swiss__board__top.swiss__table',
+    boards.slice(0, 1).map(renderBoard)
+  );
+}
+
+const renderBoard = (board: Board): VNode =>
+    h('div.swiss__board', [
+      boardPlayer(board.black),
+      miniBoard(board),
+      boardPlayer(board.white),
+    ])
+
+const boardPlayer = (player: BoardPlayer) =>
+  h('div.swiss__board__player', [
+    h('strong', '#' + player.rank),
+    renderPlayer(player, true, true)
+  ]);
+
+function miniBoard(board: Board) {
+  return h('a.mini-board.live.is2d.mini-board-' + board.id, {
+    key: board.id,
+    attrs: {
+      href: '/' + board.id,
+      'data-live': board.id,
+      'data-color': 'white',
+      'data-fen': board.fen,
+      'data-lastmove': board.lastMove
+    },
+    hook: {
+      insert(vnode) {
+        window.lichess.parseFen($(vnode.elm as HTMLElement));
+        window.lichess.pubsub.emit('content_loaded')
+      }
+    }
+  }, [
+    h('div.cg-wrap')
+  ]);
 }

notzmv/lidraughts

modules/swiss/src/main/SwissBoard.scala
@@ -2,36 +2,45 @@ package lidraughts.swiss
 
 import com.github.blemale.scaffeine.Scaffeine
 import scala.concurrent.duration._
-import play.api.libs.json._
 
 import lidraughts.common.LightUser
 import lidraughts.game.Game
-import lidraughts.db.dsl.Coll
 
 private case class SwissBoard(
     gameId: Game.ID,
-    p1: SwissBoard.Player,
-    p2: SwissBoard.Player
+    white: SwissBoard.Player,
+    black: SwissBoard.Player
 )
 
 private object SwissBoard {
   case class Player(user: LightUser, rank: Int, rating: Int)
+  case class WithGame(board: SwissBoard, game: Game)
 }
 
-final class SwissBoardApi(
-    swissColl: Coll,
+final private class SwissBoardApi(
     rankingApi: SwissRankingApi,
     asyncCache: lidraughts.memo.AsyncCache.Builder,
-    lightUserApi: lidraughts.user.LightUserApi
+    lightUserApi: lidraughts.user.LightUserApi,
+    proxyGame: Game.ID => Fu[Option[Game]]
 ) {
 
-  private val displayBoards = 5
+  private val displayBoards = 6
 
   private val boardsCache = Scaffeine()
     .expireAfterWrite(60 minutes)
-    .build[Swiss.Id, JsArray]
+    .build[Swiss.Id, List[SwissBoard]]
 
-  def apply(id: Swiss.Id): JsArray = boardsCache getIfPresent id getOrElse Json.arr()
+  def apply(id: Swiss.Id): List[SwissBoard] = ~(boardsCache getIfPresent id)
+
+  def withGames(id: Swiss.Id): Fu[List[SwissBoard.WithGame]] =
+    apply(id)
+      .map { board =>
+        proxyGame(board.gameId) map2 { g: Game =>
+          SwissBoard.WithGame(board, g)
+        }
+      }
+      .sequenceFu
+      .dmap(_.flatten)
 
   def update(data: SwissScoring.Result): Funit =
     data match {
@@ -40,91 +49,27 @@ final class SwissBoardApi(
           boardsCache
             .put(
               swiss.id,
-              JsArray {
-                players.values.toList
-                  .filter(_.present)
-                  .sortBy(-_.score.value)
-                  .flatMap(player => pairings get player.number)
-                  .distinct
-                  .take(displayBoards)
-                  .flatMap { pairing =>
-                    for {
-                      p1 <- players get pairing.white
-                      p2 <- players get pairing.black
-                      u1 <- lightUserApi sync p1.userId
-                      u2 <- lightUserApi sync p2.userId
-                      r1 <- ranks get p1.number
-                      r2 <- ranks get p2.number
-                    } yield SwissJson.boardJson(
-                      SwissBoard(
-                        pairing.gameId,
-                        p1 = SwissBoard.Player(u1, r1, p1.rating),
-                        p2 = SwissBoard.Player(u2, r2, p2.rating)
-                      )
-                    )
-                  }
-              }
+              players.values.toList
+                .filter(_.present)
+                .sortBy(-_.score.value)
+                .flatMap(player => pairings get player.number filter (_.isOngoing))
+                .distinct
+                .take(displayBoards)
+                .flatMap { pairing =>
+                  for {
+                    p1 <- players get pairing.white
+                    p2 <- players get pairing.black
+                    u1 <- lightUserApi sync p1.userId
+                    u2 <- lightUserApi sync p2.userId
+                    r1 <- ranks get p1.number
+                    r2 <- ranks get p2.number
+                  } yield SwissBoard(
+                    pairing.gameId,
+                    white = SwissBoard.Player(u1, r1 + 1, p1.rating),
+                    black = SwissBoard.Player(u2, r2 + 1, p2.rating)
+                  )
+                }
             )
         }
     }
-
-  // def roundStart(swiss: Swiss) = cache.get(swiss.id)
-
-  // private def compute(id: Swiss.Id): Fu[List[SwissBoard]] =
-  //   colls.pairing
-  //     .aggregateList(maxDocs = maxBoards, readPreference = ReadPreference.primary) { implicit framework =>
-  //       import framework._
-  //       Match(
-  //         SwissPairing.fields { f =>
-  //           $doc(f.swissId -> id, f.status -> SwissPairing.ongoing)
-  //         }
-  //       ) -> List(
-  //         PipelineOperator(
-  //           $doc(
-  //             "$lookup" -> $doc(
-  //               "from" -> colls.player.name,
-  //               "let"  -> $doc("p" -> "$p"),
-  //               "pipeline" -> $arr(
-  //                 $doc(
-  //                   "$match" -> $doc(
-  //                     "$expr" -> $doc(
-  //                       "$and" -> $arr(
-  //                         $doc("$eq" -> $arr("$s", id))
-  //                           $doc ("$in" -> $arr("$n", "$$p"))
-  //                       )
-  //                     )
-  //                   )
-  //                 ),
-  //                 $doc("$project" -> SwissPlayer.fields { f =>
-  //                   $doc(f.id -> false, f.userId -> true, f.rating -> true)
-  //                 })
-  //               ),
-  //               "as" -> "players"
-  //             )
-  //           )
-  //         ),
-  //         Project(
-  //           $doc("_id" -> false, "g" -> true, "players" -> true, "rating" -> $doc("$average" -> "$players.r"))
-  //         ),
-  //         Sort(Descending("rating")),
-  //         Project($doc("rating" -> false)),
-  //         Limit(maxBoards)
-  //       )
-  //     }
-  //     .map(_.flatMap { doc =>
-  //       for {
-  //         gameId     <- doc.str("g")
-  //         playerDocs <- doc.getAsOpt[List[Bdoc]]("players")
-  //         players <-
-  //           playerDocs
-  //             .flatMap { doc =>
-  //               for {
-  //                 userId <- doc.str("u")
-  //                 rating <- doc.int("r")
-  //               } yield (userId, rating)
-  //             }
-  //             .some
-  //             .filter(_.size == 2)
-  //       } yield (g, players)
-  //     })
 }

modules/swiss/src/main/SwissJson.scala
@@ -40,6 +40,7 @@ final class SwissJson(
       page = reqPage orElse myInfo.map(_.page) getOrElse 1
       standing <- standingApi(swiss, page)
       podium <- podiumJson(swiss)
+      boards <- boardApi.withGames(swiss.id)
     } yield Json
       .obj(
         "id" -> swiss.id.value,
@@ -63,7 +64,7 @@ final class SwissJson(
             (myInfo.isEmpty && swiss.isEnterable && isInTeam)
         },
         "standing" -> standing,
-        "boards" -> boardApi(swiss.id)
+        "boards" -> boards.map(boardJson)
       )
       .add("joinTeam" -> (!isInTeam).option(swiss.teamId))
       .add("socketVersion" -> socketVersion.map(_.value))
@@ -239,11 +240,13 @@ object SwissJson {
         "absent" -> i.player.absent
       )
 
-  private[swiss] def boardJson(board: SwissBoard) =
+  private[swiss] def boardJson(b: SwissBoard.WithGame) =
     Json.obj(
-      "id" -> board.gameId,
-      "white" -> boardPlayerJson(board.p1),
-      "black" -> boardPlayerJson(board.p2)
+      "id" -> b.game.id,
+      "fen" -> (draughts.format.Forsyth exportBoard b.game.board),
+      "lastMove" -> ~b.game.lastMoveKeys,
+      "white" -> boardPlayerJson(b.board.white),
+      "black" -> boardPlayerJson(b.board.black)
     )
 
   private def boardPlayerJson(player: SwissBoard.Player) =

ui/swiss/src/view/boards.ts
@@ -1,8 +1,48 @@
 import { h } from 'snabbdom'
 import { VNode } from 'snabbdom/vnode';
-import SwissCtrl from '../ctrl';
-import { , userName } from './util';
-import { MaybeVNodes, Player } from '../interfaces';
+import { player as renderPlayer } from './util';
+import { Board, BoardPlayer } from '../interfaces';
 
-export default function boards(ctrl: SwissCtrl) {
+export function many(boards: Board[]): VNode {
+  return h('div.swiss__boards.now-playing', boards.map(renderBoard));
+}
+
+export function top(boards: Board[]): VNode {
+  return h('div.swiss__board__top.swiss__table',
+    boards.slice(0, 1).map(renderBoard)
+  );
+}
+
+const renderBoard = (board: Board): VNode =>
+    h('div.swiss__board', [
+      boardPlayer(board.black),
+      miniBoard(board),
+      boardPlayer(board.white),
+    ])
+
+const boardPlayer = (player: BoardPlayer) =>
+  h('div.swiss__board__player', [
+    h('strong', '#' + player.rank),
+    renderPlayer(player, true, true)
+  ]);
+
+function miniBoard(board: Board) {
+  return h('a.mini-board.live.is2d.mini-board-' + board.id, {
+    key: board.id,
+    attrs: {
+      href: '/' + board.id,
+      'data-live': board.id,
+      'data-color': 'white',
+      'data-fen': board.fen,
+      'data-lastmove': board.lastMove
+    },
+    hook: {
+      insert(vnode) {
+        window.lidraughts.parseFen($(vnode.elm as HTMLElement));
+        window.lidraughts.pubsub.emit('content_loaded')
+      }
+    }
+  }, [
+    h('div.cg-wrap')
+  ]);
 }