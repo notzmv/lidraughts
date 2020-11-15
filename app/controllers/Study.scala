lichess-org/lila

allow 30 PGN exports per minute and IP - closes #6609

app/controllers/Study.scala
@@ -416,16 +416,16 @@ final class Study(
       }
     }
 
-  private val PgnRateLimitGlobal = new lila.memo.RateLimit[String](
+  private val PgnRateLimitPerIp = new lila.memo.RateLimit[IpAddress](
     credits = 30,
     duration = 1.minute,
-    name = "export study PGN global",
-    key = "export.study_pgn.global"
+    name = "export study PGN per IP",
+    key = "export.study_pgn.ip"
   )
 
   def pgn(id: String) =
     Open { implicit ctx =>
-      PgnRateLimitGlobal("-", msg = HTTPRequest.lastRemoteAddress(ctx.req).value) {
+      PgnRateLimitPerIp(HTTPRequest.lastRemoteAddress(ctx.req)) {
         OptionFuResult(env.study.api byId id) { study =>
           CanViewResult(study) {
             lila.mon.export.pgn.study.increment()

notzmv/lidraughts

app/controllers/Study.scala
@@ -359,16 +359,16 @@ object Study extends LidraughtsController {
     }
   }
 
-  private val PdnRateLimitGlobal = new lidraughts.memo.RateLimit[String](
+  private val PdnRateLimitPerIp = new lidraughts.memo.RateLimit[IpAddress](
     credits = 30,
     duration = 1 minute,
-    name = "export study PDN global",
-    key = "export.study_pdn.global"
+    name = "export study PDN per ip",
+    key = "export.study_pdn.ip"
   )
 
   def pdn(id: String) = Open { implicit ctx =>
     OnlyHumans {
-      PdnRateLimitGlobal("-", msg = HTTPRequest.lastRemoteAddress(ctx.req).value) {
+      PdnRateLimitPerIp(HTTPRequest.lastRemoteAddress(ctx.req)) {
         OptionFuResult(env.api byId id) { study =>
           CanViewResult(study) {
             lidraughts.mon.export.pdn.study()