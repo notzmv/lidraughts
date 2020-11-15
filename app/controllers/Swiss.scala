lichess-org/lila

expose swiss tournaments in TRF format

app/controllers/Swiss.scala
@@ -1,12 +1,13 @@
 package controllers
 
-import play.api.mvc._
 import play.api.libs.json.Json
+import play.api.mvc._
+import scala.concurrent.duration._
 
 import lila.api.Context
 import lila.app._
-import lila.swiss.{ Swiss => SwissModel }
 import lila.swiss.Swiss.{ Id => SwissId }
+import lila.swiss.{ Swiss => SwissModel }
 import views._
 
 final class Swiss(
@@ -201,6 +202,21 @@ final class Swiss(
             JsonOk(fuccess(lila.swiss.SwissJson.playerJsonExt(swiss, player)))
           }
         }
+      }
+    }
+
+  private val ExportLimitPerIP = new lila.memo.RateLimit[lila.common.IpAddress](
+    credits = 10,
+    duration = 1.minute,
+    name = "swiss export per IP",
+    key = "swiss.export.ip"
+  )
+
+  def exportTrf(id: String) =
+    Action.async {
+      env.swiss.api.byId(SwissId(id)) flatMap {
+        case None        => NotFound("Tournament not found").fuccess
+        case Some(swiss) => env.swiss.trf(swiss) dmap { Ok(_) }
       }
     }
 

notzmv/lidraughts

app/controllers/Swiss.scala
@@ -2,12 +2,13 @@ package controllers
 
 import play.api.libs.json._
 import play.api.mvc._
+import scala.concurrent.duration._
 
 import lidraughts.api.Context
 import lidraughts.app._
 import lidraughts.chat.Chat
-import lidraughts.swiss.{ Swiss => SwissModel }
 import lidraughts.swiss.Swiss.{ Id => SwissId }
+import lidraughts.swiss.{ Swiss => SwissModel }
 import views._
 
 object Swiss extends LidraughtsController {
@@ -198,6 +199,21 @@ object Swiss extends LidraughtsController {
       env.socketHandler.join(id, uid, ctx.me, getSocketVersion, apiVersion)
     }
   }
+
+  private val ExportLimitPerIP = new lidraughts.memo.RateLimit[lidraughts.common.IpAddress](
+    credits = 10,
+    duration = 1.minute,
+    name = "swiss export per IP",
+    key = "swiss.export.ip"
+  )
+
+  def exportTrf(id: String) =
+    Action.async {
+      env.api.byId(SwissId(id)) flatMap {
+        case None => NotFound("Tournament not found").fuccess
+        case Some(swiss) => env.trf(swiss) dmap { Ok(_) }
+      }
+    }
 
   private def WithSwiss(id: String)(f: SwissModel => Fu[Result]): Fu[Result] =
     env.api.byId(SwissId(id)) flatMap { _ ?? f }