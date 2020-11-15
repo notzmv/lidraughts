lichess-org/lila

api to export games of a swiss tournament

app/controllers/Api.scala
@@ -276,6 +276,31 @@ final class Api(
       }
     }
 
+  def swissGames(id: String) =
+    Action.async { req =>
+      env.swiss.api byId lila.swiss.Swiss.Id(id) flatMap {
+        _ ?? { swiss =>
+          val config = GameApiV2.BySwissConfig(
+            swissId = swiss.id,
+            format = GameApiV2.Format byRequest req,
+            flags = gameC.requestPgnFlags(req, extended = false),
+            perSecond = MaxPerSecond(20)
+          )
+          GlobalConcurrencyLimitPerIP(HTTPRequest lastRemoteAddress req)(
+            env.api.gameApiV2.exportBySwiss(config)
+          ) { source =>
+            val filename = env.api.gameApiV2.filename(swiss, config.format)
+            Ok.chunked(source)
+              .withHeaders(
+                noProxyBufferHeader,
+                CONTENT_DISPOSITION -> s"attachment; filename=$filename"
+              )
+              .as(gameC gameContentType config)
+          }.fuccess
+        }
+      }
+    }
+
   def gamesByUsersStream =
     AnonOrScopedBody(parse.tolerantText)()(
       anon = gamesByUsers(300),

notzmv/lidraughts

app/controllers/Api.scala
@@ -278,6 +278,27 @@ object Api extends LidraughtsController {
     }
   }
 
+  def swissGames(id: String) =
+    Action.async { req =>
+      Env.swiss.api byId lidraughts.swiss.Swiss.Id(id) flatMap {
+        _ ?? { swiss =>
+          GlobalLinearLimitPerIP(HTTPRequest lastRemoteAddress req) {
+            val flags = Game.requestPdnFlags(req, lidraughts.pref.Pref.default.draughtsResult, extended = false, algebraicPref = false)
+            val config = GameApiV2.BySwissConfig(
+              swissId = swiss.id,
+              format = GameApiV2.Format byRequest req,
+              flags = flags,
+              perSecond = MaxPerSecond(20)
+            )
+            Ok.chunked(Env.api.gameApiV2.exportBySwiss(config)).withHeaders(
+              noProxyBufferHeader,
+              CONTENT_TYPE -> Game.gameContentType(config)
+            ).fuccess
+          }
+        }
+      }
+    }
+
   def gamesByUsersStream = Action.async(parse.tolerantText) { req =>
     val userIds = req.body.split(',').take(300).toSet map lidraughts.user.User.normalize
     jsonStream(Env.game.gamesByUsersStream(userIds)).fuccess