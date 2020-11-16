lichess-org/lila

stream swiss trf for pairing and export

modules/swiss/src/main/SwissDirector.scala
@@ -9,7 +9,6 @@ import lila.game.Game
 
 final private class SwissDirector(
     colls: SwissColls,
-    trf: SwissTrf,
     pairingSystem: PairingSystem,
     gameRepo: lila.game.GameRepo,
     onStart: Game.ID => Unit
@@ -21,52 +20,55 @@ final private class SwissDirector(
 
   // sequenced by SwissApi
   private[swiss] def startRound(from: Swiss): Fu[Option[(Swiss, List[SwissPairing])]] =
-    trf
-      .fetchData(from)
-      .flatMap {
-        case (players, prevPairings) =>
-          val pendings = pairingSystem(from, players, prevPairings)
-          if (pendings.isEmpty) fuccess(none) // terminate
-          else {
-            val swiss = from.startRound
-            for {
-              pairings <- pendings.collect {
-                case Right(SwissPairing.Pending(w, b)) =>
-                  idGenerator.game dmap { id =>
-                    SwissPairing(
-                      id = id,
-                      swissId = swiss.id,
-                      round = swiss.round,
-                      white = w,
-                      black = b,
-                      status = Left(SwissPairing.Ongoing)
-                    )
-                  }
-              }.sequenceFu
-              _ <-
-                colls.swiss.update
-                  .one(
-                    $id(swiss.id),
-                    $unset("nextRoundAt") ++ $set(
-                      "round"     -> swiss.round,
-                      "nbOngoing" -> pairings.size
-                    )
+    pairingSystem(from)
+      .flatMap { pendings =>
+        if (pendings.isEmpty) fuccess(none) // terminate
+        else {
+          val swiss = from.startRound
+          for {
+            players <- SwissPlayer.fields { f =>
+              colls.player.ext
+                .find($doc(f.swissId -> swiss.id))
+                .sort($sort asc f.number)
+                .list[SwissPlayer]()
+            }
+            pairings <- pendings.collect {
+              case Right(SwissPairing.Pending(w, b)) =>
+                idGenerator.game dmap { id =>
+                  SwissPairing(
+                    id = id,
+                    swissId = swiss.id,
+                    round = swiss.round,
+                    white = w,
+                    black = b,
+                    status = Left(SwissPairing.Ongoing)
                   )
-                  .void
-              date = DateTime.now
-              byes = pendings.collect { case Left(bye) => bye.player }
-              _ <- SwissPlayer.fields { f =>
-                colls.player.update
-                  .one($doc(f.number $in byes, f.swissId -> swiss.id), $addToSet(f.byes -> swiss.round))
-                  .void
-              }
-              _ <- colls.pairing.insert.many(pairings).void
-              games = pairings.map(makeGame(swiss, SwissPlayer.toMap(players)))
-              _ <- lila.common.Future.applySequentially(games) { game =>
-                gameRepo.insertDenormalized(game) >>- onStart(game.id)
-              }
-            } yield Some(swiss -> pairings)
-          }
+                }
+            }.sequenceFu
+            _ <-
+              colls.swiss.update
+                .one(
+                  $id(swiss.id),
+                  $unset("nextRoundAt") ++ $set(
+                    "round"     -> swiss.round,
+                    "nbOngoing" -> pairings.size
+                  )
+                )
+                .void
+            date = DateTime.now
+            byes = pendings.collect { case Left(bye) => bye.player }
+            _ <- SwissPlayer.fields { f =>
+              colls.player.update
+                .one($doc(f.number $in byes, f.swissId -> swiss.id), $addToSet(f.byes -> swiss.round))
+                .void
+            }
+            _ <- colls.pairing.insert.many(pairings).void
+            games = pairings.map(makeGame(swiss, SwissPlayer.toMap(players)))
+            _ <- lila.common.Future.applySequentially(games) { game =>
+              gameRepo.insertDenormalized(game) >>- onStart(game.id)
+            }
+          } yield Some(swiss -> pairings)
+        }
       }
       .recover {
         case PairingSystem.BBPairingException(msg, input) =>

modules/swiss/src/main/SwissSheet.scala
@@ -57,4 +57,63 @@ private object SwissSheet {
         }
       }
     }
+
+}
+
+final private class SwissSheetApi(colls: SwissColls)(implicit
+    mat: akka.stream.Materializer
+) {
+
+  import akka.stream.scaladsl._
+  import org.joda.time.DateTime
+  import reactivemongo.akkastream.cursorProducer
+  import reactivemongo.api.ReadPreference
+  import lila.db.dsl._
+  import BsonHandlers._
+
+  def source(swiss: Swiss): Source[(SwissPlayer, Map[SwissRound.Number, SwissPairing], SwissSheet), _] =
+    SwissPlayer.fields { f =>
+      val readPreference =
+        if (swiss.finishedAt.exists(_ isBefore DateTime.now.minusSeconds(10)))
+          ReadPreference.secondaryPreferred
+        else ReadPreference.primary
+      colls.player
+        .aggregateWith[Bdoc](readPreference = readPreference) { implicit framework =>
+          import framework._
+          Match($doc(f.swissId -> swiss.id)) -> List(
+            PipelineOperator(
+              $doc(
+                "$lookup" -> $doc(
+                  "from" -> colls.pairing.name,
+                  "let"  -> $doc("n" -> "$n"),
+                  "pipeline" -> $arr(
+                    $doc(
+                      "$match" -> $doc(
+                        "$expr" -> $doc(
+                          "$and" -> $arr(
+                            $doc("$eq" -> $arr("$s", swiss.id)),
+                            $doc("$in" -> $arr("$$n", "$p"))
+                          )
+                        )
+                      )
+                    )
+                  ),
+                  "as" -> "pairings"
+                )
+              )
+            )
+          )
+        }
+        .documentSource()
+        .mapConcat { doc =>
+          val result = for {
+            player   <- playerHandler.readOpt(doc)
+            pairings <- doc.getAsOpt[List[SwissPairing]]("pairings")
+            pairingMap = pairings.map { p =>
+              p.round -> p
+            }.toMap
+          } yield (player, pairingMap, SwissSheet.one(swiss, pairingMap, player))
+          result.toList
+        }
+    }
 }

modules/swiss/src/main/SwissTrf.scala
@@ -1,49 +1,41 @@
 package lila.swiss
 
+import akka.stream.scaladsl._
+
 import lila.db.dsl._
 
 final class SwissTrf(
-    colls: SwissColls
-)(implicit ec: scala.concurrent.ExecutionContext) {
+    colls: SwissColls,
+    sheetApi: SwissSheetApi
+)(implicit ec: scala.concurrent.ExecutionContext, mat: akka.stream.Materializer) {
 
   import BsonHandlers._
   private type Bits = List[(Int, String)]
 
-  def apply(swiss: Swiss): Fu[String] =
-    fetchData(swiss) map {
-      case (players, pairings) => apply(swiss, players, pairings)
-    }
+  def apply(swiss: Swiss): Source[String, _] =
+    tournamentLines(swiss) concat sheetApi
+      .source(swiss)
+      .map((playerLine(swiss) _).tupled)
+      .map(formatLine)
 
-  def apply(swiss: Swiss, players: List[SwissPlayer], pairings: List[SwissPairing]): String = {
-    s"XXR ${swiss.settings.nbRounds}" ::
-      s"XXC ${chess.Color(scala.util.Random.nextBoolean).name}1" ::
-      players.map(player(swiss, SwissPairing.toMap(pairings))).map(format)
-  } mkString "\n"
-
-  def fetchData(swiss: Swiss): Fu[(List[SwissPlayer], List[SwissPairing])] =
-    SwissPlayer.fields { f =>
-      colls.player.ext
-        .find($doc(f.swissId -> swiss.id))
-        .sort($sort asc f.number)
-        .list[SwissPlayer]()
-    } zip
-      SwissPairing.fields { f =>
-        colls.pairing.ext
-          .find($doc(f.swissId -> swiss.id))
-          .sort($sort asc f.round)
-          .list[SwissPairing]()
-      }
+  private def tournamentLines(swiss: Swiss) =
+    Source(
+      List(
+        s"XXR ${swiss.settings.nbRounds}",
+        s"XXC ${chess.Color(scala.util.Random.nextBoolean).name}1"
+      )
+    )
 
   // https://www.fide.com/FIDE/handbook/C04Annex2_TRF16.pdf
-  private def player(swiss: Swiss, pairingMap: SwissPairing.PairingMap)(p: SwissPlayer): Bits = {
-    val sheet = SwissSheet.one(swiss, ~pairingMap.get(p.number), p)
+  private def playerLine(
+      swiss: Swiss
+  )(p: SwissPlayer, pairings: Map[SwissRound.Number, SwissPairing], sheet: SwissSheet): Bits =
     List(
       3  -> "001",
       8  -> p.number.toString,
       47 -> p.userId,
       84 -> f"${sheet.points.value}%1.1f"
     ) ::: {
-      val pairings = ~pairingMap.get(p.number)
       swiss.allRounds.zip(sheet.outcomes).flatMap {
         case (rn, outcome) =>
           val pairing = pairings get rn
@@ -71,9 +63,8 @@ final class SwissTrf(
         99 -> "-"
       ).map { case (l, s) => (l + swiss.round.value * 10, s) }
     }
-  }
 
-  private def format(bits: Bits): String =
+  private def formatLine(bits: Bits): String =
     bits.foldLeft("") {
       case (acc, (pos, txt)) => acc + (" " * (pos - txt.size - acc.size)) + txt
     }

notzmv/lidraughts

modules/swiss/src/main/SwissDirector.scala
@@ -10,7 +10,6 @@ final private class SwissDirector(
     swissColl: Coll,
     playerColl: Coll,
     pairingColl: Coll,
-    trf: SwissTrf,
     pairingSystem: PairingSystem,
     onStart: Game.ID => Unit
 ) {
@@ -18,52 +17,55 @@ final private class SwissDirector(
 
   // sequenced by SwissApi
   private[swiss] def startRound(from: Swiss): Fu[Option[(Swiss, List[SwissPairing])]] =
-    trf
-      .fetchData(from)
-      .flatMap {
-        case (players, prevPairings) =>
-          val pendings = pairingSystem(from, players, prevPairings)
-          if (pendings.isEmpty) fuccess(none) // terminate
-          else {
-            val swiss = from.startRound
-            for {
-              pairings <- pendings.collect {
-                case Right(SwissPairing.Pending(w, b)) =>
-                  IdGenerator.game dmap { id =>
-                    SwissPairing(
-                      id = id,
-                      swissId = swiss.id,
-                      round = swiss.round,
-                      white = w,
-                      black = b,
-                      status = Left(SwissPairing.Ongoing)
-                    )
-                  }
-              }.sequenceFu
-              _ <- swissColl
-                .update(
-                  $id(swiss.id),
-                  $unset("nextRoundAt") ++ $set(
-                    "round" -> swiss.round,
-                    "nbOngoing" -> pairings.size
+    pairingSystem(from)
+      .flatMap { pendings =>
+        if (pendings.isEmpty) fuccess(none) // terminate
+        else {
+          val swiss = from.startRound
+          for {
+            players <- SwissPlayer.fields { f =>
+              playerColl
+                .find($doc(f.swissId -> swiss.id))
+                .sort($sort asc f.number)
+                .list[SwissPlayer]()
+            }
+            pairings <- pendings.collect {
+              case Right(SwissPairing.Pending(w, b)) =>
+                IdGenerator.game dmap { id =>
+                  SwissPairing(
+                    id = id,
+                    swissId = swiss.id,
+                    round = swiss.round,
+                    white = w,
+                    black = b,
+                    status = Left(SwissPairing.Ongoing)
                   )
+                }
+            }.sequenceFu
+            _ <- swissColl
+              .update(
+                $id(swiss.id),
+                $unset("nextRoundAt") ++ $set(
+                  "round" -> swiss.round,
+                  "nbOngoing" -> pairings.size
                 )
+              )
+              .void
+            date = DateTime.now
+            byes = pendings.collect { case Left(bye) => bye.player }
+            _ <- SwissPlayer.fields { f =>
+              playerColl
+                .update($doc(f.number $in byes, f.swissId -> swiss.id), $addToSet(f.byes -> swiss.round))
                 .void
-              date = DateTime.now
-              byes = pendings.collect { case Left(bye) => bye.player }
-              _ <- SwissPlayer.fields { f =>
-                playerColl
-                  .update($doc(f.number $in byes, f.swissId -> swiss.id), $addToSet(f.byes -> swiss.round))
-                  .void
-              }
-              pairingsBson = pairings.map(pairingHandler.write)
-              _ <- pairingColl.bulkInsert(pairingsBson.toStream, ordered = true).void
-              games = pairings.map(makeGame(swiss, SwissPlayer.toMap(players)))
-              _ <- lidraughts.common.Future.applySequentially(games) { game =>
-                GameRepo.insertDenormalized(game) >>- onStart(game.id)
-              }
-            } yield Some(swiss -> pairings)
-          }
+            }
+            pairingsBson = pairings.map(pairingHandler.write)
+            _ <- pairingColl.bulkInsert(pairingsBson.toStream, ordered = true).void
+            games = pairings.map(makeGame(swiss, SwissPlayer.toMap(players)))
+            _ <- lidraughts.common.Future.applySequentially(games) { game =>
+              GameRepo.insertDenormalized(game) >>- onStart(game.id)
+            }
+          } yield Some(swiss -> pairings)
+        }
       }
       .recover {
         case PairingSystem.BBPairingException(msg, input) =>

modules/swiss/src/main/SwissSheet.scala
@@ -1,5 +1,7 @@
 package lidraughts.swiss
 
+import lidraughts.db.dsl._
+
 private case class SwissSheet(outcomes: List[SwissSheet.Outcome]) {
   import SwissSheet._
 
@@ -56,5 +58,66 @@ private object SwissSheet {
           case None => Absent
         }
       }
+    }
+
+}
+
+final private class SwissSheetApi(
+    playerColl: Coll,
+    pairingColl: Coll
+) {
+
+  import org.joda.time.DateTime
+  import reactivemongo.api.collections.bson.BSONBatchCommands.AggregationFramework.{ Match, PipelineOperator }
+  import reactivemongo.api.ReadPreference
+  import BsonHandlers._
+
+  def source(swiss: Swiss): Fu[List[(SwissPlayer, Map[SwissRound.Number, SwissPairing], SwissSheet)]] =
+    SwissPlayer.fields { f =>
+      val readPreference =
+        if (swiss.finishedAt.exists(_ isBefore DateTime.now.minusSeconds(10)))
+          ReadPreference.secondaryPreferred
+        else ReadPreference.primary
+      playerColl
+        .aggregateList(
+          Match($doc(f.swissId -> swiss.id)),
+          List(
+            PipelineOperator(
+              $doc(
+                "$lookup" -> $doc(
+                  "from" -> pairingColl.name,
+                  "let" -> $doc("n" -> "$n"),
+                  "pipeline" -> $arr(
+                    $doc(
+                      "$match" -> $doc(
+                        "$expr" -> $doc(
+                          "$and" -> $arr(
+                            $doc("$eq" -> $arr("$s", swiss.id)),
+                            $doc("$in" -> $arr("$$n", "$p"))
+                          )
+                        )
+                      )
+                    )
+                  ),
+                  "as" -> "pairings"
+                )
+              )
+            )
+          ),
+          maxDocs = Int.MaxValue,
+          readPreference = readPreference
+        )
+        .map {
+          _.flatMap { doc =>
+            val result = for {
+              player <- playerHandler.readOpt(doc)
+              pairings <- doc.getAs[List[SwissPairing]]("pairings")
+              pairingMap = pairings.map { p =>
+                p.round -> p
+              }.toMap
+            } yield (player, pairingMap, SwissSheet.one(swiss, pairingMap, player))
+            result.toList
+          }
+        }
     }
 }

modules/swiss/src/main/SwissTrf.scala
@@ -1,50 +1,34 @@
 package lidraughts.swiss
 
-import lidraughts.db.dsl._
-
 final class SwissTrf(
-    playerColl: Coll,
-    pairingColl: Coll
+    sheetApi: SwissSheetApi
 ) {
 
-  import BsonHandlers._
   private type Bits = List[(Int, String)]
 
-  def apply(swiss: Swiss): Fu[String] =
-    fetchData(swiss) map {
-      case (players, pairings) => apply(swiss, players, pairings)
+  def apply(swiss: Swiss): Fu[List[String]] =
+    sheetApi.source(swiss).map { lines =>
+      tournamentLines(swiss) ::: lines
+        .map((playerLine(swiss) _).tupled)
+        .map(formatLine)
     }
 
-  def apply(swiss: Swiss, players: List[SwissPlayer], pairings: List[SwissPairing]): String = {
-    s"XXR ${swiss.settings.nbRounds}" ::
-      s"XXC ${draughts.Color(scala.util.Random.nextBoolean).name}1" ::
-      players.map(player(swiss, SwissPairing.toMap(pairings))).map(format)
-  } mkString "\n"
-
-  def fetchData(swiss: Swiss): Fu[(List[SwissPlayer], List[SwissPairing])] =
-    SwissPlayer.fields { f =>
-      playerColl
-        .find($doc(f.swissId -> swiss.id))
-        .sort($sort asc f.number)
-        .list[SwissPlayer]()
-    } zip
-      SwissPairing.fields { f =>
-        pairingColl
-          .find($doc(f.swissId -> swiss.id))
-          .sort($sort asc f.round)
-          .list[SwissPairing]()
-      }
+  private def tournamentLines(swiss: Swiss) =
+    List(
+      s"XXR ${swiss.settings.nbRounds}",
+      s"XXC ${draughts.Color(scala.util.Random.nextBoolean).name}1"
+    )
 
   // https://www.fide.com/FIDE/handbook/C04Annex2_TRF16.pdf
-  private def player(swiss: Swiss, pairingMap: SwissPairing.PairingMap)(p: SwissPlayer): Bits = {
-    val sheet = SwissSheet.one(swiss, ~pairingMap.get(p.number), p)
+  private def playerLine(
+    swiss: Swiss
+  )(p: SwissPlayer, pairings: Map[SwissRound.Number, SwissPairing], sheet: SwissSheet): Bits =
     List(
       3 -> "001",
       8 -> p.number.toString,
       47 -> p.userId,
       84 -> f"${sheet.points.value}%1.1f"
     ) ::: {
-        val pairings = ~pairingMap.get(p.number)
         swiss.allRounds.zip(sheet.outcomes).flatMap {
           case (rn, outcome) =>
             val pairing = pairings get rn
@@ -72,9 +56,8 @@ final class SwissTrf(
           99 -> "-"
         ).map { case (l, s) => (l + swiss.round.value * 10, s) }
       }
-  }
 
-  private def format(bits: Bits): String =
+  private def formatLine(bits: Bits): String =
     bits.foldLeft("") {
       case (acc, (pos, txt)) => acc + (" " * (pos - txt.size - acc.size)) + txt
     }