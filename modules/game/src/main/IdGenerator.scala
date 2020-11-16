lichess-org/lila

IdGenerator.games with cute async recursion

modules/game/src/main/IdGenerator.scala
@@ -1,10 +1,10 @@
 package lila.game
 
 import chess.Color
-
+import java.security.SecureRandom
 import ornicar.scalalib.Random
 
-import java.security.SecureRandom
+import lila.db.dsl._
 
 final class IdGenerator(gameRepo: GameRepo)(implicit ec: scala.concurrent.ExecutionContext) {
 
@@ -18,6 +18,16 @@ final class IdGenerator(gameRepo: GameRepo)(implicit ec: scala.concurrent.Execut
     }
   }
 
+  def games(nb: Int): Fu[Set[Game.ID]] =
+    if (nb < 1) fuccess(Set.empty)
+    else if (nb == 1) game.dmap(Set(_))
+    else if (nb < 5) Set.fill(nb)(game).sequenceFu
+    else {
+      val ids = Set.fill(nb)(uncheckedGame)
+      gameRepo.coll.distinctEasy[Game.ID, Set]("_id", $inIds(ids)) flatMap { collisions =>
+        games(collisions.size) dmap { _ ++ ids }
+      }
+    }
 }
 
 object IdGenerator {

notzmv/lidraughts

modules/game/src/main/IdGenerator.scala
@@ -1,10 +1,10 @@
 package lidraughts.game
 
 import draughts.Color
-
+import java.security.SecureRandom
 import ornicar.scalalib.Random
 
-import java.security.SecureRandom
+import lidraughts.db.dsl._
 
 object IdGenerator {
 
@@ -15,6 +15,18 @@ object IdGenerator {
     GameRepo.exists(id).flatMap {
       case true => game
       case false => fuccess(id)
+    }
+  }
+
+  def games(nb: Int): Fu[List[Game.ID]] = {
+    if (nb < 1) fuccess(List.empty)
+    else if (nb == 1) game.dmap(List(_))
+    else if (nb < 5) List.fill(nb)(game).sequenceFu
+    else {
+      val ids = List.fill(nb)(uncheckedGame)
+      GameRepo.coll.distinct[Game.ID, List]("_id", $inIds(ids).some) flatMap { collisions =>
+        games(collisions.size) dmap { _ ++ (ids diff collisions) }
+      }
     }
   }
 