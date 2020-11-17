lichess-org/lila

swiss: no longer store player pairing
this is all completely nonsensical. I hate swiss.

modules/swiss/src/main/package.scala
@@ -4,5 +4,5 @@ package object swiss extends PackageObject {
 
   private[swiss] val logger = lila.log("swiss")
 
-  private[swiss] type Ranking = Map[SwissPlayer.Number, Int]
+  private[swiss] type Ranking = Map[lila.user.User.ID, Int]
 }

notzmv/lidraughts

modules/swiss/src/main/package.scala
@@ -8,5 +8,5 @@ package object swiss extends PackageObject with WithSocket {
 
   private[swiss] val logger = lidraughts.log("swiss")
 
-  private[swiss] type Ranking = Map[SwissPlayer.Number, Int]
+  private[swiss] type Ranking = Map[lidraughts.user.User.ID, Int]
 }