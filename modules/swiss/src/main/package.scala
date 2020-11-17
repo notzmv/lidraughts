lichess-org/lila

fix swiss again

modules/swiss/src/main/package.scala
@@ -4,5 +4,6 @@ package object swiss extends PackageObject {
 
   private[swiss] val logger = lila.log("swiss")
 
-  private[swiss] type Ranking = Map[lila.user.User.ID, Int]
+  private[swiss] type Ranking     = Map[lila.user.User.ID, Int]
+  private[swiss] type RankingSwap = Map[Int, lila.user.User.ID]
 }

notzmv/lidraughts

modules/swiss/src/main/package.scala
@@ -9,4 +9,5 @@ package object swiss extends PackageObject with WithSocket {
   private[swiss] val logger = lidraughts.log("swiss")
 
   private[swiss] type Ranking = Map[lidraughts.user.User.ID, Int]
+  private[swiss] type RankingSwap = Map[Int, lidraughts.user.User.ID]
 }