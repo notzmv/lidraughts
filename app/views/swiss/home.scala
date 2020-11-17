lichess-org/lila

feature swiss tournaments

app/views/swiss/home.scala
@@ -9,20 +9,20 @@ import controllers.routes
 
 object home {
 
-  def apply(
-  )(implicit ctx: Context) =
+  def apply(swisses: List[Swiss])(implicit ctx: Context) =
     views.html.base.layout(
       title = "Swiss tournaments",
       moreCss = cssTag("swiss.home")
     ) {
       main(cls := "page-small box box-pad page swiss-home")(
         h1("Swiss tournaments [BETA]"),
+        renderList(swisses),
         div(cls := "swiss-home__infos")(
           div(cls := "wiki")(
             iconTag(""),
             p(
-              "In a ",
-              a(href := "https://en.wikipedia.org/wiki/Swiss-system_tournament")("Swiss tournament"),
+              "In a Swiss tournament ",
+              a(href := "https://en.wikipedia.org/wiki/Swiss-system_tournament")("(wiki)"),
               ", each competitor does not necessarily play all other entrants. Competitors meet one-on-one in each round and are paired using a set of rules designed to ensure that each competitor plays opponents with a similar running score, but not the same opponent more than once. The winner is the competitor with the highest aggregate points earned in all rounds. All competitors play in each round unless there is an odd number of players."
             )
           ),
@@ -214,4 +214,40 @@ object home {
         )
       )
     }
+
+  private def renderList(swisses: List[Swiss])(implicit ctx: Context) =
+    table(cls := "slist ongoing")(
+      thead(
+        tr(
+          th(colspan := 4)("Now playing")
+        )
+      ),
+      tbody(
+        swisses map { s =>
+          tr(
+            td(cls := "icon")(iconTag(bits.iconChar(s))),
+            td(cls := "header")(
+              a(href := routes.Swiss.show(s.id.value))(
+                span(cls := "name")(s.name),
+                trans.by(teamIdToName(s.teamId))
+              )
+            ),
+            td(cls := "infos")(
+              span(cls := "rounds")(
+                s.settings.nbRounds,
+                " rounds swiss"
+              ),
+              span(cls := "setup")(
+                s.clock.show,
+                " • ",
+                if (s.variant.exotic) s.variant.name else s.perfType.map(_.trans),
+                " • ",
+                (if (s.settings.rated) trans.ratedTournament else trans.casualTournament)()
+              )
+            ),
+            td(cls := "text", dataIcon := "r")(s.nbPlayers.localize)
+          )
+        }
+      )
+    )
 }

modules/swiss/src/main/SwissCache.scala
@@ -5,6 +5,7 @@ import scala.concurrent.duration._
 import lila.db.dsl._
 import lila.hub.LightTeam.TeamID
 import lila.memo._
+import lila.memo.CacheApi._
 
 final private class SwissCache(
     colls: SwissColls,
@@ -48,4 +49,30 @@ final private class SwissCache(
     def get(teamId: TeamID)        = cache get teamId
     def invalidate(teamId: TeamID) = cache.put(teamId, compute(teamId))
   }
+
+  private[swiss] object feature {
+
+    private val cache = cacheApi.unit[List[Swiss]] {
+      _.refreshAfterWrite(1 minute)
+        .buildAsyncFuture { _ =>
+          colls.swiss.ext
+            .find(
+              $doc(
+                "featurable" -> true,
+                "settings.i" $lte 600 // hits the partial index
+              )
+            )
+            .sort($sort desc "nbPlayers")
+            .list[Swiss](10)
+            .map {
+              _.zipWithIndex.collect {
+                case (s, i) if s.nbPlayers >= 10 || i < 5 => s
+              }
+            }
+        }
+    }
+
+    def get = cache.getUnit
+  }
+
 }

notzmv/lidraughts

app/views/swiss/home.scala
@@ -9,19 +9,20 @@ import controllers.routes
 
 object home {
 
-  def apply()(implicit ctx: Context) =
+  def apply(swisses: List[Swiss])(implicit ctx: Context) =
     views.html.base.layout(
       title = "Swiss tournaments",
       moreCss = cssTag("swiss.home")
     ) {
         main(cls := "page-small box box-pad page swiss-home")(
           h1("Swiss tournaments [BETA]"),
+          renderList(swisses),
           div(cls := "swiss-home__infos")(
             div(cls := "wiki")(
               iconTag(""),
               p(
-                "In a ",
-                a(href := "https://en.wikipedia.org/wiki/Swiss-system_tournament")("Swiss tournament"),
+                "In a Swiss tournament ",
+                a(href := "https://en.wikipedia.org/wiki/Swiss-system_tournament")("(wiki)"),
                 ", each competitor does not necessarily play all other entrants. Competitors meet one-on-one in each round and are paired using a set of rules designed to ensure that each competitor plays opponents with a similar running score, but not the same opponent more than once. The winner is the competitor with the highest aggregate points earned in all rounds. All competitors play in each round unless there is an odd number of players."
               )
             ),
@@ -213,4 +214,40 @@ object home {
           )
         )
       }
+
+  private def renderList(swisses: List[Swiss])(implicit ctx: Context) =
+    table(cls := "slist ongoing")(
+      thead(
+        tr(
+          th(colspan := 4)("Now playing")
+        )
+      ),
+      tbody(
+        swisses map { s =>
+          tr(
+            td(cls := "icon")(iconTag(bits.iconChar(s))),
+            td(cls := "header")(
+              a(href := routes.Swiss.show(s.id.value))(
+                span(cls := "name")(s.name),
+                trans.by(teamIdToName(s.teamId))
+              )
+            ),
+            td(cls := "infos")(
+              span(cls := "rounds")(
+                s.settings.nbRounds,
+                " rounds swiss"
+              ),
+              span(cls := "setup")(
+                s.clock.show,
+                " • ",
+                if (s.variant.exotic) s.variant.name else s.perfType.map(_.name),
+                " • ",
+                (if (s.settings.rated) trans.ratedTournament else trans.casualTournament)()
+              )
+            ),
+            td(cls := "text", dataIcon := "r")(s.nbPlayers.localize)
+          )
+        }
+      )
+    )
 }

modules/swiss/src/main/SwissCache.scala
@@ -49,4 +49,29 @@ final private class SwissCache(
     def get(teamId: TeamId) = cache get teamId
     def invalidate(teamId: TeamId) = cache.put(teamId, compute(teamId))
   }
+
+  private[swiss] object feature {
+
+    private val cache = asyncCache.single[List[Swiss]](
+      name = "swiss.featurable",
+      f = swissColl
+        .find(
+          $doc(
+            "featurable" -> true,
+            "settings.i" $lte 600 // hits the partial index
+          )
+        )
+        .sort($sort desc "nbPlayers")
+        .list[Swiss](10)
+        .map {
+          _.zipWithIndex.collect {
+            case (s, i) if s.nbPlayers >= 5 || i < 5 => s
+          }
+        },
+      expireAfter = _.ExpireAfterWrite(1 minute)
+    )
+
+    def get = cache.get
+  }
+
 }