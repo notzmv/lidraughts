lichess-org/lila

sequence swiss ongoing games checks

modules/swiss/src/main/Env.scala
@@ -71,7 +71,7 @@ final class Env(
     "adjustCheater",
     "adjustBooster"
   ) {
-    case lila.game.actorApi.FinishGame(game, _, _) => api finishGame game
+    case lila.game.actorApi.FinishGame(game, _, _) => api.finishGame(game)
     // case lila.hub.actorApi.mod.MarkCheater(userId, true) => api.ejectLame(userId, _)
     // case lila.hub.actorApi.mod.MarkBooster(userId)       => api.ejectLame(userId, Nil)
   }

notzmv/lidraughts

modules/swiss/src/main/Env.scala
@@ -171,7 +171,7 @@ final class Env(
   system.lidraughtsBus.subscribeFun(
     'finishGame, 'adjustCheater, 'adjustBooster
   ) {
-      case lidraughts.game.actorApi.FinishGame(game, _, _) => api finishGame game
+      case lidraughts.game.actorApi.FinishGame(game, _, _) => api.finishGame(game)
       // case lidraughts.hub.actorApi.mod.MarkCheater(userId, true) => api.ejectLame(userId, _)
       // case lidraughts.hub.actorApi.mod.MarkBooster(userId)       => api.ejectLame(userId, Nil)
     }