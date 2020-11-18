lichess-org/lila

fix ui/swiss missing tsconfig.json

ui/swiss/src/view/main.ts
@@ -3,7 +3,7 @@ import { VNode } from 'snabbdom/vnode';
 import { spinner, dataIcon, bind, onInsert, numberRow } from './util';
 import SwissCtrl from '../ctrl';
 import * as pagination from '../pagination';
-import { MaybeVNodes, SwissData } from '../interfaces';
+import { MaybeVNodes, SwissData, Pager } from '../interfaces';
 import header from './header';
 import standing from './standing';
 import * as boards from './boards';
@@ -13,7 +13,6 @@ import playerInfo from './playerInfo';
 export default function(ctrl: SwissCtrl) {
   const d = ctrl.data;
   const content = (d.status == 'created' ? created(ctrl) : (d.status == 'started' ? started(ctrl) : finished(ctrl)));
-  const playerInfoNode = playerInfo(ctrl);
   return h('main.' + ctrl.opts.classes, [
     h('aside.swiss__side', {
       hook: onInsert(el => {
@@ -44,17 +43,17 @@ function created(ctrl: SwissCtrl): MaybeVNodes {
     nextRound(ctrl),
     controls(ctrl, pag),
     standing(ctrl, pag, 'created'),
-    h('blockquote.pull-quote', [
+    ctrl.data.quote ? h('blockquote.pull-quote', [
       h('p', ctrl.data.quote.text),
       h('footer', ctrl.data.quote.author)
-    ])
+    ]) : undefined
   ];
 }
 
-const notice = (ctrl: SwissCtrl): VNode => {
+const notice = (ctrl: SwissCtrl): VNode | undefined => {
   const d = ctrl.data;
   return (d.me && !d.me.absent && d.status == 'started' && d.nextRound) ?
-  h('div.swiss__notice.bar-glider', ctrl.trans('standByX', ctrl.data.me.name)) : undefined;
+  h('div.swiss__notice.bar-glider', ctrl.trans('standByX', d.me.name)) : undefined;
 }
 
 function started(ctrl: SwissCtrl): MaybeVNodes {
@@ -81,7 +80,7 @@ function finished(ctrl: SwissCtrl): MaybeVNodes {
   ];
 }
 
-function controls(ctrl: SwissCtrl, pag): VNode {
+function controls(ctrl: SwissCtrl, pag: Pager): VNode {
   return h('div.swiss__controls', [
     h('div.pager', pagination.renderPager(ctrl, pag)),
     joinButton(ctrl)
@@ -163,12 +162,12 @@ function joinTheGame(ctrl: SwissCtrl) {
 }
 
 function confetti(data: SwissData): VNode | undefined {
-  if (data.me && data.isRecentlyFinished && window.lichess.once('tournament.end.canvas.' + data.id))
-    return h('canvas#confetti', {
+  return (data.me && data.isRecentlyFinished && window.lichess.once('tournament.end.canvas.' + data.id)) ?
+    h('canvas#confetti', {
       hook: {
         insert: _ => window.lichess.loadScript('javascripts/confetti.js')
       }
-    });
+    }) : undefined;
 }
 
 function stats(ctrl: SwissCtrl): VNode | undefined {

notzmv/lidraughts

ui/swiss/src/view/main.ts
@@ -3,7 +3,7 @@ import { VNode } from 'snabbdom/vnode';
 import { spinner, dataIcon, bind, onInsert, numberRow } from './util';
 import SwissCtrl from '../ctrl';
 import * as pagination from '../pagination';
-import { MaybeVNodes, SwissData } from '../interfaces';
+import { MaybeVNodes, SwissData, Pager } from '../interfaces';
 import header from './header';
 import standing from './standing';
 import * as boards from './boards';
@@ -13,7 +13,6 @@ import playerInfo from './playerInfo';
 export default function(ctrl: SwissCtrl) {
   const d = ctrl.data;
   const content = (d.status == 'created' ? created(ctrl) : (d.status == 'started' ? started(ctrl) : finished(ctrl)));
-  const playerInfoNode = playerInfo(ctrl);
   return h('main.' + ctrl.opts.classes, [
     h('aside.swiss__side', {
       hook: onInsert(el => {
@@ -44,17 +43,17 @@ function created(ctrl: SwissCtrl): MaybeVNodes {
     nextRound(ctrl),
     controls(ctrl, pag),
     standing(ctrl, pag, 'created'),
-    h('blockquote.pull-quote', [
+    ctrl.data.quote ? h('blockquote.pull-quote', [
       h('p', ctrl.data.quote.text),
       h('footer', ctrl.data.quote.author)
-    ])
+    ]) : undefined
   ];
 }
 
-const notice = (ctrl: SwissCtrl): VNode => {
+const notice = (ctrl: SwissCtrl): VNode | undefined => {
   const d = ctrl.data;
   return (d.me && !d.me.absent && d.status == 'started' && d.nextRound) ?
-  h('div.swiss__notice.bar-glider', ctrl.trans('standByX', ctrl.data.me.name)) : undefined;
+  h('div.swiss__notice.bar-glider', ctrl.trans('standByX', d.me.name)) : undefined;
 }
 
 function started(ctrl: SwissCtrl): MaybeVNodes {
@@ -81,7 +80,7 @@ function finished(ctrl: SwissCtrl): MaybeVNodes {
   ];
 }
 
-function controls(ctrl: SwissCtrl, pag): VNode {
+function controls(ctrl: SwissCtrl, pag: Pager): VNode {
   return h('div.swiss__controls', [
     h('div.pager', pagination.renderPager(ctrl, pag)),
     joinButton(ctrl)
@@ -90,6 +89,11 @@ function controls(ctrl: SwissCtrl, pag): VNode {
 
 function nextRound(ctrl: SwissCtrl): VNode | undefined {
   if (!ctrl.opts.schedule || ctrl.data.nbOngoing) return;
+  const attrs: any = {
+    name: 'date',
+    placeholder: 'Schedule the next round',
+  };
+  if (ctrl.data.nextRound) attrs.value = ctrl.data.nextRound.at;
   return h('form.schedule-next-round', {
     class: {
       required: !ctrl.data.nextRound
@@ -100,11 +104,7 @@ function nextRound(ctrl: SwissCtrl): VNode | undefined {
     }
   }, [
     h('input', {
-      attrs: {
-        name: 'date',
-        placeholder: 'Schedule the next round',
-        value: ctrl.data.nextRound?.at
-      },
+      attrs,
       hook: onInsert((el: HTMLInputElement) =>
         setTimeout(() => $(el).flatpickr({
           minDate: 'today',
@@ -163,12 +163,12 @@ function joinTheGame(ctrl: SwissCtrl) {
 }
 
 function confetti(data: SwissData): VNode | undefined {
-  if (data.me && data.isRecentlyFinished && window.lidraughts.once('tournament.end.canvas.' + data.id))
-    return h('canvas#confetti', {
+  return (data.me && data.isRecentlyFinished && window.lidraughts.once('tournament.end.canvas.' + data.id)) ?
+    h('canvas#confetti', {
       hook: {
         insert: _ => window.lidraughts.loadScript('javascripts/confetti.js')
       }
-    });
+    }) : undefined;
 }
 
 function stats(ctrl: SwissCtrl): VNode | undefined {