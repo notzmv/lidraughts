lichess-org/lila

kick cheaters from swiss tournaments

modules/swiss/src/main/Env.scala
@@ -74,8 +74,8 @@ final class Env(
   ) {
     case lila.game.actorApi.FinishGame(game, _, _)           => api.finishGame(game)
     case lila.hub.actorApi.team.KickFromTeam(teamId, userId) => api.kickFromTeam(teamId, userId)
-    // case lila.hub.actorApi.mod.MarkCheater(userId, true) => api.ejectLame(userId, _)
-    // case lila.hub.actorApi.mod.MarkBooster(userId)       => api.ejectLame(userId, Nil)
+    case lila.hub.actorApi.mod.MarkCheater(userId, true)     => api.kickLame(userId)
+    case lila.hub.actorApi.mod.MarkBooster(userId)           => api.kickLame(userId)
   }
 
   ResilientScheduler(

modules/team/src/main/Env.scala
@@ -50,9 +50,11 @@ final class Env(
 
   lazy val getTeamName = new GetTeamName(cached.blockingTeamName)
 
-  lila.common.Bus.subscribeFun("shadowban", "teamIsLeader") {
+  lila.common.Bus.subscribeFun("shadowban", "teamIsLeader", "teamJoinedBy") {
     case lila.hub.actorApi.mod.Shadowban(userId, true) => api deleteRequestsByUserId userId
     case lila.hub.actorApi.team.IsLeader(teamId, userId, promise) =>
       promise completeWith cached.isLeader(teamId, userId)
+    case lila.hub.actorApi.team.TeamIdsJoinedBy(userId, promise) =>
+      promise completeWith cached.teamIdsList(userId)
   }
 }

notzmv/lidraughts

modules/swiss/src/main/Env.scala
@@ -172,8 +172,8 @@ final class Env(
     'finishGame, 'adjustCheater, 'adjustBooster
   ) {
       case lidraughts.game.actorApi.FinishGame(game, _, _) => api.finishGame(game)
-      // case lidraughts.hub.actorApi.mod.MarkCheater(userId, true) => api.ejectLame(userId, _)
-      // case lidraughts.hub.actorApi.mod.MarkBooster(userId)       => api.ejectLame(userId, Nil)
+      case lidraughts.hub.actorApi.mod.MarkCheater(userId, true) => api.kickLame(userId)
+      case lidraughts.hub.actorApi.mod.MarkBooster(userId) => api.kickLame(userId)
     }
 
   ResilientScheduler(

modules/team/src/main/Env.scala
@@ -92,8 +92,10 @@ final class Env(
 
   private lazy val notifier = new Notifier(notifyApi = notifyApi)
 
-  system.lidraughtsBus.subscribeFun('shadowban) {
+  system.lidraughtsBus.subscribeFun('shadowban, 'teamJoinedBy) {
     case lidraughts.hub.actorApi.mod.Shadowban(userId, true) => api deleteRequestsByUserId userId
+    case lidraughts.hub.actorApi.team.TeamIdsJoinedBy(userId, promise) =>
+      promise completeWith cached.teamIdsList(userId)
   }
 }
 