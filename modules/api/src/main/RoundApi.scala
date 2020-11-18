lichess-org/lila

show swiss ranks on game page

modules/api/src/main/RoundApi.scala
@@ -12,7 +12,7 @@ import lila.round.JsonView.WithFlags
 import lila.round.{ Forecast, JsonView }
 import lila.security.Granter
 import lila.simul.Simul
-import lila.swiss.Swiss
+import lila.swiss.{ GameView => SwissView }
 import lila.tournament.{ GameView => TourView }
 import lila.tree.Node.partitionTreeJsonWriter
 import lila.user.User
@@ -47,7 +47,7 @@ final private[api] class RoundApi(
           nvui = ctx.blind
         ) zip
           (pov.game.simulId ?? simulApi.find) zip
-          fetchSwiss(pov) zip
+          swissApi.gameView(pov) zip
           (ctx.me.ifTrue(ctx.isMobileApi) ?? (me => noteApi.get(pov.gameId, me.id))) zip
           forecastApi.loadForDisplay(pov) zip
           bookmarkApi.exists(pov.game, ctx.me) map {
@@ -86,7 +86,7 @@ final private[api] class RoundApi(
           withFlags = WithFlags(blurs = ctx.me ?? Granter(_.ViewBlurs))
         ) zip
           (pov.game.simulId ?? simulApi.find) zip
-          fetchSwiss(pov) zip
+          swissApi.gameView(pov) zip
           (ctx.me.ifTrue(ctx.isMobileApi) ?? (me => noteApi.get(pov.gameId, me.id))) zip
           bookmarkApi.exists(pov.game, ctx.me) map {
           case json ~ simul ~ swiss ~ note ~ bookmarked =>
@@ -125,7 +125,7 @@ final private[api] class RoundApi(
         ) zip
           tourApi.gameView.analysis(pov.game) zip
           (pov.game.simulId ?? simulApi.find) zip
-          fetchSwiss(pov) zip
+          swissApi.gameView(pov) zip
           ctx.userId.ifTrue(ctx.isMobileApi).?? { noteApi.get(pov.gameId, _) } zip
           bookmarkApi.exists(pov.game, ctx.me) map {
           case json ~ tour ~ simul ~ swiss ~ note ~ bookmarked =>
@@ -133,7 +133,6 @@ final private[api] class RoundApi(
               withTournament(pov, tour) _ compose
                 withSwiss(swiss) _ compose
                 withSimul(simul) _ compose
-                withSwiss(swiss) _ compose
                 withNote(note) _ compose
                 withBookmark(bookmarked) _ compose
                 withTree(pov, analysis, initialFen, withFlags) _ compose
@@ -286,13 +285,19 @@ final private[api] class RoundApi(
         )
     })
 
-  def withSwiss(swiss: Option[Swiss])(json: JsObject) =
-    json.add("swiss" -> swiss.map { s =>
+  def withSwiss(sv: Option[SwissView])(json: JsObject) =
+    json.add("swiss" -> sv.map { s =>
       Json
         .obj(
-          "id"      -> s.id.value,
-          "running" -> s.isStarted
+          "id"      -> s.swiss.id.value,
+          "running" -> s.swiss.isStarted
         )
+        .add("ranks" -> s.ranks.map { r =>
+          Json.obj(
+            "white" -> r.whiteRank,
+            "black" -> r.blackRank
+          )
+        })
     })
 
   private def withSimul(simulOption: Option[Simul])(json: JsObject) =
@@ -307,6 +312,4 @@ final private[api] class RoundApi(
         )
       }
     )
-
-  private def fetchSwiss(pov: Pov) = pov.game.swissId.map(Swiss.Id.apply) ?? swissApi.byId
 }

modules/swiss/src/main/SwissApi.scala
@@ -12,7 +12,7 @@ import scala.util.chaining._
 import lila.chat.Chat
 import lila.common.{ Bus, GreatPlayer, LightUser }
 import lila.db.dsl._
-import lila.game.Game
+import lila.game.{ Game, Pov }
 import lila.hub.LightTeam.TeamID
 import lila.round.actorApi.round.QuietFlag
 import lila.user.{ User, UserRepo }
@@ -253,6 +253,28 @@ final class SwissApi(
     rankingApi(swiss) map {
       _ get userId map { rank =>
         (Math.floor(rank / 10) + 1).toInt
+      }
+    }
+
+  def gameView(pov: Pov): Fu[Option[GameView]] =
+    (pov.game.swissId.map(Swiss.Id.apply) ?? byId) flatMap {
+      _ ?? { swiss =>
+        getGameRanks(swiss, pov.game) dmap {
+          GameView(swiss, _).some
+        }
+      }
+    }
+
+  private def getGameRanks(swiss: Swiss, game: Game): Fu[Option[GameRanks]] =
+    ~ {
+      game.whitePlayer.userId.ifTrue(swiss.isStarted) flatMap { whiteId =>
+        game.blackPlayer.userId map { blackId =>
+          rankingApi(swiss) map { ranking =>
+            ranking.get(whiteId) |@| ranking.get(blackId) apply {
+              case (whiteR, blackR) => GameRanks(whiteR, blackR)
+            }
+          }
+        }
       }
     }
 

notzmv/lidraughts

modules/api/src/main/RoundApi.scala
@@ -10,7 +10,7 @@ import lidraughts.round.JsonView.WithFlags
 import lidraughts.round.{ Forecast, JsonView, logger }
 import lidraughts.security.Granter
 import lidraughts.simul.Simul
-import lidraughts.swiss.Swiss
+import lidraughts.swiss.{ GameView => SwissView }
 import lidraughts.tournament.TourAndRanks
 import lidraughts.tree.Node.partitionTreeJsonWriter
 import lidraughts.user.User
@@ -20,9 +20,9 @@ private[api] final class RoundApi(
     noteApi: lidraughts.round.NoteApi,
     forecastApi: lidraughts.round.ForecastApi,
     bookmarkApi: lidraughts.bookmark.BookmarkApi,
+    swissApi: lidraughts.swiss.SwissApi,
     getTourAndRanks: Game => Fu[Option[TourAndRanks]],
-    getSimul: Simul.ID => Fu[Option[Simul]],
-    getSwiss: Swiss.Id => Fu[Option[Swiss]]
+    getSimul: Simul.ID => Fu[Option[Simul]]
 ) {
 
   def player(pov: Pov, apiVersion: ApiVersion)(implicit ctx: Context): Fu[JsObject] =
@@ -33,7 +33,7 @@ private[api] final class RoundApi(
         nvui = ctx.blind) zip
         getTourAndRanks(pov.game) zip
         (pov.game.simulId ?? getSimul) zip
-        fetchSwiss(pov) zip
+        swissApi.gameView(pov) zip
         (ctx.me.ifTrue(ctx.isMobileApi) ?? (me => noteApi.get(pov.gameId, me.id))) zip
         forecastApi.loadForDisplay(pov) zip
         bookmarkApi.exists(pov.game, ctx.me) map {
@@ -57,7 +57,7 @@ private[api] final class RoundApi(
         withFlags = WithFlags(blurs = ctx.me ?? Granter(_.ViewBlurs))) zip
         getTourAndRanks(pov.game) zip
         (pov.game.simulId ?? getSimul) zip
-        fetchSwiss(pov) zip
+        swissApi.gameView(pov) zip
         (ctx.me.ifTrue(ctx.isMobileApi) ?? (me => noteApi.get(pov.gameId, me.id))) zip
         bookmarkApi.exists(pov.game, ctx.me) map {
           case json ~ tourOption ~ simulOption ~ swissOption ~ note ~ bookmarked => (
@@ -82,7 +82,7 @@ private[api] final class RoundApi(
         withFlags = withFlags.copy(blurs = ctx.me ?? Granter(_.ViewBlurs))) zip
         getTourAndRanks(pov.game) zip
         (pov.game.simulId ?? getSimul) zip
-        fetchSwiss(pov) zip
+        swissApi.gameView(pov) zip
         ctx.userId.ifTrue(ctx.isMobileApi).?? { noteApi.get(pov.gameId, _) } zip
         bookmarkApi.exists(pov.game, ctx.me) map {
           case json ~ tourOption ~ simulOption ~ swissOption ~ note ~ bookmarked => (
@@ -199,13 +199,19 @@ private[api] final class RoundApi(
         )))
     })
 
-  def withSwiss(swiss: Option[Swiss])(json: JsObject) =
-    json.add("swiss" -> swiss.map { s =>
+  def withSwiss(sv: Option[SwissView])(json: JsObject) =
+    json.add("swiss" -> sv.map { s =>
       Json
         .obj(
-          "id" -> s.id.value,
-          "running" -> s.isStarted
+          "id" -> s.swiss.id.value,
+          "running" -> s.swiss.isStarted
         )
+        .add("ranks" -> s.ranks.map { r =>
+          Json.obj(
+            "white" -> r.whiteRank,
+            "black" -> r.blackRank
+          )
+        })
     })
 
   private def withSimul(pov: Pov, simulOption: Option[Simul], player: Boolean)(json: JsObject) =
@@ -219,6 +225,4 @@ private[api] final class RoundApi(
         .add("isUnique" -> simul.isUnique.option(true))
         .add("noAssistance" -> simul.spotlight.flatMap(_.noAssistance).ifTrue(player))
     })
-
-  private def fetchSwiss(pov: Pov) = pov.game.swissId.map(Swiss.Id.apply) ?? getSwiss
 }

modules/swiss/src/main/SwissApi.scala
@@ -12,7 +12,7 @@ import actorApi._
 import lidraughts.chat.{ Chat, ChatApi }
 import lidraughts.common.{ Bus, GreatPlayer, LightUser }
 import lidraughts.db.dsl._
-import lidraughts.game.Game
+import lidraughts.game.{ Game, Pov }
 import lidraughts.hub.lightTeam.TeamId
 import lidraughts.hub.{ Duct, DuctMap }
 import lidraughts.round.actorApi.round.QuietFlag
@@ -235,6 +235,28 @@ final class SwissApi(
     rankingApi(swiss) map {
       _ get userId map { rank =>
         (Math.floor(rank / 10) + 1).toInt
+      }
+    }
+
+  def gameView(pov: Pov): Fu[Option[GameView]] =
+    (pov.game.swissId.map(Swiss.Id.apply) ?? byId) flatMap {
+      _ ?? { swiss =>
+        getGameRanks(swiss, pov.game) dmap {
+          GameView(swiss, _).some
+        }
+      }
+    }
+
+  private def getGameRanks(swiss: Swiss, game: Game): Fu[Option[GameRanks]] =
+    ~{
+      game.whitePlayer.userId.ifTrue(swiss.isStarted) flatMap { whiteId =>
+        game.blackPlayer.userId map { blackId =>
+          rankingApi(swiss) map { ranking =>
+            ranking.get(whiteId) |@| ranking.get(blackId) apply {
+              case (whiteR, blackR) => GameRanks(whiteR, blackR)
+            }
+          }
+        }
       }
     }
 