lichess-org/lila

translate 2FA settings

modules/i18n/src/main/I18nDb.scala
@@ -18,6 +18,7 @@ object I18nDb {
   case object Coach       extends Ref
   case object Broadcast   extends Ref
   case object Streamer    extends Ref
+  case object Settings    extends Ref
 
   val site: Messages        = lila.i18n.db.site.Registry.load
   val arena: Messages       = lila.i18n.db.arena.Registry.load
@@ -32,6 +33,7 @@ object I18nDb {
   val coach: Messages       = lila.i18n.db.coach.Registry.load
   val broadcast: Messages   = lila.i18n.db.broadcast.Registry.load
   val streamer: Messages    = lila.i18n.db.streamer.Registry.load
+  val settings: Messages    = lila.i18n.db.settings.Registry.load
 
   def apply(ref: Ref): Messages = ref match {
     case Site        => site
@@ -47,6 +49,7 @@ object I18nDb {
     case Coach       => coach
     case Broadcast   => broadcast
     case Streamer    => streamer
+    case Settings    => settings
   }
 
   val langs: Set[Lang] = site.keys.toSet

modules/i18n/src/main/I18nKeys.scala
@@ -1,7 +1,7 @@
 // Generated with bin/trans-dump.js
 package lila.i18n
 
-import I18nDb.{ Activity, Arena, Broadcast, Clas, Coach, Contact, Coordinates, Emails, Learn, Patron, Site, Streamer, Study }
+import I18nDb.{ Activity, Arena, Broadcast, Clas, Coach, Contact, Coordinates, Emails, Learn, Patron, Settings, Site, Streamer, Study }
 
 // format: OFF
 object I18nKeys {
@@ -261,7 +261,6 @@ val `xJoinedTeamY` = new Translated("xJoinedTeamY", Site)
 val `xCreatedTeamY` = new Translated("xCreatedTeamY", Site)
 val `averageElo` = new Translated("averageElo", Site)
 val `location` = new Translated("location", Site)
-val `settings` = new Translated("settings", Site)
 val `filterGames` = new Translated("filterGames", Site)
 val `reset` = new Translated("reset", Site)
 val `apply` = new Translated("apply", Site)
@@ -356,6 +355,7 @@ val `firstName` = new Translated("firstName", Site)
 val `lastName` = new Translated("lastName", Site)
 val `biography` = new Translated("biography", Site)
 val `country` = new Translated("country", Site)
+val `socialMediaLinks` = new Translated("socialMediaLinks", Site)
 val `preferences` = new Translated("preferences", Site)
 val `inlineNotation` = new Translated("inlineNotation", Site)
 val `watchLichessTV` = new Translated("watchLichessTV", Site)
@@ -1485,6 +1485,21 @@ val `maxSize` = new Translated("maxSize", Streamer)
 val `installBlocker` = new Translated("installBlocker", Streamer)
 val `beSafe` = new Translated("beSafe", Streamer)
 val `keepItShort` = new Translated("keepItShort", Streamer)
+}
+
+object settings {
+val `settings` = new Translated("settings", Settings)
+val `twoFactorAuth` = new Translated("twoFactorAuth", Settings)
+val `twoFactorHelp` = new Translated("twoFactorHelp", Settings)
+val `twoFactorApp` = new Translated("twoFactorApp", Settings)
+val `scanTheCode` = new Translated("scanTheCode", Settings)
+val `enterPassword` = new Translated("enterPassword", Settings)
+val `authenticationCode` = new Translated("authenticationCode", Settings)
+val `ifYouLoseAccess` = new Translated("ifYouLoseAccess", Settings)
+val `enableTwoFactor` = new Translated("enableTwoFactor", Settings)
+val `disableTwoFactor` = new Translated("disableTwoFactor", Settings)
+val `twoFactorEnabled` = new Translated("twoFactorEnabled", Settings)
+val `twoFactorDisable` = new Translated("twoFactorDisable", Settings)
 }
 
 }

notzmv/lidraughts

modules/i18n/src/main/I18nDb.scala
@@ -12,6 +12,7 @@ object I18nDb {
   case object Activity extends Ref
   case object Coordinates extends Ref
   case object Study extends Ref
+  case object Tfa extends Ref
 
   val site: Messages = lidraughts.i18n.db.site.Registry.load
   val arena: Messages = lidraughts.i18n.db.arena.Registry.load
@@ -20,6 +21,7 @@ object I18nDb {
   val activity: Messages = lidraughts.i18n.db.activity.Registry.load
   val coordinates: Messages = lidraughts.i18n.db.coordinates.Registry.load
   val study: Messages = lidraughts.i18n.db.study.Registry.load
+  val tfa: Messages = lidraughts.i18n.db.tfa.Registry.load
 
   def apply(ref: Ref): Messages = ref match {
     case Site => site
@@ -29,6 +31,7 @@ object I18nDb {
     case Activity => activity
     case Coordinates => coordinates
     case Study => study
+    case Tfa => tfa
   }
 
   val langs: Set[Lang] = site.keys.map(Lang.apply)(scala.collection.breakOut)

modules/i18n/src/main/I18nKeys.scala
@@ -1,7 +1,7 @@
 // Generated with bin/trans-dump.js
 package lidraughts.i18n
 
-import I18nDb.{ Site, Arena, Emails, Learn, Activity, Coordinates, Study }
+import I18nDb.{ Site, Arena, Emails, Learn, Activity, Coordinates, Study, Tfa }
 
 // format: OFF
 object I18nKeys {
@@ -153,7 +153,6 @@ val `changeUsernameDescription` = new Translated("changeUsernameDescription", Si
 val `password` = new Translated("password", Site)
 val `changePassword` = new Translated("changePassword", Site)
 val `changeEmail` = new Translated("changeEmail", Site)
-val `twoFactorAuth` = new Translated("twoFactorAuth", Site)
 val `email` = new Translated("email", Site)
 val `passwordReset` = new Translated("passwordReset", Site)
 val `forgotPassword` = new Translated("forgotPassword", Site)
@@ -377,6 +376,7 @@ val `lastName` = new Translated("lastName", Site)
 val `biography` = new Translated("biography", Site)
 val `country` = new Translated("country", Site)
 val `countryOrRegion` = new Translated("countryOrRegion", Site)
+val `socialMediaLinks` = new Translated("socialMediaLinks", Site)
 val `linkSuggestions` = new Translated("linkSuggestions", Site)
 val `preferences` = new Translated("preferences", Site)
 val `inlineNotation` = new Translated("inlineNotation", Site)
@@ -1231,6 +1231,20 @@ val `nbChapters` = new Translated("nbChapters", Study)
 val `nbGames` = new Translated("nbGames", Study)
 val `nbMembers` = new Translated("nbMembers", Study)
 val `pasteYourPdnTextHereUpToNbGames` = new Translated("pasteYourPdnTextHereUpToNbGames", Study)
+}
+
+object tfa {
+val `twoFactorAuth` = new Translated("twoFactorAuth", Tfa)
+val `twoFactorHelp` = new Translated("twoFactorHelp", Tfa)
+val `twoFactorApp` = new Translated("twoFactorApp", Tfa)
+val `scanTheCode` = new Translated("scanTheCode", Tfa)
+val `enterPassword` = new Translated("enterPassword", Tfa)
+val `authenticationCode` = new Translated("authenticationCode", Tfa)
+val `ifYouLoseAccess` = new Translated("ifYouLoseAccess", Tfa)
+val `enableTwoFactor` = new Translated("enableTwoFactor", Tfa)
+val `disableTwoFactor` = new Translated("disableTwoFactor", Tfa)
+val `twoFactorEnabled` = new Translated("twoFactorEnabled", Tfa)
+val `twoFactorDisable` = new Translated("twoFactorDisable", Tfa)
 }
 
 }