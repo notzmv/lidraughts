lichess-org/lila

opt-out from messages of a team

modules/team/src/main/MemberRepo.scala
@@ -10,38 +10,48 @@ final class MemberRepo(val coll: Coll)(implicit ec: scala.concurrent.ExecutionCo
 
   import BSONHandlers._
 
-  type ID = String
-
   // expensive with thousands of members!
-  def userIdsByTeam(teamId: ID): Fu[Set[ID]] =
+  def userIdsByTeam(teamId: Team.ID): Fu[Set[Team.ID]] =
     coll.secondaryPreferred.distinctEasy[String, Set]("user", $doc("team" -> teamId))
 
-  def teamIdsByUser(userId: User.ID): Fu[Set[ID]] =
-    coll.distinctEasy[ID, Set]("team", $doc("user" -> userId))
+  def teamIdsByUser(userId: User.ID): Fu[Set[Team.ID]] =
+    coll.distinctEasy[Team.ID, Set]("team", $doc("user" -> userId))
 
-  def removeByteam(teamId: ID): Funit =
+  def removeByteam(teamId: Team.ID): Funit =
     coll.delete.one(teamQuery(teamId)).void
 
   def removeByUser(userId: User.ID): Funit =
     coll.delete.one(userQuery(userId)).void
 
-  def exists(teamId: ID, userId: User.ID): Fu[Boolean] =
+  def exists(teamId: Team.ID, userId: User.ID): Fu[Boolean] =
     coll.exists(selectId(teamId, userId))
 
-  def add(teamId: ID, userId: User.ID): Funit =
+  def add(teamId: Team.ID, userId: User.ID): Funit =
     coll.insert.one(Member.make(team = teamId, user = userId)).void
 
-  def remove(teamId: ID, userId: User.ID): Fu[WriteResult] =
+  def remove(teamId: Team.ID, userId: User.ID): Fu[WriteResult] =
     coll.delete.one(selectId(teamId, userId))
 
-  def countByTeam(teamId: ID): Fu[Int] =
+  def countByTeam(teamId: Team.ID): Fu[Int] =
     coll.countSel(teamQuery(teamId))
 
-  def filterUserIdsInTeam(teamId: ID, userIds: Set[User.ID]): Fu[Set[User.ID]] =
+  def filterUserIdsInTeam(teamId: Team.ID, userIds: Set[User.ID]): Fu[Set[User.ID]] =
     userIds.nonEmpty ??
       coll.distinctEasy[User.ID, Set]("user", $inIds(userIds.map { Member.makeId(teamId, _) }))
 
-  def teamQuery(teamId: ID)                    = $doc("team" -> teamId)
-  private def selectId(teamId: ID, userId: ID) = $id(Member.makeId(teamId, userId))
-  private def userQuery(userId: ID)            = $doc("user" -> userId)
+  def isSubscribed(team: Team, user: User): Fu[Boolean] =
+    !coll.exists(selectId(team.id, user.id) ++ $doc("unsub" -> true))
+
+  def subscribe(teamId: Team.ID, userId: User.ID, v: Boolean): Funit =
+    coll.update
+      .one(
+        selectId(teamId, userId),
+        if (v) $unset("unsub")
+        else $set("unsub" -> true)
+      )
+      .void
+
+  def teamQuery(teamId: Team.ID)                         = $doc("team" -> teamId)
+  private def selectId(teamId: Team.ID, userId: User.ID) = $id(Member.makeId(teamId, userId))
+  private def userQuery(userId: User.ID)                 = $doc("user" -> userId)
 }

modules/team/src/main/TeamApi.scala
@@ -75,6 +75,10 @@ final class TeamApi(
   def mine(me: User): Fu[List[Team]] =
     cached teamIdsList me.id flatMap teamRepo.byIdsSortPopular
 
+  def isSubscribed = memberRepo.isSubscribed _
+
+  def subscribe = memberRepo.subscribe _
+
   def countTeamsOf(me: User) =
     cached teamIdsList me.id dmap (_.size)
 
@@ -177,7 +181,7 @@ final class TeamApi(
           timeline ! Propagate(TeamJoin(user.id, team.id)).toFollowersOf(user.id)
           Bus.publish(JoinTeam(id = team.id, userId = user.id), "team")
         }
-      } recover lila.db.recoverDuplicateKey(_ => ())
+      } recover lila.db.ignoreDuplicateKey
     }
 
   def quit(teamId: Team.ID, me: User): Fu[Option[Team]] =

notzmv/lidraughts

modules/team/src/main/MemberRepo.scala
@@ -13,34 +13,47 @@ object MemberRepo {
 
   import BSONHandlers._
 
-  type ID = String
-
   // expensive with thousands of members!
-  def userIdsByTeam(teamId: ID): Fu[Set[ID]] =
+  def userIdsByTeam(teamId: Team.ID): Fu[Set[User.ID]] =
     coll.distinct[String, Set]("user", $doc("team" -> teamId).some)
 
-  def teamIdsByUser(userId: User.ID): Fu[Set[ID]] =
-    coll.distinct[ID, Set]("team", $doc("user" -> userId).some)
+  def subscribedUserIds(teamId: Team.ID): Fu[Set[User.ID]] =
+    coll.distinct[String, Set]("user", Some($doc("team" -> teamId) ++ $doc("unsub" $ne true)))
 
-  def removeByteam(teamId: ID): Funit =
+  def teamIdsByUser(userId: User.ID): Fu[Set[Team.ID]] =
+    coll.distinct[String, Set]("team", $doc("user" -> userId).some)
+
+  def removeByteam(teamId: Team.ID): Funit =
     coll.remove(teamQuery(teamId)).void
 
   def removeByUser(userId: User.ID): Funit =
     coll.remove(userQuery(userId)).void
 
-  def exists(teamId: ID, userId: User.ID): Fu[Boolean] =
+  def exists(teamId: Team.ID, userId: User.ID): Fu[Boolean] =
     coll.exists(selectId(teamId, userId))
 
-  def add(teamId: ID, userId: User.ID): Funit =
+  def add(teamId: Team.ID, userId: User.ID): Funit =
     coll.insert(Member.make(team = teamId, user = userId)).void
 
-  def remove(teamId: ID, userId: User.ID): Fu[WriteResult] =
+  def remove(teamId: Team.ID, userId: User.ID): Fu[WriteResult] =
     coll.remove(selectId(teamId, userId))
 
-  def countByTeam(teamId: ID): Fu[Int] =
+  def countByTeam(teamId: Team.ID): Fu[Int] =
     coll.countSel(teamQuery(teamId))
 
-  def selectId(teamId: ID, userId: ID) = $id(Member.makeId(teamId, userId))
-  def teamQuery(teamId: ID) = $doc("team" -> teamId)
-  def userQuery(userId: ID) = $doc("user" -> userId)
+  def isSubscribed(team: Team, user: User): Fu[Boolean] =
+    !coll.exists(selectId(team.id, user.id) ++ $doc("unsub" -> true))
+
+  def subscribe(teamId: Team.ID, userId: User.ID, v: Boolean): Funit =
+    coll
+      .update(
+        selectId(teamId, userId),
+        if (v) $unset("unsub")
+        else $set("unsub" -> true)
+      )
+      .void
+
+  def teamQuery(teamId: Team.ID) = $doc("team" -> teamId)
+  def selectId(teamId: Team.ID, userId: User.ID) = $id(Member.makeId(teamId, userId))
+  def userQuery(userId: Team.ID) = $doc("user" -> userId)
 }

modules/team/src/main/TeamApi.scala
@@ -69,6 +69,10 @@ final class TeamApi(
   def mine(me: User): Fu[List[Team]] =
     cached teamIds me.id flatMap { ids => coll.team.byIds[Team](ids.toArray) }
 
+  def isSubscribed = MemberRepo.isSubscribed _
+
+  def subscribe = MemberRepo.subscribe _
+
   def hasTeams(me: User): Fu[Boolean] = cached.teamIds(me.id).map(_.value.nonEmpty)
 
   def hasCreatedRecently(me: User): Fu[Boolean] =
@@ -147,7 +151,7 @@ final class TeamApi(
           timeline ! Propagate(TeamJoin(user.id, team.id)).toFollowersOf(user.id)
           bus.publish(JoinTeam(id = team.id, userId = user.id), 'team)
         }
-    } recover lidraughts.db.recoverDuplicateKey(_ => ())
+    } recover lidraughts.db.ignoreDuplicateKey
   }
 
   def quit(teamId: Team.ID, me: User): Fu[Option[Team]] =