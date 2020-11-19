lichess-org/lila

extract and complete team translations

modules/i18n/src/main/I18nDb.scala
@@ -21,6 +21,7 @@ object I18nDb {
   case object Tfa         extends Ref
   case object Settings    extends Ref
   case object Preferences extends Ref
+  case object Team        extends Ref
 
   val site: Messages        = lila.i18n.db.site.Registry.load
   val arena: Messages       = lila.i18n.db.arena.Registry.load
@@ -38,6 +39,7 @@ object I18nDb {
   val tfa: Messages         = lila.i18n.db.tfa.Registry.load
   val settings: Messages    = lila.i18n.db.settings.Registry.load
   val preferences: Messages = lila.i18n.db.preferences.Registry.load
+  val team: Messages        = lila.i18n.db.team.Registry.load
 
   def apply(ref: Ref): Messages = ref match {
     case Site        => site
@@ -56,6 +58,7 @@ object I18nDb {
     case Tfa         => tfa
     case Settings    => settings
     case Preferences => preferences
+    case Team        => team
   }
 
   val langs: Set[Lang] = site.keys.toSet

notzmv/lidraughts

modules/i18n/src/main/I18nDb.scala
@@ -13,6 +13,7 @@ object I18nDb {
   case object Coordinates extends Ref
   case object Study extends Ref
   case object Tfa extends Ref
+  case object Team extends Ref
 
   val site: Messages = lidraughts.i18n.db.site.Registry.load
   val arena: Messages = lidraughts.i18n.db.arena.Registry.load
@@ -22,6 +23,7 @@ object I18nDb {
   val coordinates: Messages = lidraughts.i18n.db.coordinates.Registry.load
   val study: Messages = lidraughts.i18n.db.study.Registry.load
   val tfa: Messages = lidraughts.i18n.db.tfa.Registry.load
+  val team: Messages = lidraughts.i18n.db.team.Registry.load
 
   def apply(ref: Ref): Messages = ref match {
     case Site => site
@@ -32,6 +34,7 @@ object I18nDb {
     case Coordinates => coordinates
     case Study => study
     case Tfa => tfa
+    case Team => team
   }
 
   val langs: Set[Lang] = site.keys.map(Lang.apply)(scala.collection.breakOut)