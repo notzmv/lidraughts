lichess-org/lila

redesign the blog

app/controllers/Blog.scala
@@ -16,7 +16,7 @@ object Blog extends LilaController {
 
   def index(page: Int, ref: Option[String]) = WithPrismic { implicit ctx => implicit prismic =>
     pageHit
-    blogApi.recent(prismic, page, lila.common.MaxPerPage(10)) flatMap {
+    blogApi.recent(prismic, page, lila.common.MaxPerPage(12)) flatMap {
       case Some(response) => fuccess(Ok(views.html.blog.index(response)))
       case _ => notFound
     }
@@ -53,14 +53,14 @@ object Blog extends LilaController {
 
   def all = WithPrismic { implicit ctx => implicit prismic =>
     blogApi.byYear(prismic, lila.blog.thisYear) map { posts =>
-      Ok(views.html.blog.all(lila.blog.thisYear, posts))
+      Ok(views.html.blog.index.byYear(lila.blog.thisYear, posts))
     }
   }
 
   def year(year: Int) = WithPrismic { implicit ctx => implicit prismic =>
     if (lila.blog.allYears contains year)
       blogApi.byYear(prismic, year) map { posts =>
-        Ok(views.html.blog.all(year, posts))
+        Ok(views.html.blog.index.byYear(year, posts))
       }
     else notFound
   }

app/views/blog/bits.scala
@@ -3,11 +3,34 @@ package views.html.blog
 import lila.api.Context
 import lila.app.templating.Environment._
 import lila.app.ui.ScalatagsTemplate._
+import lila.blog.MiniPost
 import lila.common.String.html.richText
 
 import controllers.routes
 
 object bits {
+
+  private[blog] def menu(year: Option[Int], hasActive: Boolean = true) =
+    st.nav(cls := "page-menu__menu subnav")(
+      a(cls := (year.isEmpty && hasActive).option("active"), href := routes.Blog.index())("Latest"),
+      lila.blog.allYears map { y =>
+        a(cls := (year has y).option("active"), href := routes.Blog.year(y))(y)
+      }
+    )
+
+  private[blog] def postCard(
+    post: MiniPost,
+    postClass: Option[String] = None,
+    header: Tag = h2
+  )(implicit ctx: Context) =
+    a(cls := postClass)(href := routes.Blog.show(post.id, post.slug))(
+      st.img(src := post.image),
+      div(cls := "content")(
+        header(cls := "title")(post.title),
+        span(post.shortlede),
+        semanticDate(post.date)
+      )
+    )
 
   private[blog] def metas(doc: io.prismic.Document)(implicit ctx: Context, prismic: lila.blog.BlogApi.Context) =
     div(cls := "meta-headline")(

app/views/blog/index.scala
@@ -3,6 +3,7 @@ package views.html.blog
 import lila.api.Context
 import lila.app.templating.Environment._
 import lila.app.ui.ScalatagsTemplate._
+import lila.blog.MiniPost
 import lila.common.paginator.Paginator
 
 import controllers.routes
@@ -16,37 +17,68 @@ object index {
       csp = bits.csp,
       moreJs = infiniteScrollTag
     )(
-        main(cls := "blog list page-small box")(
-          div(cls := "box__top")(
-            h1("Lichess Official Blog"),
-            a(cls := "atom", href := routes.Blog.atom, dataIcon := "3")
-          ),
-          div(cls := "list infinitescroll")(
-            pager.currentPageResults.map { doc =>
-              st.article(cls := "paginated")(
-                doc.getText("blog.title").map { title =>
-                  h2(a(href := routes.Blog.show(doc.id, doc.slug, prismic.maybeRef))(title))
-                },
-                bits.metas(doc),
-                doc.getImage("blog.image", "main").map { img =>
-                  div(cls := "illustration")(
-                    a(href := routes.Blog.show(doc.id, doc.slug, ref = prismic.maybeRef))(st.img(src := img.url))
-                  )
-                },
-                div(cls := "body")(
-                  doc.getStructuredText("blog.body").map { body =>
-                    raw(lila.blog.BlogApi.extract(body))
-                  },
-                  p(cls := "more")(
-                    a(cls := "button", href := routes.Blog.show(doc.id, doc.slug, ref = prismic.maybeRef), dataIcon := "G")(
-                      " Continue reading this post"
-                    )
-                  )
-                )
+        main(cls := "page-menu")(
+          bits.menu(none),
+          div(cls := "blog index page-menu__content page-small box")(
+            div(cls := "box__top")(
+              h1("Lichess Official Blog"),
+              a(cls := "atom", href := routes.Blog.atom, dataIcon := "3")
+            ),
+            (pager.currentPage == 1).??(pager.currentPageResults.headOption) map { post =>
+              frag(
+                latestPost(post),
+                h2("Previous blog posts")
               )
             },
-            pagerNext(pager, np => routes.Blog.index(np, none).url)
+            div(cls := "blog-cards list infinitescroll")(
+              pager.currentPageResults flatMap MiniPost.fromDocument("blog", "wide") map { post =>
+                bits.postCard(post, "paginated".some, h3)
+              },
+              pagerNext(pager, np => routes.Blog.index(np, none).url)
+            )
           )
         )
       )
+
+  def byYear(year: Int, posts: List[MiniPost])(implicit ctx: Context) =
+    views.html.base.layout(
+      title = s"Blog posts from $year",
+      moreCss = cssTag("blog"),
+      csp = bits.csp
+    )(
+        main(cls := "page-menu")(
+          bits.menu(year.some),
+          div(cls := "page-menu__content box")(
+            div(cls := "box__top")(h1(s"Blog posts from $year")),
+            st.section(
+              div(cls := "blog-cards")(posts map { bits.postCard(_) })
+            )
+          )
+        )
+      )
+
+  private def latestPost(doc: io.prismic.Document)(implicit ctx: Context, prismic: lila.blog.BlogApi.Context) =
+    st.article(
+      doc.getText("blog.title").map { title =>
+        h2(a(href := routes.Blog.show(doc.id, doc.slug, prismic.maybeRef))(title))
+      },
+      bits.metas(doc),
+      div(cls := "parts")(
+        doc.getImage("blog.image", "main").map { img =>
+          div(cls := "illustration")(
+            a(href := routes.Blog.show(doc.id, doc.slug, ref = prismic.maybeRef))(st.img(src := img.url))
+          )
+        },
+        div(cls := "body")(
+          doc.getStructuredText("blog.body").map { body =>
+            raw(lila.blog.BlogApi.extract(body))
+          },
+          p(cls := "more")(
+            a(cls := "button", href := routes.Blog.show(doc.id, doc.slug, ref = prismic.maybeRef), dataIcon := "G")(
+              " Continue reading this post"
+            )
+          )
+        )
+      )
+    )
 }

app/views/blog/show.scala
@@ -22,26 +22,26 @@ object show {
       moreCss = cssTag("blog"),
       csp = bits.csp
     )(
-        main(cls := s"blog page-small box post ${~doc.getText("blog.cssClasses")}")(
-          h1(
-            a(href := routes.Blog.index(), dataIcon := "I", cls := "text"),
-            doc.getText("blog.title")
-          ),
-          bits.metas(doc),
-          doc.getImage("blog.image", "main").map { img =>
-            div(cls := "illustration")(st.img(src := img.url))
-          },
-          div(cls := "body embed_analyse")(
-            doc.getHtml("blog.body", prismic.linkResolver).map(lila.blog.Youtube.fixStartTimes).map(lila.blog.ProtocolFix.remove).map(raw)
-          ),
-          NotForKids {
-            div(cls := "footer")(
-              if (prismic.maybeRef.isEmpty) {
-                (doc.getDate("blog.date").exists(_.value.toDateTimeAtStartOfDay isAfter org.joda.time.DateTime.now.minusWeeks(2))) option
-                  a(href := routes.Blog.discuss(doc.id), cls := "button text discuss", dataIcon := "d")("Discuss this blog post in the forum")
-              } else p("This is a preview.")
-            )
-          }
+        main(cls := "page-menu page-small")(
+          bits.menu(none, false),
+          div(cls := s"blog page-menu__content box post ${~doc.getText("blog.cssClasses")}")(
+            h1(doc.getText("blog.title")),
+            bits.metas(doc),
+            doc.getImage("blog.image", "main").map { img =>
+              div(cls := "illustration")(st.img(src := img.url))
+            },
+            div(cls := "body embed_analyse")(
+              doc.getHtml("blog.body", prismic.linkResolver).map(lila.blog.Youtube.fixStartTimes).map(lila.blog.ProtocolFix.remove).map(raw)
+            ),
+            NotForKids {
+              div(cls := "footer")(
+                if (prismic.maybeRef.isEmpty) {
+                  (doc.getDate("blog.date").exists(_.value.toDateTimeAtStartOfDay isAfter org.joda.time.DateTime.now.minusWeeks(2))) option
+                    a(href := routes.Blog.discuss(doc.id), cls := "button text discuss", dataIcon := "d")("Discuss this blog post in the forum")
+                } else p("This is a preview.")
+              )
+            }
+          )
         )
       )
 }

notzmv/lidraughts

app/controllers/Blog.scala
@@ -16,7 +16,7 @@ object Blog extends LidraughtsController {
 
   def index(page: Int, ref: Option[String]) = WithPrismic { implicit ctx => implicit prismic =>
     pageHit
-    blogApi.recent(prismic, page, lidraughts.common.MaxPerPage(10)) flatMap {
+    blogApi.recent(prismic, page, lidraughts.common.MaxPerPage(12)) flatMap {
       case Some(response) => fuccess(Ok(views.html.blog.index(response)))
       case _ => notFound
     }
@@ -53,14 +53,14 @@ object Blog extends LidraughtsController {
 
   def all = WithPrismic { implicit ctx => implicit prismic =>
     blogApi.byYear(prismic, lidraughts.blog.thisYear) map { posts =>
-      Ok(views.html.blog.all(lidraughts.blog.thisYear, posts))
+      Ok(views.html.blog.index.byYear(lidraughts.blog.thisYear, posts))
     }
   }
 
   def year(year: Int) = WithPrismic { implicit ctx => implicit prismic =>
     if (lidraughts.blog.allYears contains year)
       blogApi.byYear(prismic, year) map { posts =>
-        Ok(views.html.blog.all(year, posts))
+        Ok(views.html.blog.index.byYear(year, posts))
       }
     else notFound
   }

app/views/blog/bits.scala
@@ -3,11 +3,34 @@ package views.html.blog
 import lidraughts.api.Context
 import lidraughts.app.templating.Environment._
 import lidraughts.app.ui.ScalatagsTemplate._
+import lidraughts.blog.MiniPost
 import lidraughts.common.String.html.richText
 
 import controllers.routes
 
 object bits {
+
+  private[blog] def menu(year: Option[Int], hasActive: Boolean = true) =
+    st.nav(cls := "page-menu__menu subnav")(
+      a(cls := (year.isEmpty && hasActive).option("active"), href := routes.Blog.index())("Latest"),
+      lidraughts.blog.allYears map { y =>
+        a(cls := (year has y).option("active"), href := routes.Blog.year(y))(y)
+      }
+    )
+
+  private[blog] def postCard(
+    post: MiniPost,
+    postClass: Option[String] = None,
+    header: Tag = h2
+  )(implicit ctx: Context) =
+    a(cls := postClass)(href := routes.Blog.show(post.id, post.slug))(
+      st.img(src := post.image),
+      div(cls := "content")(
+        header(cls := "title")(post.title),
+        span(post.shortlede),
+        semanticDate(post.date)
+      )
+    )
 
   private[blog] def metas(doc: io.prismic.Document)(implicit ctx: Context, prismic: lidraughts.blog.BlogApi.Context) =
     div(cls := "meta-headline")(

app/views/blog/index.scala
@@ -3,6 +3,7 @@ package views.html.blog
 import lidraughts.api.Context
 import lidraughts.app.templating.Environment._
 import lidraughts.app.ui.ScalatagsTemplate._
+import lidraughts.blog.MiniPost
 import lidraughts.common.paginator.Paginator
 
 import controllers.routes
@@ -16,37 +17,68 @@ object index {
       csp = bits.csp,
       moreJs = infiniteScrollTag
     )(
-        main(cls := "blog list page-small box")(
-          div(cls := "box__top")(
-            h1("Lidraughts Official Blog"),
-            a(cls := "atom", href := routes.Blog.atom, dataIcon := "3")
-          ),
-          div(cls := "list infinitescroll")(
-            pager.currentPageResults.map { doc =>
-              st.article(cls := "paginated")(
-                doc.getText("blog.title").map { title =>
-                  h2(a(href := routes.Blog.show(doc.id, doc.slug, prismic.maybeRef))(title))
-                },
-                bits.metas(doc),
-                doc.getImage("blog.image", "main").map { img =>
-                  div(cls := "illustration")(
-                    a(href := routes.Blog.show(doc.id, doc.slug, ref = prismic.maybeRef))(st.img(src := img.url))
-                  )
-                },
-                div(cls := "body")(
-                  doc.getStructuredText("blog.body").map { body =>
-                    raw(lidraughts.blog.BlogApi.extract(body))
-                  },
-                  p(cls := "more")(
-                    a(cls := "button", href := routes.Blog.show(doc.id, doc.slug, ref = prismic.maybeRef), dataIcon := "G")(
-                      " Continue reading this post"
-                    )
-                  )
-                )
+        main(cls := "page-menu")(
+          bits.menu(none),
+          div(cls := "blog index page-menu__content page-small box")(
+            div(cls := "box__top")(
+              h1("Lidraughts Official Blog"),
+              a(cls := "atom", href := routes.Blog.atom, dataIcon := "3")
+            ),
+            (pager.currentPage == 1).??(pager.currentPageResults.headOption) map { post =>
+              frag(
+                latestPost(post),
+                h2("Previous blog posts")
               )
             },
-            pagerNext(pager, np => routes.Blog.index(np, none).url)
+            div(cls := "blog-cards list infinitescroll")(
+              pager.currentPageResults flatMap MiniPost.fromDocument("blog", "wide") map { post =>
+                bits.postCard(post, "paginated".some, h3)
+              },
+              pagerNext(pager, np => routes.Blog.index(np, none).url)
+            )
           )
         )
       )
+
+  def byYear(year: Int, posts: List[MiniPost])(implicit ctx: Context) =
+    views.html.base.layout(
+      title = s"Blog posts from $year",
+      moreCss = cssTag("blog"),
+      csp = bits.csp
+    )(
+        main(cls := "page-menu")(
+          bits.menu(year.some),
+          div(cls := "page-menu__content box")(
+            div(cls := "box__top")(h1(s"Blog posts from $year")),
+            st.section(
+              div(cls := "blog-cards")(posts map { bits.postCard(_) })
+            )
+          )
+        )
+      )
+
+  private def latestPost(doc: io.prismic.Document)(implicit ctx: Context, prismic: lidraughts.blog.BlogApi.Context) =
+    st.article(
+      doc.getText("blog.title").map { title =>
+        h2(a(href := routes.Blog.show(doc.id, doc.slug, prismic.maybeRef))(title))
+      },
+      bits.metas(doc),
+      div(cls := "parts")(
+        doc.getImage("blog.image", "main").map { img =>
+          div(cls := "illustration")(
+            a(href := routes.Blog.show(doc.id, doc.slug, ref = prismic.maybeRef))(st.img(src := img.url))
+          )
+        },
+        div(cls := "body")(
+          doc.getStructuredText("blog.body").map { body =>
+            raw(lidraughts.blog.BlogApi.extract(body))
+          },
+          p(cls := "more")(
+            a(cls := "button", href := routes.Blog.show(doc.id, doc.slug, ref = prismic.maybeRef), dataIcon := "G")(
+              " Continue reading this post"
+            )
+          )
+        )
+      )
+    )
 }

app/views/blog/show.scala
@@ -22,26 +22,26 @@ object show {
       moreCss = cssTag("blog"),
       csp = bits.csp
     )(
-        main(cls := s"blog page-small box post ${~doc.getText("blog.cssClasses")}")(
-          h1(
-            a(href := routes.Blog.index(), dataIcon := "I", cls := "text"),
-            doc.getText("blog.title")
-          ),
-          bits.metas(doc),
-          doc.getImage("blog.image", "main").map { img =>
-            div(cls := "illustration")(st.img(src := img.url))
-          },
-          div(cls := "body embed_analyse")(
-            doc.getHtml("blog.body", prismic.linkResolver).map(lidraughts.blog.Youtube.fixStartTimes).map(lidraughts.blog.ProtocolFix.remove).map(raw)
-          ),
-          NotForKids {
-            div(cls := "footer")(
-              if (prismic.maybeRef.isEmpty) {
-                (doc.getDate("blog.date").exists(_.value.toDateTimeAtStartOfDay isAfter org.joda.time.DateTime.now.minusWeeks(2))) option
-                  a(href := routes.Blog.discuss(doc.id), cls := "button text discuss", dataIcon := "d")("Discuss this blog post in the forum")
-              } else p("This is a preview.")
-            )
-          }
+        main(cls := "page-menu page-small")(
+          bits.menu(none, false),
+          div(cls := s"blog page-menu__content box post ${~doc.getText("blog.cssClasses")}")(
+            h1(doc.getText("blog.title")),
+            bits.metas(doc),
+            doc.getImage("blog.image", "main").map { img =>
+              div(cls := "illustration")(st.img(src := img.url))
+            },
+            div(cls := "body embed_analyse")(
+              doc.getHtml("blog.body", prismic.linkResolver).map(lidraughts.blog.Youtube.fixStartTimes).map(lidraughts.blog.ProtocolFix.remove).map(raw)
+            ),
+            NotForKids {
+              div(cls := "footer")(
+                if (prismic.maybeRef.isEmpty) {
+                  (doc.getDate("blog.date").exists(_.value.toDateTimeAtStartOfDay isAfter org.joda.time.DateTime.now.minusWeeks(2))) option
+                    a(href := routes.Blog.discuss(doc.id), cls := "button text discuss", dataIcon := "d")("Discuss this blog post in the forum")
+                } else p("This is a preview.")
+              )
+            }
+          )
         )
       )
 }