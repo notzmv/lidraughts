f2095bc523816f693d332948cd9abecdd2fde3c7
Stefan Schermann <stefan.schermann@gmail.com>
2020-12-28 21:26:10 +0100

    place hash after query

--- ui/analyse/src/serverSideUnderboard.ts
+++ ui/analyse/src/serverSideUnderboard.ts
@@ -154,8 +154,8 @@ export default function(element: HTMLElement, ctrl: AnalyseCtrl) {
     }
   });
   $panels.on('click', '.embed-howto', function(this: HTMLElement) {
-    const url = `${baseUrl()}/embed/${data.game.id}${location.hash}`;
-    const iframe = '<iframe src="' + url + '?theme=auto&bg=auto"\nwidth=600 height=397 frameborder=0></iframe>';
+    const url = `${baseUrl()}/embed/${data.game.id}?theme=auto&bg=auto${location.hash}`;
+    const iframe = '<iframe src="' + url + '"\nwidth=600 height=397 frameborder=0></iframe>';
     $.modal($(
       '<strong style="font-size:1.5em">' + $(this).html() + '</strong><br /><br />' +
       '<pre>' + li.escapeHtml(iframe) + '</pre><br />' +
