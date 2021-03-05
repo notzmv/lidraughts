lichess-org/lila

show players' teams in team battle games

app/controllers/Round.scala
@@ -8,7 +8,7 @@ import lila.app._
 import lila.chat.Chat
 import lila.common.HTTPRequest
 import lila.game.{ Pov, Game => GameModel, PgnDump }
-import lila.tournament.{ TourMiniView, Tournament => Tour }
+import lila.tournament.{ Tournament => Tour }
 import lila.user.{ User => UserModel }
 import views._
 
@@ -30,7 +30,7 @@ final class Round(
         else
           PreventTheft(pov) {
             pov.game.playableByAi ?? env.fishnet.player(pov.game)
-            myTour(pov.game.tournamentId, true) flatMap {
+            env.tournament.api.miniView(pov.game, true) flatMap {
               tour =>
                 gameC.preloadUsers(pov.game) zip
                   (pov.game.simulId ?? env.simul.repo.find) zip
@@ -155,7 +155,7 @@ final class Round(
           html = {
             if (pov.game.replayable) analyseC.replay(pov, userTv = userTv)
             else if (HTTPRequest.isHuman(ctx.req))
-              myTour(pov.game.tournamentId, false) zip
+              env.tournament.api.miniView(pov.game, false) zip
                 (pov.game.simulId ?? env.simul.repo.find) zip
                 getWatcherChat(pov.game) zip
                 (ctx.noBlind ?? env.game.crosstableApi.withMatchup(pov.game)) zip
@@ -199,9 +199,6 @@ final class Round(
             }
         ) map { NoCache(_) }
     }
-
-  private def myTour(tourId: Option[String], withTop: Boolean): Fu[Option[TourMiniView]] =
-    tourId ?? { env.tournament.api.miniView(_, withTop) }
 
   private[controllers] def getWatcherChat(
       game: GameModel
@@ -258,7 +255,7 @@ final class Round(
 
   def sides(gameId: String, color: String) = Open { implicit ctx =>
     OptionFuResult(proxyPov(gameId, color)) { pov =>
-      (pov.game.tournamentId ?? env.tournament.tournamentRepo.byId) zip
+      env.tournament.api.withTeamVs(pov.game) zip
         (pov.game.simulId ?? env.simul.repo.find) zip
         env.game.gameRepo.initialFen(pov.game) zip
         env.game.crosstableApi.withMatchup(pov.game) zip

app/views/game/bits.scala
@@ -40,7 +40,7 @@ object bits {
   def sides(
       pov: Pov,
       initialFen: Option[chess.format.FEN],
-      tour: Option[lila.tournament.Tournament],
+      tour: Option[lila.tournament.TourAndTeamVs],
       cross: Option[lila.game.Crosstable.WithMatchup],
       simul: Option[lila.simul.Simul],
       userTv: Option[lila.user.User] = None,

app/views/game/side.scala
@@ -16,7 +16,7 @@ object side {
   def apply(
       pov: lila.game.Pov,
       initialFen: Option[chess.format.FEN],
-      tour: Option[lila.tournament.Tournament],
+      tour: Option[lila.tournament.TourAndTeamVs],
       simul: Option[lila.simul.Simul],
       userTv: Option[lila.user.User] = None,
       bookmarked: Boolean
@@ -28,7 +28,7 @@ object side {
   def meta(
       pov: lila.game.Pov,
       initialFen: Option[chess.format.FEN],
-      tour: Option[lila.tournament.Tournament],
+      tour: Option[lila.tournament.TourAndTeamVs],
       simul: Option[lila.simul.Simul],
       userTv: Option[lila.user.User] = None,
       bookmarked: Boolean
@@ -88,8 +88,11 @@ object side {
         ),
         div(cls := "game__meta__players")(
           game.players.map { p =>
-            div(cls := s"player color-icon is ${p.color.name} text")(
-              playerLink(p, withOnline = false, withDiff = true, withBerserk = true)
+            frag(
+              div(cls := s"player color-icon is ${p.color.name} text")(
+                playerLink(p, withOnline = false, withDiff = true, withBerserk = true)
+              ),
+              tour.flatMap(_.teamVs).map(_.teams(p.color)) map { teamLink(_, false)(cls := "team") }
             )
           }
         )
@@ -124,8 +127,8 @@ object side {
       },
       tour.map { t =>
         st.section(cls := "game__tournament")(
-          a(cls := "text", dataIcon := "g", href := routes.Tournament.show(t.id))(t.fullName),
-          div(cls := "clock", dataTime := t.secondsToFinish)(div(cls := "time")(t.clockStatus))
+          a(cls := "text", dataIcon := "g", href := routes.Tournament.show(t.tour.id))(t.tour.fullName),
+          div(cls := "clock", dataTime := t.tour.secondsToFinish)(div(cls := "time")(t.tour.clockStatus))
         )
       } orElse {
         game.tournamentId map { tourId =>

modules/tournament/src/main/TeamBattle.scala
@@ -17,6 +17,8 @@ object TeamBattle {
 
   def init(teamId: TeamID) = TeamBattle(Set(teamId), 5)
 
+  case class TeamVs(teams: chess.Color.Map[TeamID])
+
   case class RankedTeam(
       rank: Int,
       teamId: TeamID,

notzmv/lidraughts

app/controllers/Round.scala
@@ -8,7 +8,7 @@ import lidraughts.app._
 import lidraughts.chat.Chat
 import lidraughts.common.HTTPRequest
 import lidraughts.game.{ Pov, GameRepo, Game => GameModel, PdnDump, PlayerRef }
-import lidraughts.tournament.{ TourMiniView, Tournament => Tour }
+import lidraughts.tournament.{ Tournament => Tour }
 import lidraughts.user.{ User => UserModel }
 import views._
 
@@ -62,7 +62,7 @@ object Round extends LidraughtsController with TheftPrevention {
   private def renderPlayer(pov: Pov)(implicit ctx: Context): Fu[Result] = negotiate(
     html = if (!pov.game.started) notFound
     else PreventTheft(pov) {
-      myTour(pov.game.tournamentId, true) flatMap { tour =>
+      Env.tournament.api.miniView(pov.game, true) flatMap { tour =>
         (pov.game.simulId ?? Env.simul.repo.find) flatMap { simul =>
           Game.preloadUsers(pov.game) zip
             getPlayerChat(pov.game, tour.map(_.tour), simul) zip
@@ -205,7 +205,7 @@ object Round extends LidraughtsController with TheftPrevention {
           if (getBool("sudo") && isGranted(_.SuperAdmin)) Redirect(routes.Round.player(pov.fullId)).fuccess
           else if (pov.game.replayable) Analyse.replay(pov, userTv = userTv, userTvGameId = userTvGameId)
           else if (HTTPRequest.isHuman(ctx.req))
-            myTour(pov.game.tournamentId, false) zip
+            Env.tournament.api.miniView(pov.game, false) zip
               (pov.game.simulId ?? Env.simul.repo.find) zip
               getWatcherChat(pov.game) zip
               (ctx.noBlind ?? Env.game.crosstableApi.withMatchup(pov.game)) zip
@@ -234,9 +234,6 @@ object Round extends LidraughtsController with TheftPrevention {
         }
       ) map NoCache
     }
-
-  private def myTour(tourId: Option[String], withTop: Boolean): Fu[Option[TourMiniView]] =
-    tourId ?? { Env.tournament.api.miniView(_, withTop) }
 
   private[controllers] def getWatcherChat(game: GameModel)(implicit ctx: Context): Fu[Option[lidraughts.chat.UserChat.Mine]] = {
     ctx.noKid && ctx.me.fold(true)(Env.chat.panic.allowed) && {
@@ -276,7 +273,7 @@ object Round extends LidraughtsController with TheftPrevention {
 
   def sides(gameId: String, color: String) = Open { implicit ctx =>
     OptionFuResult(proxyPov(gameId, color)) { pov =>
-      (pov.game.tournamentId ?? lidraughts.tournament.TournamentRepo.byId) zip
+      Env.tournament.api.withTeamVs(pov.game) zip
         (pov.game.simulId ?? Env.simul.repo.find) zip
         GameRepo.initialFen(pov.game) zip
         Env.game.crosstableApi.withMatchup(pov.game) zip

app/views/game/bits.scala
@@ -42,7 +42,7 @@ object bits {
   def sides(
     pov: Pov,
     initialFen: Option[draughts.format.FEN],
-    tour: Option[lidraughts.tournament.Tournament],
+    tour: Option[lidraughts.tournament.TourAndTeamVs],
     cross: Option[lidraughts.game.Crosstable.WithMatchup],
     simul: Option[lidraughts.simul.Simul],
     userTv: Option[lidraughts.user.User] = None,

app/views/game/side.scala
@@ -16,7 +16,7 @@ object side {
   def apply(
     pov: lidraughts.game.Pov,
     initialFen: Option[draughts.format.FEN],
-    tour: Option[lidraughts.tournament.Tournament],
+    tour: Option[lidraughts.tournament.TourAndTeamVs],
     simul: Option[lidraughts.simul.Simul],
     userTv: Option[lidraughts.user.User] = None,
     bookmarked: Boolean
@@ -28,7 +28,7 @@ object side {
   def meta(
     pov: lidraughts.game.Pov,
     initialFen: Option[draughts.format.FEN],
-    tour: Option[lidraughts.tournament.Tournament],
+    tour: Option[lidraughts.tournament.TourAndTeamVs],
     simul: Option[lidraughts.simul.Simul],
     userTv: Option[lidraughts.user.User] = None,
     bookmarked: Boolean
@@ -77,8 +77,11 @@ object side {
         ),
         div(cls := "game__meta__players")(
           game.players.map { p =>
-            div(cls := s"player color-icon is ${p.color.name} text")(
-              playerLink(p, withOnline = false, withDiff = true, withBerserk = true)
+            frag(
+              div(cls := s"player color-icon is ${p.color.name} text")(
+                playerLink(p, withOnline = false, withDiff = true, withBerserk = true)
+              ),
+              tour.flatMap(_.teamVs).map(_.teams(p.color)) map { teamLink(_, false)(cls := "team") }
             )
           }
         )
@@ -103,8 +106,8 @@ object side {
 
       tour.map { t =>
         st.section(cls := "game__tournament")(
-          a(cls := "text", dataIcon := "g", href := routes.Tournament.show(t.id))(t.fullName),
-          div(cls := "clock", dataTime := t.secondsToFinish)(div(cls := "time")(t.clockStatus))
+          a(cls := "text", dataIcon := "g", href := routes.Tournament.show(t.tour.id))(t.tour.fullName),
+          div(cls := "clock", dataTime := t.tour.secondsToFinish)(div(cls := "time")(t.tour.clockStatus))
         )
       } orElse game.tournamentId.map { tourId =>
         st.section(cls := "game__tournament-link")(tournamentLink(tourId))

modules/tournament/src/main/TeamBattle.scala
@@ -23,6 +23,8 @@ object TeamBattle {
 
   def init(teamId: TeamId) = TeamBattle(Set(teamId), 5)
 
+  case class TeamVs(teams: draughts.Color.Map[TeamId])
+
   case class RankedTeam(
       rank: Int,
       teamId: TeamId,