lichess-org/lila

add /api/tournament/:id/teams API endpoint for team battles - for #6756

app/controllers/Api.scala
@@ -259,6 +259,22 @@ final class Api(
       }
     }
 
+  def tournamentTeams(id: String) =
+    Action.async { implicit req =>
+      env.tournament.tournamentRepo byId id flatMap {
+        _ ?? { tour =>
+          env.tournament.jsonView.getTeamStanding(tour) map { arr =>
+            JsonOk(
+              Json.obj(
+                "id"    -> tour.id,
+                "teams" -> arr
+              )
+            )
+          }
+        }
+      }
+    }
+
   def tournamentsByOwner(name: String) =
     Action.async { implicit req =>
       implicit val lang = reqLang

notzmv/lidraughts

app/controllers/Api.scala
@@ -266,6 +266,21 @@ object Api extends LidraughtsController {
     }
   }
 
+  def tournamentTeams(id: String) = Action.async { req =>
+    lidraughts.tournament.TournamentRepo byId id flatMap {
+      _ ?? { tour =>
+        Env.tournament.jsonView.getTeamStanding(tour) map { arr =>
+          JsonOk(
+            Json.obj(
+              "id" -> tour.id,
+              "teams" -> arr
+            )
+          )
+        }
+      }
+    }
+  }
+
   def tournamentsByOwner(name: String) = Action.async { req =>
     (name != "lidraughts") ?? UserRepo.named(name) flatMap {
       _ ?? { user =>