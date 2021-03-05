lichess-org/lila

send info about my team in large battles

modules/tournament/src/main/TeamBattle.scala
@@ -11,6 +11,8 @@ case class TeamBattle(
 ) {
   def hasEnoughTeams     = teams.sizeIs > 1
   lazy val sortedTeamIds = teams.toList.sorted
+
+  def hasTooManyTeams = teams.sizeIs > TeamBattle.displayTeams
 }
 
 object TeamBattle {

modules/tournament/src/main/model.scala
@@ -2,6 +2,8 @@ package lila.tournament
 
 import play.api.i18n.Lang
 
+import lila.hub.LightTeam.TeamID
+
 final class LeaderboardRepo(val coll: lila.db.dsl.Coll)
 
 case class TournamentTop(value: List[Player]) extends AnyVal
@@ -25,7 +27,7 @@ case class GameView(
   def tourAndTeamVs = TourAndTeamVs(tour, teamVs)
 }
 
-case class MyInfo(rank: Int, withdraw: Boolean, gameId: Option[lila.game.Game.ID]) {
+case class MyInfo(rank: Int, withdraw: Boolean, gameId: Option[lila.game.Game.ID], teamId: Option[TeamID]) {
   def page = (rank + 9) / 10
 }
 

notzmv/lidraughts

modules/tournament/src/main/TeamBattle.scala
@@ -12,6 +12,8 @@ case class TeamBattle(
 ) {
   def hasEnoughTeams = teams.size > 1
   lazy val sortedTeamIds = teams.toList.sorted
+
+  def hasTooManyTeams = teams.size > TeamBattle.displayTeams
 }
 
 object TeamBattle {

modules/tournament/src/main/model.scala
@@ -1,12 +1,12 @@
 package lidraughts.tournament
 
-import lidraughts.hub.lightTeam.LightTeam
+import lidraughts.hub.lightTeam.{ LightTeam, TeamId }
 
 case class TournamentTop(value: List[Player]) extends AnyVal
 
 case class TourMiniView(tour: Tournament, top: Option[TournamentTop])
 
-case class MyInfo(rank: Int, withdraw: Boolean, gameId: Option[lidraughts.game.Game.ID]) {
+case class MyInfo(rank: Int, withdraw: Boolean, gameId: Option[lidraughts.game.Game.ID], teamId: Option[TeamId]) {
   def page = {
     math.floor((rank - 1) / 10) + 1
   }.toInt