lichess-org/lila

add ?matchup flag to /api/crosstable - for #7322

app/controllers/Api.scala
@@ -178,9 +178,13 @@ final class Api(
   def crosstable(u1: String, u2: String) =
     ApiRequest { req =>
       CrosstableRateLimitPerIP(HTTPRequest lastRemoteAddress req, cost = 1) {
-        env.game.crosstableApi.fetchOrEmpty(u1, u2) map { ct =>
-          toApiResult {
-            lila.game.JsonView.crosstableWrites.writes(ct).some
+        env.game.crosstableApi.fetchOrEmpty(u1, u2) flatMap { ct =>
+          (ct.results.nonEmpty && getBool("matchup", req)).?? {
+            env.game.crosstableApi.getMatchup(u1, u2)
+          } map { matchup =>
+            toApiResult {
+              lila.game.JsonView.crosstable(ct, matchup).some
+            }
           }
         }
       }(fuccess(Limited))

notzmv/lidraughts

app/controllers/Api.scala
@@ -199,9 +199,13 @@ object Api extends LidraughtsController {
 
   def crosstable(u1: String, u2: String) = ApiRequest { req =>
     CrosstableRateLimitPerIP(HTTPRequest lastRemoteAddress req, cost = 1) {
-      Env.game.crosstableApi(u1, u2, timeout = 15.seconds) map { ct =>
-        toApiResult {
-          lidraughts.game.JsonView.crosstableWrites.writes(ct).some
+      Env.game.crosstableApi(u1, u2, timeout = 15.seconds) flatMap { ct =>
+        (ct.results.nonEmpty && getBool("matchup", req)).?? {
+          Env.game.crosstableApi.getMatchup(u1, u2)
+        } map { matchup =>
+          toApiResult {
+            lidraughts.game.JsonView.crosstable(ct, matchup).some
+          }
         }
       }
     }