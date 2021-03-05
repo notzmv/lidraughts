lichess-org/lila

withdraw from arena when kicked from team

modules/team/src/main/TeamApi.scala
@@ -9,7 +9,7 @@ import scala.util.Try
 import actorApi._
 import lila.common.Bus
 import lila.db.dsl._
-import lila.hub.actorApi.team.{ CreateTeam, JoinTeam }
+import lila.hub.actorApi.team.{ CreateTeam, JoinTeam, KickFromTeam }
 import lila.hub.actorApi.timeline.{ Propagate, TeamCreate, TeamJoin }
 import lila.hub.LightTeam
 import lila.memo.CacheApi._
@@ -195,9 +195,10 @@ final class TeamApi(
 
   def kick(team: Team, userId: User.ID, me: User): Funit =
     doQuit(team, userId) >>
-      !team.leaders(me.id) ?? {
+      (!team.leaders(me.id)).?? {
         modLog.teamKick(me.id, userId, team.name)
-      }
+      } >>-
+      Bus.publish(KickFromTeam(teamId = team.id, userId = userId), "teamKick")
 
   private case class TagifyUser(value: String)
   implicit private val TagifyUserReads = Json.reads[TagifyUser]

modules/tournament/src/main/TournamentApi.scala
@@ -473,6 +473,18 @@ final class TournamentApi(
       }.sequenceFu.void
     }
 
+  private[tournament] def kickFromTeam(teamId: TeamID, userId: User.ID): Funit =
+    tournamentRepo.withdrawableIds(userId, teamId = teamId.some) flatMap {
+      _.map { tourId =>
+        Sequencing(tourId)(tournamentRepo.byId) { tour =>
+          val fu =
+            if (tour.isCreated) playerRepo.remove(tour.id, userId)
+            else playerRepo.withdraw(tour.id, userId)
+          fu >> updateNbPlayers(tourId) >>- socket.reload(tourId)
+        }
+      }.sequenceFu.void
+    }
+
   def ejectLame(userId: User.ID, playedIds: List[Tournament.ID]): Funit =
     tournamentRepo.withdrawableIds(userId) flatMap { tourIds =>
       (tourIds ::: playedIds).distinct

notzmv/lidraughts

modules/team/src/main/TeamApi.scala
@@ -3,7 +3,7 @@ package lidraughts.team
 import actorApi._
 import akka.actor.ActorSelection
 import lidraughts.db.dsl._
-import lidraughts.hub.actorApi.team.{ CreateTeam, JoinTeam }
+import lidraughts.hub.actorApi.team.{ CreateTeam, JoinTeam, KickFromTeam }
 import lidraughts.hub.actorApi.timeline.{ Propagate, TeamJoin, TeamCreate }
 import lidraughts.hub.lightTeam.LightTeam
 import lidraughts.mod.ModlogApi
@@ -174,9 +174,10 @@ final class TeamApi(
 
   def kick(team: Team, userId: User.ID, me: User): Funit =
     doQuit(team, userId) >>
-      !team.isCreator(me.id) ?? {
+      (!team.isCreator(me.id)).?? {
         modLog.teamKick(me.id, userId, team.name)
-      }
+      } >>-
+      bus.publish(KickFromTeam(teamId = team.id, userId = userId), 'teamKick)
 
   def changeOwner(team: Team, userId: User.ID, me: User): Funit =
     MemberRepo.exists(team.id, userId) flatMap { e =>

modules/tournament/src/main/TournamentApi.scala
@@ -377,7 +377,7 @@ final class TournamentApi(
   }
 
   def withdrawAll(user: User): Unit =
-    TournamentRepo.nonEmptyEnterableIds foreach {
+    TournamentRepo.nonEmptyEnterableIds() foreach {
       PlayerRepo.filterExists(_, user.id) foreach {
         _ foreach {
           withdraw(_, user.id, isPause = false, isStalling = false)
@@ -472,8 +472,22 @@ final class TournamentApi(
       }
     }
 
+  private[tournament] def kickFromTeam(teamId: TeamId, userId: User.ID): Unit =
+    TournamentRepo.nonEmptyEnterableIds(teamId.some) foreach {
+      PlayerRepo.filterExists(_, userId) foreach {
+        _ foreach { tourId =>
+          Sequencing(tourId)(TournamentRepo.byId) { tour =>
+            val fu =
+              if (tour.isCreated) PlayerRepo.remove(tour.id, userId)
+              else PlayerRepo.withdraw(tour.id, userId)
+            fu >> updateNbPlayers(tourId) >>- socketReload(tourId)
+          }
+        }
+      }
+    }
+
   def ejectLame(userId: User.ID, playedIds: List[Tournament.ID]): Unit =
-    TournamentRepo.nonEmptyEnterableIds foreach {
+    TournamentRepo.nonEmptyEnterableIds() foreach {
       PlayerRepo.filterExists(_, userId) foreach { enteredIds =>
         (enteredIds ++ playedIds).foreach { ejectLame(_, userId) }
       }