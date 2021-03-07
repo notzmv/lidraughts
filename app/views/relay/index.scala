lichess-org/lila

/broadcast translation WIP

app/views/relay/index.scala
@@ -11,13 +11,15 @@ import controllers.routes
 
 object index {
 
+  import trans.broadcast._
+
   def apply(
       fresh: Option[lila.relay.Relay.Fresh],
       pager: Paginator[lila.relay.Relay.WithStudyAndLiked],
       url: Call
   )(implicit ctx: Context) = {
 
-    def sublist(name: String, relays: Seq[lila.relay.Relay.WithStudyAndLiked]) =
+    def sublist(name: Frag, relays: Seq[lila.relay.Relay.WithStudyAndLiked]) =
       relays.nonEmpty option st.section(
         h2(name),
         div(cls := "list")(
@@ -26,28 +28,28 @@ object index {
       )
 
     views.html.base.layout(
-      title = "Live tournament broadcasts",
+      title = liveBroadcasts.txt(),
       moreCss = cssTag("relay.index"),
       moreJs = infiniteScrollTag
     ) {
       main(cls := "relay-index page-small box")(
         div(cls := "box__top")(
-          h1("Live tournament broadcasts"),
+          h1(liveBroadcasts()),
           a(
             href := routes.Relay.form,
             cls := "new button button-empty",
-            title := "New Broadcast",
+            title := newBroadcast.txt(),
             dataIcon := "O"
           )
         ),
         fresh.map { f =>
           frag(
-            sublist("Ongoing", f.started),
-            sublist("Upcoming", f.created)
+            sublist(ongoing(), f.started),
+            sublist(upcoming(), f.created)
           )
         },
         st.section(
-          h2("Completed"),
+          h2(completed()),
           div(cls := "infinitescroll")(
             pager.currentPageResults.map { show.widget(_, "paginated") },
             pagerNext(pager, np => addQueryParameter(url.url, "page", np))

modules/i18n/src/main/I18nDb.scala
@@ -16,6 +16,7 @@ object I18nDb {
   case object Contact     extends Ref
   case object Patron      extends Ref
   case object Coach       extends Ref
+  case object Broadcast   extends Ref
 
   val site: Messages        = lila.i18n.db.site.Registry.load
   val arena: Messages       = lila.i18n.db.arena.Registry.load
@@ -28,6 +29,7 @@ object I18nDb {
   val contact: Messages     = lila.i18n.db.contact.Registry.load
   val patron: Messages      = lila.i18n.db.patron.Registry.load
   val coach: Messages       = lila.i18n.db.coach.Registry.load
+  val broadcast: Messages   = lila.i18n.db.broadcast.Registry.load
 
   def apply(ref: Ref): Messages = ref match {
     case Site        => site
@@ -41,6 +43,7 @@ object I18nDb {
     case Contact     => contact
     case Patron      => patron
     case Coach       => coach
+    case Broadcast   => broadcast
   }
 
   val langs: Set[Lang] = site.keys.toSet

notzmv/lidraughts

app/views/relay/index.scala
@@ -11,13 +11,15 @@ import controllers.routes
 
 object index {
 
+  import trans.broadcast._
+
   def apply(
     fresh: Option[lidraughts.relay.Relay.Fresh],
     pager: Paginator[lidraughts.relay.Relay.WithStudyAndLiked],
     url: Call
   )(implicit ctx: Context) = {
 
-    def sublist(name: String, relays: Seq[lidraughts.relay.Relay.WithStudyAndLiked]) =
+    def sublist(name: Frag, relays: Seq[lidraughts.relay.Relay.WithStudyAndLiked]) =
       relays.nonEmpty option st.section(
         h2(name),
         div(cls := "list")(
@@ -26,23 +28,23 @@ object index {
       )
 
     views.html.base.layout(
-      title = "Live tournament broadcasts",
+      title = liveBroadcasts.txt(),
       moreCss = cssTag("relay.index"),
       moreJs = infiniteScrollTag
     ) {
         main(cls := "relay-index page-small box")(
           div(cls := "box__top")(
-            h1("Live tournament broadcasts"),
-            isGranted(_.Relay) option a(href := routes.Relay.form, cls := "new button button-empty", title := "New Broadcast", dataIcon := "O")
+            h1(liveBroadcasts()),
+            isGranted(_.Relay) option a(href := routes.Relay.form, cls := "new button button-empty", title := newBroadcast.txt(), dataIcon := "O")
           ),
           fresh.map { f =>
             frag(
-              sublist("Ongoing", f.started),
-              sublist("Upcoming", f.created)
+              sublist(ongoing(), f.started),
+              sublist(upcoming(), f.created)
             )
           },
           st.section(
-            h2("Completed"),
+            h2(completed()),
             div(cls := "infinitescroll")(
               pager.currentPageResults.map { show.widget(_, "paginated") },
               pagerNext(pager, np => addQueryParameter(url.url, "page", np))

modules/i18n/src/main/I18nDb.scala
@@ -15,6 +15,7 @@ object I18nDb {
   case object Tfa extends Ref
   case object Team extends Ref
   case object Swiss extends Ref
+  case object Broadcast extends Ref
 
   val site: Messages = lidraughts.i18n.db.site.Registry.load
   val arena: Messages = lidraughts.i18n.db.arena.Registry.load
@@ -26,6 +27,7 @@ object I18nDb {
   val tfa: Messages = lidraughts.i18n.db.tfa.Registry.load
   val team: Messages = lidraughts.i18n.db.team.Registry.load
   val swiss: Messages = lidraughts.i18n.db.swiss.Registry.load
+  val broadcast: Messages = lidraughts.i18n.db.broadcast.Registry.load
 
   def apply(ref: Ref): Messages = ref match {
     case Site => site
@@ -38,6 +40,7 @@ object I18nDb {
     case Tfa => tfa
     case Team => team
     case Swiss => swiss
+    case Broadcast => broadcast
   }
 
   val langs: Set[Lang] = site.keys.map(Lang.apply)(scala.collection.breakOut)