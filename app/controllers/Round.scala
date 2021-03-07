lichess-org/lila

fix tournament berserk on mobile - fixes veloce/lichobile#1161

app/controllers/Round.scala
@@ -59,17 +59,18 @@ final class Round(
           },
       api = apiVersion => {
         if (isTheft(pov)) fuccess(theftResponse)
-        else {
-          pov.game.playableByAi ?? env.fishnet.player(pov.game)
-          gameC.preloadUsers(pov.game) zip
-            env.api.roundApi.player(pov, none, apiVersion) zip
-            getPlayerChat(pov.game, none) map {
-            case _ ~ data ~ chat =>
-              Ok {
-                data.add("chat", chat.flatMap(_.game).map(c => lila.chat.JsonView(c.chat)))
-              }
+        else
+          env.tournament.api.gameView.mobile(pov.game) flatMap { tour =>
+            pov.game.playableByAi ?? env.fishnet.player(pov.game)
+            gameC.preloadUsers(pov.game) zip
+              env.api.roundApi.player(pov, tour, apiVersion) zip
+              getPlayerChat(pov.game, none) map {
+              case _ ~ data ~ chat =>
+                Ok {
+                  data.add("chat", chat.flatMap(_.game).map(c => lila.chat.JsonView(c.chat)))
+                }
+            }
           }
-        }
       }
     ) dmap NoCache
 

notzmv/lidraughts

app/controllers/Round.scala
@@ -85,13 +85,15 @@ object Round extends LidraughtsController with TheftPrevention {
     }.mon(_.http.response.player.website),
     api = apiVersion => {
       if (isTheft(pov)) fuccess(theftResponse)
-      else Game.preloadUsers(pov.game) zip
-        Env.api.roundApi.player(pov, none, apiVersion) zip
-        getPlayerChat(pov.game, none, none) map {
-          case _ ~ data ~ chat => Ok {
-            data.add("chat", chat.flatMap(_.game).map(c => lidraughts.chat.JsonView(c.chat)))
+      else Env.tournament.api.gameView.mobile(pov.game) flatMap { tour =>
+        Game.preloadUsers(pov.game) zip
+          Env.api.roundApi.player(pov, tour, apiVersion) zip
+          getPlayerChat(pov.game, none, none) map {
+            case _ ~ data ~ chat => Ok {
+              data.add("chat", chat.flatMap(_.game).map(c => lidraughts.chat.JsonView(c.chat)))
+            }
           }
-        }
+      }
     }.mon(_.http.response.player.mobile)
   ) map NoCache
 