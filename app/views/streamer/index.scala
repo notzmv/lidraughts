lichess-org/lila

translate /streamer

app/views/streamer/index.scala
@@ -8,6 +8,8 @@ import lila.common.paginator.Paginator
 
 object index {
 
+  import trans.streamer._
+
   private val dataDedup = attr("data-dedup")
 
   def apply(
@@ -16,7 +18,7 @@ object index {
       requests: Boolean
   )(implicit ctx: Context) = {
 
-    val title = if (requests) "Streamer approval requests" else "Lichess streamers"
+    val title = if (requests) "Streamer approval requests" else lichessStreamers.txt()
 
     def widget(s: lila.streamer.Streamer.WithUser, stream: Option[lila.streamer.Stream]) = frag(
       a(
@@ -26,7 +28,7 @@ object index {
           else routes.Streamer.show(s.user.username).url
         }
       ),
-      stream.isDefined option span(cls := "ribbon")(span("LIVE!")),
+      stream.isDefined option span(cls := "ribbon")(span(trans.streamer.live())),
       bits.pic(s.streamer, s.user),
       div(cls := "overview")(
         h1(dataIcon := "")(titleTag(s.user.title), stringValueFrag(s.streamer.name)),
@@ -52,13 +54,12 @@ object index {
         div(cls := "ats")(
           stream.map { s =>
             p(cls := "at")(
-              "Currently streaming: ",
-              strong(s.status)
+              currentlyStreaming(strong(s.status))
             )
           } getOrElse frag(
             p(cls := "at")(trans.lastSeenActive(momentFromNow(s.streamer.seenAt))),
             s.streamer.liveAt.map { liveAt =>
-              p(cls := "at")("Last stream ", momentFromNow(liveAt))
+              p(cls := "at")(lastStream(momentFromNow(liveAt)))
             }
           )
         )

app/views/streamer/picture.scala
@@ -7,14 +7,16 @@ import lila.app.ui.ScalatagsTemplate._
 
 object picture {
 
+  import trans.streamer._
+
   def apply(s: lila.streamer.Streamer.WithUser, error: Option[String] = None)(implicit ctx: Context) =
     views.html.base.layout(
-      title = s"${s.user.titleUsername} streamer picture",
+      title = xStreamerPicture.txt(),
       moreJs = jsTag("streamer.form.js"),
       moreCss = cssTag("streamer.form")
     ) {
       main(cls := "streamer-picture small-page box")(
-        h1(userLink(s.user), " streamer picture"),
+        h1(xStreamerPicture(userLink(s.user))),
         div(cls := "picture_wrap")(bits.pic(s.streamer, s.user, 250)),
         div(cls := "forms")(
           error.map { badTag(_) },
@@ -23,16 +25,16 @@ object picture {
             enctype := "multipart/form-data",
             cls := "upload"
           )(
-            p("Max size: ", lila.db.Photographer.uploadMaxMb, "MB."),
+            p(maxSize(s"${lila.db.Photographer.uploadMaxMb}MB.")),
             form3.file.image("picture"),
-            submitButton(cls := "button")("Upload profile picture")
+            submitButton(cls := "button")(uploadPicture())
           ),
           s.streamer.hasPicture option
             postForm(action := routes.Streamer.pictureDelete, cls := "delete")(
-              submitButton(cls := "button button-red")("Delete profile picture")
+              submitButton(cls := "button button-red")(deletePicture())
             ),
           div(cls := "cancel")(
-            a(href := routes.Streamer.edit, cls := "text", dataIcon := "I")("Return to streamer page form")
+            a(href := routes.Streamer.edit, cls := "text", dataIcon := "I")(trans.cancel())
           )
         )
       )

modules/i18n/src/main/I18nDb.scala
@@ -17,6 +17,7 @@ object I18nDb {
   case object Patron      extends Ref
   case object Coach       extends Ref
   case object Broadcast   extends Ref
+  case object Streamer    extends Ref
 
   val site: Messages        = lila.i18n.db.site.Registry.load
   val arena: Messages       = lila.i18n.db.arena.Registry.load
@@ -30,6 +31,7 @@ object I18nDb {
   val patron: Messages      = lila.i18n.db.patron.Registry.load
   val coach: Messages       = lila.i18n.db.coach.Registry.load
   val broadcast: Messages   = lila.i18n.db.broadcast.Registry.load
+  val streamer: Messages    = lila.i18n.db.streamer.Registry.load
 
   def apply(ref: Ref): Messages = ref match {
     case Site        => site
@@ -44,6 +46,7 @@ object I18nDb {
     case Patron      => patron
     case Coach       => coach
     case Broadcast   => broadcast
+    case Streamer    => streamer
   }
 
   val langs: Set[Lang] = site.keys.toSet

notzmv/lidraughts

app/views/streamer/index.scala
@@ -8,6 +8,8 @@ import lidraughts.common.paginator.Paginator
 
 object index {
 
+  import trans.streamer._
+
   private val dataDedup = attr("data-dedup")
 
   def apply(
@@ -16,7 +18,7 @@ object index {
     requests: Boolean
   )(implicit ctx: Context) = {
 
-    val title = if (requests) "Streamer approval requests" else "Lidraughts streamers"
+    val title = if (requests) "Streamer approval requests" else lidraughtsStreamers.txt()
 
     def widget(s: lidraughts.streamer.Streamer.WithUser, stream: Option[lidraughts.streamer.Stream]) = frag(
       a(
@@ -26,7 +28,7 @@ object index {
           else routes.Streamer.show(s.user.username).url
         }
       ),
-      stream.isDefined option span(cls := "ribbon")(span("LIVE!")),
+      stream.isDefined option span(cls := "ribbon")(span(trans.streamer.live())),
       bits.pic(s.streamer, s.user),
       div(cls := "overview")(
         h1(dataIcon := "")(titleTag(s.user.title), stringValueFrag(s.streamer.name)),
@@ -52,13 +54,12 @@ object index {
         div(cls := "ats")(
           stream.map { s =>
             p(cls := "at")(
-              "Currently streaming: ",
-              strong(s.status)
+              currentlyStreaming(strong(s.status))
             )
           } getOrElse frag(
             p(cls := "at")(trans.lastSeenActive(momentFromNow(s.streamer.seenAt))),
             s.streamer.liveAt.map { liveAt =>
-              p(cls := "at")("Last stream ", momentFromNow(liveAt))
+              p(cls := "at")(lastStream(momentFromNow(liveAt)))
             }
           )
         )

app/views/streamer/picture.scala
@@ -7,14 +7,16 @@ import lidraughts.app.ui.ScalatagsTemplate._
 
 object picture {
 
+  import trans.streamer._
+
   def apply(s: lidraughts.streamer.Streamer.WithUser, error: Option[String] = None)(implicit ctx: Context) =
     views.html.base.layout(
-      title = s"${s.user.titleUsername} streamer picture",
+      title = xStreamerPicture.txt(),
       moreJs = jsTag("streamer.form.js"),
       moreCss = cssTag("streamer.form")
     ) {
         main(cls := "streamer-picture small-page box")(
-          h1(userLink(s.user), " streamer picture"),
+          h1(xStreamerPicture(userLink(s.user))),
           div(cls := "picture_wrap")(bits.pic(s.streamer, s.user, 250)),
           div(cls := "forms")(
             error.map { badTag(_) },
@@ -23,16 +25,16 @@ object picture {
               enctype := "multipart/form-data",
               cls := "upload"
             )(
-                p("Max size: ", lidraughts.db.Photographer.uploadMaxMb, "MB."),
+                p(maxSize(s"${lidraughts.db.Photographer.uploadMaxMb}MB.")),
                 form3.file.image("picture"),
-                submitButton(cls := "button")("Upload profile picture")
+                submitButton(cls := "button")(uploadPicture())
               ),
             s.streamer.hasPicture option
               postForm(action := routes.Streamer.pictureDelete, cls := "delete")(
-                submitButton(cls := "button button-red")("Delete profile picture")
+                submitButton(cls := "button button-red")(deletePicture())
               ),
             div(cls := "cancel")(
-              a(href := routes.Streamer.edit, cls := "text", dataIcon := "I")("Return to streamer page form")
+              a(href := routes.Streamer.edit, cls := "text", dataIcon := "I")(trans.cancel())
             )
           )
         )

modules/i18n/src/main/I18nDb.scala
@@ -16,6 +16,7 @@ object I18nDb {
   case object Team extends Ref
   case object Swiss extends Ref
   case object Broadcast extends Ref
+  case object Streamer extends Ref
 
   val site: Messages = lidraughts.i18n.db.site.Registry.load
   val arena: Messages = lidraughts.i18n.db.arena.Registry.load
@@ -28,6 +29,7 @@ object I18nDb {
   val team: Messages = lidraughts.i18n.db.team.Registry.load
   val swiss: Messages = lidraughts.i18n.db.swiss.Registry.load
   val broadcast: Messages = lidraughts.i18n.db.broadcast.Registry.load
+  val streamer: Messages = lidraughts.i18n.db.streamer.Registry.load
 
   def apply(ref: Ref): Messages = ref match {
     case Site => site
@@ -41,6 +43,7 @@ object I18nDb {
     case Team => team
     case Swiss => swiss
     case Broadcast => broadcast
+    case Streamer => streamer
   }
 
   val langs: Set[Lang] = site.keys.map(Lang.apply)(scala.collection.breakOut)