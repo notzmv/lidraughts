lichess-org/lila

prevent shouting in forum topics - closes #6262

modules/chat/src/main/ChatApi.scala
@@ -5,6 +5,7 @@ import reactivemongo.api.ReadPreference
 import scala.concurrent.duration._
 
 import lila.common.config.NetDomain
+import lila.common.String.noShouting
 import lila.db.dsl._
 import lila.hub.actorApi.shutup.{ PublicSource, RecordPrivateChat, RecordPublicChat }
 import lila.memo.CacheApi._
@@ -260,20 +261,7 @@ final class ChatApi(
     private val gameUrlRegex                      = (Pattern.quote(netDomain.value) + """\b/(\w{8})\w{4}\b""").r
     private val gameUrlReplace                    = Matcher.quoteReplacement(netDomain.value) + "/$1";
     private def noPrivateUrl(str: String): String = gameUrlRegex.replaceAllIn(str, gameUrlReplace)
-    private def noShouting(str: String): String   = if (isShouting(str)) str.toLowerCase else str
     private val multilineRegex                    = """\n\n{2,}+""".r
     private def multiline(str: String)            = multilineRegex.replaceAllIn(str, """\n\n""")
-  }
-
-  private def isShouting(text: String) = text.length >= 5 && {
-    import java.lang.Character._
-    // true if >1/2 of the latin letters are uppercase
-    (text take 80).foldLeft(0) { (i, c) =>
-      getType(c) match {
-        case UPPERCASE_LETTER => i + 1
-        case LOWERCASE_LETTER => i - 1
-        case _                => i
-      }
-    } > 0
   }
 }

modules/forum/src/main/TopicApi.scala
@@ -4,6 +4,7 @@ import actorApi._
 
 import lila.common.Bus
 import lila.common.paginator._
+import lila.common.String.noShouting
 import lila.db.dsl._
 import lila.db.paginator._
 import lila.hub.actorApi.timeline.{ ForumPost, Propagate }
@@ -55,7 +56,7 @@ final private[forum] class TopicApi(
         val topic = Topic.make(
           categId = categ.slug,
           slug = slug,
-          name = data.name,
+          name = noShouting(data.name),
           userId = me.id,
           troll = me.marks.troll,
           hidden = categ.quiet || data.looksLikeVenting

notzmv/lidraughts

modules/chat/src/main/ChatApi.scala
@@ -4,6 +4,7 @@ import draughts.Color
 import reactivemongo.api.ReadPreference
 import scala.concurrent.duration._
 
+import lidraughts.common.String.noShouting
 import lidraughts.db.dsl._
 import lidraughts.hub.actorApi.shutup.{ PublicSource, RecordPublicChat, RecordPrivateChat }
 import lidraughts.user.{ User, UserRepo }
@@ -224,20 +225,7 @@ final class ChatApi(
     private val gameUrlRegex = (Pattern.quote(netDomain) + """\b/(\w{8})\w{4}\b""").r
     private val gameUrlReplace = Matcher.quoteReplacement(netDomain) + "/$1";
     private def noPrivateUrl(str: String): String = gameUrlRegex.replaceAllIn(str, gameUrlReplace)
-    private def noShouting(str: String): String = if (isShouting(str)) str.toLowerCase else str
     private val multilineRegex = """\n\n{2,}+""".r
     private def multiline(str: String) = multilineRegex.replaceAllIn(str, """\n\n""")
-  }
-
-  private def isShouting(text: String) = text.length >= 5 && {
-    import java.lang.Character._
-    // true if >1/2 of the latin letters are uppercase
-    (text take 80).foldLeft(0) { (i, c) =>
-      getType(c) match {
-        case UPPERCASE_LETTER => i + 1
-        case LOWERCASE_LETTER => i - 1
-        case _ => i
-      }
-    } > 0
   }
 }

modules/forum/src/main/TopicApi.scala
@@ -4,6 +4,7 @@ import actorApi._
 import akka.actor.ActorSelection
 
 import lidraughts.common.paginator._
+import lidraughts.common.String.noShouting
 import lidraughts.db.dsl._
 import lidraughts.db.paginator._
 import lidraughts.hub.actorApi.timeline.{ Propagate, ForumPost }
@@ -48,7 +49,7 @@ private[forum] final class TopicApi(
         val topic = Topic.make(
           categId = categ.slug,
           slug = slug,
-          name = data.name,
+          name = noShouting(data.name),
           troll = ctx.troll,
           hidden = categ.quiet || data.looksLikeVenting
         )