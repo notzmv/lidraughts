lichess-org/lila

Translate 'started streaming'

modules/i18n/src/main/I18nKeys.scala
@@ -241,6 +241,7 @@ val `backToGame` = new I18nKey("backToGame", Site)
 val `siteDescription` = new I18nKey("siteDescription", Site)
 val `xJoinedTeamY` = new I18nKey("xJoinedTeamY", Site)
 val `xCreatedTeamY` = new I18nKey("xCreatedTeamY", Site)
+val `xStartedStreaming` = new I18nKey("xStartedStreaming", Site)
 val `averageElo` = new I18nKey("averageElo", Site)
 val `location` = new I18nKey("location", Site)
 val `filterGames` = new I18nKey("filterGames", Site)

notzmv/lidraughts

modules/i18n/src/main/I18nKeys.scala
@@ -280,6 +280,7 @@ val `backToGame` = new Translated("backToGame", Site)
 val `siteDescription` = new Translated("siteDescription", Site)
 val `xJoinedTeamY` = new Translated("xJoinedTeamY", Site)
 val `xCreatedTeamY` = new Translated("xCreatedTeamY", Site)
+val `xStartedStreaming` = new Translated("xStartedStreaming", Site)
 val `averageElo` = new Translated("averageElo", Site)
 val `averagePerformance` = new Translated("averagePerformance", Site)
 val `averageScore` = new Translated("averageScore", Site)