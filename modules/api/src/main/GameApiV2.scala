lichess-org/lila

export ongoing games, but with a move delay

modules/api/src/main/GameApiV2.scala
@@ -41,8 +41,10 @@ final class GameApiV2(
   def exportOne(game: Game, configInput: OneConfig): Fu[String] = {
     val config = configInput.copy(
       flags = configInput.flags
-        .copy(evals = configInput.flags.evals && !game.playable)
-        .withDelayIf(game.playable && !configInput.noDelay)
+        .copy(
+          evals = configInput.flags.evals && !game.playable,
+          delayMoves = !configInput.noDelay
+        )
     )
     game.pgnImport ifTrue config.imported match {
       case Some(imported) => fuccess(imported.pgn)
@@ -127,7 +129,7 @@ final class GameApiV2(
       config.playerFile.??(realPlayerApi.apply) map { realPlayers =>
         gameRepo
           .sortedCursor(
-            $inIds(config.ids) ++ Query.finished,
+            $inIds(config.ids),
             Query.sortCreated,
             batchSize = config.perSecond.value
           )
@@ -294,7 +296,9 @@ final class GameApiV2(
       .add("initialFen" -> initialFen)
       .add("winner" -> g.winnerColor.map(_.name))
       .add("opening" -> g.opening.ifTrue(withFlags.opening))
-      .add("moves" -> withFlags.moves.option(withFlags applyDelay g.pgnMoves mkString " "))
+      .add("moves" -> withFlags.moves.option {
+        withFlags keepDelayIf g.playable applyDelay g.pgnMoves mkString " "
+      })
       .add("pgn" -> pgn)
       .add("daysPerTurn" -> g.daysPerTurn)
       .add("analysis" -> analysisOption.ifTrue(withFlags.evals).map(analysisJson.moves(_, withGlyph = false)))

notzmv/lidraughts

modules/api/src/main/GameApiV2.scala
@@ -28,8 +28,10 @@ final class GameApiV2(
   def exportOne(game: Game, configInput: OneConfig): Fu[String] = {
     val config = configInput.copy(
       flags = configInput.flags
-        .copy(evals = configInput.flags.evals && !game.playable)
-        .withDelayIf(game.playable && !configInput.noDelay)
+        .copy(
+          evals = configInput.flags.evals && !game.playable,
+          delayMoves = !configInput.noDelay
+        )
     )
     game.pdnImport ifTrue config.imported match {
       case Some(imported) => fuccess(imported.pdn)
@@ -97,7 +99,7 @@ final class GameApiV2(
 
   def exportByIds(config: ByIdsConfig): Enumerator[String] =
     GameRepo.sortedCursor(
-      $inIds(config.ids) ++ Query.finished,
+      $inIds(config.ids),
       Query.sortCreated,
       batchSize = config.perSecond.value
     ).bulkEnumerator() &>
@@ -180,7 +182,9 @@ final class GameApiV2(
     ).add("initialFen" -> initialFen.map(_.value))
       .add("winner" -> g.winnerColor.map(_.name))
       .add("opening" -> g.opening.ifTrue(withFlags.opening))
-      .add("moves" -> withFlags.moves.option(withFlags applyDelay g.pdnMoves mkString " "))
+      .add("moves" -> withFlags.moves.option {
+        withFlags keepDelayIf g.playable applyDelay g.pdnMoves mkString " "
+      })
       .add("daysPerTurn" -> g.daysPerTurn)
       .add("analysis" -> analysisOption.ifTrue(withFlags.evals).map(analysisJson.moves(_, withGlyph = false)))
       .add("tournament" -> g.tournamentId)