lichess-org/lila

trim and clean up text fields before validating them
to avoid accepting purely space content

modules/common/src/main/Form.scala
@@ -3,6 +3,7 @@ package lila.common
 import chess.format.FEN
 import chess.format.Forsyth
 import org.joda.time.{ DateTime, DateTimeZone }
+import play.api.data.FieldMapping
 import play.api.data.format.Formats._
 import play.api.data.format.{ Formatter, JodaFormats }
 import play.api.data.Forms._
@@ -10,6 +11,8 @@ import play.api.data.JodaForms._
 import play.api.data.validation.{ Constraint, Constraints }
 import play.api.data.{ Field, FormError, Mapping }
 import scala.util.Try
+
+import lila.common.base.StringUtils
 
 object Form {
 
@@ -58,12 +61,32 @@ object Form {
     of[Double].verifying(hasKey(choices, _))
 
   def trim(m: Mapping[String]) = m.transform[String](_.trim, identity)
-  def clean(m: Mapping[String]) =
-    trim(m)
-      .verifying("This text contains invalid chars", s => !String.hasGarbageChars(s))
+
+  // trims and removes garbage chars before validation
+  val cleanTextFormatter: Formatter[String] = new Formatter[String] {
+    def bind(key: String, data: Map[String, String]) =
+      data
+        .get(key)
+        .map(_.trim)
+        .map(StringUtils.removeGarbageChars)
+        .toRight(Seq(FormError(key, "error.required", Nil)))
+    def unbind(key: String, value: String) = Map(key -> StringUtils.removeGarbageChars(value.trim))
+  }
+
+  val cleanText: Mapping[String] = of(cleanTextFormatter)
+  def cleanText(minLength: Int = 0, maxLength: Int = Int.MaxValue): Mapping[String] =
+    (minLength, maxLength) match {
+      case (min, Int.MaxValue) => cleanText.verifying(Constraints.minLength(min))
+      case (0, max)            => cleanText.verifying(Constraints.maxLength(max))
+      case (min, max)          => cleanText.verifying(Constraints.minLength(min), Constraints.maxLength(max))
+    }
+
+  val cleanNonEmptyText: Mapping[String] = cleanText.verifying(Constraints.nonEmpty)
+  def cleanNonEmptyText(minLength: Int = 0, maxLength: Int = Int.MaxValue): Mapping[String] =
+    cleanText(minLength, maxLength).verifying(Constraints.nonEmpty)
 
   def eventName(minLength: Int, maxLength: Int) =
-    clean(text).verifying(
+    cleanText.verifying(
       Constraints minLength minLength,
       Constraints maxLength maxLength,
       Constraints.pattern(

modules/common/src/test/FormTest.scala
@@ -1,0 +1,64 @@
+package lila.common
+
+import org.specs2.mutable.Specification
+import play.api.data._
+import play.api.data.format._
+import play.api.data.format.Formats._
+import play.api.data.Forms._
+import play.api.data.validation._
+import scalatags.Text.all._
+
+import lila.common.Form._
+
+class FormTest extends Specification {
+
+  "trim" should {
+    "apply before validation" in {
+
+      FieldMapping("t", List(Constraints.minLength(1)))
+        .bind(Map("t" -> " "))
+        .must(beRight)
+
+      FieldMapping("t", List(Constraints.minLength(1)))
+        .as(cleanTextFormatter)
+        .bind(Map("t" -> " "))
+        .must(beLeft)
+
+      single("t" -> cleanText(minLength = 3))
+        .bind(Map("t" -> "     ")) must beLeft
+
+      single("t" -> cleanText(minLength = 3))
+        .bind(Map("t" -> "aa ")) must beLeft
+
+      single("t" -> cleanText(minLength = 3))
+        .bind(Map("t" -> "aaa")) must beRight
+
+      single("t" -> text)
+        .bind(Map("t" -> "")) must beRight
+      single("t" -> cleanText)
+        .bind(Map("t" -> "")) must beRight
+
+      single("t" -> cleanText)
+        .bind(Map("t" -> "   ")) must beRight
+    }
+  }
+
+  "garbage chars" should {
+    "be removed before validation" in {
+
+      val chars = List('\u200b', '\u200c', '\u200d', '\u200e', '\u200f', '\u202e', '\u1160')
+
+      val str = chars mkString ""
+
+      single("t" -> cleanText(minLength = 1))
+        .bind(Map("t" -> str)) must beLeft
+
+      single("t" -> cleanText(minLength = 1))
+        .bind(Map("t" -> s"  $str  ")) must beLeft
+
+      single("t" -> cleanText)
+        .bind(Map("t" -> s"  $str  ")) must beRight
+    }
+  }
+
+}

modules/relay/src/main/RelayForm.scala
@@ -7,7 +7,7 @@ import io.lemonlabs.uri.AbsoluteUrl
 
 import lila.security.Granter
 import lila.user.User
-import lila.common.Form.clean
+import lila.common.Form.{ cleanNonEmptyText, cleanText }
 
 final class RelayForm {
 
@@ -16,15 +16,15 @@ final class RelayForm {
 
   val form = Form(
     mapping(
-      "name"        -> clean(text(minLength = 3, maxLength = 80)),
-      "description" -> clean(text(minLength = 3, maxLength = 400)),
-      "markup"      -> optional(text(maxLength = 20000)),
+      "name"        -> cleanText(minLength = 3, maxLength = 80),
+      "description" -> cleanText(minLength = 3, maxLength = 400),
+      "markup"      -> optional(cleanText(maxLength = 20000)),
       "official"    -> optional(boolean),
       "syncUrl" -> optional {
         nonEmptyText.verifying("Invalid source", validSource _)
       },
       "syncUrlRound" -> optional(number(min = 1, max = 999)),
-      "credit"       -> optional(clean(nonEmptyText)),
+      "credit"       -> optional(cleanNonEmptyText),
       "startsAt"     -> optional(ISODateTimeOrTimestamp.isoDateTimeOrTimestamp),
       "throttle"     -> optional(number(min = 2, max = 60))
     )(Data.apply)(Data.unapply)

notzmv/lidraughts

modules/common/src/main/Form.scala
@@ -1,12 +1,15 @@
 package lidraughts.common
 
 import org.joda.time.{ DateTime, DateTimeZone }
+import play.api.data.FieldMapping
 import play.api.data.format.Formats._
 import play.api.data.format.Formatter
 import play.api.data.validation.{ Constraint, Constraints }
 import play.api.data.Forms._
 import play.api.data.{ Mapping, FormError, Field, Form => PlayForm }
 import scala.util.Try
+
+import lidraughts.common.base.StringUtils
 
 object Form {
 
@@ -47,12 +50,32 @@ object Form {
     of[Double].verifying(hasKey(choices, _))
 
   def trim(m: Mapping[String]) = m.transform[String](_.trim, identity)
-  def clean(m: Mapping[String]) =
-    trim(m)
-      .verifying("error.pattern", s => !String.hasGarbageChars(s))
+
+  // trims and removes garbage chars before validation
+  val cleanTextFormatter: Formatter[String] = new Formatter[String] {
+    def bind(key: String, data: Map[String, String]) =
+      data
+        .get(key)
+        .map(StringUtils.removeGarbageChars)
+        .map(_.trim)
+        .toRight(Seq(FormError(key, "error.required", Nil)))
+    def unbind(key: String, value: String) = Map(key -> StringUtils.removeGarbageChars(value.trim))
+  }
+
+  val cleanText: Mapping[String] = of(cleanTextFormatter)
+  def cleanText(minLength: Int = 0, maxLength: Int = Int.MaxValue): Mapping[String] =
+    (minLength, maxLength) match {
+      case (min, Int.MaxValue) => cleanText.verifying(Constraints.minLength(min))
+      case (0, max) => cleanText.verifying(Constraints.maxLength(max))
+      case (min, max) => cleanText.verifying(Constraints.minLength(min), Constraints.maxLength(max))
+    }
+
+  val cleanNonEmptyText: Mapping[String] = cleanText.verifying(Constraints.nonEmpty)
+  def cleanNonEmptyText(minLength: Int = 0, maxLength: Int = Int.MaxValue): Mapping[String] =
+    cleanText(minLength, maxLength).verifying(Constraints.nonEmpty)
 
   def eventName(minLength: Int, maxLength: Int) =
-    clean(text).verifying(
+    cleanText.verifying(
       Constraints minLength minLength,
       Constraints maxLength maxLength,
       Constraints.pattern(

modules/common/src/test/FormTest.scala
@@ -1,0 +1,64 @@
+package lidraughts.common
+
+import org.specs2.mutable.Specification
+import play.api.data._
+import play.api.data.format._
+import play.api.data.format.Formats._
+import play.api.data.Forms._
+import play.api.data.validation._
+import scalatags.Text.all._
+
+import lidraughts.common.Form._
+
+class FormTest extends Specification {
+
+  "trim" should {
+    "apply before validation" in {
+
+      FieldMapping("t", List(Constraints.minLength(1)))
+        .bind(Map("t" -> " "))
+        .must(beRight)
+
+      FieldMapping("t", List(Constraints.minLength(1)))
+        .as(cleanTextFormatter)
+        .bind(Map("t" -> " "))
+        .must(beLeft)
+
+      single("t" -> cleanText(minLength = 3))
+        .bind(Map("t" -> "     ")) must beLeft
+
+      single("t" -> cleanText(minLength = 3))
+        .bind(Map("t" -> "aa ")) must beLeft
+
+      single("t" -> cleanText(minLength = 3))
+        .bind(Map("t" -> "aaa")) must beRight
+
+      single("t" -> text)
+        .bind(Map("t" -> "")) must beRight
+      single("t" -> cleanText)
+        .bind(Map("t" -> "")) must beRight
+
+      single("t" -> cleanText)
+        .bind(Map("t" -> "   ")) must beRight
+    }
+  }
+
+  "garbage chars" should {
+    "be removed before validation" in {
+
+      val chars = List('\u200b', '\u200c', '\u200d', '\u200e', '\u200f', '\u202e', '\u1160')
+
+      val str = chars mkString ""
+
+      single("t" -> cleanText(minLength = 1))
+        .bind(Map("t" -> str)) must beLeft
+
+      single("t" -> cleanText(minLength = 1))
+        .bind(Map("t" -> s"  $str  ")) must beLeft
+
+      single("t" -> cleanText)
+        .bind(Map("t" -> s"  $str  ")) must beRight
+    }
+  }
+
+}

modules/relay/src/main/RelayForm.scala
@@ -5,6 +5,7 @@ import play.api.data._
 import play.api.data.Forms._
 import play.api.data.validation.Constraints._
 
+import lidraughts.common.Form.{ cleanNonEmptyText, cleanText }
 import lidraughts.user.User
 import lidraughts.security.Granter
 
@@ -15,9 +16,9 @@ object RelayForm {
   val maxHomepageHours = 72
 
   val form = Form(mapping(
-    "name" -> text(minLength = 3, maxLength = 80),
-    "description" -> text(minLength = 3, maxLength = 400),
-    "markup" -> optional(text(maxLength = 20000)),
+    "name" -> cleanText(minLength = 3, maxLength = 80),
+    "description" -> cleanText(minLength = 3, maxLength = 400),
+    "markup" -> optional(cleanText(maxLength = 20000)),
     "official" -> optional(boolean),
     "homepageHours" -> optional(number(min = 0, max = maxHomepageHours)),
     "syncUrl" -> nonEmptyText.verifying("Lidraughts tournaments can't be used as broadcast source", u => !isTournamentApi(u)),
@@ -25,7 +26,7 @@ object RelayForm {
     "gameIds" -> optional(nonEmptyText.verifying("Invalid game IDs", u => isCommaSeparatedGameIds(u))),
     "simulId" -> optional(nonEmptyText),
     "withProfileName" -> optional(boolean),
-    "credit" -> optional(nonEmptyText),
+    "credit" -> optional(cleanNonEmptyText),
     "startsAt" -> optional(utcDate),
     "throttle" -> optional(number(min = 2, max = 60))
   )(Data.apply)(Data.unapply))