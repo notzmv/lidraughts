lichess-org/lila

simul mini-games

modules/simul/src/main/JsonView.scala
@@ -123,13 +123,23 @@ final class JsonView(
     }
 
   private def gameJson(hostId: User.ID, g: Game) =
-    Json.obj(
-      "id"       -> g.id,
-      "status"   -> g.status.id,
-      "fen"      -> (chess.format.Forsyth exportBoard g.board),
-      "lastMove" -> ~g.lastMoveKeys,
-      "orient"   -> g.playerByUserId(hostId).map(_.color)
-    )
+    Json
+      .obj(
+        "id"       -> g.id,
+        "status"   -> g.status.id,
+        "fen"      -> (chess.format.Forsyth boardAndColor g.situation),
+        "lastMove" -> ~g.lastMoveKeys,
+        "orient"   -> g.playerByUserId(hostId).map(_.color)
+      )
+      .add(
+        "clock" -> g.clock.ifTrue(g.isBeingPlayed).map { c =>
+          Json.obj(
+            "white" -> c.remainingTime(chess.White).roundSeconds,
+            "black" -> c.remainingTime(chess.Black).roundSeconds
+          )
+        }
+      )
+      .add("winner" -> g.winnerColor.map(_.name))
 
   private def pairingJson(games: List[Game], hostId: String)(p: SimulPairing): Fu[Option[JsObject]] =
     games.find(_.id == p.gameId) ?? { game =>

modules/swiss/src/main/SwissJson.scala
@@ -260,7 +260,7 @@ object SwissJson {
     Json
       .obj(
         "id"          -> b.game.id,
-        "fen"         -> (chess.format.Forsyth exportBoard b.game.board),
+        "fen"         -> chess.format.Forsyth.boardAndColor(b.game.situation),
         "lastMove"    -> ~b.game.lastMoveKeys,
         "orientation" -> b.game.naturalOrientation.name,
         "white"       -> boardPlayerJson(b.board.white),

modules/tournament/src/main/JsonView.scala
@@ -276,7 +276,7 @@ final class JsonView(
     Json
       .obj(
         "id"          -> game.id,
-        "fen"         -> (chess.format.Forsyth exportBoard game.board),
+        "fen"         -> chess.format.Forsyth.boardAndColor(game.situation),
         "orientation" -> game.naturalOrientation.name,
         "lastMove"    -> ~game.lastMoveKeys,
         "white"       -> ofPlayer(featured.white, game player chess.White),

ui/simul/src/view/pairings.js
@@ -1,41 +1,48 @@
-var m = require('mithril');
-var util = require('./util');
-var status = require('game/status');
+const m = require('mithril');
+const util = require('./util');
+const status = require('game/status');
+const opposite = require('chessground/util').opposite;
 
 function miniPairing(ctrl) {
   return function(pairing) {
-    var game = pairing.game;
-    var player = pairing.player;
-    var result = pairing.game.status >= status.ids.mate ? (
-      pairing.winnerColor === 'white' ? '1-0' : (pairing.winnerColor === 'black' ? '0-1' : '½/½')
-    ) : '*';
-    return m('a', {
-      href: '/' + game.id + '/' + game.orient,
-      class: ctrl.data.host.gameId === game.id ? 'host' : ''
-    }, [
-      m('span', {
-        class: 'mini-board mini-board-' + game.id + ' parse-fen is2d',
-        'data-color': game.orient,
-        'data-fen': game.fen,
-        'data-lastmove': game.lastMove,
-        config: function(el, isUpdate) {
-          if (!isUpdate) lichess.parseFen($(el));
+    const game = pairing.game,
+      player = pairing.player;
+    return m(`a.mini-game.mini-game--init.mini-game-${game.id} is2d`, {
+      class: ctrl.data.host.gameId === game.id ? 'host' : '',
+      'data-live': game.clock ? game.id : '',
+      config(el, isUpdate) {
+        if (!isUpdate) {
+          window.lichess.miniGame.init(el, `${game.fen},${game.orient},${game.lastMove}`)
+          window.lichess.powertip.manualUserIn(el);
         }
-      }, m('div.cg-wrap')),
-      m('span.vstext', [
-        m('span.vstext__pl', [
-          util.playerVariant(ctrl, player).name,
-          m('br'),
-          result
+      }
+    }, [
+      m('span.mini-game__player', [
+        m('a.mini-game__user.ulpt', {
+          href: `/@/${player.name}`
+        }, [
+          m('span.name', player.title ? [m('span.title', player.title), ' ', player.name] : [player.name]),
+          ' ',
+          m('span.rating', player.rating)
         ]),
-        m('div.vstext__op', [
-          player.name,
-          m('br'),
-          player.title ? player.title + ' ' : '',
-          player.rating
-        ])
-      ])
-    ]);
+        game.clock ?
+        m(`span.mini-game__clock.mini-game__clock--${opposite(game.orient)}`, {
+          'data-time': game.clock[opposite(game.orient)]
+        }) :
+        m('span.mini-game__result', game.winner ? (game.winner == game.orient ? 0 : 1) : '½'),
+      ]),
+      m('a.cg-wrap', {
+        href: `/${game.id}/${game.orient}`
+      }),
+      m('span.mini-game__player', [
+        m('span'),
+        game.clock ?
+        m(`span.mini-game__clock.mini-game__clock--${game.orient}`, {
+          'data-time': game.clock[game.orient]
+        }) :
+        m('span.mini-game__result', game.winner ? (game.winner == game.orient ? 1 : 0) : '½'),
+      ]),
+    ])
   };
 }
 

notzmv/lidraughts

modules/simul/src/main/JsonView.scala
@@ -228,14 +228,25 @@ final class JsonView(
       )
     }
 
-  private def gameJson(hostId: User.ID, g: Game) = Json.obj(
-    "id" -> g.id,
-    "status" -> g.status.id,
-    "fen" -> (draughts.format.Forsyth exportBoard g.board),
-    "lastMove" -> ~g.lastMoveKeys,
-    "orient" -> g.playerByUserId(hostId).map(_.color),
-    "board" -> g.variant.boardSize
-  )
+  private def gameJson(hostId: User.ID, g: Game) =
+    Json
+      .obj(
+        "id" -> g.id,
+        "status" -> g.status.id,
+        "fen" -> (draughts.format.Forsyth boardAndColor g.situation),
+        "lastMove" -> ~g.lastMoveKeys,
+        "orient" -> g.playerByUserId(hostId).map(_.color),
+        "board" -> g.variant.boardSize
+      )
+      .add(
+        "clock" -> g.clock.ifTrue(g.isBeingPlayed).map { c =>
+          Json.obj(
+            "white" -> c.remainingTime(draughts.White).roundSeconds,
+            "black" -> c.remainingTime(draughts.Black).roundSeconds
+          )
+        }
+      )
+      .add("winner" -> g.winnerColor.map(_.name))
 
   private def pairingJson(games: List[Game], hostId: String, fmjd: Option[ShowFmjdRating])(p: SimulPairing): Fu[Option[JsObject]] =
     games.find(_.id == p.gameId) ?? { game =>

modules/swiss/src/main/SwissJson.scala
@@ -267,7 +267,7 @@ object SwissJson {
     Json
       .obj(
         "id" -> b.game.id,
-        "fen" -> (draughts.format.Forsyth exportBoard b.game.board),
+        "fen" -> draughts.format.Forsyth.boardAndColor(b.game.situation),
         "lastMove" -> ~b.game.lastMoveKeys,
         "orientation" -> b.game.naturalOrientation.name,
         "white" -> boardPlayerJson(b.board.white),

modules/tournament/src/main/JsonView.scala
@@ -286,7 +286,7 @@ final class JsonView(
     Json
       .obj(
         "id" -> game.id,
-        "fen" -> (draughts.format.Forsyth exportBoard game.board),
+        "fen" -> draughts.format.Forsyth.boardAndColor(game.situation),
         "orientation" -> game.naturalOrientation.name,
         "lastMove" -> ~game.lastMoveKeys,
         "white" -> ofPlayer(featured.white, game player draughts.White),

ui/simul/src/view/pairings.js
@@ -1,43 +1,63 @@
-var m = require('mithril');
-var util = require('./util');
-var ceval = require('./ceval');
-var status = require('game/status');
+const m = require('mithril');
+const util = require('./util');
+const ceval = require('./ceval');
+const status = require('game/status');
 
 function miniPairing(ctrl) {
   return function(pairing) {
-    var game = pairing.game,
+    const game = pairing.game,
       player = pairing.player,
-      result = pairing.game.status >= status.ids.aborted ? (
-        pairing.winnerColor === 'white' ? (ctrl.pref.draughtsResult ? '2-0' : '1-0')
-        : (pairing.winnerColor === 'black' ? (ctrl.pref.draughtsResult ? '0-2' : '0-1')
-        : (ctrl.pref.draughtsResult ? '1-1' : '½-½'))
-      ) : '*';
-    return m('a', {
+      title64 = player.title && player.title.endsWith('-64');
+    return m(`a.mini-game.mini-game--init.mini-game-${game.id}.is2d.is${game.board.key}`, {
       class: (ctrl.data.host.gameId === game.id ? 'host ' : '') + (ctrl.evals !== undefined ? 'gauge_displayed' : ''),
-      href: '/' + game.id + '/' + game.orient
-    }, [
-      m('span', {
-        class: 'mini-board mini-board-' + game.id + ' parse-fen is2d is' + game.board.key,
-        'data-color': game.orient,
-        'data-fen': game.fen,
-        'data-lastmove': game.lastMove,
-        'data-board': `${game.board.size[0]}x${game.board.size[1]}`,
-        config: function(el, isUpdate) {
-          if (!isUpdate) lidraughts.parseFen($(el));
+      'data-live': game.clock ? game.id : '',
+      href: '/' + game.id + '/' + game.orient,
+      config(el, isUpdate) {
+        if (!isUpdate) {
+          window.lidraughts.miniGame.init(el, `${game.fen}|${game.board.size[0]}x${game.board.size[1]}|${game.orient}|${game.lastMove}`)
+          window.lidraughts.powertip.manualUserIn(el);
         }
-      }, m('div.cg-wrap')),
-      m('span.vstext', [
-        m('span.vstext__pl', [
-          util.playerVariant(ctrl, player).name,
-          m('br'),
-          result
+      }
+    }, [
+      m('span.mini-game__player', [
+        m('a.mini-game__user.ulpt', {
+          href: `/@/${player.name}`
+        }, [
+          m('span.name', 
+            !player.title ? [player.name] : [
+              m('span.title', 
+                title64 ? { 'data-title64': true } : (u.title == 'BOT' ? { 'data-bot': true } : {}),
+                title64 ? player.title.slice(0, player.title.length - 3) : player.title
+              ),
+              ' ',
+              player.name
+            ]
+          ),
+          ' ',
+          m('span.rating', player.rating)
         ]),
-        m('div.vstext__op', [
-          player.name,
-          m('br'),
-          player.title ? (player.title.endsWith('-64') ? player.title.slice(0, player.title.length - 3) : player.title) + ' ' : '',
-          player.officialRating ? ('FMJD ' + player.officialRating) : player.rating
-        ])
+        game.clock ?
+          m(`span.mini-game__clock.mini-game__clock--${opposite(game.orient)}`, {
+            'data-time': game.clock[opposite(game.orient)]
+          }) :
+          m('span.mini-game__result', game.winner ?
+            (game.winner == game.orient ? '0' : (ctrl.pref.draughtsResult ? '2' : '1')) : 
+            (ctrl.pref.draughtsResult ? '1' : '½')
+          )
+      ]),
+      m('a.cg-wrap', {
+        href: `/${game.id}/${game.orient}`
+      }),
+      m('span.mini-game__player', [
+        m('span'),
+        game.clock ?
+        m(`span.mini-game__clock.mini-game__clock--${game.orient}`, {
+          'data-time': game.clock[game.orient]
+        }) :
+        m('span.mini-game__result', game.winner ?
+          (game.winner == game.orient ? '0' : (ctrl.pref.draughtsResult ? '2' : '1')) : 
+          (ctrl.pref.draughtsResult ? '1' : '½')
+        )
       ]),
       ctrl.evals !== undefined ? ceval.renderGauge(pairing, ctrl.evals) : null
     ]);