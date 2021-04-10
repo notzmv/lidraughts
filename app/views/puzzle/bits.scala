lichess-org/lila

mini-game and mini-board WIP

app/views/puzzle/bits.scala
@@ -12,14 +12,10 @@ object bits {
 
   private val dataLastmove = attr("data-lastmove")
 
-  def daily(p: lila.puzzle.Puzzle, fen: String, lastMove: String) =
-    a(
-      href := routes.Puzzle.daily(),
-      cls := "mini-board cg-wrap parse-fen is2d",
-      dataColor := p.color.name,
-      dataFen := fen,
-      dataLastmove := lastMove
-    )(cgWrapContent)
+  def daily(p: lila.puzzle.Puzzle, fen: chess.format.FEN, lastMove: String) =
+    views.html.board.bits.mini(fen, p.color, lastMove) {
+      a(href := routes.Puzzle.daily())
+    }
 
   def jsI18n()(implicit lang: Lang) = i18nJsObject(i18nKeys)
 

modules/puzzle/src/main/Puzzle.scala
@@ -1,7 +1,7 @@
 package lila.puzzle
 
 import chess.Color
-import chess.format.{ Forsyth, Uci }
+import chess.format.{ FEN, Forsyth, Uci }
 import org.joda.time.DateTime
 import scala.util.{ Success, Try }
 
@@ -34,11 +34,11 @@ case class Puzzle(
 
   def initialMove: Uci.Move = history.lastOption flatMap Uci.Move.apply err s"Bad initial move $this"
 
-  def fenAfterInitialMove: Option[String] = {
+  def fenAfterInitialMove: Option[FEN] = {
     for {
       sit1 <- Forsyth << fen
       sit2 <- sit1.move(initialMove).toOption.map(_.situationAfter)
-    } yield Forsyth >> sit2
+    } yield FEN(Forsyth >> sit2)
   }
 }
 

notzmv/lidraughts

app/views/puzzle/bits.scala
@@ -11,14 +11,10 @@ object bits {
 
   private val dataLastmove = attr("data-lastmove")
 
-  def daily(p: lidraughts.puzzle.Puzzle, fen: String, lastMove: String) = a(
-    href := routes.Puzzle.daily(),
-    cls := s"mini-board cg-wrap parse-fen is2d is${p.variant.boardSize.key}",
-    dataColor := p.color.name,
-    dataFen := fen,
-    dataBoard := s"${p.variant.boardSize.width}x${p.variant.boardSize.height}",
-    dataLastmove := lastMove
-  )(cgWrapContent)
+  def daily(p: lidraughts.puzzle.Puzzle, fen: draughts.format.FEN, lastMove: String) =
+    views.html.board.bits.mini(fen, p.variant.boardSize, p.color, lastMove) {
+      a(href := routes.Puzzle.daily())
+    }
 
   def jsI18n()(implicit ctx: Context) = i18nJsObject(translations)
 

modules/puzzle/src/main/Puzzle.scala
@@ -3,7 +3,7 @@ package lidraughts.puzzle
 import scala.collection.breakOut
 import draughts.Color
 import draughts.format.Forsyth.SituationPlus
-import draughts.format.{ Forsyth, Uci }
+import draughts.format.{ FEN, Forsyth, Uci }
 import draughts.variant.{ Variant, Standard }
 import org.joda.time.DateTime
 
@@ -50,11 +50,11 @@ case class Puzzle(
         }
   } err s"Bad initial move $this"
 
-  def fenAfterInitialMove: Option[String] = {
+  def fenAfterInitialMove: Option[FEN] = {
     for {
       sit1 <- Forsyth.<<@(variant, fen)
       sit2 <- sit1.move(initialMove.orig, initialMove.dest, initialMove.promotion, true).toOption.map(_.situationAfter)
-    } yield Forsyth >> SituationPlus(sit2, fenFullMove + color.fold(1, 0))
+    } yield FEN(Forsyth >> SituationPlus(sit2, fenFullMove + color.fold(1, 0)))
   }
 }
 