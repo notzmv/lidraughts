lichess-org/lila

update chess captcha

ui/site/src/main.js
@@ -198,19 +198,19 @@
     init(node) {
       if (!window.Chessground) return setTimeout(() => lichess.miniBoard.init(node), 500);
       const $el = $(node).removeClass('mini-board--init'),
-        [fen, orientation, lm] = $el.data('state').split(','),
-        config = {
-          coordinates: false,
-          viewOnly: !$el.data('playable'),
-          resizable: false,
-          fen,
-          lastMove: lm && (lm[1] === '@' ? [lm.slice(2)] : [lm[0] + lm[1], lm[2] + lm[3]]),
-          drawable: {
-            enabled: false,
-            visible: false
-          }
-        };
-      $el.data('chessground', Chessground(node, config));
+        [fen, orientation, lm] = $el.data('state').split(',');
+      $el.data('chessground', Chessground(node, {
+        orientation,
+        coordinates: false,
+        viewOnly: !node.getAttribute('data-playable'),
+        resizable: false,
+        fen,
+        lastMove: lm && (lm[1] === '@' ? [lm.slice(2)] : [lm[0] + lm[1], lm[2] + lm[3]]),
+        drawable: {
+          enabled: false,
+          visible: false
+        }
+      }));
     }
   };
 

ui/site/src/standalones/captcha.js
@@ -1,50 +1,51 @@
 $(function() {
   lichess.requestIdleCallback(function() {
-    $('div.captcha').each(function() {
-      var $captcha = $(this);
-      var $board = $captcha.find('.mini-board');
-      var $input = $captcha.find('input').val('');
-      var cg = $board.data('chessground');
-      var dests = JSON.parse($board.data('x'));
-      for (var k in dests) dests[k] = dests[k].match(/.{2}/g);
-      cg.set({
-        turnColor: cg.state.orientation,
-        movable: {
-          free: false,
-          dests: dests,
-          color: cg.state.orientation,
-          events: {
-            after: function(orig, dest) {
-              $captcha.removeClass('success failure');
-              submit(orig + ' ' + dest);
+    setTimeout(function() {
+      $('div.captcha').each(function() {
+        const $captcha = $(this),
+          $board = $captcha.find('.mini-board'),
+          // $input = $captcha.find('input').val(''),
+          cg = $board.data('chessground'),
+          fen = cg.getFen(),
+          destsObj = $board.data('moves'),
+          dests = new Map();
+        for (let k in destsObj) dests.set(k, destsObj[k].match(/.{2}/g));
+        cg.set({
+          turnColor: cg.state.orientation,
+          movable: {
+            free: false,
+            dests,
+            color: cg.state.orientation,
+            events: {
+              after(orig, dest) {
+                $captcha.removeClass('success failure');
+                submit(orig + ' ' + dest);
+              }
             }
           }
-        }
-      });
-
-      var submit = function(solution) {
-        $input.val(solution);
-        $.ajax({
-          url: $captcha.data('check-url'),
-          data: {
-            solution: solution
-          },
-          success: function(data) {
-            $captcha.toggleClass('success', data == 1);
-            $captcha.toggleClass('failure', data != 1);
-            if (data == 1) $board.data('chessground').stop();
-            else setTimeout(function() {
-              lichess.parseFen($board);
-              $board.data('chessground').set({
-                turnColor: cg.state.orientation,
-                movable: {
-                  dests: dests
-                }
-              });
-            }, 300);
-          }
         });
-      };
-    });
+
+        const submit = function(solution) {
+          // $input.val(solution);
+          $.ajax({
+            url: $captcha.data('check-url'),
+            data: {
+              solution
+            },
+            success: function(data) {
+              $captcha.toggleClass('success', data == 1).toggleClass('failure', data != 1);
+              if (data == 1) $board.data('chessground').stop();
+              else setTimeout(function() {
+                cg.set({
+                  fen: fen,
+                  turnColor: cg.state.orientation,
+                  movable: { dests }
+                });
+              }, 300);
+            }
+          });
+        };
+      });
+    }, 1000);
   });
 });

notzmv/lidraughts

ui/site/src/main.js
@@ -198,20 +198,19 @@
     init(node) {
       if (!window.Draughtsground) return setTimeout(() => lidraughts.miniBoard.init(node), 500);
       const $el = $(node).removeClass('mini-board--init'),
-        [fen, board, orientation, lm] = $el.data('state').split(','),
-        config = {
-          coordinates: 0,
-          boardSize: board ? board.split('x').map(s => parseInt(s)) : [10, 10],
-          viewOnly: !$el.data('playable'),
-          resizable: false,
-          fen,
-          lastMove: lm && [lm.slice(-4, -2), lm.slice(-2)],
-          drawable: {
-            enabled: false,
-            visible: false
-          }
-        };
-      $el.data('draughtsground', Draughtsground(node, config));
+        [fen, board, orientation, lm] = $el.data('state').split(',');
+      $el.data('draughtsground', Draughtsground(node, {
+        coordinates: 0,
+        boardSize: board ? board.split('x').map(s => parseInt(s)) : [10, 10],
+        viewOnly: !node.getAttribute('data-playable'),
+        resizable: false,
+        fen,
+        lastMove: lm && [lm.slice(-4, -2), lm.slice(-2)],
+        drawable: {
+          enabled: false,
+          visible: false
+        }
+      }));  
     }
   };
 

ui/site/src/standalones/captcha.js
@@ -1,52 +1,51 @@
 $(function() {
   lidraughts.requestIdleCallback(function() {
-    $('div.captcha').each(function() {
-      var $captcha = $(this);
-      var $board = $captcha.find('.mini-board');
-      var $input = $captcha.find('input').val('');
-      var cg = $board.data('draughtsground');
-      var dests = JSON.parse($board.data('x'));
-      for (var k in dests) dests[k] = dests[k].match(/.{2}/g);
-      cg.set({
-        turnColor: cg.state.orientation,
-        boardSize: [10, 10],
-        captureLength: 1,
-        movable: {
-          free: false,
-          dests: dests,
-          color: cg.state.orientation,
-          events: {
-            after: function(orig, dest) {
-              $captcha.removeClass('success failure');
-              submit(orig + ' ' + dest);
+    setTimeout(function() {
+      $('div.captcha').each(function() {
+        const $captcha = $(this),
+          $board = $captcha.find('.mini-board'),
+          // $input = $captcha.find('input').val(''),
+          cg = $board.data('draughtsground'),
+          dests = new Map();
+        for (let k in destsObj) dests.set(k, destsObj[k].match(/.{2}/g));
+        cg.set({
+          turnColor: cg.state.orientation,
+          boardSize: [10, 10],
+          captureLength: 1,
+          movable: {
+            free: false,
+            dests,
+            color: cg.state.orientation,
+            events: {
+              after(orig, dest) {
+                $captcha.removeClass('success failure');
+                submit(orig + ' ' + dest);
+              }
             }
           }
-        }
-      });
-
-      var submit = function(solution) {
-        $input.val(solution);
-        $.ajax({
-          url: $captcha.data('check-url'),
-          data: {
-            solution: solution
-          },
-          success: function(data) {
-            $captcha.toggleClass('success', data == 1);
-            $captcha.toggleClass('failure', data != 1);
-            if (data == 1) $board.data('draughtsground').stop();
-            else setTimeout(function() {
-              lidraughts.parseFen($board);
-              $board.data('draughtsground').set({
-                turnColor: cg.state.orientation,
-                movable: {
-                  dests: dests
-                }
-              });
-            }, 300);
-          }
         });
-      };
-    });
+
+        const submit = function(solution) {
+          // $input.val(solution);
+          $.ajax({
+            url: $captcha.data('check-url'),
+            data: {
+              solution
+            },
+            success: function(data) {
+              $captcha.toggleClass('success', data == 1).toggleClass('failure', data != 1);
+              if (data == 1) $board.data('draughtsground').stop();
+              else setTimeout(function() {
+                cg.set({
+                  fen: fen,
+                  turnColor: cg.state.orientation,
+                  movable: { dests }
+                });
+              }, 300);
+            }
+          });
+        };
+      });
+    }, 1000);
   });
 });