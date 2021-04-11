lichess-org/lila

fix mini-game delay when switching tournament table mode

ui/tournament/src/view/table.ts
@@ -74,9 +74,8 @@ function renderDuel(battle?: TeamBattle, duelTeams?: DuelTeams) {
 export default function(ctrl: TournamentController): VNode {
   return h('div.tour__table', {
     hook: {
-      postpatch() {
-        window.lichess.miniGame.initAll();
-      }
+      insert: window.lichess.miniGame.initAll,
+      postpatch: window.lichess.miniGame.initAll
     }
   }, [
     ctrl.data.featured ? featured(ctrl.data.featured) : null,

notzmv/lidraughts

ui/tournament/src/view/table.ts
@@ -84,9 +84,8 @@ function renderDuel(battle?: TeamBattle, duelTeams?: DuelTeams) {
 export default function(ctrl: TournamentController): VNode {
   return h('div.tour__table', {
     hook: {
-      postpatch() {
-        window.lidraughts.miniGame.initAll();
-      }
+      insert: window.lidraughts.miniGame.initAll,
+      postpatch: window.lidraughts.miniGame.initAll
     }
   }, [
     ctrl.data.featured ? featured(ctrl.data.featured, ctrl.data.draughtsResult) : null,