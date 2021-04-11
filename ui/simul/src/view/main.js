lichess-org/lila

fix simul mini-games

ui/simul/src/view/main.js
@@ -19,7 +19,11 @@ module.exports = function(ctrl) {
         }
       }
     }),
-    m('div.simul__main.box', handler(ctrl)),
+    m('div.simul__main.box', {
+      config() {
+        window.lichess.miniGame.initAll();
+      }
+    }, handler(ctrl)),
     m('div.chat__members.none', {
       config(el, done) {
         if (!done) $(el).watchers();

notzmv/lidraughts

ui/simul/src/view/main.js
@@ -19,7 +19,11 @@ module.exports = function(ctrl) {
         }
       }
     }),
-    m('div.simul__main.box', handler(ctrl)),
+    m('div.simul__main.box', {
+      config() {
+        window.lidraughts.miniGame.initAll();
+      }
+    }, handler(ctrl)),
     m('div.chat__members.none', {
       config(el, done) {
         if (!done) $(el).watchers();