lichess-org/lila

fix TV broadcast clock messages

modules/round/src/main/TvBroadcast.scala
@@ -55,8 +55,8 @@ final private class TvBroadcast extends Actor {
             "fen" -> s"$fen ${game.turnColor.letter}",
             "lm"  -> move
           )
-          .add("cw" -> game.clock.map(_.remainingTime(chess.White).roundSeconds))
-          .add("bw" -> game.clock.map(_.remainingTime(chess.Black).roundSeconds))
+          .add("wc" -> game.clock.map(_.remainingTime(chess.White).roundSeconds))
+          .add("bc" -> game.clock.map(_.remainingTime(chess.Black).roundSeconds))
       )
       queues.foreach(_ offer msg)
   }

notzmv/lidraughts

modules/round/src/main/TvBroadcast.scala
@@ -34,8 +34,8 @@ private final class TvBroadcast extends Actor {
           "fen" -> s"${game.turnColor.letter.toUpper}:$fen",
           "lm" -> move
         )
-          .add("cw" -> game.clock.map(_.remainingTime(draughts.White).roundSeconds))
-          .add("bw" -> game.clock.map(_.remainingTime(draughts.Black).roundSeconds))
+          .add("wc" -> game.clock.map(_.remainingTime(draughts.White).roundSeconds))
+          .add("bc" -> game.clock.map(_.remainingTime(draughts.Black).roundSeconds))
       )
   }
 