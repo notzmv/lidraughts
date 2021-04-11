lichess-org/lila

fix swiss mini games - closes #7132

ui/swiss/src/view/main.ts
@@ -13,7 +13,13 @@ import playerInfo from './playerInfo';
 export default function(ctrl: SwissCtrl) {
   const d = ctrl.data;
   const content = (d.status == 'created' ? created(ctrl) : (d.status == 'started' ? started(ctrl) : finished(ctrl)));
-  return h('main.' + ctrl.opts.classes, [
+  return h('main.' + ctrl.opts.classes, {
+    hook: {
+      postpatch() {
+        window.lichess.miniGame.initAll();
+      }
+    }
+  }, [
     h('aside.swiss__side', {
       hook: onInsert(el => {
         $(el).replaceWith(ctrl.opts.$side);

notzmv/lidraughts

ui/swiss/src/view/main.ts
@@ -13,7 +13,13 @@ import playerInfo from './playerInfo';
 export default function(ctrl: SwissCtrl) {
   const d = ctrl.data;
   const content = (d.status == 'created' ? created(ctrl) : (d.status == 'started' ? started(ctrl) : finished(ctrl)));
-  return h('main.' + ctrl.opts.classes, [
+  return h('main.' + ctrl.opts.classes, {
+    hook: {
+      postpatch() {
+        window.lidraughts.miniGame.initAll();
+      }
+    }
+  }, [
     h('aside.swiss__side', {
       hook: onInsert(el => {
         $(el).replaceWith(ctrl.opts.$side);