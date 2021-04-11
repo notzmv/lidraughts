lichess-org/lila

mini-game & mini-board improvements

modules/api/src/main/LobbyApi.scala
@@ -34,8 +34,8 @@ final class LobbyApi(
       .obj(
         "fullId"   -> pov.fullId,
         "gameId"   -> pov.gameId,
-        "fen"      -> (chess.format.Forsyth exportBoard pov.game.board),
-        "color"    -> pov.color.name,
+        "fen"      -> chess.format.Forsyth.exportBoard(pov.game.board),
+        "color"    -> (if (pov.game.variant.racingKings) chess.White else pov.color).name,
         "lastMove" -> ~pov.game.lastMoveKeys,
         "variant" -> Json.obj(
           "key"  -> pov.game.variant.key,

notzmv/lidraughts

modules/api/src/main/LobbyApi.scala
@@ -41,7 +41,7 @@ final class LobbyApi(
   def nowPlaying(pov: Pov) = Json.obj(
     "fullId" -> pov.fullId,
     "gameId" -> pov.gameId,
-    "fen" -> (draughts.format.Forsyth exportBoard pov.game.board),
+    "fen" -> draughts.format.Forsyth.exportBoard(pov.game.board),
     "color" -> pov.color.name,
     "lastMove" -> ~pov.game.lastMoveKeys,
     "variant" -> Json.obj(