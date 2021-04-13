lichess-org/lila

fix powertip was getting an index as second arg "pos"

ui/site/src/standalones/util.js
@@ -171,9 +171,9 @@ lichess.powertip = (() => {
   };
 
   function onIdleForAll(par, sel, fun) {
-    lichess.requestIdleCallback(function() {
-      Array.prototype.forEach.call(par.querySelectorAll(sel), fun);
-    });
+    lichess.requestIdleCallback(() =>
+      Array.prototype.forEach.call(par.querySelectorAll(sel), el => fun(el)) // do not codegolf to `fun`
+    )
   }
 
   return {

notzmv/lidraughts

ui/site/src/standalones/util.js
@@ -145,9 +145,9 @@ lidraughts.powertip = (() => {
   };
 
   function onIdleForAll(par, sel, fun) {
-    lidraughts.requestIdleCallback(function() {
-      Array.prototype.forEach.call(par.querySelectorAll(sel), fun);
-    });
+    lidraughts.requestIdleCallback(() =>
+      Array.prototype.forEach.call(par.querySelectorAll(sel), el => fun(el)) // do not codegolf to `fun`
+    )
   }
 
   return {