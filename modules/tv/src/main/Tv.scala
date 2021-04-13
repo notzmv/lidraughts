0a70f22a700593204dfd34c98baf90130030a180
Stefan Schermann <stefan.schermann@gmail.com>
2021-04-13 02:18:30 +0200

    remove tv filter debug value

--- modules/tv/src/main/Tv.scala
+++ modules/tv/src/main/Tv.scala
@@ -71,8 +71,7 @@ object Tv {
       name = "Top Rated",
       icon = "C",
       secondsSinceLastMove = freshBlitz,
-      filters = Seq(rated(150), standard, noBot)
-    // filters = Seq(rated(1600), standard, noBot)
+      filters = Seq(rated(1600), standard, noBot)
     )
     case object Bullet extends Channel(
       name = S.Bullet.name,
